4f74679e89b81d6d7fbfae0361d94ee8495afed5
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-25 20:15:21 +0300

    changes after review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,19 +8,20 @@ export const Person = ({
   sex,
   isMarried,
   partnerName,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${name}`}</h2>
-    {age && (<p className="Person__age">{`I am ${age}`}</p>)}
-    <p className="Person__partner">
-      {isMarried ? (
-        `My ${sex === 'f' ? `husband's` : `wife's`} name is ${partnerName}`
-      ) : (
-        `I am not married`
-      )}
-    </p>
-  </section>
-);
+}) => {
+  const partnerType = sex === 'f' ? `husband's` : `wife's`;
+  const marriedInfo = isMarried
+    ? (`My ${partnerType} name is ${partnerName}`)
+    : (`I am not married`);
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (<p className="Person__age">{`I am ${age}`}</p>)}
+      <p className="Person__partner">{marriedInfo}</p>
+    </section>
+  );
+};
 
 Person.defaultProps = {
   age: null,
