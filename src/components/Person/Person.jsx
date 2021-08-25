35ef97129081db247e7478433a611e85f6a3dc34
Derelict <oleksandr.dzyndzyura@gmail.com>
2021-08-25 19:19:32 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,7 +11,9 @@ export const Person = ({
 }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${name}`}</h2>
-    {age ? (<p className="Person__age">{`I am ${age}`}</p>) : ('')}
+    {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+    )}
     <p className="Person__partner">
       {isMarried ? (
         `My
