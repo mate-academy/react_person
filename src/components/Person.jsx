4e44986143efe4efe39bf780e0125e34fc66feac
Anna-Tkachenko <tkachenko.anna835@gmail.com>
2021-08-27 12:02:20 +0300

    Added default props for Person

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -8,19 +8,21 @@ export const Person = ({
   isMarried,
   partnerName
 }) => {
-  const partner = sex==='m' ? 'wife' : 'husband';
+  const partner = sex === 'm' ? 'wife' : 'husband';
+  const marriedMessage = isMarried
+    ? `My ${partner}'s name is ${partnerName}`
+    : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
+
       {age && (
         <p className="Person__age">I am {age}</p>
       )}
+      
       <p className="Person__partner">
-        {isMarried
-          ? `My ${partner}'s name is ${partnerName}`
-          : 'I am not married'
-        }
+        {marriedMessage}
       </p>
     </section>
   );
@@ -32,4 +34,9 @@ Person.propTypes = {
   sex: PropTypes.string.isRequired,
   isMarried: PropTypes.bool.isRequired,
   partnerName: PropTypes.string,
+};
+
+Person.defaultProps = {
+  age: null,
+  partnerName: null,
 };
