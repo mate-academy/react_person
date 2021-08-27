bfe6ff5884d7debaae856620957dbdc774ec8e06
NastyaVovk <winc4ester@gmail.com>
2021-08-27 14:09:21 +0300

    refactoring

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,37 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export function Person(props) {
   const { name, age, partnerName, sex, isMarried } = props.person;
+  const partner = sex === 'm' ? "wife's" : "husband's";
+  const marriedMessage = isMarried
+  ? `My ${partner} name is ${partnerName}`
+  : `I am not married`;
+
 
   return (
-    <section className="Person">
-      <h2 className="Person__name">My name is {name}</h2>
+    <section className="person">
+      <h2 className="person__name">My name is {name}</h2>
 
-      {age && <p className="Person__age">I am {age}</p>}
+      {age && <p className="person__age">I am {age}</p>}
 
-      <p className="Person__partner">
-        { isMarried
-          ? `My ${sex === 'm' ? "wife's" : "husband's"} name is ${partnerName}`
-          : `I am not married`
-        }
-      </p>
+      <p className="person__married"> { marriedMessage } </p>
     </section>
   );
 }
 
 Person.propTypes = {
-  person = {
+  person: PropTypes.shape({
     name: PropTypes.string.isRequired,
     sex: PropTypes.string.isRequired,
     age: PropTypes.number,
     isMarried: PropTypes.bool.isRequired,
     partner: PropTypes.string,
-  }
-}
-
-Person.defaultProps = {
-  person = {
-    age: undefined,
-    partner: undefined,
-  }
+  })
 }
