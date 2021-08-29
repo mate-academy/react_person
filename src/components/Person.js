ff8ab0cbbe32a9121fc3656d94d891e5d3282834
Ihor Nahnybida <igor653279@gmail.com>
2021-08-29 17:05:45 +0300

    Person component created

---
+++ src/components/Person.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) => {
+  const partner = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  return (
+    <section className="Person">
+
+      <h2 className="Person__name">
+        My name is
+        {' '}
+        {name}
+      </h2>
+
+      <p className="Person__age">
+        {age && `I am ${age}`}
+      </p>
+
+      <p className="Person__partner">
+        {isMarried
+          ? `My ${partner}'s name is ${partnerName}`
+          : 'I am not married'
+        }
+
+      </p>
+    </section>
+  );
+};
+
+Person.defaultProps = {
+  age: null,
+  partnerName: null,
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  age: PropTypes.number,
+  partnerName: PropTypes.string,
+};
