63129a7bd4094388aa9dbdb9da049ec9eaf3ff6b
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-30 11:25:37 +0200

    corrected props

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,38 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-export const Person = ({
-  name,
-  age,
-  sex,
-  isMarried,
-  partnerName,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {'My name is '}
-      {name}
-    </h2>
+export const Person = (props) => {
+  const { name, age, sex, isMarried, partnerName } = props;
 
-    {age && (
-    <p className="Person__age">
-      {'I am '}
-      {age}
-    </p>
-    )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {'My name is '}
+        {name}
+      </h2>
 
-    {isMarried ? (
-      <p className="Person__partner">
-        {'My '}
-        {sex === 'f' ? 'husband\'s ' : 'wife\'s '}
-        {'name is '}
-        {partnerName}
+      {age && (
+      <p className="Person__age">
+        {'I am '}
+        {age}
       </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+      )}
+
+      {isMarried ? (
+        <p className="Person__partner">
+          {'My '}
+          {sex === 'f' ? 'husband\'s ' : 'wife\'s '}
+          {'name is '}
+          {partnerName}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
 
 Person.defaultProps = {
   age: null,
