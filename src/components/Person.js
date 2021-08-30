d412c483b5a7b74c0f6fca9e71ac2c09d2a39cf0
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-30 13:46:51 +0200

    refactored parameters

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,19 +3,17 @@ import PropTypes from 'prop-types';
 import './Person.scss';
 
 export const Person = (props) => {
-  const { name, age, sex, isMarried, partnerName } = props;
+  const { name, age, sex, isMarried, partnerName } = props.person;
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {'My name is '}
-        {name}
+        {`My name is ${name}`}
       </h2>
 
       {age && (
       <p className="Person__age">
-        {'I am '}
-        {age}
+        {`I am ${age}`}
       </p>
       )}
 
@@ -23,8 +21,7 @@ export const Person = (props) => {
         <p className="Person__partner">
           {'My '}
           {sex === 'f' ? 'husband\'s ' : 'wife\'s '}
-          {'name is '}
-          {partnerName}
+          {`name is ${partnerName}`}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
@@ -33,15 +30,12 @@ export const Person = (props) => {
   );
 };
 
-Person.defaultProps = {
-  age: null,
-  partnerName: null,
-};
-
 Person.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number,
-  sex: PropTypes.string.isRequired,
-  isMarried: PropTypes.bool.isRequired,
-  partnerName: PropTypes.string,
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    sex: PropTypes.string.isRequired,
+    isMarried: PropTypes.bool.isRequired,
+    partnerName: PropTypes.string,
+  }).isRequired,
 };
