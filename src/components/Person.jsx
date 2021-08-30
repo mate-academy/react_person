cffb8a2afe6e76b1dca39fcee9896de8a0109b0a
Ula Dutchak <ulana.dutchak@gmail.com>
2021-08-30 15:31:57 +0300

    fixed

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -9,26 +9,20 @@ function Person({
   isMarried,
   partnerName,
 }) {
+  const partner = sex === 'm' ? 'wife' : 'hasband'
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {name}
+        My name is {name}
       </h2>
       {age && (
         <p className="Person__age">
-          I am
-          {age}
+          I am {age}
         </p>
       )}
       {isMarried
         ? (
-          <p className="Person__partner">
-            {sex === 'm'
-              ? `My wife's name is ${partnerName}`
-              : `My husband's name is ${partnerName}`
-          }
-          </p>
+          <p className="Person__partner">My {partner}'s name is {partnerName}</p>
         )
         : <p className="Person__partner">I am not married</p>
       }
