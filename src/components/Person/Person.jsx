55adb8f628169fe17383d173efb1efabe8e040bb
unknown <hado.roman.ihorovych@gmail.com>
2021-08-31 16:31:47 +0300

    added another files

---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({ name, age, sex, isMarried, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${name}`}</h2>
+    {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+    )}
+    {isMarried && (sex === 'm'
+      ? (
+        <p className="Person__partner">
+          {`My wife's name is ${partnerName}`}
+        </p>
+      )
+      : (
+        <p className="Person__partner">
+          {`My husband's is name is ${partnerName}`}
+        </p>
+      ))}
+    {!isMarried && (
+      <p className="Person__partner">
+        I am not married
+      </p>
+    )}
+  </section>
+);
+
+Person.defaultProps = {
+  name: 'name',
+  age: null,
+  sex: 'sex',
+  isMarried: false,
+  partnerName: 'partner',
+};
+
+Person.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  sex: PropTypes.string,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
+};
