cbfcbe711401bcf233d7d1560e2d70cede182c12
Yevhen Pecherskyi <gekape4erskii@gmail.com>
2021-09-01 18:38:22 +0300

    person done

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Person = ({name, age, sex, isMarried, partnerName}) => (
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName
+}) => (
   <section className="Person">
-    <h2 className="Person__name">{name ? `My name is ${name}` : null}</h2>
-    <p className="Person__age">{age ? `I am ${age}` : null}</p>
-    <p className="Person__partner">{
-      isMarried
+    <h2 className="Person__name">
+      {name ? `My name is ${name}` : null}
+    </h2>
+    <p className="Person__age">
+      {age ? `I am ${age}` : null}
+    </p>
+    <p className="Person__partner">
+      {isMarried
       ? (sex === 'm'
         ? `My wife's name is ${partnerName}`
         : `My husband's name is ${partnerName}`)
