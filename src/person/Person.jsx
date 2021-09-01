2c8337576351728e197c5070e29bff47f9be3676
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-09-01 13:51:52 +0300

    Fixed problem with undefined

--- src/person/Person.jsx
+++ src/person/Person.jsx
@@ -2,41 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-export const Person = ({
-  name,
-  age,
-  sex,
-  partnerName,
-  isMarried,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-    {age && (
-      <p className="Person__age">
-        {`I am ${age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `My ${sex === 'm' ? "wife's" : "husband's"} name is ${partnerName}`
+          : 'I am not married'}
       </p>
-    )}
-
-    <p className="Person__partner">
-      {isMarried
-        ? `My ${sex === 'm' ? "wife's" : "husband's"} name is ${partnerName}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
 
 Person.defaultProps = {
-  age: null,
-  partnerName: null,
+  person: {
+    age: null,
+    partnerName: null,
+  },
 };
 
 Person.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number,
-  sex: PropTypes.oneOf(['m', 'f']).isRequired,
-  isMarried: PropTypes.bool.isRequired,
-  partnerName: PropTypes.string,
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    sex: PropTypes.string.isRequired,
+    isMarried: PropTypes.bool.isRequired,
+    partnerName: PropTypes.string,
+  }),
 };
