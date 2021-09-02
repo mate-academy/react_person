c6070c09a54ec8e0940177249e106315c824fddb
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-09-02 10:36:02 +0300

    fixed code review comments

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -15,7 +15,12 @@ export const Person = ({
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <p className="Person__age">{age ? `I am ${age}` : ''}</p>
+      {age && (
+        <p className="Person__age">
+          {`I am `}
+          {age}
+        </p>
+      )}
       <p className="Person__partner">
         {isMarried
           ? `My ${sexOfPartner}\`s name is ${partnerName}`
