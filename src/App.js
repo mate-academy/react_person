b866350533e4335982575d1d7b7f301db2c53ec8
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-09-09 14:22:31 +0300

    Fixes aftert code review

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Person from './components/Person';
 import './App.scss';
 
 const misha = {
@@ -26,58 +26,10 @@ const alex = {
 
 const App = () => (
   <div className="App">
-    <Person
-      personName={misha.name}
-      age={misha.age}
-      sex={misha.sex}
-      isMarried={misha.isMarried}
-      partnerName={misha.partnerName}
-    />
-    <Person
-      personName={olya.name}
-      age={olya.age}
-      sex={olya.sex}
-      isMarried={olya.isMarried}
-      partnerName={olya.partnerName}
-    />
-    <Person
-      personName={alex.name}
-      age={alex.age}
-      sex={alex.sex}
-      isMarried={alex.isMarried}
-      partnerName={alex.partnerName}
-    />
+    <Person {...misha} />
+    <Person {...olya} />
+    <Person {...alex} />
   </div>
 );
-
-const Person = ({
-  personName,
-  age,
-  sex,
-  isMarried,
-  partnerName,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {personName}
-    </h2>
-    <p className="Person__age">{age !== 0 && `I am ${age}`}</p>
-    <p className="Person__partner">
-      {isMarried
-        ? (sex === 'm' && `My wife's name is ${partnerName}`)
-        || (sex === 'f' && `My husband's name is ${partnerName}`)
-        : 'I am not married'}
-    </p>
-  </section>
-);
-
-Person.propTypes = {
-  personName: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-  sex: PropTypes.string.isRequired,
-  isMarried: PropTypes.bool.isRequired,
-  partnerName: PropTypes.string.isRequired,
-};
 
 export default App;
