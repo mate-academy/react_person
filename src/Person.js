8810dceb3fffec4b017e66ed76f0e271ccd915d0
bohdan_kochetov <bodja1208@gmail.com>
2021-10-07 20:54:21 +0300

    fixed

--- src/Person.js
+++ src/Person.js
@@ -11,9 +11,7 @@ function Person({
   return (
     <section className="Person">
       <h2 className="Person__name">
-        `My name is $
-        {name}
-        `
+        {`My name is ${name}`}
       </h2>
       {age && (
         <p className="Person__age">{`I am ${age}`}</p>
