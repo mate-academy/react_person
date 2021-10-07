b48f5019088f06e3da70238c7f24ac7a7443a6ff
Iurii Medvedchuk <io.medvedchuk@gmail.com>
2021-10-07 21:33:52 +0300

    correction

--- src/Person.jsx
+++ src/Person.jsx
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types';
 export const Person = ({ name, age, sex, isMarried, partnerName }) => (
   <section className="Person">
     <h2 className="Person__name">
-      My name is
-      {name}
+      {`My name is ${name}`}
     </h2>
     {age && (
       <p className="Person__age">
-        I am
-        {age}
+        {`I am ${age}`}
       </p>
     )}
     <p className="Person__partner">
