f1b13a0edc19abe951988f232e2c2756e418e131
bohdan_kochetov <bodja1208@gmail.com>
2021-10-07 20:52:50 +0300

    fixed

--- src/Person.js
+++ src/Person.js
@@ -8,26 +8,24 @@ function Person({
   isMarried,
   partnerName,
 }) {
-  let personPartner;
-
-  if (isMarried) {
-    personPartner = (sex === 'f')
-      ? `My husband's name is ${partnerName}`
-      : `My wife's name is ${partnerName}`;
-  } else {
-    personPartner = `I am not married`;
-  }
-
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
+        `My name is $
         {name}
+        `
       </h2>
       {age && (
         <p className="Person__age">{`I am ${age}`}</p>
       )}
-      <p className="Person__partner">{personPartner}</p>
+      <p className="Person__partner">
+        {
+        (isMarried)
+          ? `My 
+          ${(sex === 'f') ? "husband's" : "wife's"} name is ${partnerName}`
+          : `I am not married`
+        }
+      </p>
     </section>
   );
 }
