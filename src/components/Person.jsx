7b365714310c62b3aebc50ee4383f51efe50370c
magithub@yandex.ru <magithub@yandex.ru>
2021-10-08 18:36:58 +0300

    fix

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -5,19 +5,33 @@ const Person = ({ name, age, sex, isMarried, partnerName }) => (
     <>
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age && (<p className="Person__age">I am {age}</p>)}
-      { isMarried && sex === 'm' && (<p className="Person__partner">My wife&apos;s name is {partnerName}</p>)}
-      { isMarried && sex === 'f' && (<p className="Person__partner">My husband&apos;s name is {partnerName}</p>)}
+      {age && (
+        <p className="Person__age">I am {age}</p>
+      )}
+      {(isMarried && sex === 'm') && (
+        <p className="Person__partner">My wife&apos;s name is {partnerName}</p>
+      )}
+      {(isMarried && sex === 'f') && (
+        <p className="Person__partner">My husband&apos;s name is {partnerName}</p>
+      )}
     </section>
     </>
 )
+
+Person.defaultTypes = {
+  name: null,
+  age: 0,
+  sex: null,
+  isMarried: false,
+  partnerName: null,
+}
 
 Person.propTypes = {
     name: PropTypes.string.isRequired,
     age: PropTypes.number,
     sex: PropTypes.string.isRequired,
     isMarried: PropTypes.bool.isRequired,
-    partner: PropTypes.string,
+    partnerName: PropTypes.string,
 }
 
 export default Person;
