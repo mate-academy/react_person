9d59691e225c0da7d238ebede8efcc203caa9e4e
Jane Verbitskaya <jvzns17@gmail.com>
2021-10-08 15:36:24 +0300

    Solution-2

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -11,8 +11,7 @@ function Person({
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {name}
+        {`My name is ${name}`}
       </h2>
 
       <p className="Person__age">{age && `I am ${age}`}</p>
