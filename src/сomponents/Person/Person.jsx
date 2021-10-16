978b6bed6de69e5afece2a9261c801a0834a61f4
dinexpod <work.toxa@gmail.com>
2021-10-16 21:57:19 +0300

    code review advices

--- src/сomponents/Person/Person.jsx
+++ src/сomponents/Person/Person.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Person = ({ person }) => {
-  const { name, age, partnerName } = person;
-
-  return (
-    <div className={Person}>
-      {name && <h2>{`My name is ${name}`}</h2>}
-      {age && <p>{`I am ${age}`}</p>}
-      <p>
-        {partnerName
-          ? `My wife's name is ${partnerName}`
-          : `I am not married`
-        }
-      </p>
-    </div>
-  );
-};
+export const Person = ({ name, age, partnerName }) => (
+  <div className={Person}>
+    {name && <h2>{`My name is ${name}`}</h2>}
+    {age && <p>{`I am ${age}`}</p>}
+    <p>
+      {partnerName
+        ? `My wife's name is ${partnerName}`
+        : `I am not married`
+      }
+    </p>
+  </div>
+);
 
 Person.propTypes = {
-  person: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number,
-    partnerName: PropTypes.string.isRequired,
-  }).isRequired,
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  partnerName: PropTypes.string.isRequired,
+};
+
+Person.defaultProps = {
+  age: null,
 };
