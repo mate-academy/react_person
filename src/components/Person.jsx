4f4d1b6ee30324a2beb9a76c48df8c0c2237ef9a
DY-JS <delo1604@ukr.net>
2021-10-17 14:31:01 +0300

    Solution

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-function Person ({name, age = null, sex, isMarried = false, partnerName = null}) {
+function Person({ name, age, sex, isMarried = false, partnerName }) {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <p className="Person__age">{age && `I am ${age}`}</p>
-      <p className="Person__partner">{(isMarried)
-        && (`My ${sex === 'm' ? 'wife\'s' : 'husband\'s'} name is ${partnerName}`)}
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">
+        {isMarried
+          ? `My ${sex === "m" ? "wife's" : "husband's"} name is ${partnerName}`
+          : "I am not married"}
       </p>
     </section>
-  )
+  );
 }
 
 export default Person;
@@ -18,7 +20,12 @@ export default Person;
 Person.propTypes = {
   name: PropTypes.string.isRequired,
   age: PropTypes.number,
-  sex: PropTypes.oneOf(['m', 'f']).isRequired,
+  sex: PropTypes.oneOf(["m", "f"]).isRequired,
   isMarried: PropTypes.bool.isRequired,
   partnerName: PropTypes.string,
+};
+
+Person.defaultProps = {
+  age: null,
+  partnerName: null,
 };
