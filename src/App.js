f9e25461b13d0b1e8bb5490194a705dad0301c9f
Alex <alex.krynytskyi@gmail.com>
2021-10-19 20:17:53 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React from "react";
-import "./App.scss";
-import Person from "./components/Person";
+import React from 'react';
+import './App.scss';
+import Person from './components/Person';
 
 const users = [
   {
-    name: "Misha",
+    name: 'Misha',
     age: 37,
-    sex: "m",
+    sex: 'm',
     isMarried: true,
-    partnerName: "Natasha",
+    partnerName: 'Natasha',
   },
   {
-    name: "Olya",
-    sex: "f",
+    name: 'Olya',
+    sex: 'f',
     isMarried: true,
-    partnerName: "Maksym",
+    partnerName: 'Maksym',
   },
 
   {
-    name: "Alex",
+    name: 'Alex',
     age: 25,
-    sex: "m",
+    sex: 'm',
     isMarried: false,
   },
 ];
 
 const App = () => (
   <div className="App">
-    {users.map(({ name, age, isMarried, partnerName, sex }) => {
-      return (
-        <Person
-          name={name}
-          age={age}
-          isMarried={isMarried}
-          partnerName={partnerName}
-          sex={sex}
-        />
-      );
-    })}
+    {users.map(({ name, age, isMarried, partnerName, sex }) => (
+      <Person
+        name={name}
+        age={age}
+        isMarried={isMarried}
+        partnerName={partnerName}
+        sex={sex}
+      />
+    ))}
   </div>
 );
 
