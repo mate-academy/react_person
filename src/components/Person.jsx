7856b814e0eaa5facf8324813d1dc165989ed382
Tarasulko <internalpeace10@outlook.com>
2021-10-24 23:34:46 +0300

    Final version

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -7,22 +7,21 @@ const Person = ({
   sex,
   isMarried,
   partnerName,
-}) => {
-  if (name) {
-    return (
-      <section className='Person'>
-        <h2 className='Person__name'>My name is {name}</h2>
-        <p className="Person__age">{age && `I am ${age}`}</p>
-        <p className="Person__partner">
-          {isMarried === true
-            ? `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`
-            : 'I am not married'
-          }
-        </p>
-      </section>
-    );
-  }
-}
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      My name is
+      {name}
+    </h2>
+    <p className="Person__age">{age && `I am ${age}`}</p>
+    <p className="Person__partner">
+      {isMarried === true
+        ? `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`
+        : 'I am not married'
+      }
+    </p>
+  </section>
+);
 
 Person.propTypes = {
   name: PropTypes.string.isRequired,
@@ -30,6 +29,11 @@ Person.propTypes = {
   sex: PropTypes.string.isRequired,
   isMarried: PropTypes.bool.isRequired,
   partnerName: PropTypes.string,
+};
+
+Person.defaultProps = {
+  age: 18,
+  partnerName: 'Misha',
 };
 
 export default Person;
