f10375d84826f495a59124e76eb296410161bde8
Olesya Parfylo <olesya.parfylo@gmail.com>
2021-11-02 15:24:07 +0200

    Fixed

--- src/App.js
+++ src/App.js
@@ -3,26 +3,32 @@ import { Person } from './Components/Person';
 
 import './App.scss';
 
+const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
+
 export const App = () => (
   <div className="App">
-    <Person
-      name="Misha"
-      age={37}
-      sex="m"
-      isMarried
-      partnerName="Natasha"
-    />
-    <Person
-      name="Olya"
-      sex="f"
-      isMarried
-      partnerName="Maksym"
-    />
-    <Person
-      name="Alex"
-      age={25}
-      sex="m"
-      isMarried={false}
-    />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
--- src/Components/Person.jsx
+++ src/Components/Person.jsx
@@ -2,44 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-export const Person = ({
-  name,
-  age,
-  sex,
-  isMarried,
-  partnerName,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {name}
-    </h2>
-    <p className="Person__age">
-      {age ? (
-        `I am ${age}`
-      ) : (
-        ''
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {` ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          I am
+          {` ${age}`}
+        </p>
       )}
-    </p>
-    <p className="Person__partner">
-      {(!isMarried && 'I am not married')
-        || (isMarried && sex === 'm'
-        && `My wife's name is ${partnerName}`)
-        || (isMarried && sex === 'f'
-        && `My husband's name is ${partnerName}`)}
-    </p>
-  </section>
-);
+      <p className="Person__partner">
+        {(!isMarried && 'I am not married')
+          || (isMarried && sex === 'm'
+          && `My wife's name is ${partnerName}`)
+          || (isMarried && sex === 'f'
+          && `My husband's name is ${partnerName}`)}
+      </p>
+    </section>
+  );
+};
 
 Person.defaultProps = {
-  age: null,
-  partnerName: '',
+  person: {
+    age: null,
+    partnerName: '',
+  },
 };
 
 Person.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number,
-  sex: PropTypes.string.isRequired,
-  isMarried: PropTypes.bool.isRequired,
-  partnerName: PropTypes.string,
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    sex: PropTypes.string.isRequired,
+    isMarried: PropTypes.bool.isRequired,
+    partnerName: PropTypes.string,
+  }),
 };
