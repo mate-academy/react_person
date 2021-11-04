834fc632b9f597b5b20e8e326e777ca129028213
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-11-04 12:38:51 +0200

    fix contidional age

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -13,9 +13,11 @@ export const Person = ({
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <p className="Person__age">
-        {age ? `I am ${age}` : age}
-      </p>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
       <p className="Person__partner">
         {
           isMarried
