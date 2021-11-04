5e1a1799a3dd789042a4848d6410fff711944315
Misha Rosa <misharosa.lviv@gmail.com>
2021-11-04 12:09:44 +0200

    Age was edits

--- src/component/Person/Person.jsx
+++ src/component/Person/Person.jsx
@@ -2,24 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Person = ({ name, age, sex, isMarried, partnerName }) => {
-
   const personSex = (sex === 'm') ? 'wife' : 'husband';
 
-   return (
-     <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {' '}
-      {name}
-    </h2>
-    <p className="Person__age">
-      {age && `I am ${age}`}
-    </p>
-    <p className="Person__partner">
-      {partnerName ? `My ${personSex}'s name is ${partnerName}` : 'I am not married'}
-    </p>
-  </section>
-   )};
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {' '}
+        {name}
+      </h2>
+      {age
+      &&
+      <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">
+        {partnerName ? `My ${personSex}'s name is ${partnerName}
+        ` : 'I am not married'}
+      </p>
+    </section>
+  );
+};
 
 Person.defaultProps = {
   sex: '',
