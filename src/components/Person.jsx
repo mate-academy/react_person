f6d41703f291135a9e9325802f20b064d9e5abac
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-11-04 12:57:19 +0200

    add ternar to Married

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -8,22 +8,14 @@ function Person({
   isMarried,
   partnerName,
 }) {
-  let partner;
-
-  if (sex === 'm') {
-    partner = 'wife';
-  } else {
-    partner = 'husband';
-  }
-
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <p className="Person__age">{ age ? `I am ${age}` : null}</p>
+      <p className="Person__age">{ age && `I am ${age}`}</p>
       <p className="Person__partner">
         {
         isMarried
-          ? `My ${partner}'s name is ${partnerName}`
+          ? `My ${(sex === 'm') ? 'wife' : 'husband'}'s name is ${partnerName}`
           : 'I am not married'
         }
       </p>
