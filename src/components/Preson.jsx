8a98d0cd3c8564211686fdc5c898bbb393c1d2ce
Alina Kostiuk <alina.hariaha@gmail.com>
2021-11-04 21:06:29 +0200

    Fix PR comments.

--- src/components/Preson.jsx
+++ src/components/Preson.jsx
@@ -11,24 +11,22 @@ const Person = ({
   const partner = (sex === 'f') ? 'husband' : 'wife';
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-        {age && (
-          <p className="Person__age">
-            {`I am ${age}`}
-          </p>
-        )}
-
-        <p className="Person__partner">
-          { (!isMarried)
-            ? 'I am not married'
-            : `My ${partner}'s name is ${partnerName}`
-          }
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
         </p>
-      </section>
-    </>
+      )}
+
+      <p className="Person__partner">
+        { (isMarried)
+          ? `My ${partner}'s name is ${partnerName}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
   );
 };
 
