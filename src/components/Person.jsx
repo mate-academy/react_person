06b0037b5ddd960ee7512167efd25d9b9e2531ee
Denys Stryzhalko <samu3lkacis@gmail.com>
2021-11-04 16:13:58 +0200

    person's age paragraph formatted

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -10,13 +10,11 @@ const Person = ({ name, age, sex, isMarried, partnerName }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {
-      age && (
-      <p className="Person__age">
-        {`I am ${age}`}
-      </p>
-      )
-      }
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
       <p className="Person__partner">
         {partnerDescription}
       </p>
