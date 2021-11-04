bb01957594c4b72ba5a986523de9dc6d6e3de405
Denys Anosov <anosovdenys@gmail.com>
2021-11-04 15:06:27 +0200

    changes according to PR comments

--- src/App.js
+++ src/App.js
@@ -26,15 +26,9 @@ const alex = {
 
 const App = () => (
   <div className="App">
-    <Person
-      {...misha}
-    />
-    <Person
-      {...olya}
-    />
-    <Person
-      {...alex}
-    />
+    <Person {...misha} />
+    <Person {...olya} />
+    <Person {...alex} />
   </div>
 );
 
--- src/components/Person.js
+++ src/components/Person.js
@@ -2,32 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-export function Person({ name, age, sex, isMarried, partnerName }) {
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
   const partnerGender = sex === 'm' ? `wife's` : `husband's`;
 
   return (
     <div className="App">
       <section className="Person">
-        {name && (
-          <h2 className="Person__name">{`My name is ${name}`}</h2>
-        )}
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
 
         {age && (
           <p className="Person__age">{`I am ${age}`}</p>
         )}
 
-        {isMarried
-          ? (
-            <p className="Person__partner">
-              {`My ${partnerGender} name is ${partnerName}`}
-            </p>
-          )
-          : (
-            <p className="Person__partner">
-              I am not married
-            </p>
-          )
-        }
+        <p className="Person__partner">
+          {isMarried
+            ? `My ${partnerGender} name is ${partnerName}`
+
+            : `I am not married`
+          }
+        </p>
       </section>
     </div>
   );
@@ -35,7 +34,7 @@ export function Person({ name, age, sex, isMarried, partnerName }) {
 
 Person.defaultProps = {
   age: null,
-  partnerName: null,
+  partnerName: 'in sectret',
 };
 
 Person.propTypes = {
