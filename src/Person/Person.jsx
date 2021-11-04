91dbb51a0c6f6677d22178746efe41faccceda70
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-04 10:34:17 +0300

    Complex logic moved to separate variable

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -2,36 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-const Person = ({ name, age, sex, isMarried, partnerName }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
+const Person = ({ name, age, sex, isMarried, partnerName }) => {
+  const marriedSentence
+    = `My
+    ${sex === 'm' ? 'wife' : 'husband'}
+    's name is ${partnerName}`;
 
-    {age && (
-      <p className="Person__age">
-        {`I am ${age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried || 'I am not married'}
+
+        {isMarried && marriedSentence}
       </p>
-    )}
+    </section>
+  );
+};
 
-    <p className="Person__partner">
-      {isMarried || 'I am not married'}
-
-      {isMarried
-        && `My 
-            ${sex === 'm' ? 'wife' : 'husband'}'s
-            name is ${partnerName}`
-      }
-    </p>
-  </section>
-);
+Person.defaultProps = {
+  isMarried: false,
+  partnerName: '',
+};
 
 Person.propTypes = {
   name: PropTypes.string.isRequired,
   age: PropTypes.number.isRequired,
   sex: PropTypes.string.isRequired,
-  isMarried: PropTypes.bool.isRequired,
-  partnerName: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
 };
 
 export default Person;
