4a0065f2390fe80c8e01f88b55320eee400941e3
Max <gorodtira51@gmail.com>
2021-11-04 16:14:03 +0200

    fixed

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Person = (props) => {
-  const isMale = props.sex === 'm';
+  const partner = props.sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${props.name}`}</h2>
       {props.age && <p className="Person__age">{`I am ${props.age}`}</p>}
-      {props.isMarried && (isMale
-        ? (
+      {props.isMarried
+        && (
           <p className="Person__partner">
-            {`My wife's name is ${props.partnerName}`}
+            {`My ${partner}'s name is ${props.partnerName}`}
           </p>
-        )
-        : (
-          <p className="Person__partner">
-            {`My husband's name is ${props.partnerName}`}
-          </p>
-        ))}
+        )}
       {!props.isMarried
         && <p className="Person__partner">I am not married</p>}
     </section>
@@ -28,13 +23,12 @@ export const Person = (props) => {
 Person.propTypes = {
   name: PropTypes.string.isRequired,
   age: PropTypes.number,
-  sex: PropTypes.string,
+  sex: PropTypes.string.isRequired,
   isMarried: PropTypes.bool.isRequired,
   partnerName: PropTypes.string,
 };
 
 Person.defaultProps = {
   age: null,
-  sex: null,
   partnerName: 'a secret',
 };
