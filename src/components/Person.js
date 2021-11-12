cc2485d8c3c901b2324d411581a036b1a8b17446
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-11-12 11:38:28 +0200

    Task Corrected

--- src/components/Person.js
+++ src/components/Person.js
@@ -20,26 +20,31 @@ export const Person = ({
       <div className="App">
         <section className="Person">
           <h2 className="Person__name">
-            My name is
-            {' '}
-            {name}
+            {`
+              My name is
+              ${name}
+            `}
           </h2>
           {age ? (
             <p className="Person__age">
-              I am
-              {' '}
-              {age}
+              {`
+                I am
+                ${age}
+              `}
             </p>
           ) : null}
           {partnerName ? (
             <p className="Person__partner">
-              My
-              {sex === 'm' ? (
-                ` wife's `
-              ) : ` husband's `}
-              name is
-              {' '}
-              {partnerName}
+              {`
+                My
+                ${
+                  sex === 'm'
+                    ? ` wife's `
+                    : ` husband's `
+                }
+                name is
+                ${partnerName}
+              `}
             </p>
           ) : null}
         </section>
