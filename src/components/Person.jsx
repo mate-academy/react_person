17637b662d4616ac3a28319106de8fa1fb06f15e
Dariia Romanova <dariia.romanova@gmail.com>
2021-12-01 20:59:56 +0200

    Solution

---
+++ src/components/Person.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Person.scss';
+
+export const Person = ({ name, age, sex, isMarried, partnerName }) => (
+  <section className="Person">
+    <div className="Person__content">
+      <h2 className="Person__name">
+        {'My name is '}
+        {name}
+      </h2>
+
+      <p className="Person__age">
+        {age ? (
+          `I am ${age}`
+        ) : null
+        }
+      </p>
+
+      {isMarried ? (
+        <p className="Person__partner">
+          {
+            sex === 'm'
+              ? 'My wife'
+              : 'My husband'
+          }
+          {'\'s name is '}
+          {partnerName}
+        </p>
+      ) : (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      )}
+    </div>
+  </section>
+);
+
+Person.defaultProps = {
+  isMarried: false,
+  age: null,
+  sex: '',
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  isMarried: PropTypes.bool,
+  sex: PropTypes.string,
+};
