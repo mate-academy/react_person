314397cb4fdcf7e3a2c89378434d73674ec13e9e
Vitalii Voitovich <voitovichatque@gmail.com>
2021-12-02 18:22:39 +0200

    Person solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Person } from './components/Person';
+
 import './App.scss';
 
 const misha = {
@@ -25,22 +27,9 @@ const alex = {
 
 const App = () => (
   <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
+    <Person {...misha} />
+    <Person {...olya} />
+    <Person {...alex} />
   </div>
 );
 
---
+++ src/components/Person.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = (
+  {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  },
+) => {
+  let personPartnerSex;
+
+  if (sex === 'm') {
+    personPartnerSex = 'wife';
+  }
+
+  if (sex === 'f') {
+    personPartnerSex = 'husband';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age
+        ? <p className="Person__age">{`I am ${age}`}</p>
+        : null
+      }
+      <p className="Person__partner">
+        {isMarried
+          ? `My ${personPartnerSex}'s name is ${partnerName}`
+          : `I'm not married`
+        }
+      </p>
+    </section>
+  );
+};
+
+Person.defaultProps = {
+  age: 0,
+  isMarried: 'Unknown data',
+  partnerName: 'Unknown data',
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
+};
