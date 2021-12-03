745bde5c2b4a1dbfcf534324778e0603a196a526
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-03 14:25:50 +0200

    solution was added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Person } from './components/Person';
 
 const misha = {
   name: 'Misha',
@@ -24,24 +25,27 @@ const alex = {
 };
 
 const App = () => (
-  <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
-  </div>
+  <>
+    <Person
+      name={misha.name}
+      age={misha.age}
+      partner={misha.partnerName}
+      sex={misha.sex}
+      isMarried={misha.isMarried}
+    />
+    <Person
+      name={olya.name}
+      partner={olya.partnerName}
+      sex={olya.sex}
+      isMarried={olya.isMarried}
+    />
+    <Person
+      name={alex.name}
+      age={alex.age}
+      sex={alex.sex}
+      isMarried={alex.isMarried}
+    />
+  </>
 );
 
 export default App;
---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Person.scss';
+
+export const Person = ({ name, age, partner, isMarried, sex }) => (
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is {name}
+      </h2>
+      <p className="Person__age">
+        {age && `I am ${age}`}
+      </p>
+      <p className="Person__partner">
+        {(isMarried && sex === 'm') && `My wife's name is ${partner}`}
+        {(isMarried && sex === 'f') && `My husband's name is ${partner}`}
+        {isMarried === false && `I am not married`}
+      </p>
+    </section>
+  </div>
+);
