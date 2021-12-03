ee027adfb62062a977a02a9a39eafb8768e45141
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-12-03 14:32:32 +0200

    minor fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,7 +5,8 @@ export const Person = ({ name, age, partner, isMarried, sex }) => (
   <div className="App">
     <section className="Person">
       <h2 className="Person__name">
-        My name is {name}
+        My name is
+        {name}
       </h2>
       <p className="Person__age">
         {age && `I am ${age}`}
