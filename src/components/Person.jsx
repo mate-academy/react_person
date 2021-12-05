ba713f4133e7533d61282f128e74064fca253c33
Veronika Korona <Korona.vero@gmail.com>
2021-12-05 14:30:45 +0100

    Fixed

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -12,16 +12,12 @@ export const Person = ({ name, age, sex, isMarried, partnerName }) => {
       <p className="Person__age">
         {age && (`I am ${age}`)}
       </p>
-      {!isMarried ? (
-        <p className="Person__partner">
-          I am not married
-        </p>
-      ) : (
-        <p className="Person__partner">
-          {`My ${lovePartner}'s name is ${partnerName}`}
-        </p>
-      )
-      }
+      <p className="Person__partner">
+        {!isMarried
+          ? 'I am not married'
+          : `My ${lovePartner}'s name is ${partnerName}`
+        }
+      </p>
     </section>
   );
 };
