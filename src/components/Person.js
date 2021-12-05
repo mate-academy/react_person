2c4a94d6937e1266a4638bd0c18649788b0b2677
nadiaH <nadiahrb79@gmailcom>
2021-12-05 18:19:34 +0200

    spacing

--- src/components/Person.js
+++ src/components/Person.js
@@ -12,20 +12,22 @@ export const Person = ({ name, age, sex, isMarried, partnerName }) => (
     <p className="Person__age">
       {age && `I am ${age}`}
     </p>
-    {isMarried ? (
-      <p className="Person__partner">
-        {
-            sex === 'm'
-              ? 'My wife'
-              : 'My husband'
-          }
-        {`'s name is ${partnerName}`}
-      </p>
-    ) : (
-      <p className="Person__partner">
-        I am single
-      </p>
-    )}
+    {isMarried
+      ? (
+        <p className="Person__partner">
+          {
+              sex === 'm'
+                ? 'My wife'
+                : 'My husband'
+            }
+          {`'s name is ${partnerName}`}
+        </p>
+      )
+      : (
+        <p className="Person__partner">
+          I am single
+        </p>
+      )}
   </section>
 );
 
