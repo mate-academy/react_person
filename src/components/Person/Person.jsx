01de49b2f875a565ad323e3d4ab3a1dfe2cdeaf0
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-06 16:59:29 +0200

    solution

---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,34 @@
+// eslint-disable max-len
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Person.css';
+
+export function Person({ name, age, partnerName, isMarried, sex }) {
+  const partner = sex === 'f' ? 'husband' : 'wife';
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        <p className="Person__age">
+          {age
+            ? `I am ${age}`
+            : null}
+        </p>
+        <p className="Person__partner">
+          {isMarried
+            ? `My ${partner}'s name is ${partnerName}`
+            : 'I am not married'
+          }
+        </p>
+      </section>
+    </div>
+  );
+}
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  sex: PropTypes.string.isRequired,
+};
