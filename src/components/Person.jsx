3446f82e2d4e8be7362b1952507cab622e9ee8e6
Aleksandr Prokopovich <aldmpr@gmail.com>
2021-12-06 17:29:04 +0200

    Another

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -6,10 +6,16 @@ const Person = ({ name, age, sex, isMarried, partnerName }) => {
 
   return (
     <section className="person">
-      <h2 className="person__name">{`My name is ${name}`}</h2>
-      <p className="person__age">{age ? `I am ${age}` : null}</p>
+      <h2 className="person__name">
+        {`My name is ${name}`}
+      </h2>
+      <p className="person__age">
+        {age ? `I am ${age}` : null}
+      </p>
       <p className="person__partner">
-        { isMarried ? `My ${partner} is ${partnerName}` : `I am not married` }
+        { isMarried
+          ? `My ${partner} is ${partnerName}`
+          : `I am not married` }
       </p>
     </section>
   );
