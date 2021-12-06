c399e355df79085a3390baf7992b660ddbbc0dc1
Artem Skachkov <92296409+skachkov23@users.noreply.github.com>
2021-12-06 12:45:09 +0200

    Update src/components/Person.jsx
    Co-authored-by: Illia Vovchenko <37161315+illiarerun@users.noreply.github.com>

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -16,8 +16,10 @@ export function Person({ person }) {
     <>
       <section className="Person">
         <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-        {person.age
-          ? (<p className="Person__age">{`I am ${person.age}`}</p>) : null
+        {person.age && (
+          <p className="Person__age">
+            {`I am ${person.age}`}
+          </p>) 
         }
         <p className="Person__partner">{partnerText}</p>
       </section>
