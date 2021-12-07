0fd2aba8e19889f6bda3d0c7d9d85d6f2278ef69
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-07 19:57:05 +0200

    correct age rendering

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,7 +11,13 @@ export function Person({ name, age, partnerName, isMarried, sex }) {
     <div className="App">
       <section className="Person">
         <h2 className="Person__name">{`My name is ${name}`}</h2>
-        <p className="Person__age">{age && `I am ${age}`}</p>
+
+        {age ? (
+          <p className="Person__age">{age && `I am ${age}`}</p>
+        )
+          : null
+        }
+
         <p className="Person__partner">
           {isMarried
             ? `My ${partner}'s name is ${partnerName}`
