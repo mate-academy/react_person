d6445995c5a34cb99fd075e9b09e6e7b44891bff
Alina Shchavinska <shchavinska.alina@gmail.com>
2021-12-08 13:01:46 +0200

    added isMarried

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-export const Person = ({ name, age, sex, partnerName }) => {
+export const Person = ({ name, age, sex, isMarried, partnerName }) => {
   const partner = sex === 'f' ? 'husband' : 'wife';
 
   return (
     <section className="person">
-      {name && (
-        <h2 className="person__name">
-          {`My name is ${name}`}
-        </h2>
-      )}
+      <h2 className="person__name">
+        {`My name is ${name}`}
+      </h2>
       {age && (
         <p className="person__age">
           {`I am ${age}`}
         </p>
       )}
       <p className="person__partner">
-        {partnerName
+        {isMarried
           ? `My ${partner}'s name is ${partnerName}`
           : 'Single'}
       </p>
@@ -30,11 +28,13 @@ Person.defaultProps = {
   age: null,
   partnerName: null,
   sex: 'M or F',
+  isMarried: false,
 };
 
 Person.propTypes = {
   name: PropTypes.string.isRequired,
   age: PropTypes.number,
   sex: PropTypes.string,
+  isMarried: PropTypes.bool,
   partnerName: PropTypes.string,
 };
