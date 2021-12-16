eadc5e593ad8208103e5b83462bc8db9bc038049
Oleh Khashchevskyi <hashchevskyj@gmail.com>
2021-12-16 18:55:56 +0200

    fixed code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,15 +7,11 @@ function Person({ name, age, sex, isMarried, partnerName }) {
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {name}
+        {`My name is ${name}`}
       </h2>
       {age && (
       <p className="Person__age">
-        I am
-        {' '}
-        {age}
+        {`I am ${age}`}
       </p>
       )}
       <p className="Person__partner">
@@ -42,7 +38,6 @@ Person.defaultProps = {
   sex: 'm',
   isMarried: false,
   partnerName: null,
-
 };
 
 export { Person };
