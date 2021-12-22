d2d0ea7a31877e521c9d9153c88f9163f5b44f69
Anastasiia Krainova <ankr@opinov8.com>
2021-12-22 16:57:25 +0200

    create Person component

---
+++ src/components/Person/Person.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Person.scss';
+
+const Person = ({ name, age, sex, isMarried, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
+    {
+      age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )
+    }
+    {
+      isMarried
+        ? (
+          <p className="Person__partner">
+            {`My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`}
+          </p>
+        )
+        : <p className="Person__partner">I am not married</p>
+    }
+  </section>
+);
+
+export default Person;
