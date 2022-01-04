462f0fe859b08bf8880ac4afb5b2439f1092e98a
KHTRE <KHTREE@gmail.com>
2022-01-04 16:25:28 +0200

    works

---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Person.scss';
+import PropTypes from 'prop-types';
+
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${name}`}</h2>
+    <p className="Person__age">
+      {age ? (
+        `I am ${age}`
+      ) : (
+        ''
+      )}
+    </p>
+
+    <p className="Person__partner">
+      {partnerName ? (
+        `My wife's name is ${partnerName}`
+      ) : (
+        'I am not married'
+      )}
+    </p>
+  </section>
+);
+
+Person.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  sex: PropTypes.string,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
+};
