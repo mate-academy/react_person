7592d6b6a604de0090766929c27d324a77ce7ec3
Pavlo Melnyk <pashamelnyk95@gmail.com>
2022-01-11 23:16:54 +0200

    Code improvements

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const personSex = sex => (sex === 'm' ? ` wife` : ` husband`);
+
 const Person = ({ name, age, sex, isMarried, partnerName }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${name}`}</h2>
@@ -8,13 +10,12 @@ const Person = ({ name, age, sex, isMarried, partnerName }) => (
       <p className="Person__age">{`I am ${age}`}</p>
     )}
 
-    {isMarried ? (
-      <p className="Person__partner">
-        {`My ${sex === 'm' ? ` wife` : ` husband`}'s name is ${partnerName}`}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
+    <p className="Person__partner">
+      {isMarried
+        ? `My ${personSex(sex)}'s name is ${partnerName}`
+        : 'I am not married'
+      }
+    </p>
   </section>
 );
 
