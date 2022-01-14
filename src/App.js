a5fb06dfe4596eb61bde3b7ecff2e3d0097cb9dd
dmytroyevlakhov <36257947+danheim@users.noreply.github.com>
2022-01-14 10:41:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,12 +28,12 @@ const App = () => (
     <section className="Person">
       <h2 className="Person__name">My name is Misha</h2>
       <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
+      <p className="Person__partner">My partner&apos;s name is Natasha</p>
     </section>
 
     <section className="Person">
       <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
+      <p className="Person__partner">My partner&apos;s name is Maksym</p>
     </section>
 
     <section className="Person">
