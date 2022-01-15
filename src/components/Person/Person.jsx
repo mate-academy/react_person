a2e5d5e3cc8661fcd40c1ed9f276eb0d0d9fec2b
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2022-01-15 08:59:46 +0200

    done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './Person.scss';
 
-export const Person = ({ name, age, partner, isMarried, sex }) => {
-  const husbandOrWife = sex === 'm' ? 'wife' : 'husband';
+export const Person = ({ name, age, partnerName, isMarried, sex }) => {
+  const partnerStatus = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {age
-        ? (<p className="Person__age">{`I am ${age}`}</p>)
-        : null
+      {age && (<p className="Person__age">{`I am ${age}`}</p>)
       }
       <p className="Person__partner">
-        {(isMarried && (`My ${husbandOrWife}'s name is ${partner}`))
-          || (`I'm not married yet`)
+        {isMarried
+          ? (`My ${partnerStatus}'s name is ${partnerName}`)
+          : (`I'm not married yet`)
         }
       </p>
     </section>
