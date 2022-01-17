f3cbbb93155646c62d461055db199a5c73cb454d
Yulia Koshkina <julyasav4uk@gmail.com>
2022-01-17 16:32:46 +0200

    fixed

--- src/components/Person.js
+++ src/components/Person.js
@@ -12,11 +12,15 @@ export const Person = ({
 }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${name}`}</h2>
-    <p className="Person__age">{age ? `I am ${age}` : null}</p>
+    {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+    )}
     <p className="Person__partner">
       {isMarried
         ? `My ${sex === 'm' ? `wife's` : `husband's`} name is ${partnerName}`
-        : `I am not married`
+        : 'I am not married'
       }
     </p>
   </section>
