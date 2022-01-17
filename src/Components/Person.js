fece4ff28be34353a0383a08ac367cb5ce75ee0f
Aleksandra Mileant <aleksandramileant1@gmail.com>
2022-01-17 16:39:40 +0100

    done

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -14,7 +14,7 @@ const Person = ({
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && (
-        <p className="Person__age">{age && `I am ${age}`}</p>
+        <p className="Person__age">{`I am ${age}`}</p>
       )}
       <p className="Person__partner">
         {isMarried
