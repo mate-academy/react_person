91a07e02dcdc4fc36be11f34141e1786ec5611e3
Aleksandra Mileant <aleksandramileant1@gmail.com>
2022-01-17 16:44:34 +0100

    last commit

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -8,18 +8,25 @@ const Person = ({
   isMarried,
   partnerName,
 }) => {
-  const getSexOfPartner = (sex === 'f' ? 'husband' : 'wife');
+  const getSexOfPartner = sex === 'f'
+    ? 'husband'
+    : 'wife';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
       {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
       )}
       <p className="Person__partner">
         {isMarried
           ? `My ${getSexOfPartner()}'s name is ${partnerName}`
-          : `I am single`}
+          : `I am single`
+        }
       </p>
     </section>
   );
