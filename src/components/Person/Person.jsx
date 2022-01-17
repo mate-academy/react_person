5ce2f35b040d1b23d99f0e84cb3d6e3d912e7c14
Taras Zherebetskiy <tarasj76@gmail.com>
2022-01-17 10:27:39 +0200

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,19 +12,18 @@ const Person = (props) => {
     partnerName,
   } = props;
 
-  const personSex = (sex === 'm') ? ` wife` : ` husband`;
+  const personStatus = (sex === 'm') ? ` wife` : ` husband`;
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age
-        ? <p className="Person__age">{`I am ${age}`}</p>
-        : <></>
+        && <p className="Person__age">{`I am ${age}`}</p>
       }
       {isMarried
         ? (
           <p className="Person__partner">
-            {`My ${personSex}'s name is ${partnerName}`}
+            {`My ${personStatus}'s name is ${partnerName}`}
           </p>
         )
         : (
