ff17860ad9b009e4a0c75f3dff017acf355f5016
Pavel Shulga <78903159+Mikedacheka@users.noreply.github.com>
2022-01-19 11:26:28 +0200

    Update src/components/Person.jsx
    agree
    
    Co-authored-by: dmytroyevlakhov <36257947+danheim@users.noreply.github.com>

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -16,15 +16,12 @@ export const Person = ({
         {name}
       </h2>
 
-      {(age ? (
+      {age && (
         <p className="Person__age">
           I am
           {' '}
           {age}
         </p>
-      ) : (
-        null
-      )
       )}
 
       {
