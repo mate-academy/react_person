484579cf75d86b619463179b340bd93b8b3297e8
Artem <tema.maksimovich@gmail.com>
2022-01-20 13:08:18 +0200

    Task is done

--- src/App.js
+++ src/App.js
@@ -29,22 +29,6 @@ const App = () => (
     <Person {...misha} />
     <Person {...olya} />
     <Person {...alex} />
-    {/* <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section> */}
   </div>
 );
 
