7d1e2d6a47acee79619109edd3c53829cdb6d512
Vasiliy <voronvasia@gmail.com>
2022-01-24 20:10:43 +0200

    bug fixed

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -3,12 +3,16 @@ import './Person.scss';
 
 export const Person = ({ name, age, sex, isMarried, partnerName }) => (
   <section className="Person">
-    <h2 className="Person__name">
-      {name && `My name is ${name}`}
-    </h2>
-    <p className="Person__age">
-      {age && `I am ${age}`}
-    </p>
+    {name && (
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+    )}
+    {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+    )}
     <p className="Person__partner">
       {!isMarried ? 'I am not married'
         : `My
