8c0c97eaa17efaadf75876a1f03c91e08a8a5dd9
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-02-10 13:43:58 +0200

    optional

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Person({ name, age, sex, isMarried, partnerName }) {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <p className="Person__age">{`I am ${age}`}</p>
+
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+
       <p className="Person__partner">
-        {(isMarried && (
-          `My ${sex === 'f' ? 'husband' : 'wife'}'s name is ${partnerName}`
-        ))
-        || `I am not married`}
+        {
+          (isMarried && (
+            `My ${sex === 'f' ? 'husband' : 'wife'}'s name is ${partnerName}`
+          )) || `I am not married`
+        }
       </p>
     </section>
   );
 }
+
+Person.defaultProps = {
+  name: 'Guest',
+  age: undefined,
+  sex: undefined,
+  isMarried: false,
+  partnerName: undefined,
+};
+
+Person.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  sex: PropTypes.string,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
+};
 
 export default Person;
