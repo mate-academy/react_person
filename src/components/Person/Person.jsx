634e499a89e943adaaf67ea80ec1138328f9a108
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-02-11 16:35:14 +0200

    do not repeat yourself

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,14 +10,11 @@ const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age ? <p className="Person__age">{`I am ${age}`}</p> : null}
-      {!isMarried
-        ? <p className="Person__partner">I am not married</p>
-        : (
-          <p className="Person__partner">
-            {`My ${partnerSex}'s name is ${partnerName}`}
-          </p>
-        )
-        }
+      <p className="Person__partner">
+        {!isMarried
+          ? 'I am not married'
+          : `My ${partnerSex}'s name is ${partnerName}`}
+      </p>
     </section>
   );
 };
