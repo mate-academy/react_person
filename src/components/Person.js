ed306f74600eb0a8830347b5aa27d075e074cbe4
Oleksandr Polotniuk <oleksandr.polotniuk@gmail.com>
2022-02-11 12:54:01 +0200

    some fixes

--- src/components/Person.js
+++ src/components/Person.js
@@ -12,32 +12,34 @@ export const Person = ({
   <section className="Person">
     <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-    {age ? (<p className="Person__age">{`I am ${age}`}</p>) : (null)}
+    {age
+      ? (<p className="Person__age">{`I am ${age}`}</p>)
+      : (null)
+    }
 
-    {!isMarried ? (<p className="Person__partner">I am not married</p>) : (
-      <p className="Person__partner">
-        {
-        sex === 'm'
-          ? `My wife's name is ${partnerName}`
-          : `My husband's name is ${partnerName}`
-        }
-      </p>
-    )}
+    {!isMarried
+      ? (<p className="Person__partner">I am not married</p>)
+      : (
+        <p className="Person__partner">
+          {
+          sex === 'm'
+            ? `My wife's name is ${partnerName}`
+            : `My husband's name is ${partnerName}`
+          }
+        </p>
+      )}
   </section>
 );
 
 Person.defaultProps = {
-  name: 'Unknown',
   age: null,
-  sex: null,
-  isMarried: false,
   partnerName: null,
 };
 
 Person.propTypes = {
-  name: string,
+  name: string.isRequired,
   age: number,
-  sex: string,
-  isMarried: bool,
+  sex: string.isRequired,
+  isMarried: bool.isRequired,
   partnerName: string,
 };
