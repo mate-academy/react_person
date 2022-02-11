a7a53cbdae1adf859632a747f8796842a3878cf0
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-02-11 13:13:23 +0200

    sex was fixed

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-const Person = ({ name, age, partnerName, sex, isMarried }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
+const Person = ({ name, age, partnerName, sex, isMarried }) => {
+  const typeOfSex = sex === 'm' ? 'wife' : 'husband';
 
-    {age && (
-      <p className="Person__age">
-        {`I am ${age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `My ${typeOfSex}'s name is ${partnerName}`
+          : `I am not married`
+        }
       </p>
-    )}
-
-    <p className="Person__partner">
-      {isMarried
-        ? `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`
-        : `I am not married`
-      }
-    </p>
-  </section>
-);
+    </section>
+  );
+};
 
 Person.defaultProps = {
   age: undefined,
