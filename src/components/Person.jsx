7e4a4bbe1dbcc945ff5a8f97fac343c236a84c99
Vadym Mishchenko <m.i.c.h.m.a.n.94@gmail.com>
2022-02-11 12:54:53 +0200

    react person

---
+++ src/components/Person.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Person.scss';
+
+const Person = ({ name, age, partnerName, sex, isMarried }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
+
+    {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+    )}
+
+    <p className="Person__partner">
+      {isMarried
+        ? `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`
+        : `I am not married`
+      }
+    </p>
+  </section>
+);
+
+Person.defaultProps = {
+  age: undefined,
+  partnerName: undefined,
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  partnerName: PropTypes.string,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+};
+
+export default Person;
