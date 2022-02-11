7e231bcdc6e00981955ac8dca98cc91d4add29c4
Misha Novikov <novikov1807@gmail.com>
2022-02-11 11:26:45 +0200

    Complite

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Person } from './components/index';
+
+import './App.scss';
+
+const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
+
+const App = () => (
+  <div className="App">
+    <Person
+      name={misha.name}
+      age={misha.age}
+      partner={misha.partnerName}
+    />
+
+    <Person
+      name={olya.name}
+      partner={olya.partnerName}
+      sex={olya.sex}
+    />
+    <Person
+      name={alex.name}
+      age={alex.age}
+      partner={alex.partnerName}
+    />
+  </div>
+);
+
+export default App;
