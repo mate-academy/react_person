32011c8de376ce29d754b2d255340245c8af44e5
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-02-11 15:45:14 +0200

    fixed mistakes Anna told me about

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.sex === 'm' && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    )}
-    {person.isMarried === true
-      ? (
-        <p className="Person__partner">
-          My
-          {person.sex === 'm' ? ` wife's ` : ` husband's `}
-          {`name is `}
-          {person.partnerName}
-        </p>
-      )
-      : (<p className="Person__partner">I am not married</p>)
-    }
-  </section>
-);
+export const Person = ({ person }) => {
+  let parthnerPhrase = `wife's`;
+  let marriedTag = (
+    <p className="Person__partner">I am not married</p>
+  );
+
+  if (person.sex === 'f') {
+    parthnerPhrase = `husband's`;
+  }
+
+  if (person.isMarried) {
+    marriedTag = (
+      <p className="Person__partner">
+        {`My ${parthnerPhrase} name is ${person.partnerName}`}
+      </p>
+    );
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
+      {marriedTag}
+    </section>
+  );
+};
