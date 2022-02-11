9ea952c17ae35b720bbcf564c7bfd5f19fd79c18
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-02-11 14:22:29 +0200

    optimization

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -2,27 +2,21 @@ import React from 'react';
 import './Person.scss';
 
 function Person({ name, age, sex, isMarried, partnerName }) {
+  const spouse = sex === 'm' ? 'wife' : 'husband';
+
   return (
     <section className="Person">
-      {name && (
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      )}
 
-      {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
-      )}
+      <p className="Person__age">{`I am ${age}`}</p>
 
-      {isMarried ? (
-        <p className="Person__partner">
-          My
-          {` ${sex === 'm' ? 'wife' : 'husband'}'s `}
-          name is
-          {` ${partnerName}`}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
-
+      <p className="Person__partner">
+        {
+          isMarried
+            ? `My ${spouse}'s name is ${partnerName}`
+            : `I am not married`
+        }
+      </p>
     </section>
   );
 }
