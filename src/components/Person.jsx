6a87db9cab9d189f8b25b7ff6e8891b46ba0db3a
Oleksandr Strilchuk <virass.m@gmail.com>
2022-02-22 12:27:25 +0200

    Fix

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -15,7 +15,7 @@ function Person({
         <h2 className="Person__name">
           {`My name is ${name}`}
         </h2>
-        {age > 0 && (
+        {age && (
           <p className="Person__age">
             {`I am ${age}`}
           </p>
