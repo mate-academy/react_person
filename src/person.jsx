be149ee8a9572f06aac6395f9c702bc24163e7cf
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-03-09 22:39:18 +0200

    solution

--- src/person.jsx
+++ src/person.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+class Partner {
+  constructor(name, spouseSex) {
+    this.noun = spouseSex === 'm' ? 'wife' : 'husband';
+    this.name = name;
+  }
+
+  toPossesiveCase() {
+    return `${this.noun}'s`;
+  }
+}
+
 export function Person({ name, sex, age, isMarried, partnerName }) {
+  const partner = new Partner(partnerName, sex);
+
   return (
     <section className="Person">
-      <h2 className="Person__name">{`my name is ${name}`}</h2>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && (
-        <p className="Person__age">{age}</p>)}
-        <p className="Person__partner">{
-          isMarried && sex === 'm' ?
-          `My wife's name is ${partnerName}`:
-          `My husband's name is ${partnerName}`
-          !isMarried &&
-        }</p>
+        <p className="Person__age">{`I am ${age}`}</p>)}
+      <p className="Person__partner">
+        {isMarried ? `My ${partner.toPossesiveCase()} name is ${partner.name}`
+          : `I am not married`}
+      </p>
     </section>
   );
 }
