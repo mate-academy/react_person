8beaee4038d90df44295f2ff8a70ef968d6031be
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-03-10 17:26:59 +0200

    little refactoring

--- src/App.js
+++ src/App.js
@@ -27,27 +27,15 @@ const alex = {
 const App = () => (
   <div className="App">
     <Person
-      name={misha.name}
-      age={misha.age}
-      sex={misha.sex}
-      isMarried={misha.isMarried}
-      partnerName={misha.partnerName}
+      {...misha}
     />
 
     <Person
-      name={olya.name}
-      age={olya.age}
-      sex={olya.sex}
-      isMarried={olya.isMarried}
-      partnerName={olya.partnerName}
+      {...olya}
     />
 
     <Person
-      name={alex.name}
-      age={alex.age}
-      sex={alex.sex}
-      isMarried={alex.isMarried}
-      partnerName={alex.partnerName}
+      {...alex}
     />
   </div>
 );
