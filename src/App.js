0cd4606bd89e2d8472c1b258aaf5b5ad2ae53249
Anna Serdechna <anserdechna@gmail.com>
2022-03-11 09:35:52 +0200

    remove comments

--- src/App.js
+++ src/App.js
@@ -3,28 +3,6 @@ import React from 'react';
 import { Person } from './component/Person';
 import './App.scss';
 
-// const misha = {
-//   name: 'Misha',
-//   age: 37,
-//   sex: 'm',
-//   isMarried: true,
-//   partnerName: 'Natasha',
-// };
-
-// const olya = {
-//   name: 'Olya',
-//   sex: 'f',
-//   isMarried: true,
-//   partnerName: 'Maksym',
-// };
-
-// const alex = {
-//   name: 'Alex',
-//   age: 25,
-//   sex: 'm',
-//   isMarried: false,
-// };
-
 const App = () => (
   <div className="App">
     <Person
@@ -50,12 +28,3 @@ const App = () => (
 );
 
 export default App;
-
-/**
- * Create a Person component and use it instead of repeating markup in the App
-
-Use conditional rendering
-Move styles to correct file
-Extra task (optional):
-Add props validation using PropTypes library and defaultProps
- */
