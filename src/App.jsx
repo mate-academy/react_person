fd122a5bd9dea71abfe0019502ddd45ebcc74d63
Dmytro Pelovych <falcon.patron@gmail.com>
2022-03-19 11:14:22 +0200

    Solution for `React Person`

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Person } from './components/Person/Person';
+
+import './App.scss';
+
+const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
+
+const App = () => (
+  <div className="App">
+    {Person(misha)}
+    {Person(olya)}
+    {Person(alex)}
+  </div>
+);
+
+export { App };
---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Person.scss';
+
+const defineEngagementState = (sex, isMarried, partnerName) => {
+  if (!isMarried) {
+    return 'I am not married';
+  }
+
+  const partnerSex = () => {
+    if (!sex) {
+      return 'partner\'s';
+    }
+
+    return sex === 'f' ? ('husband\'s') : ('wife\'s');
+  };
+
+  return partnerName
+    ? `My ${partnerSex()} name is ${partnerName}`
+    : 'I am married';
+};
+
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
+
+    {age && age >= 0 && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+    )}
+
+    <p className="Person__partner">
+      {defineEngagementState(sex, isMarried, partnerName)}
+    </p>
+  </section>
+);
+
+Person.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  sex: PropTypes.oneOf(['m', 'f', null]),
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string || PropTypes.objectOf(null),
+};
+
+Person.defaultProps = {
+  name: 'unknown',
+  age: -1,
+  sex: null,
+  isMarried: false,
+  partnerName: null,
+};
