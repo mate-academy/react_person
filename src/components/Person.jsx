fbabdc575abccf292a3f74c243ec73f99f0ab143
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-03-23 00:11:18 +0200

    Person details

---
+++ src/components/Person.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Person.scss';
+
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) => {
+  let partner = 'I am not married';
+
+  if (isMarried) {
+    partner = sex === 'm' ? `My wife's name is ${partnerName}`
+      : `My husband's name is ${partnerName}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age !== 0 && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {partner}
+      </p>
+    </section>
+  );
+};
+
+Person.defaultProps = {
+  age: 0,
+  partnerName: '',
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  partnerName: PropTypes.string,
+};
