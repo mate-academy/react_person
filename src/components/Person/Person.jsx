53695fdf847282ce134a0bdd756aa72144b240f9
Oleksii Fedorenko <alex4rest09@gmail.com>
2022-03-25 07:54:29 +0200

    solution v2

---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Person.scss';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  let spouse;
+
+  sex === 'm'
+    ? spouse = 'wife'
+    : spouse = 'husband';
+
+  let married;
+
+  isMarried
+    ? married = `My ${spouse}'s name is ${partnerName}`
+    : married = `I'm not married`;
+
+  let personAge;
+
+  age === undefined
+    ? personAge = ''
+    : personAge = `I'm ${age}`;
+
+  return (
+    <section className="person">
+      <h2 className="person__name">{`My name is ${name}`}</h2>
+      <p className="person__age">{`${personAge}`}</p>
+      <p className="person__partner">
+        {`${married}`}
+      </p>
+    </section>
+  );
+}
+
+Person.defaultProps = {
+  age: '',
+  isMarried: false,
+  partnerName: null,
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  sex: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
+};
