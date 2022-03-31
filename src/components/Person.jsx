b856c7b88a45d7a030426017cf425d8cf772ea3c
Ivan Major <tarstarcas@gmail.com>
2022-03-31 20:47:01 +0300

    V2

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -12,12 +12,14 @@ const Person = ({
   <section className="Person">
     <h2 className="Person__name">
       My name is
+      {' '}
       {name}
     </h2>
 
     {age && (
     <p className="Person__age">
       I am
+      {' '}
       {age}
     </p>
     )}
