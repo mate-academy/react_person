00cc919e5bf74c06a3d2276f9274bc2a0fc28a88
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-04-15 21:49:33 +0300

    The solution was implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Person from './components/Person/Person';
 import './App.scss';
 
 const misha = {
   name: 'Misha',
   age: 37,
-  sex: 'm',
+  // sex: 'm',
   isMarried: true,
   partnerName: 'Natasha',
 };
@@ -24,24 +25,13 @@ const alex = {
 };
 
 const App = () => (
-  <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
-  </div>
+  <>
+    <div className="App">
+      <Person {...misha} />
+      <Person {...olya} />
+      <Person {...alex} />
+    </div>
+  </>
 );
 
 export default App;
