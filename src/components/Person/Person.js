1c93608c695b6af17ab6c0f090cb77a69e671815
Misha_Ivchenko <ivchenkomykhail@meta.ua>
2022-04-18 18:00:36 +0300

    Update src/components/Person/Person.js
    Co-authored-by: Alisa Ivashkevych <57598825+aliceblunt8@users.noreply.github.com>

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -13,9 +13,9 @@ const Person = ({
   <>
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {name}
+        {`My name is ${name}`}
+       
+      
       </h2>
       {age && (
       <p className="Person__age">
