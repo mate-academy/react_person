1f902023017f7a8c4f6afd7cc6bef5a35deb643c
Ruslan Hubanov <ruslangubanov88@gmail.com>
2022-04-29 16:02:43 +0300

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,14 +14,12 @@ export const Person = ({
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {name}
+        {`My name is ${name}`}
       </h2>
       {age
         && (
           <p className="Person__age">
-            I am
-            {age}
+            {`I am ${age}`}
           </p>
         )}
       <p className="Person__partner">
