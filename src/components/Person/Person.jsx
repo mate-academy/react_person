c35f4df85224cb3640f0df277e77e23ba070daf5
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-04-29 11:25:39 +0200

    style fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,8 +13,14 @@ export const Person = (props) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
       <p className="Person__partner">
         {isMarried
           ? `My ${sex === 'm' ? 'wife' : 'husbend'}'s name is ${partnerName}`
