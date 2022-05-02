d994369f15f55734eb8accfb092dd1659f11fca3
Anastasiia Sharkova <sharkova.nastik@gmail.com>
2022-05-02 12:08:32 +0200

    person PR-2

--- src/components/Person.js
+++ src/components/Person.js
@@ -10,11 +10,7 @@ const Person = ({
   partnerName,
 }) => (
   <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {' '}
-      {name}
-    </h2>
+    <h2 className="Person__name">My name is {name}</h2>
     <p className="Person__age">
       {age ? `I am ${age}` : null}
     </p>
