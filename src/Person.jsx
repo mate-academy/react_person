e85ca15b3f70cec62c8e3205fa7162c47dd7a985
Alexandr Nikytenko <nika200382@gmail.com>
2022-05-04 18:14:25 +0300

    add file

---
+++ src/Person.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Person = ({ props }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = props;
+
+  const partnerSex = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      <p className="Person__age">
+        {age && `I am ${age}`}
+      </p>
+
+      <p className="Person__partner">
+        {isMarried
+          ? `My ${partnerSex} name is ${partnerName}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
+
+Person.defaultProps = {
+  age: null,
+  sex: null,
+  isMarried: null,
+  partnerName: null,
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
+};
+
+export default Person;
