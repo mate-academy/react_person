90e281784ebad60d5d47aa6a3034e903dcc3aefd
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2022-05-11 20:14:44 +0300

    solution v.1.1.2 without cheating linter

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  // eslint-disable-next-line
-  <App />,
-  document.getElementById('root'),
-);
