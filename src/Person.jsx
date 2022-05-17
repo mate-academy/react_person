0d0fef2b113ce9587748b1ac0c613a0d880f2148
kshvetsova <katshvetsova@gmail.com>
2022-05-17 21:29:37 +0300

    fixed Solution

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { PropTypes } from 'prop-types';
 
 export const Person = ({ name, sex, age, isMarried, partnerName }) => (
   <section className="Person">
