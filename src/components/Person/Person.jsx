ecd4c811842a4b809c9afe443eec534aefc0938a
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-25 19:39:25 +0300

    added new files

---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Person.scss';
+
+export const Person = ({ personInfo }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = personInfo;
+
+  return (
+    // eslint-disable-next-line
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      {isMarried && (
+        <p className="Person__partner">
+          {`My ${sex === 'f' ? ('husband') : ('wife')}'s name is ${partnerName}`}
+        </p>
+      )}
+    </section>
+  );
+};
+
+Person.defaultProps = {
+  age: 0,
+  partnerName: '',
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string,
+};
