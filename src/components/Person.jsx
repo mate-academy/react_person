1838b06a35a79d048516ba121eba6f674d93be5e
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-05-25 21:29:38 +0300

    fixed

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,32 +1,58 @@
 import React from 'react';
 import './Person.scss';
+import PropTypes from 'prop-types';
 
 export const Person = ({ person }) => {
-  const partner = person.sex === 'm'
+  const {
+    name,
+    sex,
+    age,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const partner = sex === 'm'
     ? 'wife'
     : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {`My name is ${person.name}`}
+        {`My name is ${name}`}
       </h2>
 
       {person.age && (
         <p className="Person__age">
-          {`I am ${person.age}`}
+          {`I am ${age}`}
         </p>
       )}
 
       <p className="Person__sex">
-        {`Sex: ${person.sex}`}
+        {`Sex: ${sex}`}
       </p>
 
       <p className="Person__partner">
-        {!person.isMarried
+        {!isMarried
           ? ('I am not married')
-          : (`My ${partner}'s name is ${person.partnerName}`)}
+          : (`My ${partner}'s name is ${partnerName}`)}
       </p>
     </section>
   );
+};
+
+Person.defaultProps = {
+  person: {
+    age: 0,
+    partnerName: '',
+  },
+};
+
+Person.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    sex: PropTypes.string.isRequired,
+    isMarried: PropTypes.bool.isRequired,
+    partnerName: PropTypes.string,
+  }),
 };
