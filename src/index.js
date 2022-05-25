42fa36dc98b17aa0de8adb9a56f4357d91e5a82a
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-25 20:21:34 +0200

    implement persons v.1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
   <App />,
   document.getElementById('root'),
 );
