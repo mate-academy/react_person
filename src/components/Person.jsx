4d5130ec075a50f886da0207a8c3cf72578c5988
RomekIvantsiv <ivantsiv.romek@gmail.com>
2022-05-25 16:49:30 +0200

    First try

---
+++ src/components/Person.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+// eslint-disable-next-line
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+// eslint-disable-next-line
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
+
+export const Person = ({ person }) => {
+  const {
+    name, age, sex, isMarried, partnerName,
+  } = person;
+
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+        <p className="Person__age">
+          { age
+            ? `I am ${age}`
+            : null}
+        </p>
+
+        { isMarried
+          ? (
+            <p className="Person__partner">
+              { sex === 'm'
+                ? `My wife's name is ${partnerName}`
+                : `My husband's name is ${partnerName}`}
+            </p>
+          )
+          : <p>I am not married</p>}
+      </section>
+    </>
+  );
+};
