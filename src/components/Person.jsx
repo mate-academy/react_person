95fb3ac57aadfaf13365aa55bc99a82b1d2c48d9
Daria Liubymova <dellachaise@gmail.com>
2022-05-26 17:53:52 +0300

    Refactored code

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -16,8 +16,8 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age ? <p className="Person__age">{`I am ${age}`}</p> : null}
-      {isMarried ? <p className="Person__partner">{`My ${partner} name is ${partnerName}`}</p> : 'I am not married'}
+      <p className="Person__age">{age && `I am ${age}`}</p>
+      {isMarried ? <p className="Person__partner">{`My ${partner}'s name is ${partnerName}`}</p> : 'I am not married'}
     </section>
   );
 };
