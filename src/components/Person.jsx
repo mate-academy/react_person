6e9ff618a97fde2d172865b09177cd2272fc1230
Danylo Dzyuban <dz.danylo@gmail.com>
2022-05-27 13:23:38 +0300

    Solution

---
+++ src/components/Person.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Person.scss';
+import PropTypes from 'prop-types';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const partner = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? (`My ${partner}'s name is ${partnerName}`)
+          : ('I am not married')}
+      </p>
+    </section>
+  );
+};
+// eslint-disable-next-line
+Person.PropTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    sex: PropTypes.string,
+    isMarried: PropTypes.bool.isRequired,
+    partnerName: PropTypes.string,
+  }),
+};
