5f269b43c7e9bb4bccc18e0955cc38a83baeb98f
Maryna <savna023@gmail.com>
2022-05-29 19:29:52 +0300

    fixed

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -5,25 +5,32 @@ import './Person.scss';
 
 export function Person({ person }) {
   const personPartner = person.sex === 'f'
-    ? 'husband' : 'wife';
+    ? 'husband'
+    : 'wife';
 
   return (
     <section className="Person">
-      <>
-        <h2 className="Person__name">
-          {`My name is ${person.name}`}
-        </h2>
-        <p className="Person__age">
-          { person.age && `I am ${person.age}` }
-        </p>
-        <p className="Person__partner">
-          {person.isMarried ? `My ${personPartner}'s name is ${person.partnerName}`
-            : 'I am not married'}
-        </p>
-      </>
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      <p className="Person__age">
+        { person.age && (
+          `I am ${person.age}`
+        ) }
+      </p>
+      <p className="Person__partner">
+        {person.isMarried
+          ? `My ${personPartner}'s name is ${person.partnerName}`
+          : 'I am not married'}
+      </p>
     </section>
   );
 }
+
+Person.defaultProps = {
+  age: 0,
+  partnerName: '',
+};
 
 Person.prototypes = {
   personInfo: PropTypes.shape({
