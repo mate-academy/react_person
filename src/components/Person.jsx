1f457d790fed689823d9ea5e03bd4f0d1c60b45b
MabuzaM <mabuzamfano@gmail.com>
2022-05-29 20:12:02 +0200

    Task complete

---
+++ src/components/Person.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Person.scss';
+
+const Person = ({ name, age, partnerName }) => {
+  return (
+    <section className="Person">
+      {
+        name && (
+          <h2 className="Person__name">
+            My name is&nbsp;
+            {name}
+          </h2>
+        )
+
+      }
+
+      {
+        age && (
+          <p className="Person__age">
+            I am&nbsp;
+            { age }
+          </p>
+        )
+      }
+
+      {
+        partnerName
+          ? (
+            <p className="Person__partner">
+              My wife&apos;s name is&nbsp;
+              { partnerName }
+            </p>
+          )
+          : <p>I am not married</p>
+      }
+    </section>
+  );
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  partnerName: PropTypes.string.isRequired,
+};
+
+export default Person;
