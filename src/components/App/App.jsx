ecfe781005d3c052a4c7a3acd0dcf1c84d3e9ce7
Hubert Wolak <29hihub@gmail.com>
2022-05-30 14:21:34 +0200

    Solution

---
+++ src/components/App/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Person } from '../Person';
+
+// eslint-disable-next-line
+const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+// eslint-disable-next-line
+const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+// eslint-disable-next-line
+const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
+
+export const App = () => (
+  <div className="App">
+    <Person {...misha} />
+    <Person {...olya} />
+    <Person {...alex} />
+  </div>
+);
+
+export default App;
