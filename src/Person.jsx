41dd510f4ee5189aff7c001a8b75581408d311cf
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-05-30 14:26:57 +0300

    add some text

--- src/Person.jsx
+++ src/Person.jsx
@@ -4,17 +4,44 @@ import PropTypes from 'prop-types';
 const Person = ({
   name,
   age,
-  partner,
-}) => (
-  <>
-    <h2>{`${name}`}</h2>
-    <p>{`${age}`}</p>
-    <p>{`${partner}`}</p>
-  </>
-);
+  partnerName,
+  sex,
+  isMarried,
+}) => {
+  const partner = (sex === 'f') ? 'husband' : 'wife';
+
+  return (
+    <>
+      <h2>
+        My name is
+        {' '}
+        {`${name}`}
+      </h2>
+
+      {age && (
+        <p>{`I am ${age}`}</p>
+      )}
+
+      {isMarried ? (
+        <p>
+          {`My ${partner} name is ${partnerName}`}
+        </p>
+      ) : (<p> I am not married </p>)}
+    </>
+  );
+};
+
+Person.defaultProps = {
+  age: null,
+  partnerName: null,
+  isMarried: false,
+};
 
 Person.propTypes = {
   name: PropTypes.string.isRequired,
+  age: PropTypes.number || PropTypes.object,
+  partnerName: PropTypes.string,
+  isMarried: PropTypes.bool,
 };
 
 export default Person;
