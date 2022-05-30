c3904ddcf68cce41a684a678f502748019112546
Andrii Rutkovskii <greateman94@gmail.com>
2022-05-30 17:07:28 +0300

    index for export

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.scss';
-import { PersonalInfo } from './componenta/personal_info/index';
-import { misha, olya, alex } from './componenta/users/index';
+import { PersonalInfo } from './componenta/personalInfo';
+import { misha, olya, alex } from './componenta/users/users';
 
 const App = () => (
   <div className="App">
     <PersonalInfo person={misha} />
-
     <PersonalInfo person={olya} />
     <PersonalInfo person={alex} />
   </div>
