944bc559e1584a39c3f147c2c05454083e3f478a
Hubert Wolak <29hihub@gmail.com>
2022-05-30 14:53:44 +0200

    Solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,14 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
+  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
   rules: {
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: true,
-      optionalDependencies: false,
-      peerDependencies: false,
-    }],
-    'react/prop-types': 0,
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
       ignoreComments: true,
     }],
-    'jsx-a11y/label-has-associated-control': ['error', {
-      assert: 'either',
+    'jsx-a11y/label-has-associated-control': ["error", {
+      assert: "either",
     }],
-    'jsx-a11y/control-has-associated-label': 'off',
-    'react/jsx-filename-extension': ['warn', {
-      extensions: ['.jsx'],
-    }],
+    "react/jsx-filename-extension": [0],
+    "import/extensions": "off",
   },
 };
