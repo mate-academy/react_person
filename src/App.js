15fb113c6a9c441f3d16e45edbce6268f97efa34
RomekIvantsiv <ivantsiv.romek@gmail.com>
2022-05-31 17:51:15 +0200

    Remarks were fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './components/Person.scss';
-import {
-  Person, misha, olya, alex,
-} from './components/Person';
+import { Person } from './components/Person';
+
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+// eslint-disable-next-line
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+// eslint-disable-next-line
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
 
 const App = () => (
   <div className="App">
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,40 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-// eslint-disable-next-line
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-// eslint-disable-next-line
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
-
 export const Person = ({ person }) => {
   const {
     name, age, sex, isMarried, partnerName,
   } = person;
 
-  let partner = 'husband';
-
-  if (sex === 'm') {
-    partner = 'wife';
-  }
+  const personPartner = sex === 'f' ? 'wife' : 'husband';
 
   return (
 
@@ -42,14 +14,15 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
       <p className="Person__age">
-        { age
-          ? `I am ${age}`
-          : null}
+        { age && (
+          `I am ${age}`
+        )}
       </p>
 
       <p className="Person__partner">
         {isMarried
-          ? (`My ${partner}'s name  is ${partnerName}`) : ('I am not married') }
+          ? (`My ${personPartner}'s name is ${partnerName}`)
+          : ('I am not married') }
       </p>
 
     </section>
