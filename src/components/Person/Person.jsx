232b0962368d287814010e787edbc7c0dd49fa90
Lebogang Mogane <mogane420@gmail.com>
2022-05-31 02:26:40 +0200

    solution

---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Person.scss';
+
+function Person({ personalInfo }) {
+  const {
+    name,
+    age,
+    partnerName,
+    isMarried,
+    sex,
+  } = personalInfo;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is {name}
+      </h2>
+      <p className="Person__age">
+        I am {age}
+      </p>
+      {isMarried ? (
+        <p className="Person__partner">
+          {sex === 'm' ? (
+            `My wife's name is ${partnerName}`
+          ) : (
+            `My husband's name is ${partnerName}`
+          )}
+        </p>
+      ) : 'I am not married'}
+    </section>
+  );
+}
+
+export default Person;
