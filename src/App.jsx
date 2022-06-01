da4d354dee5064161879246cbefb8f79d9319fa9
Andrei Sucman <sucmanandrei@gmail.com>
2022-06-01 15:17:31 +0300

    removed spreads

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,9 @@ const alex = {
 
 const App = () => (
   <div className="App">
-    <Person {...misha} />
-
-    <Person {...olya} />
-
-    <Person {...alex} />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
 
