2c77fc2e743bf266a418b54f39931f45447c0bd3
Nazar Muliar <nazarmuliar26@gmail.com>
2022-06-06 17:11:46 +0300

    added defaultProps and propTypes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './Person.scss';
+import PropTypes from 'prop-types';
 
 const Person = ({
   name: firstName, age, sex, isMarried, partnerName,
@@ -23,5 +24,18 @@ const Person = ({
     </section>
   </>
 );
+
+Person.defaultProps = {
+  age: null,
+  partnerName: null,
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string,
+};
 
 export default Person;
