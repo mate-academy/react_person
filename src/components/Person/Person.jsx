28469f430a1a19eef1ac3391b47ccfd48ef90b12
Nazar Muliar <nazarmuliar26@gmail.com>
2022-06-06 17:05:28 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// Write code here
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './Person.scss';
+
+const Person = ({
+  name: firstName, age, sex, isMarried, partnerName,
+}) => (
+  <>
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${firstName}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+      <p className="Person__partner">
+        {!isMarried
+          ? 'I am not married'
+          : `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`}
+      </p>
+    </section>
+  </>
+);
+
+export default Person;
