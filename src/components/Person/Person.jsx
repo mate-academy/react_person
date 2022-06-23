0500401887c8c556431da6300638e91a8b5c200b
Olya Bushovska <OlyaBushovska767@gmail.com>
2022-06-23 16:43:25 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
 // Write code here
+import React from 'react';
+
+export default function Person(props) {
+  return (
+    <div>
+      <h2 className="Person__name">
+        My name is
+        {props.name}
+      </h2>
+      {props.age ? (
+        <p className="Person__age">
+          I am
+          {props.age}
+        </p>
+      ) : (
+        null
+      )}
+      <p className="Person__partner">
+        My wife&apos;s name is
+        {props.partnerName}
+      </p>
+    </div>
+  );
+}
