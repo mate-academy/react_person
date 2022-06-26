63a8ad304a7e6a49a6307f414d9c4d0a733360be
Olya Bushovska <OlyaBushovska767@gmail.com>
2022-06-26 14:49:01 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,21 +5,24 @@ export default function Person(props) {
   return (
     <div>
       <h2 className="Person__name">
-        My name is
-        {props.name}
+        My name is {props.name}
       </h2>
       {props.age ? (
         <p className="Person__age">
-          I am
-          {props.age}
+          I am {props.age}
         </p>
       ) : (
         null
-      )}
-      <p className="Person__partner">
-        My wife&apos;s name is
-        {props.partnerName}
+      )} 
+      {props.partnerName ? (
+        <p className="Person__partner">
+          My wife&apos;s or husband&apos;s name is {props.partnerName}
       </p>
+      ) : (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      )}
     </div>
   );
 }
