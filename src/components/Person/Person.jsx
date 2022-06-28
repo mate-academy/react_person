e4e7bf088f014fd4cda0a70f053cf2ea699843a0
Denys Halopa <galyopa@gmail.com>
2022-06-28 23:52:57 +0300

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// Write code here
+import React from 'react';
+import './Person.scss';
+
+export const Person = ({ name, age, isMarried, sex, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
+
+    {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+    )}
+
+    <p className="Person__partner">
+      {
+        isMarried
+          ? `My
+          ${
+            sex === 'f'
+              ? `husband's`
+              : `wife's`
+          }
+          name is ${partnerName}`
+          : 'I am not married'
+       }
+    </p>
+  </section>
+);
