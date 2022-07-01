8f30ba9f8a95a745e80ec8189fc6aae1db71c93d
Yaroslav Baiduk <byaa99@gmail.com>
2022-07-01 14:05:43 +0300

    Compleated

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// Write code here
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Person = ({
+  name,
+  age,
+  sex,
+  partnerName,
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
+
+    {age && (<p className="Person__age">{`I am ${age}`}</p>)}
+
+    {partnerName ? (
+      <p className="Person__partner">
+        {`My ${sex === 'm' ? 'wife\'s' : 'husband\'s'} name is ${partnerName}`}
+      </p>
+    ) : (
+      <p className="Person__partner">I am not married</p>
+    )}
+
+  </section>
+);
+
+Person.propTypes = {
+  name: propTypes.string.isRequired,
+};
+
+export default Person;
