40651eb8b40ad830248a131a6f22b888d76f6e60
Pavlo <Pmoroz2004@gmail.com>
2022-07-01 19:20:17 +0300

    my solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// Write code here
+import React from 'react';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  const partnerSex = () => (sex === 'm' ? 'wife' : 'husband');
+
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {` ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          I am
+          {` ${age}`}
+        </p>
+      )}
+      <p>{isMarried ? `My ${partnerSex()} ${partnerName}` : 'I am not married'}</p>
+    </section>
+  );
+}
