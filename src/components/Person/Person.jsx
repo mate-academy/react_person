8ed8c8369b1c64e7e331d2292a7e77664a2615b0
Oleksandr Kapustei <angelion541@gmail.com>
2022-07-01 17:43:15 +0300

    solution fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,42 +1,20 @@
 import React from 'react';
 
-const Person = ({ name, age = 0, sex, isMarried, partnerName }) => {
-  let blockMarriage = <p className="Person__partner">I am not married</p>;
-  let blockAge = '';
+const Person = ({ name, age = 0, sex, isMarried, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
 
-  if (age > 0) {
-    blockAge = (
-      <p className="Person__age">
-        {`I am ${age}`}
-      </p>
-    );
-  }
+    {age && <p className="Person__age">{`I am ${age}`}</p>}
 
-  if (isMarried && sex === 'm') {
-    blockMarriage = (
-      <p className="Person__partner">
-        {`My wife's name is ${partnerName}`}
-      </p>
-    );
-  }
-
-  if (isMarried && sex === 'f') {
-    blockMarriage = (
-      <p className="Person__partner">
-        {`My husband's name is ${partnerName}`}
-      </p>
-    );
-  }
-
-  return (
-    <section className="Person">
-      <h2 className="Person__name">
-        {`My name is ${name}`}
-      </h2>
-      { blockAge }
-      { blockMarriage }
-    </section>
-  );
-};
+    <p className="Person__partner">
+      {isMarried
+        ? `My ${sex === 'f' ? 'husband' : 'wife'}'s name is ${partnerName}`
+        : 'I am not married'
+      }
+    </p>
+  </section>
+);
 
 export default Person;
