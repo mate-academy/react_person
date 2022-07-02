3b13be19683c27a811e4bc94421d4492a8572aad
SAMAEV <andreysamaev@gmail.com>
2022-07-02 17:08:19 +0300

    partners

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// Write code here
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Person.scss';
+
+const Person = ({
+  name,
+  sex,
+  isMarried,
+  age,
+  partnerName,
+}) => (
+  <div className="Person">
+    <h2 className="Person__name">
+      My name is
+      {name}
+    </h2>
+    <p className="Person__age">
+      {age ? `I am ${age}` : null}
+    </p>
+    <p className="Person__partner">
+      {isMarried ? `My ${sex === 'f' ? 'husband`s' : 'wife`s'} name is ${partnerName}` : 'I am not married'}
+    </p>
+  </div>
+);
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  partnerName: PropTypes.string,
+};
+
+export default Person;
