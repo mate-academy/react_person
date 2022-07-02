7abff1a73a44d2e4aa51ae1df299f045a28b5c79
SpaceHook <maxdumiak39@gmail.com>
2022-07-02 21:46:35 +0300

    ready

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// Write code here
+import React from 'react';
+import './Person.scss';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  let partnerSex = 'My husband`s name is ';
+
+  if (sex === 'm') {
+    partnerSex = 'My wife`s name is ';
+  }
+
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">
+          My name is
+          {' '}
+          {name}
+        </h2>
+        <p className="Person__age">{age ? `I am ${age}` : null}</p>
+        <p className="Person__partner">
+          {isMarried ? partnerSex + partnerName : 'I am not married'}
+        </p>
+      </section>
+    </>
+  );
+}
