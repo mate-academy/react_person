de4ea9bbbf65300eaca8987328f8cb57ad573820
Illia Vodonis <illavodonis@gmail.com>
2022-07-03 22:48:17 +0300

    solution 01

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,33 @@
-// Write code here
+import React from 'react';
+import './Person.scss';
+
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) => {
+  const ifMarried = (sex === 'm'
+    ? `My wife name is ${partnerName}`
+    : `My husband's name ${partnerName}`);
+
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (<p className="Person__age">{`I am ${age}`}</p>)}
+
+      <p className="Person__partner">
+        {
+          isMarried
+            ? ifMarried
+            : 'I am not married'
+        }
+      </p>
+    </div>
+  );
+};
