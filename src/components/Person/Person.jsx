8781d753edf640893a84d32dd1c99b6d5df1baa5
Viacheslav Tsymbal <slava.tsymbal@ukr.net>
2022-07-03 14:04:41 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// Write code here
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './Person.scss';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  const partner = (sex === 'm')
+    ? (`My wife's name is ${partnerName}`)
+    : (`My husband's name is ${partnerName}`);
+
+  return (
+    <>
+      <h2>{`My name is ${name}`}</h2>
+      {age ? (<p>{`I am ${age}`}</p>) : null}
+      {isMarried ? (<p>{partner}</p>) : <p>I am not married</p>}
+    </>
+  );
+}
