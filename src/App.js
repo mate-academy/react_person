4042a8b42178928ea5d633ab2b6db936b2a3e988
Ostap Salo <jlumon211@gmail.com>
2022-07-03 17:26:05 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Person } from './components/Person/Person';
 
 // eslint-disable-next-line
 const misha = {
@@ -27,23 +28,11 @@ const alex = {
 };
 
 const App = () => (
+  // eslint-disable-next-line react/jsx-filename-extension
   <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
+    <Person {...misha} />
+    <Person {...olya} />
+    <Person {...alex} />
   </div>
 );
 
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
 // Write code here
+import React from 'react';
+import './Person.scss';
+
+export function Person(
+  person,
+) {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried ? (
+          `My ${person.sex === 'm' ? 'wife`s' : 'husband`s'} name is ${person.partnerName}`)
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+}
