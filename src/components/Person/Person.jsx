b8fcb30b41721cd302744a711a5873c70c28ad51
Mariia Nedilko <mary.nedilko1@gmail.com>
2022-07-03 23:53:16 +0200

    Solution created

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
-// Write code here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) => (
+  // eslint-disable-next-line
+  <section className="Person">
+    <h2 className="Person__name">
+      My name is
+      {' '}
+      {name}
+    </h2>
+
+    {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+    <p className="Person__partner">
+      {isMarried
+        ? `My ${sex === 'm' ? 'wife' : 'husband'} is ${partnerName}`
+        : 'I am not married'}
+    </p>
+  </section>
+);
+
+Person.prototype = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  sex: PropTypes.string,
+  isMarried: PropTypes.string,
+  partnerName: PropTypes.string,
+};
