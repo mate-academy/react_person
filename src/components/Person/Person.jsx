5339163108964fede0f37b294af192d33b1bcc06
Ihor Shynkar <aedricson@gmail.com>
2022-07-03 20:21:39 +0300

    component Person was added to page

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// Write code here
+import React from 'react';
+import './Person.scss';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  return (
+    <>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      <p className="Person__age">{age && (`I am ${age}`)}</p>
+
+      <p className="Person__partner">
+        {((isMarried && sex === 'm') && (`My wife's name is ${partnerName}`))
+        || ((isMarried && sex === 'f') && (`My husband's name is ${partnerName}`))
+        || ('I am not married')}
+      </p>
+    </>
+  );
+}
