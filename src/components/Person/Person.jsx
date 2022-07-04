2c627d749fc272069078c67e9c8af5522d878843
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-07-04 11:24:21 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// Write code here
+import React from 'react';
+import './Person.scss';
+
+export function Person({ person }) {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {person.name}
+      </h2>
+
+      <p className="Person__age">
+        {person.age || null}
+      </p>
+
+      <p className="Person__partner">
+        {person.isMarried ? person.partnerName : null}
+      </p>
+    </section>
+  );
+}
