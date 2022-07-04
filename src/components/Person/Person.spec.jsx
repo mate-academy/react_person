4cd9169cea5b3e871c53fce5c6bfea94065297c7
murat-yesimbietov <murat.y@mate.academy>
2022-07-04 10:51:41 +0300

    [react_person]: added eslint-disable for extension and fixed import

--- src/components/Person/Person.spec.jsx
+++ src/components/Person/Person.spec.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
+
 import React from 'react';
 import { mount } from '@cypress/react';
-import Person from './Person';
+import { Person } from './Person';
 
 describe('Person', () => {
   it('should show name, age and partner\'s name of the person', () => {
