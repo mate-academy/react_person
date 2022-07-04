80b5dee6affd9969ea819ea0f2f3a2409199dcb6
Kris Mur <budkevych.h@gmail.com>
2022-07-04 08:08:36 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './App.scss';
+import { Person } from './components/Person/Person';
 
-// eslint-disable-next-line
 const misha = {
   name: 'Misha',
   age: 37,
@@ -10,7 +9,6 @@ const misha = {
   partnerName: 'Natasha',
 };
 
-// eslint-disable-next-line
 const olya = {
   name: 'Olya',
   sex: 'f',
@@ -18,7 +16,6 @@ const olya = {
   partnerName: 'Maksym',
 };
 
-// eslint-disable-next-line
 const alex = {
   name: 'Alex',
   age: 25,
@@ -27,23 +24,11 @@ const alex = {
 };
 
 const App = () => (
+  // eslint-disable-next-line react/jsx-filename-extension
   <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
+    <Person {...misha} />
+    <Person {...olya} />
+    <Person {...alex} />
   </div>
 );
 
