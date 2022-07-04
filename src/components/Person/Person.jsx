911138cc15fe71bc0473341fceeab0387be2e722
Serhii Vorona <seregavorona94@gmail.com>
2022-07-04 19:26:53 +0300

    person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// Write code here
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './Person.scss';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  const marriedTo = (sex === 'm')
+    ? (`My wife's name is ${partnerName}`)
+    : (`My husband's name is ${partnerName}`);
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age ? (<p className="Person__age">{`I am ${age}`}</p>) : null}
+      {isMarried
+        ? (<p className="Person__partner">{marriedTo}</p>)
+        : <p className="Person__partner">I am not married</p>}
+    </div>
+  );
+}
