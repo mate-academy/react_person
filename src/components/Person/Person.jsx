bf6a8e033ed85340b8218d860702a80e7f5cffe0
Bogdan Pyshchyk <bohdan.pyshchyk@gmail.com>
2022-07-04 08:53:23 +0300

    Person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
 // Write code here
+import React from 'react';
+import './Person.scss';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  let partnerSex = 'I am not married';
+
+  if (isMarried) {
+    partnerSex = sex === 'm'
+      ? `My wife's name is ${partnerName}`
+      : `My husband's name is ${partnerName}`;
+  }
+
+  return (
+    <>
+      <div className="Person">
+        <h2 className="Person__name">
+          {`My name is ${name}`}
+        </h2>
+
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+        <p className="Person__partner">
+          {partnerSex}
+        </p>
+      </div>
+    </>
+  );
+}
