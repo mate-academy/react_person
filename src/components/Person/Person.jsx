54db00fc4ba5c7e0eb3a65b5cf8df9bbc8672992
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-07-05 09:37:50 +0300

    finished React person task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
-// Write code here
+import React from 'react';
+import './Person.scss';
+
+export const Person = ({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) => {
+  const partner = (sex === 'm')
+    ? 'wife\'s'
+    : 'husband\'s';
+
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+      <p className="Person__partner">
+        {
+          isMarried
+            ? `My ${partner} name is ${partnerName}`
+            : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
