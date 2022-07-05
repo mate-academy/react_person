56cb7de0c222831e86c970616f720873a3f71d90
Lesya Savka <lesyasimple@gmail.com>
2022-07-05 11:25:30 +0300

    some improovment

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,20 +10,26 @@ function Person({ person }) {
     partnerName,
   } = person;
 
+  const partner = sex === 'f'
+    ? 'husband\'s'
+    : 'wife\'s';
+
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
         {isMarried
-          ? (`My ${sex === 'f' ? 'husband\'s' : 'wife\'s'} mame is ${partnerName}`)
+          ? (`My ${partner} mame is ${partnerName}`)
           : 'I am not married'}
       </p>
     </section>
