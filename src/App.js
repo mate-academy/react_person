ae4d85edf30a22783d4643ccdef549b6b6fe18ec
Kate Khon <katerynakhon@gmail.com>
2022-07-05 23:06:02 +0300

    commit after m review

--- src/App.js
+++ src/App.js
@@ -28,9 +28,24 @@ const alex = {
 
 const App = () => (
   <div className="App">
-    <Person {...misha} />
-    <Person {...olya} />
-    <Person {...alex} />
+    <Person
+      name={misha.name}
+      age={misha.age}
+      sex={misha.sex}
+      partnerName={misha.partnerName}
+    />
+    <Person
+      name={olya.name}
+      age={olya.age}
+      sex={olya.sex}
+      partnerName={olya.partnerName}
+    />
+    <Person
+      name={alex.name}
+      age={alex.age}
+      sex={alex.sex}
+      partnerName={alex.partnerName}
+    />
   </div>
 );
 
