9c01ea1575df8a6eacb4dd02fd80c78d37d37f7d
Oleksandr Kapustei <angelion541@gmail.com>
2022-07-06 16:29:16 +0300

    solution fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import './Person.scss';
 
-const Person = ({ name, age, sex, isMarried, partnerName }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
+const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
 
-    {age && <p className="Person__age">{`I am ${age}`}</p>}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
-    <p className="Person__partner">
-      {isMarried
-        ? `My ${sex === 'f' ? 'husband' : 'wife'}'s name is ${partnerName}`
-        : 'I am not married'
-      }
-    </p>
-  </section>
-);
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `My ${sex === 'f' ? 'husband' : 'wife'}'s name is ${partnerName}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
 
 export default Person;
