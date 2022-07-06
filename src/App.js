767ad41aade44a6fffbf75d9c3cb56b3bade0b62
Ivan Grabovsky <ivan.grabovsky.ua@gmail.com>
2022-07-06 15:14:41 +0300

    fixes after mentor review

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { Person } from './components/Person';
 
-import {
-  Person,
-  misha,
-  olya,
-  alex,
-} from './components/Person';
+// eslint-disable-next-line
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+// eslint-disable-next-line
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+// eslint-disable-next-line
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
 
 const App = () => (
   <div className="App">
-    <Person {...misha} />
-    <Person {...olya} />
-    <Person {...alex} />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
 
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,55 +2,24 @@
 import React from 'react';
 import './Person.scss';
 
-// eslint-disable-next-line
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-// eslint-disable-next-line
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-// eslint-disable-next-line
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
-
-export function Person({
-  name,
-  age,
-  sex,
-  isMarried,
-  partnerName,
-}) {
-  const partner = sex === 'm' ? 'wife' : 'husband';
+export function Person({ person }) {
+  const partner = person.sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {`My name is ${name}`}
+        {`My name is ${person.name}`}
       </h2>
 
-      {age && (
+      {person.age && (
         <p className="Person__age">
-          {`I am ${age}`}
+          {`I am ${person.age}`}
         </p>
       )}
 
       <p className="Person__partner">
-        {isMarried
-          ? `My ${partner}'s name is ${partnerName}`
+        {person.isMarried
+          ? `My ${partner}'s name is ${person.partnerName}`
           : 'I am not married'}
       </p>
     </section>
