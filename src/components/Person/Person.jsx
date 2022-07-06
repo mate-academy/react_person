2adb07abb7d2cb20fbbaef8aa8ba6a757b840f1c
Julia Makarenko <julys.makarenko@gmail.com>
2022-07-06 15:49:52 +0300

    Add react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Person.scss';
 
-export const Person = ({
+export function Person({
   name,
   age,
   sex,
   isMarried,
   partnerName,
-}) => {
+}) {
   const partner = (sex === 'f')
     ? 'husband\'s'
     : 'wife\'s';
@@ -17,16 +17,18 @@ export const Person = ({
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {age ? (
+
+      {(age) && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
-      ) : null}
-      {isMarried ? (
-        <p className="Person__partner">
-          {`My ${partner} name is ${partnerName}`}
-        </p>
-      ) : 'I am not married'}
+      )}
+
+      <p className="Person__partner">
+        {(isMarried)
+          ? (`My ${partner}'s name is ${partnerName}`)
+          : ('I am not married')}
+      </p>
     </section>
   );
-};
+}
