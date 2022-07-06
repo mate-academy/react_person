3f1cfcdda866acd335e1a1a72d928c561830a5eb
Artem Furdela <furdela.artem@gmail.com>
2022-07-06 23:37:49 +0300

    React person first commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// Write code here
+import React from 'react';
+
+export const Person = ({ personObj }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${personObj.name}`}
+    </h2>
+
+    {personObj.age && (
+      <>
+        <p className="Person__age">
+          {`I am ${personObj.age}`}
+        </p>
+      </>
+    )}
+
+    <p className="Person__partner">
+      { personObj.isMarried
+        ? `My ${personObj.sex === 'm' ? 'wife\'s' : 'husband\'s'} name is ${personObj.partnerName}`
+        : 'I am not married' }
+    </p>
+  </section>
+);
