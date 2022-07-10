c21aea9cce1fdd032f7bc91444d317833ed8ee7f
Mykhailo Parfeniuk <mypa@brighta.biz>
2022-07-10 15:07:22 +0300

    Task completed (minor update)

--- src/App.js
+++ src/App.js
@@ -32,25 +32,6 @@ const App = () => (
     <Person person={misha} />
     <Person person={olya} />
     <Person person={alex} />
-
-    <span>------</span>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
   </div>
 );
 
