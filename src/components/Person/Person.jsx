eefdd4a75098a9ab9e0ff3e0d49ca259b0b4b460
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-07-11 13:17:25 +0300

    added blank lines between elements

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -18,11 +18,13 @@ export const Person = ({
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
         {
           isMarried
