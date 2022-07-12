cfaae5fea6449837c498a58fde20830ad2d1daa5
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-07-13 00:00:49 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({ person }) {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
+      {person.age
+        ? (<p className="Person__age">{`I am ${person.age}`}</p>)
+        : ('')}
+
+      <p className="Person__partner">
+        {
+          (person.isMarried) // eslint-disable-line
+            ? ((person.sex === 'm')
+              ? `My wife's name is ${person.partnerName}`
+              : `My husband's name is ${person.partnerName}`)
+            : `I am not married`
+        }
+      </p>
+
+    </section>
+  );
+}
