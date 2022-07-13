4421ee81d3ea2566e8436e7096d2464316f8fcbe
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-07-13 13:24:24 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 
 export function Person({ person }) {
+  const partner = person.sex === 'm'
+    ? `My wife's name is ${person.partnerName}`
+    : `My husband's name is ${person.partnerName}`;
+
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
       {person.age
-        ? (<p className="Person__age">{`I am ${person.age}`}</p>)
-        : ('')}
+        && (<p className="Person__age">{`I am ${person.age}`}</p>)
+      }
 
       <p className="Person__partner">
         {
-          (person.isMarried) // eslint-disable-line
-            ? ((person.sex === 'm')
-              ? `My wife's name is ${person.partnerName}`
-              : `My husband's name is ${person.partnerName}`)
+          (person.isMarried)
+            ? partner
             : `I am not married`
         }
       </p>
-
     </section>
   );
 }
