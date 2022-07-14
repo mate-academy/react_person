2ba6747940e6695b4ab6efffb138d32c7c02f95b
Roman Nikitenko <Lomkaaa@ukr.net>
2022-07-14 12:09:35 +0300

    Fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
 export const Person = (props) => {
-  let hidden = false;
   const personName = `My name is ${props.person.name}`;
   const personAge = `I am ${props.person.age}`;
   let personPartners = `My ${props.person.sex === 'f' ? 'husband' : 'wife'}'s name is
     ${props.person.partnerName}`;
-
-  if (!props.person.age) {
-    hidden = true;
-  }
 
   if (!props.person.isMarried) {
     personPartners = 'I am not married';
@@ -20,9 +15,11 @@ export const Person = (props) => {
       <h2 className="Person__name">
         {personName}
       </h2>
-      <p className="Person__age" hidden={hidden}>
-        {personAge}
-      </p>
+      {props.person.age && (
+        <p className="Person__age">
+          {personAge}
+        </p>
+      )}
       <p className="Person__partner">
         {personPartners}
       </p>
