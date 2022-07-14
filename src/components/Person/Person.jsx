adee105b1f57531387a9f04497bb9f8be973bff0
Yuri Taraday <kaiiru3@gmail.com>
2022-07-14 16:13:44 +0300

    Second commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Person.scss';
+import PropTypes from 'prop-types';
 
 export const Person = ({
   name,
@@ -10,7 +11,7 @@ export const Person = ({
 }) => {
   return (
     <section className="Person">
-      <h2 className='Person__name'>{`My name is ${name}`}</h2>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
       {isMarried ? (
         <p className="Person__partner">
@@ -19,4 +20,19 @@ export const Person = ({
       ) : <p className="Person__partner">I am not married</p>}
     </section>
   );
+};
+
+Person.defaultProps = {
+  age: null,
+  sex: '',
+  isMarried: false,
+  partnerName: '',
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
 };
