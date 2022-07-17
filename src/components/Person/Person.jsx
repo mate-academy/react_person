136cc1a85a3f7f8a06dd2292cbcbc1a24ecfb7d7
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-07-17 18:39:48 +0300

    done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({ person }) {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{person.name}</h2>
+      {person.age ? (
+        <p className="Person__age">
+          I am
+          {' '}
+          {person.age}
+        </p>
+      ) : <p>{undefined}</p>
+      }
+
+      {person.isMarried ? (
+        <p className="Person__partner">
+          My
+          {
+            person.isMarried && person.sex === 'm'
+              ? (' wife')
+              : (' husband')
+          }
+          &apos;s name is
+          {' '}
+          {person.partnerName}
+        </p>
+      ) : <p>I am not married</p>
+      }
+    </section>
+  );
+}
