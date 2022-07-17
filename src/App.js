dae99bb83cd54124458185663d455addf361eb80
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-07-18 01:09:50 +0800

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.scss';
+import Person from './components/Person/Person';
+// import './App.scss';
 
 // eslint-disable-next-line
 const misha = {
@@ -28,22 +29,21 @@ const alex = {
 
 const App = () => (
   <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">My wife&apos;s name is Natasha</p>
-    </section>
+    <Person
+      name="Misha"
+      age={37}
+      partnerName="Natasha"
+    />
 
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">My husband&apos;s name is Maksym</p>
-    </section>
+    <Person
+      name="Olya"
+      partnerName="Maksym"
+    />
 
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
+    <Person
+      name="Alex"
+      age={25}
+    />
   </div>
 );
 
