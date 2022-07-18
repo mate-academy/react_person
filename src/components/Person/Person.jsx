2a9cdb8d1af5b09b68a6ea69d7eb6582fa37cafe
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-07-18 23:59:32 +0300

    first commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,30 @@
 // export function Person({ person }) {}
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName: pN } = person;
+
+  const partnerHandler = () => {
+    if (isMarried && sex === 'm') {
+      return `My wife\`s name is ${pN}`;
+    }
+
+    if (isMarried && sex === 's') {
+      return `My husbands\`s name is ${pN}`;
+    }
+
+    return `I am not married`;
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      { age
+        && <p className="Person__age">{`I am ${age}`}</p>
+      }
+      <p className="Person__partner">
+        {partnerHandler()}
+      </p>
+    </section>
+  );
+};
