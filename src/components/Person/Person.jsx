0bf272bf1b3a66352f7e2d352258de171dc5bd71
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-07-29 17:01:41 +0300

    person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({ person }) {
+  const partner = person.sex === 'm'
+    ? `My wife's name is ${person.partnerName}`
+    : `My husband's name is ${person.partnerName}`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age ? (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )
+        : null
+      }
+      <p className="Person__partner">
+        {person.isMarried ? (partner) : 'I am not married'}
+      </p>
+    </section>
+  );
+}
