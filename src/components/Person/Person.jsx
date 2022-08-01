e4e2b3b7e1deeaf55005e06ae92ad4a00ffce2c9
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-08-01 11:41:36 +0300

    Restructure

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export function Person({ person }) {
+export function Person({ name, age, isMarried, sex, partnerName }) {
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {person.age && (
-        <p className="Person__age">{`I am ${person.age}`}</p>
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
       )}
 
       <p className="Person__partner">
-        {person.isMarried
-          ? `My ${person.sex === 'm' ? `wife` : `husband`}'s name is  ${person.partnerName}`
+        {isMarried
+          ? `My ${sex === 'm' ? `wife` : `husband`}'s name is  ${partnerName}`
           : `I am not married`
         }
       </p>
