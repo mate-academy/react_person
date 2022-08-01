07771381102d4ddc9d0d0268492141f10c7708b7
Denys Korotkoruchko <denenergii@gmail.com>
2022-08-01 13:28:33 +0300

    ready

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,11 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({ person }) {
+  return (
+    <section section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <p className="Person__age">{person.age ? `I am ${person.age}` : null}</p>
+      <p className="Person__partner">{person.isMarried ? `My ${(person.sex === 'm') ? 'wife\'s' : 'husband'} name is ${person.partnerName}` : 'I am not married'}</p>
+    </section>
+  );
+}
