f5113b82ee0708cfa7d78ffec8ebe615722f9f3d
Denys Danyliuk <denysdanyliuuk@gmail.com>
2022-08-01 11:19:08 +0300

    destructured person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const wifeOrHusband = person.sex === 'm'
+  const { sex, name, age, isMarried, partnerName } = person;
+  const wifeOrHusband = sex === 'm'
     ? 'wife'
     : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {`My name is ${person.name}`}
+        {`My name is ${name}`}
       </h2>
-      {person.age && (<p className="Person__age">{`I am ${person.age}`}</p>)}
+      {age && (<p className="Person__age">{`I am ${age}`}</p>)}
       <p className="Person__partner">
-        {person.isMarried
-          ? `My ${wifeOrHusband}'s name is ${person.partnerName}`
+        {isMarried
+          ? `My ${wifeOrHusband}'s name is ${partnerName}`
           : 'I am not married'
         }
       </p>
