c6f11c592c75c7ee4ab714763fd0c5ab848e8749
Gena Kupriiuk <gamesgolden04@gmail.com>
2022-08-02 23:17:53 +0300

    Editted solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,22 +8,18 @@ export function Person({ person }) {
   return (
     <>
       <h2 className="Person__name">
-        `My name is $
-        {person.name}
-        `
+        {`My name is ${person.name}`}
       </h2>
 
       {person.age && (
         <p className="Person__age">
-          `I am $
-          {person.age}
-          `
+          {`I am ${person.age}`}
         </p>
       )}
 
       <p className="Person__partner">
         {person.isMarried
-          ? `My ${partner}&apos;s name is ${person.partnerName}`
+          ? `My ${partner}'s name is ${person.partnerName}`
           : `I am not married`
         }
         ;
