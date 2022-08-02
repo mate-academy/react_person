8f3152e6765379fae3edb3e2e36a1c8846bb839f
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-08-02 20:06:52 +0300

    task is ready

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,46 @@
-// export function Person({ person }) {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Person({ person }) {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
+      {person.age > 0 && (<p className="Person__age">{`I am ${person.age}`}</p>)}
+
+      {person.isMarried === true
+        ? (
+          <p className="Person__partner">
+            {person.sex === 'm'
+              ? `My wife's name is ${person.partnerName}`
+              : `My husband's name is ${person.partnerName}`
+            }
+          </p>
+        )
+        : (
+          <p className="Person__partner">not married</p>
+        )
+      }
+    </section>
+  );
+}
+
+Person.defaultProps = {
+  person: {
+    name: '',
+    age: 0,
+    sex: '',
+    isMarried: false,
+    partnerName: '',
+  },
+};
+
+Person.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string,
+    age: PropTypes.number,
+    sex: PropTypes.oneOf(['m'], ['f']),
+    isMarried: PropTypes.bool,
+    partnerName: PropTypes.string,
+  }),
+};
