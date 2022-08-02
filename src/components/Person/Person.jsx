dfc8b388724e5818044583d0df601787898a943d
Olena Rodiontseva <elena.rodiontseva@gmail.com>
2022-08-02 16:25:58 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -16,12 +16,16 @@ export function Person({
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is {name}
+        My name is
+        {' '}
+        {name}
       </h2>
 
       {age && (
         <p className="Person__age">
-          I am {age}
+          I am
+          {' '}
+          {age}
         </p>
       )}
 
