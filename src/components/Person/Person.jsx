80675e30165449f189940198838144ed1c100703
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-08-04 13:14:34 +0300

    React People

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
 // export function Person({ person }) {}
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const pers = person.sex === 'm'
+    ? "wife's"
+    : "husband's";
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {' '}
+        {person.name}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          I am
+          {' '}
+          {person.age}
+        </p>
+      )}
+
+      {person.isMarried
+        ? `My ${pers} name is ${person.partnerName}`
+        : 'I am not married'
+      }
+    </section>
+  );
+};
