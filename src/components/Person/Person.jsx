c141577c2674f055217fafc3182515c20661de28
AndreyLeonov1 <65847113+AndreyLeonov1@users.noreply.github.com>
2022-08-04 10:27:32 +0300

    Solution with restruct

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Person.scss';
 
-export const Person = ({ person }) => (
+export const Person = ({ person: {
+  name,
+  age,
+  isMarried,
+  sex,
+  partnerName,
+} }) => (
   <section className="Person">
     <h2 className="Person__name">
-      {`My name is ${person.name}`}
+      {`My name is ${name}`}
     </h2>
 
-    {person.age && (<p className="Person__age">{`I am ${person.age}`}</p>)}
+    {age && (<p className="Person__age">{`I am ${age}`}</p>)}
     <p className="Person__partner">
-      {person.isMarried
-        ? `My ${person.sex === 'm' ? 'wife' : 'husband'}'s name is ${person.partnerName}`
+      {isMarried
+        ? `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`
         : 'I am not married'}
     </p>
   </section>
