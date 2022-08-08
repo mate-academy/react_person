f54f50bda0ae6dcd6ccd175c264b527acad9eddf
Andriy Nosov <nosovandriy@gmail.com>
2022-08-08 19:40:28 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export const Person = ({
+  person: {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  },
+}) => (
+  <section className="Person">
+
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
+
+    <p className="Person__age">
+      {age ? `I am ${age}` : ''}
+    </p>
+
+    <p className="Person__partner">
+      {isMarried
+        ? `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}`
+        : 'I am not married'}
+    </p>
+
+  </section>
+);
