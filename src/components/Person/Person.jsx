d239daf73b846f348b4d5d4a3dbb5d7e17a97c6d
Olha Chebotarova <chebotarovaolha@gmail.com>
2022-08-09 16:09:59 -0400

    person task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({ person }) {
+  return (
+    <>
+      <h2 className="Person__name">
+        My name is
+        {` ${person.name}`}
+      </h2>
+      {person.age && (<p className="Person__age">I am {person.age}</p>)}
+      <p className="Person__partner">
+        {person.isMarried
+          ? `My ${person.sex === 'm' ? `wife's` : `husband's`} name is ${person.partnerName}`
+          : `I am not married`}
+      </p>
+    </>
+  );
+}
