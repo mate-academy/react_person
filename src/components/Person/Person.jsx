cc26647fe80c607b464984f399b1623dd341b599
Zolkiewskyi Oleg <zolkiewskyioleg@gmail.com>
2022-08-11 18:27:20 +0300

    Fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,28 +4,20 @@ export const Person = ({ person }) => (
   <>
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {person.name}
+        {`My name is ${person.name}`}
       </h2>
       {person.age && (
       <p className="Person__age">
-        I am
-        {' '}
-        {person.age}
+        {`I am ${person.age}`}
       </p>
       )}
 
       {person.isMarried ? (
         <p className="Person__partner">
-          My
-          {' '}
-          {person.sex === 'm' ? 'wife' : 'husband'}
-          {"'"}
-          s name
+          {`My ${person.sex === 'm' ? 'wife' : 'husband'}'s 
+          name
           is
-          {' '}
-          {person.partnerName}
+          ${person.partnerName}`}
         </p>
       ) : (
         <p className="Person__partner">
