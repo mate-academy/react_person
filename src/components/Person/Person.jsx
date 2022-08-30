70f7c320241ae007d722dc6fd217f5d8a4b502ab
Synytsyn Denys <yungdena54@gmail.com>
2022-08-30 14:12:51 +0300

    Implemented React person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({ person }) {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let partner = '';
+
+  if (isMarried) {
+    partner = sex === 'f'
+      ? `My husband's name is ${partnerName}`
+      : `My wife's name is ${partnerName}`;
+  } else {
+    partner = 'I\'m not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {!age || (
+        <p className="Person__age">I am {age}</p>
+      )}
+
+      <p className="Person__partner">{partner}</p>
+    </section>
+  );
+}
