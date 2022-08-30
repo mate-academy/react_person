17ca89f17dd641c4eeb7abeeabdee55db4f418c2
Oleksandr Babmyndra <alphamonik1@gmail.com>
2022-08-30 11:09:42 +0300

    React Person component was created

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({ person }) {
+  const genderMarried = person.sex === 'm' ? 'wife' : 'husband';
+  const marriedText = person.isMarried
+    ? `My ${genderMarried}'s name is ${person.partnerName}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is&nbsp;
+        { person.name }
+      </h2>
+      { person.age
+        && (
+        <p className="Person__age">
+          I am&nbsp;
+          { person.age }
+        </p>
+        )
+      }
+      <p className="Person__partner">{ marriedText }</p>
+    </section>
+  );
+}
