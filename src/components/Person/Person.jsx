c1a4b40c35ee98462174525de60cf33b85e00836
Nikita Havryliuk <chinter.games1@gmail.com>
2022-08-31 15:00:18 +0300

    some fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export function Person({ person }) {
-  const {
+export function Person({
+  person: {
     name,
     age,
     sex,
     isMarried,
     partnerName,
-  } = person;
-
+  },
+}) {
   const partnerPhrase = sex === 'f'
     ? `My husband's name is ${partnerName}`
     : `My wife's name is ${partnerName}`;
@@ -17,14 +17,12 @@ export function Person({ person }) {
     : 'I am not married';
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-        {!age || <p className="Person__age">{`I am ${age}`}</p>}
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
 
-        <p className="Person__partner">{partner}</p>
-      </section>
-    </>
+      <p className="Person__partner">{partner}</p>
+    </section>
   );
 }
