80678d9a9e5a04235faa547a8e31a7e32af7471b
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-09-01 00:21:07 +0300

    desctructured object

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 
-export const Person = ({ person }) => {
-  const isMarriedMan = person.sex === 'm' && person.isMarried;
-  const isMarriedWoman = person.sex === 'f' && person.isMarried;
+export const Person = ({ person:
+  { name, age, sex, isMarried, partnerName } }) => {
+  const isMarriedMan = sex === 'm' && isMarried;
+  const isMarriedWoman = sex === 'f' && isMarried;
 
   let text = '';
 
   if (isMarriedMan) {
-    text = `${person.partnerName} is my wife`;
+    text = `${partnerName} is my wife`;
   }
 
   if (isMarriedWoman) {
-    text = `${person.partnerName} is my husband`;
+    text = `${partnerName} is my husband`;
   }
 
   return (
     <>
       <section className="Person">
-        <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-        {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
         <p className="Person__partner">{text || 'I am not married'}</p>
       </section>
     </>
