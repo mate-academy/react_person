b8166fff39369bfa21b7eda654999bb296c4b6b1
Konstantin Ivanchenko <89596175+Konsttantin@users.noreply.github.com>
2022-09-01 23:53:47 +0300

    review fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age
-      ? <p className="Person__age">{`I am ${person.age}`}</p>
-      : null
-    }
+    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
 
     <p className="Person__partner">
       {person.isMarried
