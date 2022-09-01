1c056fca6aa299fc0a6106e92761d253a607aa34
Taras Kozii <taras_kozii@ukr.net>
2022-09-01 21:51:30 +0300

    solution v01

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+// eslint-disable-next-line func-names
+export const Person = function({ person }) {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (<p className="Person__age">{`I am ${person.age}`}</p>)}
+
+      {!person.isMarried && (
+        <p className="Person__partner">I am not married</p>
+      )}
+
+      {person.isMarried && (
+        <p className="Person__partner">
+          {person.sex === 'm'
+            ? `${person.partnerName} is my wife`
+            : `${person.partnerName} is my husband`
+          }
+        </p>
+      )}
+    </section>
+  );
+};
