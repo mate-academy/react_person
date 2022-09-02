9ae86f071e629efba99cadb6375e0d89868e9b30
Sasha Kolomiiets <kinbean444@gmail.com>
2022-09-03 00:24:21 +0300

    added proptypes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Person = ({ person:
-  { name, age, sex, isMarried, partnerName } }) => (
+export const Person = ({
+  person: {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  },
+}) => {
+  const partnerDefine = sex === 'm' ? 'wife' : 'husband';
 
+  return (
     <section className="Person">
-      {name && (
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      )}
-      {age && (
-      <p className="Person__age">{`I am ${age}`}</p>
-      )}
-      <p className="Person__partner">{isMarried ? `My ${sex === 'm' ? 'wife' : 'husband'}'s name is ${partnerName}` : 'I am not married'}</p>
+      <p className="Person__age">{age && `I am ${age}`}</p>
+      <p className="Person__partner">
+        {isMarried ? `My ${partnerDefine}'s name is ${partnerName}`
+          : 'I am not married'}
+      </p>
     </section>
-);
+  );
+};
+
+Person.defaultProps = {
+  age: 0,
+  partnerName: '',
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string,
+};
