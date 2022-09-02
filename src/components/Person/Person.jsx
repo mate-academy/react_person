9e1dfd6c1cebe8aa748077d09e57dd5e2520679a
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-09-02 15:36:11 +0300

    fixed according with comments

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Person = ({ person:
-  { name, age, sex, isMarried, partnerName } }) => {
-  const isMarriedMan = sex === 'm' && isMarried;
-  const isMarriedWoman = sex === 'f' && isMarried;
-
-  let text = '';
-
-  if (isMarriedMan) {
-    text = `${partnerName} is my wife`;
-  }
-
-  if (isMarriedWoman) {
-    text = `${partnerName} is my husband`;
-  }
+export function Person({
+  person: {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  },
+}) {
+  const partnerPhrase = sex === 'f'
+    ? `My husband's name is ${partnerName}`
+    : `My wife's name is ${partnerName}`;
+  const partner = isMarried
+    ? partnerPhrase
+    : 'I am not married';
 
   return (
     <>
       <section className="Person">
         <h2 className="Person__name">{`My name is ${name}`}</h2>
         {age && <p className="Person__age">{`I am ${age}`}</p>}
-        <p className="Person__partner">{text || 'I am not married'}</p>
+        <p className="Person__partner">{partner}</p>
       </section>
     </>
   );
+}
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string.isRequired,
 };
