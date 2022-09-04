77e4c80fe7865c0ea2b8077190259c916f94a1ff
Maks Sobko <tiserettt@gmail.com>
2022-09-04 19:54:36 +0300

    changes after mentor review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
-export const Person = ({
-  name,
-  age,
-  sex,
-  isMarried,
-  partnerName,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${name}`}</h2>
-    {age && <p className="Person__age">{`I am ${age}`}</p>}
-    <p className="Person__partner">
-      { isMarried
-        ? `My ${sex === 'm' ? 'wife\'s' : 'husband\'s'} name is ${partnerName}`
-        : 'I am not married'
-      }
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">
+        { isMarried
+          ? `My ${sex === 'm' ? 'wife\'s' : 'husband\'s'} name is ${partnerName}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
