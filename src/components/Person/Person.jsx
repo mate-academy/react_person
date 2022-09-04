b02afcd5823cf87822efa8e415c5ca4f0c39268f
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-09-04 21:54:16 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,44 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+  let partnerRole = '';
+
+  switch (sex) {
+    case 'm':
+      partnerRole = 'wife';
+      break;
+    case 'f':
+      partnerRole = 'husband';
+      break;
+    default: throw new Error('Wrong sex value');
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      { age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      { isMarried ? (
+        <p className="Person__partner">
+          {`${partnerName} is my ${partnerRole}`}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
