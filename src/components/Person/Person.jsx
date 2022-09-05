8f8d8fbc8a94e9b6dd5825bd6d7bf91867efca3d
Anastasiia Khmelovska <hmelanast1@gmail.com>
2022-09-05 23:12:30 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -19,15 +19,11 @@ export const Person = ({
   return (
     <>
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {name}
+        {`My name is ${name}`}
       </h2>
       {age && (
         <p className="Person__age">
-          I am
-          {' '}
-          {age}
+          {`I am ${age}`}
         </p>
       )}
 
