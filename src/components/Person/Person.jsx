0e40ec0b9b56880ee9220d907f49a863e6336dd4
Pavel Maistrenko <maystrenko.p@gmail.com>
2022-09-05 14:29:43 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -21,9 +21,9 @@ function Person({
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age ? (
+      {person.age && (
         <p className="Person__age">I am {person.age}</p>
-      ) : (null)}
+      )}
       <p className="Person__partner">{message}</p>
     </section>
   );
