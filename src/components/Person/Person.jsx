702d019ce29b3a7623033fd1db10d7f20eca64af
Andrii Stiebalov <stiebalov@gmail.com>
2022-09-05 20:55:25 +0300

    The person componet was added

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      {name && (
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+      )}
+
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
+
+      <p className="Person__partner">
+        {!isMarried
+          ? `I am not married`
+          : `${partnerName} is my ${sex === 'm' ? `wife` : `husband`}`}
+      </p>
+    </section>
+  );
+};
