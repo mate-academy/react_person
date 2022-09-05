5fe9da9a12aeb2aa5464b94fbba66d9dfb97bc5d
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-09-05 19:43:43 +0300

    First commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,41 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried === false
+          ? 'I am not married'
+          : `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
+        }
+      </p>
+    </section>
+  );
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string.isRequired,
+};
