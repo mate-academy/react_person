d1665e2a52abd6e2dd4bd2f8667baf26126c792d
Mark Shapka <mark.shapka1@gmail.com>
2022-09-05 15:59:57 +0300

    - moved person objects to the App.jsx file; - changed format code into App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Person, misha, olya, alex } from './components/Person';
+import { Person } from './components/Person';
+
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
 
 export const App = () => (
   <div className="App">
-    <Person
-      person={misha}
-    />
+    <Person person={misha} />
 
-    <Person
-      person={olya}
-    />
+    <Person person={olya} />
 
-    <Person
-      person={alex}
-    />
+    <Person person={alex} />
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,34 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
-
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">
       {`My name is ${person.name}`}
     </h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+    {person.age && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    )}
     {
       isMarriedFunct(person.isMarried, person.sex, person.partnerName)
     }
