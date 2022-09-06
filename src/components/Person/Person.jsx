27bdb2514b249e91cef4260e2379b5c4d25a4d6d
roma-shafihullin <lanister0789@gmail.com>
2022-09-06 16:04:08 +0300

    codestyle checked, defaultProps added

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Person = ({
   person: {
@@ -15,7 +16,24 @@ export const Person = ({
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {!age || <p className="Person__age">{`I am ${age}`}</p>}
-      <p className="Person__partner">{isMarried ? `${partnerName} is my ${partner}` : 'I am not married'}</p>
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'}
+      </p>
     </section>
   );
+};
+
+Person.defaultProps = {
+  age: 0,
+  partnerName: '',
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string,
 };
