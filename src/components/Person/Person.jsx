58b3ddd485eef735666d82bf2667010a8baaa0e2
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-09-06 15:24:43 +0300

    const age in PErsone.jsx was changed to {age && <p className="Person__age">{`I am ${age}`}</p>}

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -16,15 +16,11 @@ export const Person = ({ person }) => {
     ? `${partnerName} is my ${partnerSex}`
     : `I am not married`;
 
-  const haveAge = age
-    ? <p className="Person__age">{`I am ${age}`}</p>
-    : '';
-
   return (
     <div className="App">
       <section className="Person">
         <h2 className="Person__name">{`My name is ${name}`}</h2>
-        {haveAge}
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
         <p className="Person__partner">{isMarriedPersone}</p>
       </section>
     </div>
