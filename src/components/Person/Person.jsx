d4cd8861462ea3b6ccb98cb0f514ffe33a5cd25f
Zinchenko Anastasiia <zincenkonasta6@gmail.com>
2022-09-06 12:37:40 +0300

    Persone.jsx changed and create Persone function

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partnerSex = sex === 'f'
+    ? 'husband'
+    : 'wife';
+
+  const isMarriedPersone = isMarried
+    ? `${partnerName} is my ${partnerSex}`
+    : `I am not married`;
+
+  const haveAge = age
+    ? <p className="Person__age">{`I am ${age}`}</p>
+    : '';
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {haveAge}
+        <p className="Person__partner">{isMarriedPersone}</p>
+      </section>
+    </div>
+  );
+};
