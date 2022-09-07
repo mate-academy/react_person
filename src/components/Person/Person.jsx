c5ae7d09e64102cd832bb344f0e21252fc8b79bd
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-09-07 13:21:31 +0300

    solution 1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,6 +11,11 @@ export const Person = ({ person: {
   <section className="Person">
     <h2 className="Person__name">{`My name is ${name}`}</h2>
     {age && (<p className="Person__age">{`I am ${age}`}</p>)}
-    <p className="Person__partner">{isMarried ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}` : 'I am not married'}</p>
+    <p className="Person__partner">
+      {isMarried
+        ? `${partnerName} is my ${sex === 'f'
+          ? 'husband' : 'wife'}`
+        : 'I am not married'}
+    </p>
   </section>
 );
