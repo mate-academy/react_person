c073698740d937e247208ea0f772398fe2c66d5e
Vadym Tanasiienko <108836504+Desertiks@users.noreply.github.com>
2022-09-08 13:58:12 +0300

    Add space in Person component
    Co-authored-by: Danylo Onishchuk <78209611+danylo-onishchuk@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -18,7 +18,7 @@ export const Person = (prop) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && (
-      <p className="Person__age">{`I am ${age}`}</p>
+        <p className="Person__age">{`I am ${age}`}</p>
       )}
       <p className="Person__partner">
         {isMarried
