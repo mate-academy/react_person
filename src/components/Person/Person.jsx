362e2e667a99c4afef18056d602b2d07c417c18f
Artem Kodesnikov <artiguard@gmail.com>
2022-09-08 14:41:36 +0300

    fixes after review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,19 +11,18 @@ export const Person = ({ person: {
     ? `${partnerName} is my wife`
     : `${partnerName} is my husband`;
 
-  const married = isMarried === false
-    ? 'I am not married'
-    : gender;
-
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
-        {age && (
-          <p className="Person__age">{`I am ${age}`}</p>
-        )}
-        <p className="Person__partner">{married}</p>
-      </section>
-    </>
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
+      <p className="Person__partner">
+        {isMarried
+          ? (gender)
+          : ('I am not married')
+      }
+      </p>
+    </section>
   );
 };
