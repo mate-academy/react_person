43d835867d81e85a69d03cfae65341732ef0d5dc
Yurii Kozlov <yurka22870@gmail.com>
2022-09-08 13:28:29 +0300

    nested ternary operator was replaced with a separate variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
+export const Person = ({ person }) => {
+  const personSex = person.sex === 'm' ? `${person.partnerName} is my wife` : `${person.partnerName} is my husband`;
 
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
 
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {person.sex === 'm' ? (
-          `${person.partnerName} is my wife`
-        ) : (
-          `${person.partnerName} is my husband`
-        )}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {personSex}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
