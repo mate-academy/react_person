0c2865a824577f0193a72a7b02569bf59daabaf9
Shmelyov Oleg <shmelyov.oleg13@gmail.com>
2022-09-09 12:12:21 +0300

    fix: change ternary to variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,20 +8,26 @@ export const Person = ({
     isMarried,
     partnerName,
   },
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
-    {age && (
-      <p className="Person__age">
-        {`I am ${age}`}
+}) => {
+  const relationshipStatus = sex === 'f'
+    ? `${partnerName} is my husband`
+    : `${partnerName} is my wife`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+      <p className="Person__partner">
+        {isMarried
+          ? relationshipStatus
+          : 'I am not married'}
       </p>
-    )}
-    <p className="Person__partner">
-      {isMarried
-        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
