dd64c2105e35ddc57ce33eaad9676275b4d5fc88
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-09-09 11:14:35 +0300

    unnecessary variables was deleted

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
-export function Person({ person }) {
-  const isPartner = person.sex === 'm' ? `${person.partnerName} is my wife` : `${person.partnerName} is my husband`;
-  const partner = person.isMarried ? isPartner : 'I am not married';
-
-  return (
-    <section className="Person">
-      <h2 className="Person__name">
-        {`My name is ${person.name}`}
-      </h2>
-      {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-      )}
-      <p className="Person__partner">{partner}</p>
-    </section>
-  );
-}
+export const Person = ({ person: {
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+} }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${name}`}</h2>
+    {age && (<p className="Person__age">{`I am ${age}`}</p>)}
+    <p className="Person__partner">
+      {isMarried ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}` : 'I am not married'}
+    </p>
+  </section>
+);
