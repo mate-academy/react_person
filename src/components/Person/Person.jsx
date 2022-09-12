0397a40cbabc5a2f4ca6a9c1cfc3209d20295e00
Maryna Mazur <marinemazur@gmail.com>
2022-09-12 21:15:55 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,40 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({ person }) => {
+  const personPartner = () => {
+    if (person.isMarried) {
+      if (person.sex === 'm') {
+        return `${person.partnerName} is my wife`;
+      }
+
+      return `${person.partnerName} is my husband`;
+    }
+
+    return 'I am not married';
+  };
+
+  return (
+    <>
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      <p className="Person__age">
+        {person.age ? `I am ${person.age}` : null}
+      </p>
+      <p className="Person__partner">
+        {personPartner()}
+      </p>
+    </>
+  );
+};
+
+Person.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number,
+    sex: PropTypes.string.isRequired,
+    isMarried: PropTypes.bool,
+    partnerName: PropTypes.string,
+  }).isRequired,
+};
