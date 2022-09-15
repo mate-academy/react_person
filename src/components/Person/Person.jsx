db80830ea986bcd47e7fac03051675e2b47c5f32
Pavel Maistrenko <maystrenko.p@gmail.com>
2022-09-15 12:14:47 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,32 +1,32 @@
-/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable consistent-return */
 import React from 'react';
 
 function Person({
   person,
 }) {
-  let message = '';
-
-  if (!person.isMarried) {
-    message = 'I am not married';
-  }
-
-  if (person.isMarried && person.sex === 'm') {
-    message = `${person.partnerName} is my wife`;
-  }
-
-  if (person.isMarried && person.sex === 'f') {
-    message = `${person.partnerName} is my husband`;
-  }
-
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
       {person.age && (
-        <p className="Person__age">I am {person.age}</p>
+        <p className="Person__age">{`I am ${person.age}`}</p>
       )}
-      <p className="Person__partner">{message}</p>
+      <p className="Person__partner">{checkStatusMarriage(person)}</p>
     </section>
   );
+}
+
+function checkStatusMarriage(person) {
+  if (!person.isMarried) {
+    return 'I am not married';
+  }
+
+  if (person.sex === 'm') {
+    return `${person.partnerName} is my wife`;
+  }
+
+  if (person.sex === 'f') {
+    return `${person.partnerName} is my husband`;
+  }
 }
 
 export { Person };
