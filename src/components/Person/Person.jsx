8e9981faf4dd7984e9daf90890a6c0001b49b51b
HusakOleh <husakoleh91@gmail.com>
2022-09-18 18:20:15 +0300

    add material ui

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import './Person.scss';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 export const Person = ({ person }) => {
   const partner = person.sex === 'f' ? 'husband' : 'wife';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">
-        My name is&nbsp;
-        { person.name }
-      </h2>
-      {
-        person.age
-        && (
-          <p className="Person__age">
-            I am&nbsp;
-            {person.age}
-          </p>
-        )
-      }
-
-      <p className="Person__partner">
-        {
-          person.isMarried
-            ? `${person.partnerName} is my ${partner}`
-            : 'I am not married'
-        }
-      </p>
+      <Card>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              {`My name is ${person.name}`}
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              {
+                person.age
+                && (
+                  <p className="Person__age">
+                    {`I am ${person.age}`}
+                  </p>
+                )
+              }
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              {
+                person.isMarried
+                  ? `${person.partnerName} is my ${partner}`
+                  : 'I am not married'
+              }
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </section>
   );
 };
