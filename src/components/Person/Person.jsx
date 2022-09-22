e8f5af06d0ca6e5cb10b067f9f9993d71f8e82f5
p-omogit-e <polinakotyrlo@gmail.com>
2022-09-22 13:59:35 +0300

    done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      My name is
+      {' '}
+      {person.name}
+    </h2>
+    <p className="Person__age">{person.age && `I am ${person.age}`}</p>
+    {person.sex === 'm'
+      ? (<p className="Person__partner">{person.isMarried ? `${person.partnerName} is my wife` : 'I am not married'}</p>)
+      : (<p className="Person__partner">{person.isMarried ? `${person.partnerName} is my husband` : 'I am not married'}</p>)}
+  </section>
+);
+
+export default Person;
