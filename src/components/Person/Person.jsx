c8fb3bc8991df062304b90167cd74fddfee37405
grafit-off <graf.off.it@gmail.com>
2022-10-08 10:14:14 +0300

    fix: fixed identation after mentor review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName
+  } = person;
+
   const partner = sex === 'm'
     ? 'wife'
     : 'husband';
@@ -11,11 +18,13 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
         {isMarried
           ? (`${partnerName} is my ${partner}`)
