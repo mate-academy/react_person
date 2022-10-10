c73ce3ed4b466e6184a236f39bfa7e13775970ca
Viktoria Neimesh <valencia88vika@gmail.com>
2022-10-10 07:23:14 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+const Partner = ({ sex, name }) => (sex === 'm' ? `${name} is my wife` : `${name} is my husband`);
+
+const Person = ({ person }) => {
+  const { name, age, isMarried, sex, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+      <p className="Person__age">
+        I am
+        {' '}
+        {age}
+      </p>
+      )}
+      <p className="Person__partner">
+        {(isMarried) ? (<Partner sex={sex} name={partnerName} />) : `I am not married`}
+      </p>
+    </section>
+  );
+};
+
+export default Person;
--- src/components/Person/Person.spec.jsx
+++ src/components/Person/Person.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import { Person } from './Person';
+import Person from './Person';
 
 describe('Person', () => {
   describe('', () => {
