51f49a50fa7db434780e13a7963cff3c6ed4754f
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-10-10 21:06:46 +0300

    was optimised after students review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -19,13 +19,11 @@ export const Person = ({ person }) => {
         <h2 className="Person__name">
           {`My name is ${name}`}
         </h2>
-        {age
-          ? (
-            <p className="Person__age">
-              {`I am ${age}`}
-            </p>
-          )
-          : null}
+        {age && (
+          <p className="Person__age">
+            {`I am ${age}`}
+          </p>
+        )}
         <p className="Person__partner">
           {isMarried
             ? `${partnerName} is my ${partnerStatus}`
