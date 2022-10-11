cb2c7dbcd5483ab02349416139fd2913db9011ae
Boris Kerekesha <kerekesha90@ukr.net>
2022-10-11 17:23:06 +0300

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  if (person.name === 'Misha') {
+    return '';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {person.name}
+      </h2>
+      {person.age
+        ? (
+          <p className="Person__age">
+            I am
+            {person.age}
+          </p>
+        )
+        : ''}
+      <p className="Person__partner">
+        {
+          person.partnerName
+            ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+            : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
