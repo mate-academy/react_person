46ffa7aa8aa1d51c27f9304097e0b2cd2a3b011e
Artem Hulko <agulko96@gmail.com>
2022-10-11 18:27:50 +0300

    implemented Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  let partner;
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      partner = `${person.partnerName} is my wife`;
+    } else {
+      partner = `${person.partnerName} is my husband`;
+    }
+  } else {
+    partner = `I am not married`;
+  }
+
+  if (!person.age) {
+    return (
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+        <p className="Person__partner">{partner}</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <p className="Person__age">{`I am ${person.age}`}</p>
+      <p className="Person__partner">{partner}</p>
+    </section>
+  );
+};
