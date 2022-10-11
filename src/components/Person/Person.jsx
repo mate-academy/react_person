a0934466e35f3e72a4b56ae9b1e6aeef60009dfd
Serhii Romaniuk <deepspark@gmail.com>
2022-10-11 20:20:29 +0300

    small correction

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,18 +8,23 @@ export const Person = ({ person }) => {
     isMarried,
     partnerName,
   } = person;
-  const marriedText = (sex === 'm')
+  const partnerStatus = (sex === 'm')
     ? 'wife'
     : 'husband';
 
   return (
-    <>
+    <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
-      { isMarried
-        ? <p className="Person__partner">{`${partnerName} is my ${marriedText}`}</p>
-        : <p className="Person__partner">I am not married</p>
+      {
+        <p className="Person__partner">
+          {
+            isMarried
+              ? `${partnerName} is my ${partnerStatus}`
+              : 'I am not married'
+          }
+        </p>
       }
-    </>
+    </section>
   );
 };
