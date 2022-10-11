c45549707461cf63076c3233bd32d35fa17be93b
Vlad Malinin <vladmalinin1999@gmail.com>
2022-10-11 12:55:05 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable no-nested-ternary */
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const partnerSex = sex === 'f'
+    ? 'husband'
+    : 'wife';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
+      {isMarried
+        ? <p className="Person__partner">{`${partnerName} is my ${partnerSex}`}</p>
+        : <p className="Person__partner">I am not married</p>
+      }
+    </section>
+  );
+};
