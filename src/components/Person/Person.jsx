fcd9919fedb80613828ac68e27e80381b1641940
Myroslav Diiak <diiakm23819@gmail.com>
2022-10-12 11:18:29 +0300

    fixed solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,11 +14,11 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {person.age ? (
+      {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
-      ) : null}
+      )}
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
