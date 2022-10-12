b9a7a8c460b0d5daa3ce90f6054dcbd53e1dd381
Vlad Harkusha <sivari888@gmail.com>
2022-10-12 13:36:23 +0300

    checklist fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
   const partner = (sex === 'm')
     ? 'wife'
