5f218250ee86dfac2caf894638b805691ffd5887
Iryna Borniak <la.aerea.1@gmail.com>
2022-10-13 18:06:11 +0300

    refactor: fix format

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
   return (
     <section className="Person">
