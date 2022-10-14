a0c92057d8035569694f55fb195d88b4c10ca8e8
Boris Kerekesha <kerekesha90@ukr.net>
2022-10-14 11:20:19 +0300

    person react

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 
-export const Person = ({ person }) => {
-  if (person.name === 'Misha') {
-    return '';
-  }
-
-  return (
-    <section className="Person">
-      <h2 className="Person__name">
-        My name is
-        {person.name}
-      </h2>
-      {person.age
-        ? (
-          <p className="Person__age">
-            I am
-            {person.age}
-          </p>
-        )
-        : ''}
-      <p className="Person__partner">
-        {
-          person.partnerName
-            ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-            : 'I am not married'
-        }
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      My name is
+      {person.name}
+    </h2>
+    {person.age && (
+      <p className="Person__age">
+        I am
+        {person.age}
       </p>
-    </section>
+    )}
+    <p className="Person__partner">
+      {getPartnerName(person)}
+    </p>
+  </section>
+);
+
+function getPartnerName(person) {
+  return (person.partnerName
+    ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+    : 'I am not married'
   );
-};
+}
