39d9daee32a9f3d7730d8797e6af6cb438d41029
Bibicutu <pauleugen@gmail.com>
2022-10-25 21:47:51 +0300

    First commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    {person.age
+      && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+      )
+    }
+    {/* <p className="Person__age">
+      {person.age && `I am ${person.age}`}
+    </p> */}
+    <p className="Person__partner">
+      {person.isMarried || `I am not married`}
+      {person.isMarried && `${person.partnerName} is my `}
+      {person.isMarried && ((person.sex === 'f') ? (
+        `husband`
+      ) : (
+        `wife`
+      ))}
+    </p>
+  </section>
+);
