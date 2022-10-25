7d725ce3c177b0b3d9211088bb809fd4ad47eb5f
Bradley Matjie <bradleysaint45@gmail.com>
2022-10-25 15:26:07 +0200

    person Component created

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,6 +9,7 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+      
       {age && (
       <p className="Person__age">
         {`I am ${age}`}
