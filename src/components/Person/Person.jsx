fd5be3a2b3c5b5b98a472ff5fcbdfdb1eb7a3dd0
Nhlanhla Thubana <nm.thubana@gmail.com>
2022-10-26 11:38:40 +0200

    Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person: {
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+} }) => {
+  const partnerText = sex === 'm'
+    ? `${partnerName} is my wife`
+    : `${partnerName} is my husband`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is&nbsp;
+        {name}
+      </h2>
+      <p className="Person__age">
+        {age && (`I am ${age}`)}
+      </p>
+      <p className="Person__partner">
+        {isMarried ? (partnerText) : ('I am not married')}
+      </p>
+    </section>
+  );
+};
