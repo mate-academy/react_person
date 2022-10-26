4e46afb4d18d10c0273cf743074e97170d4a43a2
Nhlanhla Thubana <nm.thubana@gmail.com>
2022-10-26 13:12:17 +0200

    Changed to interpolation

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,13 +13,10 @@ export const Person = ({ person: {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">
-        My name is&nbsp;
-        {name}
-      </h2>
-      <p className="Person__age">
-        {age && (`I am ${age}`)}
-      </p>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+      )}
       <p className="Person__partner">
         {isMarried ? (partnerText) : ('I am not married')}
       </p>
