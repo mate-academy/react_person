8763f15684f816faaff5c1399d102534a82262d9
andriibilous4 <92803546+andriibilous4@users.noreply.github.com>
2022-11-01 10:58:36 +0200

    Update src/components/Person/Person.jsx
    Co-authored-by: Lora Levitska <70009205+loralevitska@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">
-      My name is
-      {` ${person.name}`}
+      {` My name is ${person.name}`}
     </h2>
     {person.age && (
     <p className="Person__age">
