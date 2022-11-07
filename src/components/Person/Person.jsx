47d28b2e035691792eabec605ac5bed06a1fe93f
Eugene P <eugenep@MacBook-Pro.local>
2022-11-08 01:02:02 +0200

    person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+let femalePartner;
+
+export function Person({ person }) {
+  if (person.isMarried && person.sex === 'f') {
+    femalePartner = true;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is {person.name}
+      </h2>
+
+      { person.age && (
+        <p className="Person__age">I am {person.age}</p>
+      )
+      }
+
+      { !person.isMarried ? (
+        <p className="Person__partner">I am not married</p>
+      )
+        : (
+          ? <p className="Person__partner">{person.partnerName} is my husband</p>
+          : <p className="Person__partner">{person.partnerName} is my wife</p>
+        )
+  }
+    </section>
+  );
+}
