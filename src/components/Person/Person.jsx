6fd21500b558024926e9ac681005cbd8b77a3433
Yurii-Pozho <46326784+Yurii-Pozho@users.noreply.github.com>
2022-11-15 16:37:37 +0200

    react person added

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import './person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const genderPartner = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age
+        ? (<p className="Person__age">{`I am ${age}`}</p>)
+        : null}
+
+      <p className="Person__partner">
+        {isMarried
+          ? (`${partnerName} is my ${genderPartner}`)
+          : `I am not married`}
+      </p>
+    </section>
+  );
+};
