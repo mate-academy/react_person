a3d9b11a6c4516d24108210f24f8bff904d91e10
Svitlana Yudina <osbka31@gmail.com>
2022-11-16 20:00:41 +0200

    last fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,6 +13,10 @@ export function Person({ person }) {
     ? 'wife'
     : 'husband';
 
+  const marriedStatus = isMarried
+    ? `${partnerName} is my ${partner}`
+    : 'I am not married';
+
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
@@ -20,9 +24,7 @@ export function Person({ person }) {
         <p className="Person__age">{`I am ${age}`}</p>
       )}
       <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${partner}`
-          : 'I am not married'}
+        {marriedStatus}
       </p>
     </section>
   );
