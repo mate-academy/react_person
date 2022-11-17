c5d0390cb6cfa99bf7ef38e576f6de48f6072738
Yurii-Pozho <46326784+Yurii-Pozho@users.noreply.github.com>
2022-11-17 12:29:54 +0200

    fix everything

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import './person.scss';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-  const genderPartner = sex === 'm' ? 'wife' : 'husband';
+  const genderPartner = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const isMarriedDescription = isMarried
+    ? `${partnerName} is my ${genderPartner}`
+    : `I am not married`;
 
   return (
     <section className="Person">
@@ -12,14 +24,12 @@ export const Person = ({ person }) => {
         {`My name is ${name}`}
       </h2>
 
-      {age
-        ? (<p className="Person__age">{`I am ${age}`}</p>)
-        : null}
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
 
       <p className="Person__partner">
-        {isMarried
-          ? (`${partnerName} is my ${genderPartner}`)
-          : `I am not married`}
+        {isMarriedDescription}
       </p>
     </section>
   );
