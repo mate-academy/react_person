c59ed748f382b3206976faaa06dc3cd58a120487
Spirit <baolt.king@gmail.com>
2022-11-19 17:15:45 +0200

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && <p className="Person__age">I am 37</p>}
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${
-          person.sex === 'm' ? 'wife' : 'husband'
-        }`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+  const MarriedData = `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+      <p className="Person__partner">
+        {isMarried
+          ? MarriedData
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
+
+Person.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    sex: PropTypes.string.isRequired,
+    isMarried: PropTypes.bool.isRequired,
+    partnerName: 'Natasha',
+  }).isRequired,
+};
