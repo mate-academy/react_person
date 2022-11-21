fc4f1a5dcf027f6d651baa799212a01d5a37bca2
Yurii Ostapchuk <ostapchuk.y.o@gmail.com>
2022-11-21 15:39:34 +0200

    develop - 2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Person = ({ person }) => {
   const {
@@ -14,22 +15,28 @@ export const Person = ({ person }) => {
     : 'husband';
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">
-          {`My name is ${name}`}
-        </h2>
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
-        {age && (
-          <p className="Person__age">{`I am ${age}`}</p>
-        )}
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
 
-        <p className="Person__partner">
-          {(isMarried)
-            ? `${partnerName} is my ${partner}` : `I am not married`}
-        </p>
-
-      </section>
-    </>
+      <p className="Person__partner">
+        {(isMarried)
+          ? `${partnerName} is my ${partner}`
+          : `I am not married`}
+      </p>
+    </section>
   );
+};
+
+Person.prototypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string,
 };
