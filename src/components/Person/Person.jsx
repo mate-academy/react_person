008f3639fd643fd14edd71e612542198a1640383
Aleks <offsset@gmail.com>
2022-11-21 11:01:42 +0200

    olution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">
-      My name is
-      {person.name}
+      My name is {person.name}
     </h2>
     <p className="Person__age">
       {person.age ? `I am ${person.age}` : `` }
