81b556c7b4187b674113a7d8dc853b2ccd7a20f7
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-11-23 19:55:20 +0200

    cod react person after reveiw

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,30 +4,28 @@ import PropTypes from 'prop-types';
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  function secondHalf(gender) {
+    return (
+      gender === 'f' ? 'husband' : 'wife'
+    );
+  }
+
   return (
     <div className="App">
       <section className="Person">
         <h2 className="Person__name">
-          My name is&nbsp;
-          {name}
+          {`My name is ${name}`}
         </h2>
         {age ? (
           <p className="Person__age">
-            I am&nbsp;
-            {age}
+            {`I am ${age}`}
           </p>
         ) : (
           <p className="Person__age" />
         )}
         {isMarried ? (
           <p className="Person__partner">
-            {partnerName}
-            &nbsp;is my&nbsp;
-            {sex === 'f' ? (
-              'husband'
-            ) : (
-              'wife')
-            }
+            {`${partnerName} is my ${secondHalf(sex)}`}
           </p>
         ) : (
           <p className="Person__partner">
