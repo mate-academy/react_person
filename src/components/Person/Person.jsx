8b28521cba10cd2bfec3f334c03089528e03455a
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-11-24 16:41:31 +0200

    fixed2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -22,11 +22,9 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {
-        age && (
-          <p className="Person__age">{`I am ${age}`}</p>
-        )
-      }
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
       <p className="Person__partner">{partner}</p>
     </section>
   );
