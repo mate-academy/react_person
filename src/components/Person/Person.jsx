e85973354d8f71f1f6a4a201c80b89303c464f10
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-11-30 13:40:09 +0200

    refactor

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age
-      ? <p className="Person__age">{`I am ${person.age}`}</p>
-      : null
-    }
-    <p className="Person__partner">
-      {(() => {
-        if (person.isMarried) {
-          return person.sex === 'm'
-            ? `${person.partnerName} is my wife`
-            : `${person.partnerName} is my husband`;
-        }
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName, sex } = person;
+  let married;
 
-        return `I am not married`;
-      })()}
-    </p>
-  </section>
-);
+  if (isMarried) {
+    married = sex === 'm'
+      ? `${partnerName} is my wife`
+      : `${partnerName} is my husband`;
+  } else {
+    married = `I am not married`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">
+        {married}
+      </p>
+    </section>
+  );
+};
