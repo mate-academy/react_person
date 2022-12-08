73f35d6768361d9587878bbc7ae84e5ae1f1d329
Margarita Chorniak <chorniak.rita@gmail.com>
2022-12-08 12:59:40 +0200

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const partner = (sex === 'f') ? 'husband' : 'wife';
+  const status = (isMarried)
+    ? `${partnerName} is my ${partner}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">{status}</p>
+    </section>
+  );
+};
