2f7e9f2afde90eba000a297b3c42037e2d7d6e88
Oleksandr Filo <filo.sanya@gmail.com>
2022-12-18 14:35:13 +0200

    conditional rendering fixed according to checklist

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
-  const manIsMarried = sex === 'm' && isMarried;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+  const personPartner = (
+    sex === 'm'
+      ? 'wife'
+      : 'husband'
+  );
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
       {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
       )}
 
       <p className="Person__partner">
         {
-          person.isMarried
-            ? `${partnerName} is my ${manIsMarried ? 'wife' : 'husband'}`
+          isMarried
+            ? `${partnerName} is my ${personPartner}`
             : 'I am not married'
         }
       </p>
