3cb895f4cf073f64299dd41e8bf0ebf87ab8035f
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-12-22 23:29:04 +0100

    removed some comments

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -35,15 +35,3 @@ export const Person = ({ person }) => {
     </section>
   );
 };
-
-/* <section className="Person">
-<h2 className="Person__name">My name is Olya</h2>
-<p className="Person__partner">Maksym is my husband</p>
-</section>
-
-<section className="Person">
-<h2 className="Person__name">My name is Alex</h2>
-<p className="Person__age">I am 25</p>
-<p className="Person__partner">I am not married</p>
-</section>
- */
