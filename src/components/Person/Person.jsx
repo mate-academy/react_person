f0ca4de3c1904d220d3be9813600b0b215dd7808
illia_kots <illyahennadievich@gmail.com>
2022-12-22 20:01:49 +0200

    React_Person was done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// export const Person = ({ person }) => ();
+import React from "react";
+
+export const Person = ({ person }) => {
+  const man = person.sex === 'm';
+  const married = person.isMarried === true;
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">
+          {`My name is ${person.name}`}
+        </h2>
+        {person.age && (
+        <p className="Person__age">
+          I am {person.age}
+        </p>
+        )}
+        <p className="Person__partner">
+          {(married 
+          && (man 
+            ? `${person.partnerName} is my wife` 
+            : `${person.partnerName} is my husband`))
+          || ('I am not married')}
+        </p>
+      </section>
+    </div>
+  )
+};
