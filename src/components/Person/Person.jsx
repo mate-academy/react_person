fa7d831ae770aa8b5e4f2a69a0ee5f156035bf13
mr-valentos <nerpavlad@gmail.com>
2022-12-22 21:46:52 +0200

    rm nested ternary

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,15 +9,17 @@ export const Person = ({ person }) => {
     sex,
   } = person;
 
+  const partner = sex === 'm'
+    ? `wife`
+    : `husband`;
+
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">
         {isMarried
-          ? `${partnerName} is my ${sex === 'm'
-            ? `wife`
-            : `husband`}`
+          ? `${partnerName} is my ${partner}`
           : 'I am not married'}
       </p>
     </section>
