a475c9b6d8ba8f58a2d8e69bdd03d4a45fd176e8
Vladimir Arkanov <vladimirarkanov@icloud.com>
2022-12-23 10:01:45 +0200

    first commit, was done according to task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {age
+          ? <p className="Person__age">{`I am ${age}`}</p>
+          : null
+        }
+        {isMarried === true
+          ? <p className="Person__partner">{`${partnerName} is my ${sex === 'm' ? `wife` : `husband`}`}</p>
+          : <p className="Person__partner">I am not married</p>
+        }
+      </section>
+    </div>
+  );
+};
