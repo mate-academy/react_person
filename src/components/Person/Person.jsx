d67615ca08e7ecfdd83d1327a3e058f41fb4e2f0
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2022-12-23 16:17:26 +0200

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = (props) => {
+  const {
+    person,
+  } = props;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age
+        ? (<p className="Person__age">{`I am ${age}`}</p>)
+        : null
+      }
+
+      {isMarried
+        ? (<p className="Person__partner">{`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}</p>)
+        : (<p className="Person__partner">I am not married</p>)
+      }
+    </section>
+  );
+};
