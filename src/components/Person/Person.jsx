d65fd2ecd17b4d1849b1981474f608746c618114
Anatolii <batstolya@gmail.com>
2022-12-23 18:59:25 +0200

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -16,11 +16,13 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
         {notMarried || (sex === 'm'
           ? `${partnerName} is my wife`
