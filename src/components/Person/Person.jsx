74dab68da0d58b9b99064e8c5b9291f135ae5319
Dima Kolesnyk <kolesnyk.dmytro.victorovych@gmail.com>
2022-12-25 14:15:18 +0200

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,55 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import propTypes from 'prop-types';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const partnerSex = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerSex}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
+
+// I'm curious why the code below isn't reachable???
+
+Person.defaultProps = {
+  person: {
+    age: null,
+  },
+};
+
+Person.propTypes = {
+  person: propTypes.shape({
+    name: propTypes.string.isRequired,
+    age: propTypes.number,
+    sex: propTypes.string,
+    isMarried: propTypes.bool,
+    partnerName: propTypes.string,
+  }),
+};
