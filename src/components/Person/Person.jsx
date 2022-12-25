906f70739b67e860a785b95a58e8428a0e09aba1
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2022-12-25 17:58:30 +0100

    fixed ternary operator

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,26 +9,24 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
+  const marriedWith = sex === 'm'
+    ? `${partnerName} is my wife`
+    : `${partnerName} is my husband`;
+
   return (
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {age ? (
+      {age && (
         <p className="Person__age">{`I am ${age}`}</p>
-      ) : (
-        <></>
       )}
-      {isMarried ? (
-        <p className="Person__partner">
-          {sex === 'm'
-            ? (`${partnerName} is my wife`)
-            : (`${partnerName} is my husband`)
-          }
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">
+        {isMarried
+          ? marriedWith
+          : `I am not married`
+        }
+      </p>
     </section>
   );
 };
