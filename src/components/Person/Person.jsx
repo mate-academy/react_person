e113d5c2d1765b6b06e4f07e157bf337495ff2f6
Ilija Pejanovic <ikadvm96@gmail.com>
2022-12-29 17:36:53 +0100

    changed Person.jsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,43 +1,38 @@
 // export const Person = ({ person }) => ();
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {(`My name is ${person.name}`)}
-    </h2>
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  const partner = (sex === 'm')
+    ? 'wife'
+    : 'husband';
+
+  const status = (isMarried)
+    ? `${partnerName} is my ${partner}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {status}
       </p>
-    )}
-
-    <p className="Person__partner">
-      {(() => {
-        switch (person.isMarried) {
-          case false:
-            return `I am not married`;
-          case true:
-            switch (person.sex) {
-              case 'm':
-                return `${person.partnerName} is my wife`;
-
-              case 'f':
-                return `${person.partnerName} is my husband`;
-
-              default:
-                break;
-            }
-
-            break;
-
-          default:
-            break;
-        }
-
-        return this;
-      })()}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
