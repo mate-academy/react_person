1425fb79472ca57246091e3ba2ed4031ab71e798
Ivan Melnychuk <jacksmilereacher@gmail.com>
2023-01-09 18:44:07 +0200

    improved code readability

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,20 +12,15 @@ export const Person = ({
 }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${name}`}</h2>
-    {
-      age && (
-        <p className="Person__age">{`I am ${age}`}</p>
-      )
-    }
-    {
-      <p className="Person__partner">
-        {
-          isMarried
-            ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
-            : 'I am not married'
-        }
-      </p>
-    }
+    {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+    )}
+    <p className="Person__partner">
+      {isMarried
+        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+        : 'I am not married'
+      }
+    </p>
   </section>
 );
 
