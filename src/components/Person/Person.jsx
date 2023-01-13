cde332f04b70fd79427ec9ac9320789f6996ee9d
miamarichka <ilchyshyn.marichka@gmail.com>
2023-01-14 01:15:41 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,50 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable react/default-props-match-prop-types */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-constant-condition */
+/* eslint-disable indent */
+/* eslint-disable arrow-body-style */
+import { bool, number, string } from 'prop-types';
+import React from 'react';
+
+export const Person = ({ person }) => {
+    return (
+      <section className="Person">
+        <h2 className="Person__name">
+          My name is {person.name}
+        </h2>
+        {person.age && (
+        <p className="Person__age">
+          I am {person.age}
+        </p>
+            )}
+
+        <p className="Person__partner">
+          { person.isMarried
+          ? `${person.partnerName} is my ${
+            person.sex === 'm'
+              ? 'wife'
+              : 'husband'
+          }`
+          : 'I am not married'}
+        </p>
+
+      </section>
+    );
+};
+
+Person.propTypes = {
+    name: string,
+    age: number,
+    sex: 'm' || 'f',
+    isMarried: bool,
+    partnerName: string,
+};
+
+Person.defaultProps = {
+    person: {
+        name: '',
+        age: null,
+        partnerName: null,
+    },
+};
