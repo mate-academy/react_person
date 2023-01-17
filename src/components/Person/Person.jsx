2ecc9afbd66a86aa74f9665e370410b17f1cd522
Aleks <offsset@gmail.com>
2023-01-17 17:37:47 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
+function getMessedge(human) {
+  return human.isMarried
+    ? `${human.partnerName} is my ${(human.sex === 'm')
+      ? 'wife' : 'husband'}` : 'I am not married';
+}
+
+function getAge(ageHuman) {
+  if (ageHuman.age) {
+    return (
+      <p className="Person__age">
+        `I am $
+        {ageHuman.age}
+        `
+      </p>
+    );
+  }
+
+  return null;
+}
+
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">
-      My name is {person.name}
+      My name is
+      {' '}
+      {person.name}
     </h2>
-    <p className="Person__age">
-      {person.age ? `I am ${person.age}` : `` }
-    </p>
+    {getAge(person)}
     <p className="Person__partner">
-      {
-      person.isMarried
-        ? `${person.partnerName} is my ${(person.sex === 'm')
-          ? 'wife' : 'husband'}` : 'I am not married' }
+      { getMessedge(person)
+       }
     </p>
   </section>
 
