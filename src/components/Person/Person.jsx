ab5068fc7e087e81767f7e9c1689c41b668dc114
Sergey Mironenko <serg1prime@gmail.com>
2023-01-18 03:49:20 +0200

    Task was done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const iff = (condition, then, otherwise) => (
+    condition ? then : otherwise
+  );
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {' '}
+        {person.name}
+      </h2>
+      {person.age && (
+        <p className="Person__age">
+          I am
+          {' '}
+          {person.age}
+        </p>
+      )}
+      <p className="Person__partner">
+        {person.isMarried ? (
+          iff(person.sex === 'm',
+            `${person.partnerName} is my wife`,
+            `${person.partnerName} is my husband`)
+        ) : `I am not married`}
+      </p>
+    </section>
+  );
+}
