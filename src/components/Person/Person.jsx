204c004e372980a6690deecce945f35d2c89c456
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-01-20 18:31:07 +0200

    React person - completed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    <p className="Person__age">{person.age && `I am ${person.age}`}</p>
+    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
     <p className="Person__partner">
       {person.isMarried
         ? `${person.partnerName} is my ${
