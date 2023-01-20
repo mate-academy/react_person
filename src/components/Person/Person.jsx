5ff8a7de0939841f89764cd10f6af9995623312b
Mykyta Krainik <nikinta2003@gmail.com>
2023-01-20 21:16:53 +0200

    Add Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,30 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let relationshipStatus = '';
+
+  if (!isMarried) {
+    relationshipStatus = 'I am not married';
+  } else {
+    const partnerRole = sex === 'f' ? 'husband' : 'wife';
+
+    relationshipStatus = `${partnerName} is my ${partnerRole}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">{relationshipStatus}</p>
+    </section>
+  );
+};
