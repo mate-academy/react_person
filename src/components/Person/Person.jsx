7c5e9615cbbc2d889a5939324c1922c4539517ed
Zykhor Andrii <zuxoran@gmail.com>
2023-01-21 01:23:01 +0200

    task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,33 @@
-// export const Person = ({ person }) => ();
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({ name, age, sex, isMarried, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      My name is
+      {' '}
+      {name}
+    </h2>
+    {age && (
+      <p className="Person__age">
+        I am
+        {' '}
+        {age}
+      </p>
+    )}
+
+    <p className="Person__partner">
+      {isMarried ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}` : 'I am not married'}
+    </p>
+  </section>
+);
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string.isRequired,
+
+};
