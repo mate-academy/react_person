ee95148dba93749d42dedccb2f53a6eba2d506c1
Vlad Boikov <vladboikov95@hotmail.com>
2023-01-21 12:41:52 +0200

    Marriage implementation has changed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,11 +6,8 @@ export function Person(props) {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {/* <p className="Person__age">{age && `I am ${age}`}</p> */}
       {age && (<p className="Person__age">{`I am ${age}`}</p>)}
-      {isMarried ? (
-        <p className="Person__partner">{`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}</p>
-      ) : `I am not married`}
+      <p className="Person__partner">{isMarried ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}` : 'I am not married'}</p>
     </section>
   );
 }
