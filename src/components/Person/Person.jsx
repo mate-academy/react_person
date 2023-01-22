304ec59d0fdbbf89d57cec279b9ea1665d9be6dd
Dima Korol <korovaidima@gmail.com>
2023-01-22 14:51:25 +0100

    removed unnecessary variables

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,36 +1,30 @@
 import React from 'react';
 
-export const Person = (obj) => {
-  const objKeys = { ...obj };
-  const people = objKeys.person;
-
+export const Person = ({ person }) => {
   const {
     name,
     age,
     sex,
     isMarried,
     partnerName,
-  } = people;
+  } = person;
 
   const partner = sex === 'm'
     ? 'wife'
     : 'husband';
 
-  const withPartner = `${partnerName} is my ${partner}`;
-  const checkForPartner = isMarried === false
-    ? 'I am not married'
-    : withPartner;
-
-  const withAge = age
-    ? <p className="Person__age">{`I am ${age}`}</p>
-    : '';
-
   return (
     <div className="App">
       <section className="Person">
         <h2 className="Person__name">{`My name is ${name}`}</h2>
-        {withAge}
-        <p className="Person__partner">{checkForPartner}</p>
+
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+        {isMarried === false
+          ? 'I am not married'
+          : `${partnerName} is my ${partner}`
+        }
+
       </section>
     </div>
   );
