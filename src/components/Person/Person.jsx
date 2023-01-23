e342aa11cbe9aa9efde99916cefd8f5e94ea6b5d
Your Name <you@example.com>
2023-01-23 17:19:20 +0200

    develop

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,48 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age = null,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const partnerSex = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+      )}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerSex}`
+          : 'I am not married'
+}
+      </p>
+    </section>
+
+  );
+};
+
+Person.defaultProp = {
+  name: null,
+  partnerName: null,
+  age: undefined,
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool.isRequired,
+  partnerName: PropTypes.string.isRequired,
+};
