c7cca294668e3606648242228ff0225381244667
Andrew Borys <borysandrey1988@gmail.com>
2023-01-24 17:04:54 +0200

    solution_2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,6 +10,8 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
+  const partner = sex === 'm' ? 'wife' : 'husband';
+
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
@@ -22,7 +24,7 @@ export const Person = ({ person }) => {
 
       <p className="Person__partner">
         {isMarried
-          ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+          ? `${partnerName} is my ${partner}`
           : 'I am not married'}
       </p>
     </section>
