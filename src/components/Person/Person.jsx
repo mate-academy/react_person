f74f21916d21baac827ad2f4e0466f7b663b77a3
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-01-24 10:29:22 +0200

    fixed component variables destruction

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Person.scss';
 
-export const Person = ({
-  name,
-  age = null,
-  sex,
-  isMarried,
-  partnerName,
-}) => {
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
   const partnerStatus = sex === 'm'
     ? 'wife'
     : 'husband';
@@ -18,10 +19,12 @@ export const Person = ({
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age
         ? <p className="Person__age">{`I am ${age}`}</p>
         : null
       }
+
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${partnerStatus}`
