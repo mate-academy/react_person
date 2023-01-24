140c7b40749149e453777b2f5eff1c1d54f5221a
maasiimka <mrozisalovec@gmail.com>
2023-01-24 15:48:25 +0200

    Changed some things

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
+
   const partnerMessage = partnerName + (sex === 'm'
     ? ' is my wife'
     : ' is my husband');
@@ -10,7 +11,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+
       {age && <p className="Person__age">{`I am ${age}`}</p>}
+
       <p className="Person__partner">
         {isMarried
           ? partnerMessage
