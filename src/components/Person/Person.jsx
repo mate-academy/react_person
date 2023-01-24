9de2206db2297b7fdbca6ddf0c9cb957ef340f80
Ihor Mariash <imariash86@gmail.com>
2023-01-24 20:49:53 +0200

    requested changes were done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  let partner = (<p className="Person__partner">I am not married</p>);
-
-  if (isMarried && sex === 'f') {
-    partner = (<p className="Person__partner">{`${partnerName} is my husband`}</p>);
-  }
-
-  if (isMarried && sex === 'm') {
-    partner = (<p className="Person__partner">{`${partnerName} is my wife`}</p>);
-  }
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age
-        ? (<p className="Person__age">{`I am ${age}`}</p>)
-        : null}
-      {partner}
+      {age && (<p className="Person__age">{`I am ${age}`}</p>)}
+      {isMarried
+        ? (<p className="Person__partner">{`${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`}</p>)
+        : (<p className="Person__partner">I am not married</p>)}
     </section>
   );
+};
+
+Person.propTypes = {
+  name: propTypes.string.isRequired,
+  age: propTypes.number,
+  sex: propTypes.string.isRequired,
+  isMarried: propTypes.bool.isRequired,
+  partnerName: propTypes.string,
+};
+
+Person.defaultProps = {
+  age: null,
+  partnerName: '',
 };
