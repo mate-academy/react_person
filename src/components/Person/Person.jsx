259489cdfae0399598a434597e5aad2104fcae60
Yaroslav <yaroslavfoorgala@gmail.com>
2023-01-27 18:24:06 +0200

    Initial commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,34 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import './Person.scss';
+
+export function Person({
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+}) {
+  const partnerRole = (sex === 'm') ? 'wife' : 'husband';
+
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">
+          {`My name is ${name}`}
+        </h2>
+
+        {(age) && (
+          <p className="Person__age">
+            {`I am ${age}`}
+          </p>
+        )}
+
+        <p className="Person__partner">
+          {(isMarried)
+            ? (`My ${partnerRole}'s name is ${partnerName}`)
+            : ('I am not married')}
+        </p>
+      </section>
+    </>
+  );
+}
