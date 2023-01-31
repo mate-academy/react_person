aa18c33790e8d4b2caf03777e081a0ba8fc3bde0
Anton Vasyliev <magisterol963@gmail.com>
2023-01-31 22:28:56 +0200

    Thank you for that!  That was a very detailed review :)

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,35 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age
-    && (
-    <p className="Person__age">
-      {`I am ${person.age}`}
-    </p>
-    )}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+export const Person = ({ person: {
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+} }) => {
+  const getMarriedStatus = () => {
+    if (isMarried) {
+      return `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`;
+    }
+
+    return 'I am not married';
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is {name}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          I am {age}
+        </p>
+      )}
+      <p className="Person__partner">
+        {getMarriedStatus()}
+      </p>
+    </section>
+  );
+};
