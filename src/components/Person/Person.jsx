cf6374cc8f2d7471a77fb1590a92f7ccea4924dd
Zykhor Andrii <zuxoran@gmail.com>
2023-02-02 16:23:41 +0200

    some fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-// { name, age, sex, isMarried, partnerName }
 
 export const Person = ({ person: {
   name, age, sex, isMarried, partnerName,
@@ -11,20 +10,18 @@ export const Person = ({ person: {
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {name}
+        {`My name is ${name}`}
       </h2>
       {age && (
         <p className="Person__age">
-          I am
-          {' '}
-          {age}
+        {`I am ${age}`}
         </p>
       )}
 
       <p className="Person__partner">
-        {isMarried ? `${partnerName} is my ${gender}` : 'I am not married'}
+        {isMarried
+          ? `${partnerName} is my ${gender}`
+          : 'I am not married'}
       </p>
     </section>
   );
