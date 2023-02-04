bb5cf67a7560e2902b5182383cae7d97a925c9ab
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-02-04 15:08:47 +0200

    Use constant to get partner of person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
+  const partner = person.sex === 'm' ? 'wife' : 'husband';
   const personPartnerParagraph = person.isMarried ? (
     <p className="Person__partner">
-      {`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
+      {`${person.partnerName} is my ${partner}`}
     </p>
   ) : (
     <p className="Person__partner">I am not married</p>
@@ -12,7 +13,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      {person.age && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
       {personPartnerParagraph}
     </section>
   );
