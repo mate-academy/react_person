24aa9c16500b8adec94541120a68ff964c939d52
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-02-05 21:38:57 +0000

    react person task completed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const partner = person.sex === 'm'
+    ? 'wife'
+    : 'husband';
+  const familyStatus = person.isMarried
+    ? `${person.partnerName} is my ${partner}`
+    : `I am not married`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age && (
+      <p className="Person__age">
+        I am {person.age}
+      </p>
+      )}
+      <p className="Person__partner">
+        {familyStatus}
+      </p>
+    </section>
+  );
+};
