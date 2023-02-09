e5528f90dd52a73855db385863aadfb114f18d33
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-02-09 14:41:10 +0200

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import { Person } from './components/Person/Person';
 
-export const misha = {
+const misha = {
   name: 'Misha',
   age: 37,
   sex: 'm',
@@ -10,14 +10,14 @@ export const misha = {
   partnerName: 'Natasha',
 };
 
-export const olya = {
+const olya = {
   name: 'Olya',
   sex: 'f',
   isMarried: true,
   partnerName: 'Maksym',
 };
 
-export const alex = {
+const alex = {
   name: 'Alex',
   age: 25,
   sex: 'm',
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,25 +3,20 @@ import React from 'react';
 export const Person = ({ person }) => {
   const { name, age, partnerName, isMarried, sex } = person;
 
+  // prettier-ignore
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {name}
+        {`My name is ${name}`}
       </h2>
       {age && (
       <p className="Person__age">
-        I am
-        {' '}
-        {age}
+        {`I am ${age}`}
       </p>
       )}
       {isMarried ? (
         <p className="Person__partner">
-          {partnerName}
-          {' '}
-          is my
+          {`${partnerName} is my `}
           {sex === 'm' ? 'wife' : 'husband'}
         </p>
       ) : (<p className="Person__partner">I am not married</p>)}
