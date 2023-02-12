1dde3901ef8f3a4bf4ce517f02f081149cd8a2ea
Bohdan Rudenko <RudBeee@gmail.com>
2023-02-12 19:12:29 +0200

    first try

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  const partner = person.sex === 'm'
+    ? (`${person.partnerName} is my wife`)
+    : (`${person.partnerName} is my husband`);
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
+      {person.age
+        ? (<p className="Person__age">{`I am ${person.age}`}</p>)
+        : ('')
+      }
+
+      <p className="Person__partner">
+        {!person.isMarried
+          ? ('I am not married')
+          : partner
+        }
+
+      </p>
+    </section>
+  );
+}
