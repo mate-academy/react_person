40440a8eed9fc6aa17f8990776c86bcaaf6536f9
Vitalii Skomskiy <skomskiy@ukr.net>
2023-02-16 14:52:39 +0200

    React commit 3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
 export function Person({ person }) {
-  const partner = person.sex === 'f' ? 'husband' : 'wife';
+  const { name, age, isMarried, partnerName, sex } = person;
+  const partner = sex === 'f' ? 'husband' : 'wife';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {`My name is ${person.name}`}
+        {`My name is ${name}`}
       </h2>
-      {person.age && (
+
+      {age && (
         <p className="Person__age">
-          {`I am ${person.age}`}
+          {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
-        {person.isMarried ? `${person.partnerName} is my ${partner}` : 'I am not married'}
+        {isMarried ? `${partnerName} is my ${partner}` : 'I am not married'}
       </p>
     </section>
   );
