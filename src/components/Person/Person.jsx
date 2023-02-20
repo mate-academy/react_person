9fc05604ebbd567ff91087ba13f9abdf0653685d
Evgen Maximov <evgen@debian-BULLSEYE-live-builder-AMD64>
2023-02-20 23:25:24 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,49 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  let partnerTitle = '';
+
+  if (sex === 'm') {
+    partnerTitle = 'wife';
+  } else {
+    partnerTitle = 'husband';
+  }
+
+  let mariedField = '';
+
+  if (isMarried) {
+    mariedField = `${partnerName} is my ${partnerTitle}`;
+  } else {
+    mariedField = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (<p className="Person__age">{`I am ${age}`}</p>)}
+      <p className="Person__partner">{mariedField}</p>
+    </section>
+  );
+};
+
+Person.defaultProps = {
+  age: null,
+  isMarried: null,
+};
+
+Person.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  sex: PropTypes.string.isRequired,
+  isMarried: PropTypes.bool,
+  partnerName: PropTypes.string.isRequired,
+};
