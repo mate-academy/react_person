462314bcff93c424fb4ba2dae8dc2f4c3680d2d9
Evgen Maximov <evgen@debian-BULLSEYE-live-builder-AMD64>
2023-02-21 21:46:46 +0200

    Solution++

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,27 +10,17 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
-  let partnerTitle = '';
+  const partnerTitle = sex === 'm' ? 'wife' : 'husband';
 
-  if (sex === 'm') {
-    partnerTitle = 'wife';
-  } else {
-    partnerTitle = 'husband';
-  }
-
-  let mariedField = '';
-
-  if (isMarried) {
-    mariedField = `${partnerName} is my ${partnerTitle}`;
-  } else {
-    mariedField = 'I am not married';
-  }
+  const marriedField = isMarried
+    ? `${partnerName} is my ${partnerTitle}`
+    : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && (<p className="Person__age">{`I am ${age}`}</p>)}
-      <p className="Person__partner">{mariedField}</p>
+      <p className="Person__partner">{marriedField}</p>
     </section>
   );
 };
