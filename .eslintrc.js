2de85c464195c45f30d189eba3963f63d8ff5b97
Ashton <nonlocality@gmail.com>
2023-02-25 20:14:04 +0200

    Initial solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const { off } = require("process");
+
 module.exports = {
   extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
   rules: {
@@ -5,6 +7,7 @@ module.exports = {
       devDependencies: true,
       optionalDependencies: false,
       peerDependencies: false,
+
     }],
     'react/prop-types': 0,
     'max-len': ['error', {
@@ -14,6 +17,7 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': ['error', {
       assert: 'either',
     }],
+    'react/jsx-one-expression-per-line': 'off',
     'jsx-a11y/control-has-associated-label': 'off',
   },
 };
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerLabel;
+
+  if (isMarried) {
+    partnerLabel = sex === 'm' ? 'wife' : 'husband';
+  } else {
+    partnerLabel = 'not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerName ? `${partnerName} is my ${partnerLabel}` : `I am ${partnerLabel}`}</p>
+    </section>
+  );
+};
+
+export default Person;
