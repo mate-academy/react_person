0bd4ed33def1463054d48b50ef3e4c98fd06091f
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-26 07:31:00 +0200

    my first task by mate academy

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const partnerName1 = partnerName || '';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {name}
+      </h2>
+      {age && (
+      <p className="Person__age">
+        I am
+        {age}
+      </p>
+      )}
+
+      {isMarried && (
+        <p className="Person__partner">
+          {sex === `m`
+            ? `${partnerName1} is my wife`
+            : `${partnerName1} is my husband`}
+        </p>
+      )}
+      {!isMarried && <p className="Person__partner">I am not married</p>}
+    </section>
+  );
+};
