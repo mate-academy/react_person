6ed8e0acb73c90771675236eeb1f89795301dc17
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-26 08:27:55 +0200

    Last bugFix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const partnerName1 = partnerName || "";
+  const partnerName1 = partnerName || '';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">I am {age}</p>}
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
 
       {isMarried && (
         <p className="Person__partner">
