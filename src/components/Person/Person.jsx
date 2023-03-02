9cb9f5158d4eec0e611a2e5e47f936ea452691c5
Kate <novak5683@gmail.com>
2023-03-02 12:00:00 +0100

    result

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const checkPartner = person.sex === 'm'
+    ? `${person.partnerName} is my wife`
+    : `${person.partnerName} is my husband`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      { person.age ? (<p className="Person__age">{`I am ${person.age}`}</p>) : null }
+      <p className="Person__partner">
+        { person.isMarried ? checkPartner : 'I am not married' }
+      </p>
+    </section>
+  );
+};
