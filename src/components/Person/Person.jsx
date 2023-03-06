f16461e93a205976f92548d6222e865120064ea0
ivanm2706 <ivanmorhun664@gmail.com>
2023-03-06 16:48:12 +0200

    added camelCase

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Person = ({ name, age, sex, isMarried, partnerName }) => {
-  const WifeOrHusbund = sex === 'm' ? 'wife' : 'husband';
+  const wifeOrHusbund = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
@@ -20,7 +20,7 @@ export const Person = ({ name, age, sex, isMarried, partnerName }) => {
       <p className="Person__partner">
         {!isMarried
           ? 'I am not married'
-          : `${partnerName} is my ${WifeOrHusbund}`
+          : `${partnerName} is my ${wifeOrHusbund}`
         }
       </p>
     </section>
