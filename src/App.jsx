a0389059846dbfff636067c912338f24e5eb84b2
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-03-06 17:20:17 +0100

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -32,27 +32,27 @@ export function App() {
   );
 }
 
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
+// export const misha = {
+//   name: 'Misha',
+//   age: 37,
+//   sex: 'm',
+//   isMarried: true,
+//   partnerName: 'Natasha',
+// };
 
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
+// export const olya = {
+//   name: 'Olya',
+//   sex: 'f',
+//   isMarried: true,
+//   partnerName: 'Maksym',
+// };
 
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
+// export const alex = {
+//   name: 'Alex',
+//   age: 25,
+//   sex: 'm',
+//   isMarried: false,
+// };
 
 // export const App = () => (
 //   <div className="App">
