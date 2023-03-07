f5118c3b0f1005459795ff11a8d91b6018ba83be
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-03-07 09:34:21 +0100

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <div className="App">
-    <section className="Person">
-      <h2 className="Persone__name">
-        My name is
-        {' '}
-        {person.name}
-      </h2>
-      <p className="Person__age">
-        {person.age && (
+export const Person = ({ person }) => {
+  const spouseTitle = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Persone__name">
+          {`My name is ${person.name}`}
+        </h2>
+        <p className="Person__age">
+          {person.age && (
           <>
-            I am
-            {' '}
-            {person.age}
+            {`I am ${person.age}`}
           </>
-        )}
-      </p>
-      <p className="Person__partner">
-        {person.isMarried === false
-          ? 'I am not married'
-          : `${person.partnerName} is my
-          ${person.sex === 'm' ? 'wife' : 'husband'}`
-        }
-      </p>
-    </section>
-  </div>
-);
+          )}
+        </p>
+        <p className="Person__partner">
+          {person.isMarried === false
+            ? 'I am not married'
+            : `${person.partnerName} is my ${spouseTitle}`
+          }
+        </p>
+      </section>
+    </div>
+  );
+};
