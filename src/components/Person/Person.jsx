eb4a21b7a3a816b05ef23f80d4a582f3ef83d934
Ashton <mfckr.x@gmail.com>
2023-03-08 14:09:47 +0000

    Update src/components/Person/Person.jsx
    Co-authored-by: Oleksandr Polotniuk <96409563+polosanya@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,8 +14,14 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+      
       {age && (<p className="Person__age">{`I am ${age}`}</p>)}
-      <p className="Person__partner">{partnerName ? `${partnerName} is my ${partnerLabel}` : `I am not married`}</p>
+      
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerLabel}` 
+          : `I am not married`}
+      </p>
     </section>
   );
 };
