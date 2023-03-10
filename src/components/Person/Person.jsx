ca81ded713d3f6beb874bb1f094ef4248561e4f1
vadosik15 <vadim.bratus@gmail.com>
2023-03-10 16:15:17 +0200

    changed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,36 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const personMaried = isMarried
+    ? (`${partnerName} is my ${
+      sex === 'm'
+        ? ('wife')
+        : ('husband')}`)
+    : ('I am not married');
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        { `My name is ${name}`}
+      </h2>
+
+      {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+      )}
+
+      <p className="Person__partner">
+        {personMaried}
+      </p>
+    </section>
+  );
+}
