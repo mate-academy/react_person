0b953cb871dfa9cda05812c3e3a314ed9cc09481
gretoq <dovhun3d@gmail.com>
2023-03-10 14:25:23 +0200

    Fixed after review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
   const personPartnerText = isMarried
     ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
     : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
       {age && <p className="Person__age">{`I am ${age}`}</p>}
 
-      <p className="Person__partner">{personPartnerText}</p>
+      <p className="Person__partner">
+        {personPartnerText}
+      </p>
     </section>
   );
-};
-
-Person.defaultProps = {
-  age: undefined,
-  partnerName: undefined,
-};
-
-Person.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number,
-  sex: PropTypes.string.isRequired,
-  isMarried: PropTypes.bool.isRequired,
-  partnerName: PropTypes.string,
 };
