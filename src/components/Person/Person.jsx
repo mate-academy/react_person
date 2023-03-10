7b7941c44d6029cd2e6b9733500fe267f705609a
Mark Babich <morak.bobama@gmail.com>
2023-03-10 16:20:09 +0200

    Person task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,36 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+  const partnerSex = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const partnerInfo = isMarried
+    ? `${partnerName} is my ${partnerSex}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {partnerInfo}
+      </p>
+    </section>
+  );
+}
