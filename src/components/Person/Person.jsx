d87b1e7498c41446e63b8e355bbc844f841c1a57
Roman Kvas <naruto.yzymaki4@gmail.com>
2023-03-11 13:13:34 +0100

    Fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const partner = person.sex === 'f'
+    ? 'husband'
+    : 'wife';
 
-    {person.age > 0
-      && <p className="Person__age">{`I am ${person.age}`}</p>
-    }
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-    {person.isMarried === true
-      ? <p className="Person__partner">{`${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`}</p>
-      : <p className="Person__partner">I am not married</p>
-    }
-  </section>
-);
+      {person.age > 0
+        && <p className="Person__age">{`I am ${person.age}`}</p>
+      }
+
+      {person.isMarried === true
+        ? <p className="Person__partner">{`${person.partnerName} is my ${partner}`}</p>
+        : <p className="Person__partner">I am not married</p>
+      }
+    </section>
+  );
+};
 
 Person.propTypes = {
   person: PropTypes.shape({
