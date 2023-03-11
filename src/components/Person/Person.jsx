32fcecead7b992ff9e8c5ae5167e5221ed0f3716
Roman Kvas <naruto.yzymaki4@gmail.com>
2023-03-11 11:07:01 +0100

    Practice solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
+    {person.age > 0
+      && <p className="Person__age">{`I am ${person.age}`}</p>
+    }
+
+    {person.isMarried === true
+      ? <p className="Person__partner">{`${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`}</p>
+      : <p className="Person__partner">I am not married</p>
+    }
+  </section>
+);
+
+Person.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string,
+    sex: PropTypes.string,
+    age: PropTypes.number,
+    isMarried: PropTypes.bool,
+    partnerName: PropTypes.string,
+  }).isRequired,
+};
