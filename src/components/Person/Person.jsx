9b4e1041f4bea1a54cc87b08f0bffaeaa83a6c03
andrej <andersontwentytwo@gmail.com>
2023-03-13 17:49:14 +0200

    Replaced && || condition with ternary, codestyle

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,16 +4,18 @@ import './Person.scss';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partnerSex = (sex === 'm' && 'wife') || 'husband';
+  const partnerSex = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age && (
         <p className="Person__age">{`I am ${age}`}</p>
       )}
+
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${partnerSex}`
