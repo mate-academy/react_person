c60c9be22b5f79f9ded240fa870538ba3c8c0ac5
Kateryna Babina <katerynababina01@gmail.com>
2023-03-13 09:23:37 +0200

    fixed

--- src/components/Person/Person/Person.jsx
+++ src/components/Person/Person/Person.jsx
@@ -19,14 +19,14 @@ export const Person = ({ person }) => {
     : `I am not married`;
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
-        {age && (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {age && (
         <p className="Person__age">{`I am ${age}`}</p>
-        )}
-        <p className="Person__partner">{isUserMarried}</p>
-      </section>
-    </>
+      )}
+
+      <p className="Person__partner">{isUserMarried}</p>
+    </section>
   );
 };
