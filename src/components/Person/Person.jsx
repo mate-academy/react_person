228209489430f4db6d3846c612b8f53134b201ac
Denys Dubas <den.dubas.44@gmail.com>
2023-03-13 12:13:17 +0200

    added src

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,37 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const maried = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const isPartner = isMarried
+    ? `${partnerName} is my ${maried}`
+    : `I am not married`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isPartner}
+      </p>
+    </section>
+  );
+}
