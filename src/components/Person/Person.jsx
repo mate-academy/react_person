48d60cb91977e7250c2646aa686abf77e48d2298
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-13 13:32:18 +0100

    done task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({
+  name, age, sex, isMarried, partnerName,
+}) => {
+  const haveAge = age && `I am ${age}`;
+  const whatsSex = sex === 'f'
+    ? 'wife'
+    : 'husband';
+
+  const havePartner = isMarried
+    ? `${partnerName} is my ${whatsSex}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      <p className="Person__age">
+        {haveAge}
+      </p>
+      <p className="Person__partner">
+        {havePartner}
+      </p>
+    </section>
+  );
+};
