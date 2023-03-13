5fcb031352be1eb4dfd76da2ff5c7bdb73b3f549
Olecsiy <infernoregent@gmail.com>
2023-03-13 11:58:03 +0200

    sol1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age = null,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const ageInfo = age !== null
+    ? <p className="Person__age">{`I am ${age}`}</p>
+    : null;
+
+  const partnerPerson = sex === 'f'
+    ? 'husband'
+    : 'wife';
+
+  const marriage = isMarried
+    ? <p className="Person__partner">{`${partnerName} is my ${partnerPerson}`}</p>
+    : <p className="Person__partner">I am not married</p>;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {ageInfo}
+      {marriage}
+    </section>
+  );
+};
