b92595e2167620c1ecdcce42c43cd00b221fbc2c
Степан <stepan12@MacBook-Air-Stepan.local>
2023-03-13 12:52:15 +0200

    person done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = (props) => {
+  const { person } = props;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const partner = isMarried
+    ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+    : 'I am not married';
+
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
+        <p className="Person__partner">{partner}</p>
+      </section>
+    </>
+  );
+};
