963ca9513c711ffeb02b527a85f19ee20aa7580c
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-03-14 13:00:46 +0200

    Was fixed some issues

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
   const possiblePartner = sex === 'm' ? 'wife' : 'husband';
-  const familyStatus = isMarried ? (
-    <p className="Person__partner">{`${partnerName} is my ${possiblePartner}`}</p>
-  ) : (
-    <p className="Person__partner">I am not married</p>
-  );
 
   return (
     <>
       <section className="Person">
         <h2 className="Person__name">{`My name is ${name}`}</h2>
+
         {age && <p className="Person__age">{`I am ${age}`}</p>}
-        {familyStatus}
+
+        <p className="Person__partner">{isMarried ? `${partnerName} is my ${possiblePartner}` : ('I am not married') }</p>
       </section>
     </>
   );
