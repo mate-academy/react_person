968b6cd531e64af842155f0c11473b5c03db5129
Sasha Kononenko <nurchiksasha@gmail.com>
2023-03-14 12:58:49 +0200

    fied mistakes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
   const partner = sex === 'f'
     ? 'husband'
     : 'wife';
@@ -11,20 +16,15 @@ export const Person = ({ person }) => {
     ? `${partnerName} is my ${partner}`
     : 'I am not married';
 
+  const personAge = age === person.age
+    ? <p className="Person__age">{`I am ${age}`}</p>
+    : undefined;
+
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      {personAge}
       <p className="Person__partner">{married}</p>
     </section>
   );
-};
-
-Person.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-  sex: PropTypes.string.isRequired,
-  isMarried: PropTypes.bool.isRequired,
-  partnerName: PropTypes.string.isRequired,
-
 };
