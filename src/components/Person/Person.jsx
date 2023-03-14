630c33b85964e40d18ce2ffef96818242a8aa278
Maindloll <valeriy.mikheyev@gmail.com>
2023-03-14 13:48:02 +0200

    fixed test problems

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,10 +8,10 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
       {person.age && (
-         <p className="Person__age">{`I am ${person.age}`}</p>
+      <p className="Person__age">{`I am ${person.age}`}</p>
       )}
 
-      <p className="Person__partner">{person.partnerName ? `${person.partnerName} is my ${partnerSex}` : 'I am not married'}</p>
+      <p className="Person__partner">{person.isMarried ? `${person.partnerName} is my ${partnerSex}` : 'I am not married'}</p>
     </section>
   );
 };
