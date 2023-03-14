ceb3aa7db34787a825ed13ce3223277cb1b0c508
Maindloll <valeriy.mikheyev@gmail.com>
2023-03-14 11:25:49 +0200

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const partnerSex = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <p className="Person__age">{person.age ? `I am ${person.age}` : ''}</p>
+      <p className="Person__partner">{person.partnerName ? `${person.partnerName} is my ${partnerSex}` : 'I am not married'}</p>
+    </section>
+  );
+};
