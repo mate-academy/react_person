8135eb0dd1d528dcaa20507c18fa298e4415259d
Alex Nastychenko <nastychenko1993@gmail.com>
2023-03-15 15:18:08 +0200

    fix name and age

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,10 +12,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {name}
+        {`My name is ${name}`}
       </h2>
-      <p className="Person__age">{age && `I am ${{ age }}`}</p>
+      <p className="Person__age">{age && `I am ${age}`}</p>
       <p className="Person__partner">
         {isMarried ? (`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`)
           : 'I am not married'}
