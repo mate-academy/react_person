9c7467f4cd695922aaad4721ca1ba95290ed76fd
Степан <stepan12@MacBook-Air-Stepan.local>
2023-03-15 17:09:23 +0200

    fixed comments

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,17 +10,29 @@ export const Person = (props) => {
     partnerName,
   } = person;
 
+  const partnerStatus = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
   const partner = isMarried
-    ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+    ? `${partnerName} is my ${partnerStatus}`
     : 'I am not married';
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
-        {age && <p className="Person__age">{`I am ${age}`}</p>}
-        <p className="Person__partner">{partner}</p>
-      </section>
-    </>
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {partner}
+      </p>
+    </section>
   );
 };
