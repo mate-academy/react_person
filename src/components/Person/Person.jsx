e65168a4dc266afbb5146c0bf7fa5db5da29e9fe
NikitaStambul <stambul.n@gmail.com>
2023-03-15 13:58:53 +0200

    code fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,15 +12,15 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
       <p className="Person__age">
-        {
-          age
-            ? `I am ${age}`
-            : null
-        }
+        {age && `I am ${age}`}
       </p>
-      <p className="Person__partner">{partnerFieldText}</p>
+      <p className="Person__partner">
+        {partnerFieldText}
+      </p>
     </section>
   );
 };
