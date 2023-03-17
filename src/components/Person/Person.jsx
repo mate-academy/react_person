3bc39d963eb024167cd10d9af6008fc05edb1724
Anton-Tretiak <tretiak.ap@gmail.com>
2023-03-17 17:33:42 +0200

    fixes after mentor code review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
   const personPartner = sex === 'm'
     ? 'wife'
@@ -13,13 +19,19 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
-      {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
+      {age > 0 && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
       )}
 
-      <p className="Person__partner">{personMaried}</p>
+      <p className="Person__partner">
+        {personMaried}
+      </p>
     </section>
   );
 };
