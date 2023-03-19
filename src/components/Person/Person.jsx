e41b2c49c45f1a1efc41beb75eb6e449570a84e3
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-03-19 22:48:56 +0000

    the task was solved

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,22 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-nested-ternary */
+import React from 'react';
+
+export const Person = ({ person }) => (
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is {person.name}
+      </h2>
+      {person.age
+      && <p className="Person__age">I am {person.age}</p>}
+      <p className="Person__partner">
+        {person.isMarried
+          ? (person.sex === 'f' ? `${person.partnerName} is my husband`
+            : `${person.partnerName} is my wife`)
+          : `I am not married`
+        }
+      </p>
+    </section>
+  </div>
+);
