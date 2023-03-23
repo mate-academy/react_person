acea9f1feca4921007cee09ebfcb5c356a5ddf8c
Arthur Krepko <krepko2003krepko@gmail.com>
2023-03-23 16:07:19 +0200

    reject considered

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,7 +13,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && (
+      {(age > 0) && (
         <p className="Person__age">{`I am ${age}`}</p>
       )}
       {(isMarried === false) && (
