6ee6a3560651c33b498e2de24e5aa33e5b3cd8b2
Sasha Dranuta <dranuta97@gmail.com>
2023-03-23 16:32:18 +0200

    Formatting was fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -15,13 +15,18 @@ export function Person({ person }) {
 
   const isPersonMarried = isMarried
     ? `${partnerName} is my ${partner}`
-    : `I am not married`;
+    : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
       {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
       )}
 
       <p className="Person__partner">
