bb20060aba0186f8e16f089ad3ae4c1b6f518eea
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-03-25 16:25:39 +0200

    Update

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,36 @@
+import React from 'react';
 // export const Person = ({ person }) => ();
+export function Person({ person }) {
+  let marriedText;
+
+  if (person.sex === 'm' && person.isMarried) {
+    marriedText = `${person.partnerName} is my wife`;
+  }
+
+  if (person.isMarried === false) {
+    marriedText = `Im not married`;
+  }
+
+  if (person.sex === 'f' && person.isMarried) {
+    marriedText = `${person.partnerName} is my husband`;
+  }
+
+  return (
+    <>
+      <h2 className="Person__name">
+        {`My name is `}
+        {person.name}
+      </h2>
+      {person.age ? (
+        <p className="Person__age">
+          I am
+          {' '}
+          {person.age}
+        </p>
+      ) : ''}
+      <p className="Person__partner">
+        {marriedText}
+      </p>
+    </>
+  );
+}
