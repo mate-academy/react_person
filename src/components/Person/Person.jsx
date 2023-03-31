67d03eb9f5021879524cbeae584a124f5dba8718
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-31 14:45:30 +0200

    task complete

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const partnerSex = person.sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const age = person.age
+    ? <p className="Person__age">{`I am ${person.age}`}</p>
+    : null;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {age}
+      <p className="Person__partner">
+        {person.isMarried
+          ? `${person.partnerName} is my ${partnerSex}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
