7ac22a3b45190cbc4b1309aa8e9b52b45b0e492a
Vadym Dubniak <dubnyakv@gmail.com>
2023-04-04 19:31:08 +0300

    added component person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried = false, partnerName } = person;
+  const partnerSex = sex === 'm' ? 'wife' : 'husband';
+  const partnerInfo = isMarried ? `${partnerName} is my ${partnerSex}` : 'I am not married';
+  const agePart = age
+    ? (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+    )
+    : null;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {agePart}
+      <p className="Person__partner">{partnerInfo}</p>
+    </section>
+  );
+};
