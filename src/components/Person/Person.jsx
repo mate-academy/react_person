8b40672a519f13f2a126d17bc6ff8aa0a5d0f3d9
Vlad <bojko.vladislav16@gmail.com>
2023-04-08 10:00:04 +0300

    added destructuring, changed condition

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, partnerName, isMarried } = person;
 
-    <p className="Person__age">
-      {person.age ? `I am ${person.age}` : null}
-    </p>
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my
-        ${person.sex === 'm'
-          ? 'wife'
-          : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+      <p className="Person__age">
+        {age && `I am ${age}`}
+      </p>
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my
+          ${sex === 'm'
+            ? 'wife'
+            : 'husband'}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
