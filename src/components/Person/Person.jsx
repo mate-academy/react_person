50386c9d3bcc0cf49b783cfd497ebc7ddf2485a1
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-04-08 16:21:49 +0300

    function react_person works

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, ifMarrid = false, partnerName } = person;
+
+  const partnerSex = sex === 'm'
+    ? 'wife'
+    : 'husbend';
+
+  const personMarrid = ifMarrid
+    ? `${partnerName} is my ${partnerSex}`
+    : 'I am not married';
+
+  const personAge = age
+    ? (
+      <p classNmae="Person__age">
+        {`I am ${age}`}
+      </p>
+    )
+    : null;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {personAge}
+
+      <p className="Person__partner">
+        {personMarrid}
+      </p>
+    </section>
+  );
+};
