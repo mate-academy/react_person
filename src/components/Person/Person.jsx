c76fd6e6dc19b8934a7c7989dde831a74dba3844
Andrii <remote.makandriival@gmail.com>
2023-04-09 13:02:50 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-export const Person = ({
-  name,
-  age,
-  sex,
-  isMarried,
-  partnerName,
-}) => {
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
   const isAge = age && `I am ${age}`;
+
   const isSex = sex === 'm'
     ? 'wife'
     : 'husband';
+
   const isPartner = isMarried
     ? `${partnerName} is my ${isSex}`
     : `I am not married`;
