198f69bf77d4c1ec4b695d948dea32c5ec53e7c3
Oleh Kolodii <bigknife4@gmail.com>
2023-04-10 17:36:19 +0300

    fixes after mentor review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,30 +3,28 @@ import React from 'react';
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const PersonAge = () => (
-    age
-      ? <p className="Person__age">{`I am ${age}`}</p>
-      : ''
+  const personAge = age && <p className="Person__age">{`I am ${age}`}</p>;
+  const partner = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const personParthner = (
+    <p className="Person__partner">{`${partnerName} is my ${partner}`}</p>
   );
 
-  const PersonParthner = () => (
-    <p className="Person__partner">
-      {`${partnerName} is my `}
-      {sex === 'm' ? 'wife' : 'husband'}
-    </p>
-  );
-
-  const UnmarriedPerson = () => (
+  const unmarriedPerson = (
     <p className="Person__partner">I am not married</p>
   );
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <PersonAge />
+
+      {personAge}
+
       {isMarried
-        ? <PersonParthner />
-        : <UnmarriedPerson />}
+        ? personParthner
+        : unmarriedPerson }
     </section>
   );
 };
