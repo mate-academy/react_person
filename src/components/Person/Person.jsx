11a125916face3518055c5412d6b702463317984
Hamuud <artemlask2014@gmail.com>
2023-04-10 18:05:28 +0300

    Create person list

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const isGender = sex === 'f'
+    ? 'husband'
+    : 'wife';
+
+  const isAge = age
+    ? <p className="Person__age">{`I am ${age}`}</p>
+    : null;
+
+  const isPartner = isMarried
+    ? `${partnerName} is my ${isGender}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {isAge}
+      <p className="Person__partner">
+        {isPartner}
+      </p>
+    </section>
+  );
+};
