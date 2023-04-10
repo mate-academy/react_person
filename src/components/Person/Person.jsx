39bfbd95b2321311520c8206e159a4da997f5e33
Valery Sazonov <vksazonov@gmail.com>
2023-04-10 20:29:14 +0300

    Destructurization of person was added

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const partnerPosition = person.sex === 'm' ? 'wife' : 'husband';
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+  const partnerPosition = sex === 'm'
+    ? 'wife'
+    : 'husband';
 
   return (
     <>
       <section className="Person">
         <h2 className="Person__name">
-          {`My name is ${person.name}`}
+          {`My name is ${name}`}
         </h2>
 
         {person.age && (
           <p className="Person__age">
-            {`I am ${person.age}`}
+            {`I am ${age}`}
           </p>
         )}
 
         <p className="Person__partner">
-          {person.isMarried ? (
-            `${person.partnerName} is my ${partnerPosition}`
+          {isMarried ? (
+            `${partnerName} is my ${partnerPosition}`
           ) : (
             `I am not married`
           )}
