0466bd5d1c96afea00a546421b221d2d73906f65
glots-a <124785462+glots-a@users.noreply.github.com>
2023-04-10 17:57:41 +0300

    adited code style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,9 +14,11 @@ export const Person = (props) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+      
       {age && (
         <p className="Person__age">{`I am ${age}`}</p>
       )}
+      
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${partner}`
