4c37042024e2e40731a9c522fa9c0f5581867405
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-04-11 00:13:58 +0300

    fix code style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,11 +7,13 @@ function Person({ person }) {
         <h2 className="Person__name">
           {`My name is ${person.name}`}
         </h2>
+
         {person.age && (
         <p className="Person__age">
           {`I am ${person.age}`}
         </p>
         )}
+
         <p className="Person__partner">
           {person.isMarried
             ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
