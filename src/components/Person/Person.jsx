dc122dc7f6337aa5e5c39e9298d4e096d7163eb2
Nazarii Chepil <nazzikk1998@gmail.com>
2023-04-11 17:56:23 +0300

    refactored

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,21 +14,23 @@ export const Person = ({ person }) => {
     : 'husband';
 
   return (
-    <>
+    <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age && (
       <p className="Person__age">
         {`I am ${age}`}
       </p>
       )}
+
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${personStatus}`
           : 'I am not married'
         }
       </p>
-    </>
+    </section>
   );
 };
