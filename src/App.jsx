20e042bddad10d168a4e877a4722ee64fd49b6e9
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-04-11 18:28:32 +0300

    Code style improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-
 import { Person } from './components/Person';
 
 export const misha = {
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
   const spouse = sex === 'm'
     ? 'wife'
@@ -11,12 +17,10 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age ? (
+      {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
-      ) : (
-        null
       )}
 
       <p className="Person__partner">
