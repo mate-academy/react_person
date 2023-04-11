b369956e516f4bb55d99c2f888ceee37b5657426
Mys Yaroslav <gotluck13@gmail.com>
2023-04-11 15:15:34 +0300

    corrected solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,43 +2,17 @@ import React from 'react';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  let partner;
 
-  if (sex === 'm') {
-    partner = 'wife';
-  } else {
-    partner = 'husband';
-  }
-
-  if (age) {
-    return (isMarried
-      ? (
-        <>
-          <h2>{`My name is ${name}`}</h2>
-          <p>{`I am ${age}`}</p>
-          <p>{`${partnerName} is my ${partner}`}</p>
-        </>
-      )
-      : (
-        <>
-          <h2>{`My name is ${name}`}</h2>
-          <p>{`I am ${age}`}</p>
-          <p>I am not married</p>
-        </>
-      ));
-  }
-
-  return (isMarried
-    ? (
-      <>
-        <h2>{`My name is ${name}`}</h2>
-        <p>{`${partnerName} is my ${partner}`}</p>
-      </>
-    )
-    : (
-      <>
-        <h2>{`My name is ${name}`}</h2>
-        <p>I am not married</p>
-      </>
-    ));
+  return (
+    <>
+      <h2>{`My name is ${name}`}</h2>
+      {age && (<p>{`I am ${age}`}</p>)}
+      <p>
+        {isMarried
+          ? (`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`)
+          : (`I am not married`)
+        }
+      </p>
+    </>
+  );
 };
