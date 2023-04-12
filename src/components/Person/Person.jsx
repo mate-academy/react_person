20faa3915cc48299b230f44031cb1400196b1b75
Maksym Didyk <didyk.maksym@gmail.com>
2023-04-12 13:08:14 +0300

    changed ternary  to &&

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -18,13 +18,11 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {age
-        ? (
-          <p className="Person__age">
-            {`I am ${age}`}
-          </p>
-        )
-        : null
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>git 
+      )
       }
       <p className="Person__partner">
         {isMarried
