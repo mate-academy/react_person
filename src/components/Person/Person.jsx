7891f73ae7c44caca2ea9551caaa7f3c96059b4e
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-04-12 11:59:05 +0300

    Code style improvement

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -16,7 +16,10 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
       {age && (
         <p className="Person__age">
           {`I am ${age}`}
