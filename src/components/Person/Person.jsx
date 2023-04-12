9f7ad5665164bc91208d3cf24862f493e06218eb
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-04-12 23:39:29 +0300

    React person task is done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const personPartner = person.sex === 'm'
+    ? <p className="Person__partner">{`${person.partnerName} is my wife`}</p>
+    : <p className="Person__partner">Maksym is my husband</p>;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      { person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+      {person.isMarried
+        ? personPartner
+        : <p className="Person__partner">I am not married</p>
+      }
+    </section>
+  );
+};
