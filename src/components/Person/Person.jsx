725f1e8ab71a68499c145eba1ff1c2624f00aee1
kukla1989 <kukla.1989@ukr.net>
2023-04-23 14:57:27 +0300

    improve code style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,23 +9,24 @@ export function Person({ person: {
   isMarried,
   partnerName,
 } }) {
-  let marriage;
-
   const partner = sex === 'm'
     ? 'wife'
     : 'husband';
 
-  if (isMarried) {
-    marriage = `${partnerName} is my ${partner}`;
-  } else {
-    marriage = 'I am not married';
-  }
-
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && <p className="Person__age">{age ? `I am ${age}` : ''}</p> }
-      <p className="Person__partner">{marriage}</p>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && <p className="Person__age">{age && `I am ${age}`}</p> }
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'
+        }
+      </p>
     </section>
   );
 }
