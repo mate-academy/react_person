6057bb22952e33d7d7fec0cd57b721f7f3ebc964
AnastassiaH <nastyagurska@gmail.com>
2023-04-24 16:00:32 +0300

    optimize code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName, sex } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age
         && (
           <p className="Person__age">
-            {`I am ${person.age}`}
+            {`I am ${age}`}
           </p>
         )
       }
-    <p className="Person__partner">
-      {person.isMarried ? (
-        `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-      ) : 'I am not married'}
-    </p>
-  </section>
-);
+      <p className="Person__partner">
+        {isMarried ? (
+          `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+        ) : 'I am not married'}
+      </p>
+    </section>
+  );
+};
