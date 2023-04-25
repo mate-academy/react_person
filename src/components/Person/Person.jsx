4d7a4a416e89a55dc4ddd2a96dd2f6c0967d8126
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-25 18:39:13 +0300

    Done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,33 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person(person) {
+  const { name, age = null, sex, isMarried, partnerName } = person.person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {' '}
+        {name}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          I am
+          {' '}
+          {age}
+        </p>
+      )
+      }
+      {isMarried ? (
+        <p className="Person__partner">
+          {partnerName}
+          {' '}
+          is my
+          {' '}
+          {sex === 'f' ? <>husband</> : <>wife</>}
+        </p>
+      )
+        : (<p className="Person__partner">I am not married</p>)}
+    </section>
+  );
+}
