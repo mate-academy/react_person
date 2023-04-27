68d62ad63a10cf134f899c784f204b54519f3692
Mikołaj <ostermi@wp.pl>
2023-04-27 09:17:20 +0100

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    { person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <p className="Person__partner">{!person.isMarried ? 'I am not married' : `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}</p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName, sex } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      { age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">{`${!isMarried ? 'I am not married' : `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}`}</p>
+    </section>
+  );
+};
