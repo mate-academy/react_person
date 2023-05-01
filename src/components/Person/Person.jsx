98e34a24e4fd2acf0c5cbafa0d39f5519aa2d816
Pavlo-Petrashevskyi <119119324+Pavlo-Petrashevskyi@users.noreply.github.com>
2023-05-01 21:39:16 +0300

    Update src/components/Person/Person.jsx
    Co-authored-by: Viktor-Kost <117587463+Viktor-Kost@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName , sex} = person
+  
+  return (<section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
     {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
     <p className="Person__partner">
