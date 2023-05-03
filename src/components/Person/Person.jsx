c2e1ea4edd6b27748b4c9dc89ff3ce12f4466bbf
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-05-03 20:56:39 +0300

    all tests good

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age ? (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      ) : (null)
+      }
+      <p className="Person__partner">
+        {person.isMarried ? `${person.partnerName} is ${person.sex === 'f' ? 'my husband' : 'my wife'}` : `I am not married`}
+      </p>
+    </section>
+  );
+}
