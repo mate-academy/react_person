f00ab768e258b035481ad06e75081beef36115aa
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-05-04 11:11:38 +0200

    fix2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,10 +9,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age
-        ? <p className="Person__age">{`I am ${age}`}</p>
-        : null
-      }
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
       {isMarried
         ? <p className="Person__partner">{`${partnerName} is my ${married}`}</p>
         : <p className="Person__partner">I am not married</p>
