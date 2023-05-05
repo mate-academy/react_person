239cd6792f5048cf5aa661325702d87f3de1859e
Nadiein Oleh <olegnadein1994@gmail.com>
2023-05-05 09:57:53 +0300

    Refactoting the code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import './Person.scss';
+import './person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age
-      ? <p className="Person__age">{`I am ${person.age}`}</p>
-      : null
+export const Person = ({ person }) => {
+  const isMarried = (personStatus) => {
+    if (!personStatus.isMarried) {
+      return 'I am not married';
     }
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName}  is my ${person.sex === 'm'
-          ? `wife`
-          : ' husband'}`
-        : 'I am not married'
-    }
-    </p>
-  </section>
-);
+
+    return personStatus.sex === 'm'
+      ? `${personStatus.partnerName} is my wife`
+      : `${personStatus.partnerName} is my husband`;
+  };
+
+  return (
+    <section className="person">
+      <h2 className="person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age
+        ? <p className="person__age">{`I am ${person.age}`}</p>
+        : null
+      }
+
+      <p className="person__partner">
+        {isMarried(person)}
+      </p>
+    </section>
+  );
+};
