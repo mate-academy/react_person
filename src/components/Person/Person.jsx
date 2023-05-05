f1188eba5c6c27befe2bf4644f7fd99adb9f34f8
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-05-05 11:38:59 +0200

    fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const nameInfo = `My name is ${name}`;
   const ageInfo = age
     ? `I am ${age}`
     : ``;
-  let marriageInfo = '';
+  let marriageInfo = `I am not married`;
 
   if (isMarried) {
     const partner = sex === 'f'
@@ -14,18 +13,12 @@ export const Person = ({ person }) => {
       : 'wife';
 
     marriageInfo = `${partnerName} is my ${partner}`;
-  } else {
-    marriageInfo = `I am not married`;
   }
-
-  const ageTag = age
-    ? <p className="Person__age">{ageInfo}</p>
-    : undefined;
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{nameInfo}</h2>
-      {ageTag}
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{ageInfo}</p>}
       <p className="Person__partner">{marriageInfo}</p>
     </section>
   );
