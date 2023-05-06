527988746f774cd38024c81459794c8b8f1e0f92
Daniel <henio09@wp.pl>
2023-05-06 14:07:28 +0200

    fixed typo

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -19,7 +19,7 @@ export const Person = ({ person }) => {
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${partnerGender}`
-          : 'I am not Maried'}
+          : 'I am not married'}
       </p>
     </section>
   );
