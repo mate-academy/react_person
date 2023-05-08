a573f69821e8728abfa6292bcc456b6551636490
Nadiein Oleh <olegnadein1994@gmail.com>
2023-05-08 09:46:30 +0300

    fixed test

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,15 +13,15 @@ export const Person = ({ person }) => {
   };
 
   return (
-    <section className="person">
-      <h2 className="person__name">
+    <section className="Person">
+      <h2 className="Person__name">
         {`My name is ${person.name}`}
       </h2>
 
-      {person.age && <p className="person__age">{`I am ${person.age}`}</p>
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>
       }
 
-      <p className="person__partner">
+      <p className="Person__partner">
         {isMarried(person)}
       </p>
     </section>
