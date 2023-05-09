af5850c67342e30a92c3c18f51ae2cf296afb8fa
Nazar Vorona <voron1765@gmail.com>
2023-05-10 01:06:56 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,33 +4,33 @@ import React from 'react';
 // eslint-disable-next-line react/jsx-one-expression-per-line
 
 export const Person = ({ person: {
-  name, age, sex, isMarried, partnerName,
-} }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      My name is {name}
-    </h2>
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+} }) => {
+  const gender = sex;
 
-    {age ? (
-      <p className="Person__age">
-        I am {age}
-      </p>
-    ) : null}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is {name}
+      </h2>
 
-    {sex === 'm' && isMarried ? (
-      <p className="Person__partner">
-        {partnerName} is my wife
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
+      {age && (
+        <p className="Person__age">
+          I am {age}
+        </p>
+      )}
 
-    {sex === 'f' && isMarried ? (
-      <p className="Person__partner">
-        {partnerName} is my husband
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+      {sex && isMarried ? (
+        <p className="Person__partner">
+          {partnerName} is my {gender === 'm' ? 'wife' : 'husband'}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
