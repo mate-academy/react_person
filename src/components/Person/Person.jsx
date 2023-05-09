7c1630c6559a522ac6ef8d8bf7e68bd7ec3e8987
Irina Kulish <irinakulish20@gmail.com>
2023-05-09 10:06:46 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,9 +12,7 @@ export const Person = ({ person }) => {
         <h2 className="Person__name">{`My name is ${name}`}</h2>
         {age && (
           <p className="Person__age">
-            I am
-            {' '}
-            {age}
+            {`I am ${age}`}
           </p>
         )}
         <p className="Person__partner">
