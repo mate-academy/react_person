3ca312c133638213e825395855acd61c3147effe
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-05-10 11:46:39 +0300

    check on partner was moved into variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const personPartner = person.sex === 'm'
+    ? `${person.partnerName} is my wife`
+    : `${person.partnerName} is my husband`;
 
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried ? personPartner : 'I am not married'}
       </p>
-    )}
-
-    <p className="Person__partner">
-      {person.isMarried || `I am not married`}
-
-      {person.isMarried && (person.sex === 'm'
-        ? `${person.partnerName} is my wife`
-        : `${person.partnerName} is my husband`)
-      }
-    </p>
-  </section>
-);
+    </section>
+  );
+};
