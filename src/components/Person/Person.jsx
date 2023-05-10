3d2f02245ee4dfef92e5109a2c573949433b0721
Anna Grechana <grechanaanna@gmial.com>
2023-05-10 17:13:55 +0300

    1st version

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,42 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  let partner;
+
+  if (person.sex === 'm') {
+    partner = 'wife';
+  } else {
+    partner = 'husband';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {' '}
+        {person.name}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          I am
+          {' '}
+          {person.age}
+        </p>
+      )}
+
+      { person.isMarried ? (
+        <p className="Person__partner">
+          {person.partnerName}
+          {' '}
+          is my
+          {partner}
+        </p>
+      ) : (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      ) }
+    </section>
+  );
+};
