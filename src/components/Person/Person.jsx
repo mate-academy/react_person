6109c2f2f6739d1012e18d69b377b041f51c7f80
Anna Grechana <grechanaanna@gmial.com>
2023-05-10 22:20:02 +0300

    2nd version

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 
-export const Person = ({ person }) => {
-  let partner;
-
-  if (person.sex === 'm') {
-    partner = 'wife';
-  } else {
-    partner = 'husband';
-  }
+export const Person = ({ name, age, sex, isMarried, partnerName }) => {
+  const partner = (sex === 'm') ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
         My name is
         {' '}
-        {person.name}
+        {name}
       </h2>
 
-      {person.age && (
+      {age && (
         <p className="Person__age">
           I am
           {' '}
-          {person.age}
+          {age}
         </p>
       )}
 
-      { person.isMarried ? (
+      { isMarried ? (
         <p className="Person__partner">
-          {person.partnerName}
+          {partnerName}
           {' '}
           is my
           {' '}
