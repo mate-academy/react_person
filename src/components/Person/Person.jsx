9376398249a6aa809bd77b88a553f3eeab81f830
Serhii Lysenko <diom34@gmail.com>
2023-05-11 14:50:41 +0300

    changed conditional rendering

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,12 +9,10 @@ export const Person = ({ person }) => {
         {`My name is ${person.name}`}
       </h2>
 
-      {person.age ? (
+      {person.age && (
         <p className="Person__age">
           {`I am ${person.age}`}
         </p>
-      ) : (
-        null
       )}
 
       <p className="Person__partner">
