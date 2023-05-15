6a78dd4a0a43f764f2dfe9db6498676eabd27b9d
Mateusz Michalowski <mateusz.michalowski000@gmail.com>
2023-05-15 13:43:40 +0200

    Solution v2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const marriedPrefix = sex === 'm' ? ' is my wife' : ' is my husband';
-  const marriedSuffix = isMarried ? partnerName + marriedPrefix : `I am not married`;
+  const marriedPartnerType = sex === 'm' ? 'wife' : 'husband';
+  const marriedText = isMarried ? `${partnerName} is my ${marriedPartnerType}` : `I am not married`;
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
-      <p className="Person__partner">{marriedSuffix}</p>
+      <p className="Person__partner">{marriedText}</p>
     </section>
   );
 };
