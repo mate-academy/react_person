63e181bdbf1355e34dddbfb692ddcb3b2fd9814e
Denys Didenko <ddidenko441@gmail.com>
2023-05-19 16:29:15 +0300

    fix mistakes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,42 +1,35 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const personPartner = person.sex === 'm'
+    ? `${person.partnerName} is my wife`
+    : `${person.partnerName} is my husband`;
 
-    {person.age
-      && (
-        <p className="Person__age">
-          {`I am ${person.age}`}
-        </p>
-      )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
 
-    {person.isMarried
-      && person.sex === 'f'
-      && (
-        <p className="Person__partner">
-          {`${person.partnerName} is my husband`}
-        </p>
-      )
-    }
+      {person.age
+        && (
+          <p className="Person__age">
+            {`I am ${person.age}`}
+          </p>
+        )}
 
-    {person.isMarried
-      && person.sex === 'm'
-      && (
-        <p className="Person__partner">
-          {`${person.partnerName} is my wife`}
-        </p>
-      )
-    }
-
-    {!person.isMarried
-      && (
-        <p className="Person__partner">
-          I am not married
-        </p>
-      )
-    }
-  </section>
-);
+      {person.isMarried
+        ? (
+          <p>
+            {personPartner}
+          </p>
+        )
+        : (
+          <p>
+            I am not married
+          </p>
+        )
+      }
+    </section>
+  );
+};
