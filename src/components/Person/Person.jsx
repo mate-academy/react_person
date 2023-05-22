cc96c92433b49aa4893e83c6147a011b4f31f7e6
Victor Buldenko <gookidoo@gmail.com>
2023-05-22 17:45:36 +0300

    <Person {...misha} /> === <Person person={misha} /> if remove deep destruct props

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({
+  person: { name, age, sex, isMarried, partnerName } = {},
+}) {
+  let msg = '';
+
+  if (!isMarried) {
+    msg = `I am not married`;
+  } else if (sex === 'f') {
+    msg = `${partnerName} is my husband`;
+  } else {
+    msg = `${partnerName} is my wife`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age ? <p className="Person__age">{`I am ${age}`}</p> : null}
+      <p className="Person__partner">{msg}</p>
+    </section>
+  );
+}
