44ff7843760b4eda214c6f3f9ea46e1a3d308e18
Victor Buldenko <gookidoo@gmail.com>
2023-05-22 22:44:45 +0300

    make function

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,21 +3,25 @@ import React from 'react';
 export function Person({
   person: { name, age, sex, isMarried, partnerName } = {},
 }) {
-  let msg = '';
+  function maritalStatusMessage() {
+    let text = '';
 
-  if (!isMarried) {
-    msg = `I am not married`;
-  } else if (sex === 'f') {
-    msg = `${partnerName} is my husband`;
-  } else {
-    msg = `${partnerName} is my wife`;
+    if (!isMarried) {
+      text = `I am not married`;
+    } else if (sex === 'f') {
+      text = `${partnerName} is my husband`;
+    } else {
+      text = `${partnerName} is my wife`;
+    }
+
+    return text;
   }
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age ? <p className="Person__age">{`I am ${age}`}</p> : null}
-      <p className="Person__partner">{msg}</p>
+      <p className="Person__partner">{maritalStatusMessage()}</p>
     </section>
   );
 }
