e2adaaf8f9f48d431893716775d3beb934da75f2
Maksym Nemera <nickmitchell@ukr.net>
2023-05-27 19:21:21 +0300

    fixes codestyle

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
   const spouse = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
       {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
       )}
+
       <p className="Person__partner">
-        {isMarried ? (
-          `${partnerName} is my ${spouse}`
-        ) : 'I am not married'
-        }
+        {isMarried
+          ? (
+            `${partnerName} is my ${spouse}`
+          ) : (
+            'I am not married'
+          )}
       </p>
     </section>
   );
