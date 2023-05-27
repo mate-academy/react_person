fe21802675681bd31bcebf14e11859f0c7dddede
Vladyslav Kucher <vladkucher75755@gmail.com>
2023-05-26 22:08:56 -0400

    done first try

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  return (
+    <>
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age ? (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      ) : null}
+      <p className="Person__partner">{person.partnerName ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}` : `I am not married`}</p>
+    </>
+  );
+}
