e29fa8e4a09f21fe67de151b95a4143a87a1eb7c
AlexKherson <q0992358472@gmail.com>
2023-05-31 00:16:12 +0300

    added person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+export function Person({ person }) {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      { isMarried
+        ? (
+          <p className="Person__partner">
+            {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+          </p>
+        )
+        : (<p className="Person__partner">I am not married</p>) }
+    </section>
+  );
+}
