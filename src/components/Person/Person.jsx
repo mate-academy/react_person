87138881a4377403aae5a2222dd79e0539729bb0
Emil Sukiennik <ewilemil@gmail.com>
2023-05-31 23:20:03 +0200

    initial commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = (props) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = props.person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+      <p className="Person__partner">
+        {!isMarried && ('I am not married')}
+        {(isMarried && sex === 'f') && (`${partnerName} is my husband`)}
+        {(isMarried && sex === 'm') && (`${partnerName} is my wife`)}
+      </p>
+    </section>
+  );
+};
