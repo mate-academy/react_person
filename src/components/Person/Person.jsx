b4616d32256863a2129659a25185973bfca4faa1
Vladyslav Kucher <vladkucher75755@gmail.com>
2023-06-03 16:11:24 -0400

    fixed all

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 export function Person({ person }) {
   const firstValue = 'wife';
   const secondValue = 'husband';
+  const { name, age, partnerName, sex } = person;
+  const partner = sex === 'm' ? firstValue : secondValue;
 
   return (
     <>
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
       {person.age
-       && (<p className="Person__age">{`I am ${person.age}`}</p>)
+       && (<p className="Person__age">{`I am ${age}`}</p>)
       }
       <p className="Person__partner">
-        {person.partnerName
-          ? `${person.partnerName} is my ${person.sex === 'm'
-            ? firstValue
-            : secondValue}`
+        {partnerName
+          ? `${partnerName} is my ${partner}`
           : `I am not married`}
       </p>
     </>
