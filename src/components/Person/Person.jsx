79d6868566ddbf0037acf4795f4cec6195f30c14
Vladyslav Kucher <vladkucher75755@gmail.com>
2023-06-02 22:45:10 -0400

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export function Person({ person }) {
+  const firstValue = 'wife';
+  const secondValue = 'husband';
+
   return (
     <>
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age ? (
-        <p className="Person__age">{`I am ${person.age}`}</p>
-      ) : null}
-      <p className="Person__partner">{person.partnerName ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}` : `I am not married`}</p>
+      {person.age
+       && (<p className="Person__age">{`I am ${person.age}`}</p>)
+      }
+      <p className="Person__partner">
+        {person.partnerName
+          ? `${person.partnerName} is my ${person.sex === 'm'
+            ? firstValue
+            : secondValue}`
+          : `I am not married`}
+      </p>
     </>
   );
 }
