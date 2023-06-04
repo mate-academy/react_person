cd18b6a9c7b8c0e0fc086d2c87e2dfba489f0173
Stepan Haida <stepanhaida@gmail.com>
2023-06-04 17:15:36 +0300

    add mui style library

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,3 +1,4 @@
+import { Box, Typography, useTheme } from '@mui/material';
 import React from 'react';
 
 export const Person = ({ person }) => {
@@ -9,21 +10,47 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
+  const theme = useTheme().palette;
+
   return (
-    <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+    <Box
+      className="Person"
+      mb={1}
+      maxWidth={400}
+      backgroundColor={theme.primary.main}
+      borderRadius={2}
+      p={2}
+      color={theme.primary.contrastText}
+    >
+      <Typography
+        className="Person__name"
+        variant="h3"
+        mb={2}
+      >
+        {`My name is ${name}`}
+      </Typography>
 
       {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
+        <Typography mb={2} className="Person__age">{`I am ${age}`}</Typography>
       )}
 
       {isMarried ? (
-        <p className="Person__partner">
+        <Typography
+          className="Person__partner"
+          backgroundColor={theme.info.main}
+          borderRadius={1}
+        >
           {`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}
-        </p>
+        </Typography>
       ) : (
-        <p className="Person__partner">I am not married</p>
+        <Typography
+          className="Person__partner"
+          backgroundColor={theme.error.main}
+          borderRadius={1}
+        >
+          I am not married
+        </Typography>
       )}
-    </section>
+    </Box>
   );
 };
