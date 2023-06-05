2b255004fdbf29a22057b1aa870191cd23f81422
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-06-05 13:56:37 +0300

    made task react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,38 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const personPartner = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const personStatus = isMarried
+    ? `${partnerName} is my ${personPartner}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {personStatus}
+      </p>
+    </section>
+  );
+};
