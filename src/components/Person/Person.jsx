f56be384997c6c6d3b71b5baaad61f79804795f3
AlexKherson <q0992358472@gmail.com>
2023-06-05 19:45:38 +0300

    refactoring: replace isMan to partnerStatus in Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,19 +10,16 @@ export function Person({ person }) {
     partnerName,
   } = person;
 
-  const isMan = sex === 'm';
+  const partnerStatus = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
-      { isMarried
-        ? (
-          <p className="Person__partner">
-            {partnerName} is my {isMan ? 'wife' : 'husband'}
-          </p>
-        )
-        : (<p className="Person__partner">I am not married</p>) }
+      <p className="Person__partner">{ isMarried
+        ? (`${partnerName} is my ${partnerStatus}`)
+        : ('I am not married')}
+      </p>
     </section>
   );
 }
