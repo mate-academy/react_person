d196d3821a006e0016a357035c3f274c643470d7
Vladyslav Kucher <vladkucher75755@gmail.com>
2023-06-04 20:35:31 -0400

    destructured

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-export function Person({ person }) {
+export function Person({ person: { age, partnerName, sex, name } }) {
   const firstValue = 'wife';
   const secondValue = 'husband';
-  const { name, age, partnerName, sex } = person;
   const partner = sex === 'm' ? firstValue : secondValue;
 
   return (
     <>
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {person.age
+      {age
        && (<p className="Person__age">{`I am ${age}`}</p>)
       }
       <p className="Person__partner">
