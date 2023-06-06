d61dffdb468e92e2a05a86ce361b05e7d272a312
Maryan Mats <cholgyni@gmail.com>
2023-06-06 15:52:56 +0300

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,9 +14,14 @@ export function Person({ person }) {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && (<p className="Person__age">I am {age}</p>)}
+      {age && (<p className="Person__age">{`I am ${age}`}</p>)}
 
-      <p className="Person__partner">{isMarried ? `${partnerName} is my ${partnerStatus}` : `I am not married`}</p>
+      <p className="Person__partner">
+        {isMarried
+        ? `${partnerName} is my ${partnerStatus}`
+        : `I am not married`
+        }
+      </p>
     </section>
-  )
-};
+  );
+}
