4683ed3e8929680c77c3d504eb8f089c0e6ab4a4
Maksim Kadygrob <kadygrobmaksim33@gmail.com>
2023-06-06 14:54:22 +0300

    little fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,6 +9,8 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
+  const PartnerRole = sex === 'm' ? 'wife' : 'husband';
+
   return (
     <section className="Person">
       <h2 className="Person__name">
@@ -16,15 +18,17 @@ export const Person = ({ person }) => {
       </h2>
 
       {
-        age
-          ? <p className="Person__age">{`I am ${age}`}</p>
-          : null
+        age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+        )
       }
 
       <p className="Person__partner">
         {
           isMarried
-            ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+            ? `${partnerName} is my ${PartnerRole}`
             : 'I am not married'
         }
       </p>
