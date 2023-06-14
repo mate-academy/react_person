9da1200b294aee6384202a4857f206e090f91f00
Katya Zaremska <unnarummel@gmail.com>
2023-06-14 07:38:06 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 export const Person = ({ person }) => {
   const { name, age, isMarried, partnerName, sex } = person;
+  const partnerRole = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {age
-        && (
-          <p className="Person__age">
-            {`I am ${age}`}
-          </p>
-        )
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )
       }
       <p className="Person__partner">
         {isMarried ? (
-          `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+          `${partnerName} is my ${partnerRole}`
         ) : 'I am not married'}
       </p>
     </section>
