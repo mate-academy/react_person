69779628b2ce45367c81319e063aa7a9bd78f836
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-14 09:53:55 +0300

    ReactPerson

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, isMarried, sex, partnerName } = person;
+
+  let text = 'I am not married';
+
+  if (isMarried === true && sex === 'm') {
+    text = `${partnerName} is my wife`;
+  }
+
+  if (isMarried === true && sex === 'f') {
+    text = `${partnerName} is my husband`;
+  }
+
+  const textAge = age ? <p className="Person__age">{`I am ${age}`}</p> : '';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {textAge}
+      <p className="Person__partner">{text}</p>
+    </section>
+  );
+};
