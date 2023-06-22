d851a17ab7af8afdca154b6d332acddc835ec795
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-06-22 13:46:52 +0300

    person task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    {Object.prototype.hasOwnProperty.call(person, 'age') && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    )}
+    <p className="Person__partner">
+      {person.isMarried === false ? (
+        `I am not married`
+      ) : (
+        `${person.partnerName} is my ${message(person.sex)}`
+      )}
+    </p>
+  </section>
+);
+
+const message = str => (str === 'm' ? 'wife' : 'husband');
