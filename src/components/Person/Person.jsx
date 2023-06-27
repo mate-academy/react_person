b347a1e3c1d8ef277b0b71ab3d2720c29315dee1
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-06-27 20:37:26 +0300

    person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,36 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age = 0,
+    sex,
+    isMarried,
+    partnerName = '',
+  } = person;
+
+  const ageElement = age
+  && <p className="Person__age">{`I am ${age}`}</p>;
+
+  const partnerStatus = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const marriedMessage = isMarried
+    ? `${partnerName} is my ${partnerStatus}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {ageElement}
+
+      <p className="Person__partner">
+        {marriedMessage}
+      </p>
+    </section>
+  );
+};
