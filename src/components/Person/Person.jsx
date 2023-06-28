bc45a2124380464dca00e6a80af4472350d1b0f1
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-06-28 10:46:26 +0300

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,13 +6,20 @@ function getPartnerSex(sex) {
 
 export const Person = ({ person }) => (
   <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    <p className="Person__age">{person.age && `I am ${person.age}`}</p>
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+
+    {person.age !== undefined && (
+      <p className="Person__age">
+        {person.age}
+      </p>
+    )}
+
     <p className="Person__partner">
-      {
-        person.isMarried
-          ? `${person.partnerName} is my ${getPartnerSex(person.sex)}`
-          : 'I am not married'
+      {person.isMarried
+        ? `${person.partnerName} is my ${getPartnerSex(person.sex)}`
+        : 'I am not married'
       }
     </p>
   </section>
