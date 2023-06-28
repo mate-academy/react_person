53729295ca48a1826c03225203b6f5fb3bf6c12a
Maksym Ptashnyk <super.zet65@gmail.com>
2023-06-29 00:58:26 +0300

    Update Person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,7 +13,15 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <p className="Person__age">{age ? `I am ${age}` : ''}</p>
+      
+      {
+        !age && (
+          <p className="Person__age">
+            {`I am ${age}`}
+          </p>
+        )
+      }
+      
       <p className="Person__partner">
         {
           !isMarried
