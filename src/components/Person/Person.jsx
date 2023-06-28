b1a0058a11703b82cf0f9f580e3d35be18e73afe
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-06-28 10:37:11 +0300

    fixes after code review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import './Person.scss';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
+  const partnerStatus = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
@@ -10,7 +12,7 @@ export const Person = ({ person }) => {
       {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
         {isMarried
-          ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
+          ? `${partnerName} is my ${partnerStatus}`
           : 'I am not married'
         }
       </p>
