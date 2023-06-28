5e44cd666bc6046c6b7c1287ac14573e6cb61ed7
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-06-28 10:48:12 +0300

    improved code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,26 +2,28 @@ import './Person.scss';
 import React from 'react';
 
 export const Person = ({ person }) => {
-  let partnerString = '';
+  const partnerStatus = person.sex === 'm'
+    ? 'wife'
+    : 'husband';
 
-  if (person.sex === 'm' && person.isMarried === true) {
-    partnerString = `${person.partnerName} is my wife`;
-  }
-
-  if (person.sex === 'f' && person.isMarried === true) {
-    partnerString = `${person.partnerName} is my husband`;
-  }
-
-  if (person.isMarried === false) {
-    partnerString = `I am not married`;
-  }
+  const partnerString = person.isMarried
+    ? `${person.partnerName} is my ${partnerStatus}`
+    : 'I am not married';
 
   return (
     <div className="App">
       <section className="Person">
-        <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-        {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-        <p className="Person__partner">{partnerString}</p>
+        <h2 className="Person__name">
+          {`My name is ${person.name}`}
+        </h2>
+        {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+        )}
+        <p className="Person__partner">
+          {partnerString}
+        </p>
       </section>
     </div>
   );
