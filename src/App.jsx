c17e2a203c908c074ba09d5555c6c0db92701993
Evhen Ivchenko <ivchenkoevhen@gmail.com>
2023-07-01 21:10:41 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.scss';
 import { Person } from './components/Person/Person';
+import './App.scss';
 
 export const misha = {
   name: 'Misha',
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,32 +1,36 @@
 // export const Person = ({ person }) => ();
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {` ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
 
-    { (person.age) && (
-      <p className="Person__age">
-        I am
-        {` ${person.age}`}
-      </p>
-    )
-    }
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {` ${name}`}
+      </h2>
 
-    {person.isMarried && (
-      <p className="Person__partner">
-        {`${person.partnerName} `}
-        is my
-        {` ${person.sex === 'm' ? 'wife' : 'husband'}`}
-      </p>
-    )}
+      { (age) && (
+        <p className="Person__age">
+          I am
+          {` ${age}`}
+        </p>
+      )
+      }
 
-    {!person.isMarried && (
-      <p className="Person__partner">
-        I am not married
-      </p>
-    )}
+      {isMarried && (
+        <p className="Person__partner">
+          {`${partnerName} `}
+          is my
+          {` ${sex === 'm' ? 'wife' : 'husband'}`}
+        </p>
+      )}
 
-  </section>
-);
+      {!isMarried && (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      )}
+
+    </section>
+  );
+};
