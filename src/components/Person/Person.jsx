9bc743159bd28f9db2b6d76f63d87842443cedf3
Volodymyr Alieksieiev <125214087+vovaAlieksieiev@users.noreply.github.com>
2023-07-05 23:36:00 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,23 +1,38 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+import React from 'react';
 
-    {person.age && (
-    <p className="Person__age">
-      {`I am ${person.age}`}
-    </p>
-    )}
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm'
-          ? 'wife'
-          : 'husband'}`
-        : 'I am not married'
-}
-    </p>
+  const married = `${partnerName} is my ${sex === 'f' ? 'husband'
+    : 'wife'}`;
+  const notMarried = 'I am not married';
+  const checkMarried = isMarried
+    ? married
+    : notMarried;
 
-  </section>
-);
+  return (
+
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+      )}
+
+      <p className="Person__partner">
+        {checkMarried}
+      </p>
+
+    </section>
+  );
+};
