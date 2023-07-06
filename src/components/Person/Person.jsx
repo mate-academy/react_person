1c5a5ee558615d1707d54b980a9e6b14828d88b3
Dmytro Vovk <bellboy.dv@gmail.com>
2023-07-06 18:55:19 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    {person.age && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    )}
+    <p className="Person__partner">
+      {!person.isMarried
+        ? 'I am not married'
+        : Maried(person.sex, person.partnerName)}
+    </p>
+  </section>
+);
+
+function Maried(sex, partnerName) {
+  return sex === 'm'
+    ? `${partnerName} is my wife`
+    : `${partnerName} is my husband`;
+}
