51049392976a93fc3c9facd3dcd9a0f3384b037d
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2023-07-09 10:23:59 +0200

    Add person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const nameText = `My name is ${person.name}`;
+  const ageText = `I am ${person.age}`;
+
+  let marriageStatus;
+
+  if (person.isMarried) {
+    marriageStatus = `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`;
+  } else {
+    marriageStatus = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{nameText}</h2>
+      {person.age && <p className="Person__age">{ageText}</p>}
+      <p className="Person__partner">{marriageStatus}</p>
+    </section>
+  );
+};
