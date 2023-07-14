1171770223c9eaaf2f9bf3d9f7f432f67ed74385
Nazar Yants <nazyants@gmail.com>
2023-07-14 16:34:46 +0300

    fix error

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,11 +3,13 @@ export const Person = ({ person }) => (
     <h2 className="Person__name">
       {`My name is ${person.name}`}
     </h2>
-    <p className="Person__age">
-      {person.age && (
-        `I am ${person.age}`
-      )}
-    </p>
+    {person.age && (
+      <p className="Person__age">
+        {
+          `I am ${person.age}`
+        }
+      </p>
+    )}
     <p className="Person__partner">
       {isMarried(person)}
     </p>
