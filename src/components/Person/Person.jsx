1e2071961c13c4447f35edadeecac07cc336f355
Nazar Yants <nazyants@gmail.com>
2023-07-14 16:24:14 +0300

    react person solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    <p className="Person__age">
+      {person.age && (
+        `I am ${person.age}`
+      )}
+    </p>
+    <p className="Person__partner">
+      {isMarried(person)}
+    </p>
+  </section>
+);
+
+function isMarried(person) {
+  if (person.isMarried && person.sex === 'm') {
+    return `${person.partnerName} is my wife`;
+  }
+
+  if (person.isMarried && person.sex === 'f') {
+    return `${person.partnerName} is my husband`;
+  }
+
+  return 'I am not married';
+}
