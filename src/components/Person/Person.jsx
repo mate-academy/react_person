523ba3b35aa61f74e685f3c49dc67ece93276f6e
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-07-14 21:06:40 +0300

    replaced nested ternary

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,28 +1,36 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const MALE = 'm';
 
-    {
-      person.age
-        && (
-          <p className="Person__age">
-            {`I am ${person.age}`}
-          </p>
-        )
+  const partnerType = () => {
+    if (person.sex === MALE) {
+      return 'wife';
     }
 
-    <p className="Person__partner">
+    return 'husband';
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
       {
-        person.isMarried
-          ? `${person.partnerName} is my ${
-            person.sex === 'm'
-              ? 'wife'
-              : 'husband'
-          }`
-          : `I am not married`
+        person.age
+          && (
+            <p className="Person__age">
+              {`I am ${person.age}`}
+            </p>
+          )
       }
-    </p>
-  </section>
-);
+
+      <p className="Person__partner">
+        {
+          person.isMarried
+            ? `${person.partnerName} is my ${partnerType()}`
+            : `I am not married`
+        }
+      </p>
+    </section>
+  );
+};
