0026d7d6ce6507ce1339dc3ee631607764aea089
domkab <dominykas.bobinas@gmail.com>
2023-07-20 20:13:00 +0300

    1.0.1 removed nested ternaries

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,26 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age
-      && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-      )
-    }
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${
-          person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'
+export const Person = ({ person }) => {
+  const partnerRelationship = person.sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age
+        && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+        )
       }
-    </p>
-  </section>
-);
+      <p className="Person__partner">
+        {person.isMarried
+          ? `${person.partnerName} is my ${partnerRelationship}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
