b5b9ef3353022ad91f74b6477d8a976282ded887
AndruhaMan <andreysut12@gmail.com>
2023-07-22 19:14:11 +0300

    Reformat code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,9 +12,10 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age !== undefined
-        && <p className="Person__age">{`I am ${age}`}</p>
-      }
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
+
       {isMarried
         ? <p className="Person__partner">{`${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`}</p>
         : <p className="Person__partner">I am not married</p>
