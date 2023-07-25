7b80513c5eb0ccc6ccdb0653e11e1afa2dee202f
Anastasia <radchenckooo@gmail.com>
2023-07-25 12:36:36 +0300

    commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,9 +3,7 @@ export const Person = ({ person }) => (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
       <p className="Person__age">
-        {person.age
-          && `I am ${person.age}`
-        }
+        {person.age && `I am ${person.age}`}
       </p>
       <p className="Person__partner">
         {person.isMarried
