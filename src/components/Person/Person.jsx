cec584df167945e128ee98eb36b0d2e36772b947
Anastasia <radchenckooo@gmail.com>
2023-07-25 12:32:19 +0300

    commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <p className="Person__age">
+        {person.age
+          && `I am ${person.age}`
+        }
+      </p>
+      <p className="Person__partner">
+        {person.isMarried
+          ? (
+            <p className="Person__partner">
+              { person.sex === 'm'
+                ? `${person.partnerName} is my wife`
+                : `${person.partnerName} is my husband`}
+            </p>
+          ) : (
+            <p className="Person__partner">
+              I am not married
+            </p>
+          )
+        }
+      </p>
+    </section>
+  </div>
+);
