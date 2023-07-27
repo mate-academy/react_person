9032e2c8668adf5f2072f4a968d3a8ddc104ef38
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-07-27 17:54:35 +0300

    add react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -33,16 +33,16 @@ export const misha = {
   partnerName: 'Natasha',
 };
 
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
 export const alex = {
   name: 'Alex',
   age: 25,
   sex: 'm',
   isMarried: false,
+};
+
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
 };
