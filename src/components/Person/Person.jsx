e1a0c1e841c45f202f9e665c7fec8d6d93ce8ace
Vladyslav Liakh <vladislavliakh@gmail.com>
2023-07-28 01:02:31 +0300

    react-person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+import cn from 'classnames';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    <p className={cn(person.age && 'Person__age')}>
+      {`I am ${person.age}`}
+    </p>
+    <p className="Person__partner">
+      {person.isMarried
+        ? `${person.sex === 'm'
+          ? `${person.partnerName} is my wife`
+          : `${person.partnerName} is my husband`}`
+        : `I am not married`
+      }
+    </p>
+  </section>
+);
