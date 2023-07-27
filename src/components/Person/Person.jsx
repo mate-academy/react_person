655762c23082b0ce36921f62fe78481443756aaa
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-07-27 10:42:39 +0300

    commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => (
+  <>
+    <section className="Person">
+      <h2 className="Person__name">
+        {person.name}
+      </h2>
+      {person.age && (
+        <p className="Person__age">
+            {person.age}
+        </p>
+      )}
+      <p className="Person__partner">
+        {person.isMarried === true ? `${person.partnerName} is my  ${person.sex === 'f' ? 'wife' : 'husband'}` : 'I am not married'}
+      </p>
+    </section>
+  </>
+);
