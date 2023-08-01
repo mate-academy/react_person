3c5f6fbda9606a229b0b9726d829b81787286d47
Levi199601 <udovichenkoivan16@gmail.com>
2023-07-31 17:14:40 -0700

    task completed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+
+export const Person = ({ person }) => (
+
+  <div className="Person">
+    <h2 className="Person__name">
+      My name is
+      {person.name}
+    </h2>
+    <div className="Person__age">
+      {person.age > 0 && (
+        `i am ${person.age}`
+      )}
+    </div>
+    <p className="Person__partner">
+      {person.isMarried === true ? (
+        `${person.partnerName} is my
+        ${
+          person.sex === 'm' ? (
+            'wife'
+          ) : (
+            'husband'
+          )
+        }`
+      ) : (
+        'I am a not married'
+      )}
+    </p>
+  </div>
+);
