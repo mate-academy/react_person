df3f21ddffb5ff6dc6dfc7b3ccb97c6fb9a40020
Andriy Monko <Andriy17monko@gmail.com>
2023-08-01 12:22:24 +0300

    practical task 3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
-// export const Person = ({ person }) => ();
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+
+    {person.hasOwnProperty('age')
+      ? (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      ) : null}
+
+    {person.isMarried === false
+      ? (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      ) : null}
+
+    { person.hasOwnProperty('partnerName') && person.sex === 'f' && (
+    <p className="Person__partner">
+      {`${person.partnerName} is my husband`}
+    </p>
+    )}
+
+    { person.hasOwnProperty('partnerName') && person.sex === 'm' && (
+    <p className="Person__partner">
+      {`${person.partnerName} is my wife`}
+    </p>
+    )}
+
+  </section>
+);
