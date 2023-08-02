805a52c38679be948ae0d2b4b336c291c5168d8e
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-02 17:43:11 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,40 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable react/jsx-one-expression-per-line */
+export const Person = ({ person }) => {
+  if (person.sex === 'f') {
+    return (
+      <section className="Person">
+        <h2 className="Person__name">My name is {person.name}</h2>
+        {typeof (person.age) === 'number'
+          ? <p className="Person__age">I am {person.age}</p>
+          : null
+        }
+        {person.isMarried
+          ? (
+            <p className="Person__partner">
+              {person.partnerName} is my husband
+            </p>
+          )
+          : <p className="Person__partner">I am not married</p>
+        }
+      </section>
+    );
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {typeof (person.age) === 'number'
+        ? <p className="Person__age">I am {person.age}</p>
+        : null
+      }
+      {person.isMarried
+        ? (
+          <p className="Person__partner">
+            {person.partnerName} is my wife
+          </p>
+        )
+        : <p className="Person__partner">I am not married</p>
+        }
+    </section>
+  );
+};
