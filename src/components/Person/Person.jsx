61021a08190ea73453c0808968f718c9d59d0b5b
Levi199601 <udovichenkoivan16@gmail.com>
2023-08-01 21:06:42 -0700

    bug fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,25 @@
 
-export const Person = ({ person }) => (
+const sortSex = (gender) => {
+  if (gender === 'm') {
+    return 'wife'
+  } else {
+    return 'husband'
+  }
+}
 
+export const Person = ({ person }) => (
   <div className="Person">
     <h2 className="Person__name">
       {`My name is ${person.name}`}
     </h2>
     <div className="Person__age">
       {person.age > 0 && (
-        `i am ${person.age}`
+        `I am ${person.age}`
       )}
     </div>
     <p className="Person__partner">
       {person.isMarried === true ? (
-        `${person.partnerName} is my
-        ${
-          person.sex === 'm' ? (
-            'wife'
-          ) : (
-            'husband'
-          )
-        }`
+        `${person.partnerName} is my ${sortSex(person.sex)}`
       ) : (
         'I am a not married'
       )}
