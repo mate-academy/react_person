c0157ad744604bd6dbfc68639d0f1642c07efc8b
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-04 10:54:08 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,28 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 export const Person = ({ person }) => {
-  const spouse = person.sex === 'f'
+  const {
+    name,
+    age,
+    partnerName,
+    isMarried,
+    sex,
+  } = person;
+
+  const spouse = sex === 'f'
     ? 'husband'
     : 'wife';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
       {person.age
-        ? <p className="Person__age">I am {person.age}</p>
+        ? <p className="Person__age">I am {age}</p>
         : null
         }
-      {person.isMarried
+      {isMarried
         ? (
           <p className="Person__partner">
-            {`${person.partnerName} is my ${spouse}`}
+            {`${partnerName} is my ${spouse}`}
           </p>
         )
         : <p className="Person__partner">I am not married</p>
