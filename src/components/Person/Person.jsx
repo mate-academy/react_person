1656f19d1a253cd77c8a181d85ab792fc07cc003
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-08-04 09:58:32 +0300

    commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,34 +2,31 @@ import React from 'react';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  let partner = '';
+  let partner = 'I am not married';;
 
   if (isMarried === true && sex === 'm') {
     partner = `${partnerName} is my wife`;
   }
 
   if (isMarried === true && sex === 'f') {
-    
     partner = `${partnerName} is my husband`;
-  } else if (isMarried === false) {
-    partner = 'I am not married';
-  }
+  } 
 
   return (
-    <>
       <section className="Person">
         <h2 className="Person__name">
           {`My name is ${name}`}
         </h2>
+
         {age && (
         <p className="Person__age">
             {`I am ${age}`}
         </p>
         )}
+
         <p className="Person__partner">
           {partner}
         </p>
       </section>
-    </>
   );
 };
