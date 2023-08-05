da68301e928acb0ee97c792193578c67e69a4db4
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-08-05 14:04:34 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import cn from 'classnames';
+
+export const Person = ({ person: {
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+} }) => (
+  <section className={cn('Person')}>
+    <h2 className={cn('Person__name')}>{`My name is ${name}`}</h2>
+    {age && (
+      <p className={cn('Person__age')}>{`I am ${age}`}</p>
+    )}
+    <p className={cn('Person__partner')}>
+      {isMarried ? (
+        `${partnerName} is my ${sex === 'm' ? (
+          'wife'
+        ) : (
+          'husband'
+        )}`
+      ) : (
+        'I am not married'
+      )}
+    </p>
+  </section>
+);
