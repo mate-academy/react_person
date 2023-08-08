5824d6126f986605cdc5d847cd43cca5b5708824
Dmitriy <31568683+drhtka@users.noreply.github.com>
2023-08-09 00:16:20 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// export const Person = ({ person }) => ();
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const getPartner = (married) => {
+    if (married) {
+      return (
+        sex === 'm'
+          ? `${partnerName} is my wife`
+          : `${partnerName} is my husband`
+      );
+    }
+
+    return 'I am not married';
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age
+        && (<p className="Person__age">{`${age}`}</p>)
+      }
+      <p className="Person__partner">
+        {getPartner(isMarried)}
+      </p>
+    </section>
+  );
+};
