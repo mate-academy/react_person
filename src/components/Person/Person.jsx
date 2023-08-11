1a3ec78ba919c413c43722c30e41e35a7a71e1e8
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-11 18:14:29 +0200

    comments adressed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,27 +1,23 @@
 import './Person.scss';
 
 export const Person = ({ person }) => (
-  <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">
-        {`My name is ${person.name}`}
-      </h2>
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
 
-      { person.age !== undefined && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-      )}
+    { person.age !== undefined && (
+    <p className="Person__age">
+      {`I am ${person.age}`}
+    </p>
+    )}
 
-      <p className="Person__partner">
-        {!person.isMarried
-          ? `I am not married`
-          : `${person.partnerName} is my ${
-            person.sex === 'f'
-              ? `husband`
-              : `wife`
-          }`}
-      </p>
-    </section>
-  </div>
+    <p className="Person__partner">
+      {!person.isMarried
+        ? `I am not married`
+        : `${person.partnerName} is my ${getTitle(person)}`}
+    </p>
+  </section>
 );
+
+const getTitle = person => (person.sex === 'f' ? `husband` : `wife`);
