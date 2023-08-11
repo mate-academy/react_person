307ad88706b4c3dce7c6513557e7fdbf7441fcb9
Vladyslav Kulakov <vladyslav@Vladyslavs-MacBook-Pro.local>
2023-08-11 14:00:57 +0300

    fix personAge

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -25,7 +25,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      <p className="Person__age">{personAge}</p>
+      {personAge && <p className="Person__age">{personAge}</p>}
       <p className="Person__partner">{partner}</p>
     </section>
   );
