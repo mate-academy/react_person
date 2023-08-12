dd3ce6ff247a6352c5d63d51a1801a3a78c80415
oleg-gord <gord_oll@ukr.net>
2023-08-12 08:24:14 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
 // export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let textAboutPartner;
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      textAboutPartner = `${person.partnerName} is my wife`;
+    } else {
+      textAboutPartner = `${person.partnerName} is my husband`;
+    }
+  } else {
+    textAboutPartner = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {
+        person.age && <p className="Person__age">{`I am ${person.age}`}</p>
+      }
+      <p className="Person__partner">{textAboutPartner}</p>
+    </section>
+  );
+};
