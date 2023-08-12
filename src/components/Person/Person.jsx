e700ae648aa4da59c203b686a4e81a9f03dd763d
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-12 13:33:05 +0200

    variable added

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,23 +1,27 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const partnerTitle = getTitle(person);
 
-    { person.age !== undefined && (
-    <p className="Person__age">
-      {`I am ${person.age}`}
-    </p>
-    )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
 
-    <p className="Person__partner">
-      {!person.isMarried
-        ? `I am not married`
-        : `${person.partnerName} is my ${getTitle(person)}`}
-    </p>
-  </section>
-);
+      { person.age !== undefined && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+      )}
+
+      <p className="Person__partner">
+        {!person.isMarried
+          ? `I am not married`
+          : `${person.partnerName} is my ${partnerTitle}`}
+      </p>
+    </section>
+  );
+};
 
 const getTitle = person => (person.sex === 'f' ? `husband` : `wife`);
