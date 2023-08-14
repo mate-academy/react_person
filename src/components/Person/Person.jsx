db3c57eaa4cc898ff02612c9f1cdecdb6960476c
Sergey Arnautov <sirius9312@ukr.net>
2023-08-14 20:23:42 +0300

    React person Fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,29 +1,22 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partnerWifeHusband = isMarried ? (`${partnerName} is my ${sex === 'm' ? ('wife') : ('husband')}`) : (`I am not married`);
 
-    {
-      person.age && (
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
         <p className="Person__age">
-          {`I am ${person.age}`}
+          {`I am ${age}`}
         </p>
-      )
-    }
+      )}
 
-    <p className="Person__partner">
-      {
-        person.isMarried ? (
-          `${person.partnerName} is my ${person.sex === 'm' ? (
-            'wife'
-          ) : (
-            'husband'
-          )}`
-        ) : (
-          `I am not married`
-        )
-      }
-    </p>
-  </section>
-);
+      <p className="Person__partner">
+        {partnerWifeHusband}
+      </p>
+    </section>
+  );
+};
