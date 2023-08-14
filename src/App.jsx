b233fe7655e8ae33f07ffd1abd44b4e7f1c03178
Yuriy Bilskiy <yuriy.bilskiy@gmail.com>
2023-08-14 10:59:21 +0300

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -27,36 +27,13 @@ export const alex = {
 export const App = () => (
   <div className="App">
     <Person
-      name={misha.name}
-      age={misha.age}
-      partner={misha.partnerName}
-      sex={misha.sex}
+      person={misha}
     />
     <Person
-      name={olya.name}
-      partner={olya.partnerName}
-      sex={olya.sex}
+      person={olya}
     />
     <Person
-      name={alex.name}
-      age={alex.age}
-      sex={alex.sex}
+      person={alex}
     />
-    {/* <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">Natasha is my wife</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">Maksym is my husband</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section> */}
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,10 +1,18 @@
-export const Person = ({ name, age, partner, sex }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${name}`}</h2>
-    {age && <p className="Person__age">{`I am ${age}`}</p>}
-    <p className="Person__partner">
-      {`${!partner === true ? 'I am not married' : `${partner} is my
-      ${sex === 'm' ? 'wife' : 'husband'}`}`}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const personPartner = `${person.partnerName}
+  is my ${person.sex === 'm' ? 'wife' : 'husband'}`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+
+      <p className="Person__partner">
+        {`${!person.isMarried === true
+          ? 'I am not married'
+          : personPartner}`}
+      </p>
+    </section>
+  );
+};
