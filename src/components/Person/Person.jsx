3bb5268dc1267e63ddedf63fae5ce03fedc12987
Alina Isakova <adorosh062@gmail.com>
2023-08-15 09:52:42 +0300

    react person with destructing & variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,23 +1,28 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <>
-      <h2 className="Person__name">
-        {`My name is ${person.name}`}
-      </h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partner = sex === 'm' ? 'wife' : 'husband';
 
-      {person.age && (
-        <p className="Person__age">
-          {`I am ${person.age}`}
-        </p>
-      )}
+  return (
+    <section className="Person">
+      <>
+        <h2 className="Person__name">
+          {`My name is ${name}`}
+        </h2>
 
-      <p className="Person__partner">
-        {person.isMarried === true ? (
-          `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        ) : (
-          'I am not married'
+        {age && (
+          <p className="Person__age">
+            {`I am ${age}`}
+          </p>
         )}
-      </p>
-    </>
-  </section>
-);
+
+        <p className="Person__partner">
+          {isMarried ? (
+            `${partnerName} is my ${partner}`
+          ) : (
+            'I am not married'
+          )}
+        </p>
+      </>
+    </section>
+  );
+};
