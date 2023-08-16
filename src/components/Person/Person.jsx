69dee7da1935f0fc7147513415d72d8bd1d1706e
Evelina Moore <luna.rybnikova@gmail.com>
2023-08-17 00:42:57 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,11 +14,15 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is {name}
+        My name is
+        {' '}
+        {name}
       </h2>
       {age && (
       <p className="Person__age">
-        I am {age}
+        I am
+        {' '}
+        {age}
       </p>
       )}
       <p className="Person__partner">{partnerText}</p>
