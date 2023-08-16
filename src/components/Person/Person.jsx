2c7acfd5f3c9b6d0047e2407a76bd441750b3501
Evelina Moore <luna.rybnikova@gmail.com>
2023-08-17 00:17:07 +0300

    task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,28 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const genderPartner = sex === "m" ? "wife" : "husband";
+  const genderPartner = sex === 'm' ? 'wife' : 'husband';
 
   let partnerText;
+
   if (isMarried) {
     partnerText = `${partnerName} is my ${genderPartner}`;
   } else {
-    partnerText = "I am not married";
+    partnerText = 'I am not married';
   }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">I am {age}</p>}
+      <h2 className="Person__name">
+        My name is
+        {name}
+      </h2>
+      {age && (
+      <p className="Person__age">
+        I am
+        {age}
+      </p>
+      )}
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
