5a53dfe880883cb783ea16772465f5cb16c839d8
Andrew <deyneka2003@gmail.com>
2023-08-17 12:54:36 +0300

    destruct person, create function

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,29 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age ? (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-    ) : ''}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-    {/* {!person.isMarried ? (
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  function marriedPerson(marriedStatus) {
+    if (!marriedStatus) {
+      return 'I am not married';
+    }
+
+    const partnerGender = sex === 'm' ? 'wife' : 'husband';
+
+    return `${partnerName} is my ${partnerGender}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age ? (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      ) : ''}
       <p className="Person__partner">
-        I am not married
+        {marriedPerson(isMarried)}
       </p>
-    ) : (
-      <p className="Person__partner">
-        {`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
-      </p>
-    )} */}
-  </section>
-);
+    </section>
+  );
+};
