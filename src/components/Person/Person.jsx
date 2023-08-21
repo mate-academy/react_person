c3d16b4aa7e482578a8e841590279038fb72b468
Andrzej Pietkiewicz <pietan@me.com>
2023-08-21 21:24:59 +0200

    reactPerson solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
 // export const Person = ({ person }) => ();
+export const Person = ({ name,
+  age,
+  partnerName,
+  isMarried }) => (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      {isMarried
+        ? <p className="Person__partner">{`${partnerName} is my wife`}</p>
+        : <p className="Person__partner">I am not married</p>}
+    </section>
+);
