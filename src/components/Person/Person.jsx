a61d7808d6d267d7202e0d90e485db8581b29b0f
Anton <crossnaders4949@gmail.com>
2023-08-21 22:01:44 +0200

    Fixed mistakes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,6 @@
 // export const Person = ({ person }) => ();
 export const Person = ({ person }) => {
-  const newVariable = person.sex === 'm' ? (
+  const partnersInfo = person.sex === 'm' ? (
     `${person.partnerName} is my wife`
   ) : (`${person.partnerName} is my husband`);
 
@@ -11,9 +11,10 @@ export const Person = ({ person }) => {
       </h2>
       {person.age && (<p className="Person__age">{`I am ${person.age}`}</p>)}
       <p className="Person__partner">
-        {person.isMarried === true ? (
-          newVariable
-        ) : (`I am not married`)}
+        {person.isMarried
+          ? partnersInfo
+          : `I am not married`
+        }
       </p>
     </section>
   );
