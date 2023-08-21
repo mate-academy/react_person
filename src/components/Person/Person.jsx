2bd99bbe916708289d8e2112be237ba4ba44399f
Anton Tsurkan <wesses1999@gmail.com>
2023-08-21 20:44:22 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,47 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+const SEX_FEMALE = 'f';
+const SEX_MALE = 'm';
+
+function getPartnerInformation(sex, partnerName) {
+  if (sex === SEX_FEMALE) {
+    return (
+      <p className="Person__partner">{`${partnerName} is my husband`}</p>
+    );
+  }
+
+  if (sex === SEX_MALE) {
+    return (
+      <p className="Person__partner">{`${partnerName} is my wife`}</p>
+    );
+  }
+
+  return null;
+}
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    sex,
+    age,
+    isMarried,
+    partnerName,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {typeof age === 'number' && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      { isMarried
+        ? getPartnerInformation(sex, partnerName)
+        : (<p className="Person__partner">I am not married</p>)
+      }
+    </section>
+  );
+};
