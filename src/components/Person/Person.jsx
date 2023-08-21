9a3d76cc058779fd0d77cfe07ceb76c7510361f9
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-21 19:43:01 +0300

    initial commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,34 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export function Person({ person }) {
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{ `My name is ${person.name}` }</h2>
+
+        {person.age
+          && (
+          <p className="Person__age">
+            {`I am ${person.age}`}
+          </p>
+          )
+        }
+
+        {person.isMarried ? (
+          <p className="Person__partner">
+            {person.sex === 'f'
+              ? `${person.partnerName} is my husband`
+              : `${person.partnerName} is my wife`
+          }
+          </p>
+        )
+          : (
+            <p className="Person__partner">
+              I am not married
+            </p>
+          )
+        }
+      </section>
+    </>
+  );
+}
