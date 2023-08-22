173e5131e21eb0853540448d493db0efe04774e6
Anton Tsurkan <wesses1999@gmail.com>
2023-08-22 10:13:48 +0300

    fix: remade getPartner function

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,11 @@
 import './Person.scss';
 
 const SEX_FEMALE = 'f';
-const SEX_MALE = 'm';
 
-function getPartnerInformation(sex, partnerName) {
-  if (sex === SEX_FEMALE) {
-    return (
-      <p className="Person__partner">{`${partnerName} is my husband`}</p>
-    );
-  }
-
-  if (sex === SEX_MALE) {
-    return (
-      <p className="Person__partner">{`${partnerName} is my wife`}</p>
-    );
-  }
-
-  return null;
+function getPartnerStatus(sex) {
+  return sex === SEX_FEMALE
+    ? 'husband'
+    : 'wife';
 }
 
 export const Person = ({ person }) => {
@@ -32,14 +21,14 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {typeof age === 'number' && (
+      {!!age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
 
       { isMarried
-        ? getPartnerInformation(sex, partnerName)
+        ? (<p className="Person__partner">{`${partnerName} is my ${getPartnerStatus(sex)}`}</p>)
         : (<p className="Person__partner">I am not married</p>)
       }
     </section>
