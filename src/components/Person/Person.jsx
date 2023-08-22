e72efdb20140b65709e8e75c0ec6ffdbaa05b5f9
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-08-22 18:29:10 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,19 +11,23 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
+  const messageAboutPartner = sex === SEX_MALE
+    ? `${partnerName} is my wife`
+    : `${partnerName} is my husband`;
+
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
-      )}
+      {typeof age === typeof 0
+        ? (
+          <p className="Person__age">{`I am ${age}`}</p>
+        )
+        : null}
 
       {isMarried ? (
         <p className="Person__partner">
-          {`${partnerName} is my ${sex === SEX_MALE
-            ? 'wife'
-            : 'husband'}`}
+          {messageAboutPartner}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
