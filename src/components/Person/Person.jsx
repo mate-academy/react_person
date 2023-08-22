bcc98c4ca0a9e777fb63107e4399248463640c95
Maksym <morenko.maxim@gmail.com>
2023-08-22 15:02:28 +0300

    destructing of props

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is `}
-      {person.name}
-    </h2>
-    {person.age && (
-      <p className="Person__age">
-        {`I am `}
-        {person.age}
-      </p>
-    )}
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {person.partnerName}
-        {` is my `}
-        {person.sex === 'm' ? 'wife' : 'husband'}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    isMarried,
+    partnerName,
+    sex,
+  } = person;
+  const partnerStatus = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is `}
+        {name}
+      </h2>
+      {person.age && (
+        <p className="Person__age">
+          {`I am `}
+          {age}
+        </p>
+      )}
+      <p className="Person__partner">{isMarried ? `${partnerName} is my ${partnerStatus}` : 'I am not married'}</p>
+    </section>
+  );
+};
