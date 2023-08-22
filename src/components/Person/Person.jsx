9974776f80f5a1981e231155f4d52a12700c2078
Nikita Zaika <zaikanikitaua@gmail.com>
2023-08-23 02:20:09 +0300

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,36 @@
-// export const Person = ({ person }) => ();
+const FEMALE = 'f';
+const HUSBAND = 'husband';
+const WIFE = 'wife';
+const NOT_MARRIED_STATUS_MESSAGE = 'I am not married';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {Boolean(age) && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${sex === FEMALE
+            ? HUSBAND
+            : WIFE}`
+          : NOT_MARRIED_STATUS_MESSAGE}
+      </p>
+    </section>
+  );
+};
