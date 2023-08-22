d846ed99432900e0237dc54e219c6be8c5a2496d
Dmytro Hrynchyi <desevoker@gmail.com>
2023-08-22 20:29:09 +0300

    add constant, destructuring and improve readability

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,29 @@
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  const partnerType = (person.sex === 'm') ? 'wife' : 'husband';
+  const SEX_MALE = 'm';
+
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const partnerType = (sex === SEX_MALE) ? 'wife' : 'husband';
+
+  const partnerText = isMarried
+    ? `${partnerName} is my ${partnerType}`
+    : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      {!!age && <p className="Person__age">{`I am ${age}`}</p>}
 
-      <p className="Person__partner">
-        {person.isMarried ? (
-          `${person.partnerName} is my ${partnerType}`
-        ) : (
-          'I am not married'
-        )}
-      </p>
+      <p className="Person__partner">{partnerText}</p>
     </section>
   );
 };
