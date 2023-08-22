e195ce07e4312e9507a05fb23ca30d52ee649723
Dolnys <thedolnys@gmail.com>
2023-08-22 09:55:54 +0200

    remove unnecessary spaces

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,8 +1,7 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">
-      {` My name is
-      ${person.name}`}
+      {`My name is ${person.name}`}
     </h2>
     {person.age && (
     <p className="Person__age">
