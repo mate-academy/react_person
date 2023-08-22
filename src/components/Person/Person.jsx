aa89ed7c780a23992332cf6c71f41358fba7988b
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-22 21:36:12 +0300

    change check logic for Person__age, remove function

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,14 +2,7 @@ const SEX_MALE = 'm';
 
 export const Person = ({ person }) => {
   const { name, age, partnerName, sex, isMarried } = person;
-
-  const getPartnerInfo = () => {
-    if (isMarried) {
-      return `${partnerName} is my ${sex === SEX_MALE ? 'wife' : 'husband'}`;
-    }
-
-    return 'I am not married';
-  };
+  const partnerInfo = `${partnerName} is my ${sex === SEX_MALE ? `wife` : `husband`}`;
 
   return (
     <section className="Person">
@@ -17,14 +10,17 @@ export const Person = ({ person }) => {
         {`My name is ${name}`}
       </h2>
 
-      {typeof age === 'number' && (
+      {Boolean(age) && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
 
       <p className="Person__partner">
-        {getPartnerInfo()}
+        {isMarried
+          ? (partnerInfo)
+          : `I am not married`
+        }
       </p>
     </section>
   );
