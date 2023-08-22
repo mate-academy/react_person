07c662328102b73030a7b7ab956bef7433b1bded
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-08-22 22:09:08 +0300

    corrected ternary operator

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -16,7 +16,9 @@ export const Person = ({ person }) => {
         </p>
       )}
       <p className="Person__partner">
-        {isMarried ? `${partnerName} is my ${sexPartner}` : 'I am not married'}
+        {isMarried
+          ? `${partnerName} is my ${sexPartner}`
+          : 'I am not married'}
       </p>
     </section>
   );
