7cc353cd65065bb5ede95c292e2975c5b2f1f532
Vitalii-Petruk <vitalik3807083@gmail.com>
2023-08-23 00:09:20 +0300

    react_person solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const spouseGender = person.sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  const marriageInformation = person.isMarried
+    ? `${person.partnerName} is my ${spouseGender}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {Boolean(person.age) > 0 && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">{marriageInformation}</p>
+    </section>
+  );
+};
