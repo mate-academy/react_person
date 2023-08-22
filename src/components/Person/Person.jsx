368f01c90f4f4f228b478ade46d5ccdcc2ce4c5d
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-22 11:12:29 +0300

    add var for married person, change jsx in person, delete initial var for props, delete nest ternarn

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,39 +3,31 @@ import React from 'react';
 export function Person({ person }) {
   const {
     name,
-    age = null,
+    age,
     sex,
     isMarried,
     partnerName,
   } = person;
 
+  const whoIsPartner = sex === 'f'
+    ? `${partnerName} is my husband`
+    : `${partnerName} is my wife`;
+
   return (
     <>
       <section className="Person">
-        <h2 className="Person__name">{ `My name is ${name}` }</h2>
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
 
         {age
           && (
           <p className="Person__age">
             {`I am ${age}`}
           </p>
-          )
-        }
+          )}
 
-        {isMarried ? (
-          <p className="Person__partner">
-            {sex === 'f'
-              ? `${partnerName} is my husband`
-              : `${partnerName} is my wife`
-          }
-          </p>
-        )
-          : (
-            <p className="Person__partner">
-              I am not married
-            </p>
-          )
-        }
+        <p className="Person__partner">
+          {isMarried ? `${whoIsPartner}` : 'I am not married'}
+        </p>
       </section>
     </>
   );
