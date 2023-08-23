9db7e9237036408e080acab751c1ea71f6ab5e30
irinagos <irynagos47@gmail.com>
2023-08-23 08:43:44 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+const PERSON_FEMALE_SEX = 'f';
+
+export const Person = ({ person }) => {
+  let partnerMessage = '';
+
+  if (!person.isMarried) {
+    partnerMessage = 'I am not married';
+  } else {
+    const partnerRole = person.sex === PERSON_FEMALE_SEX ? 'husband' : 'wife';
+
+    partnerMessage = `${person.partnerName} is my ${partnerRole}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age !== undefined && (
+        <p className="Person__age">
+            {`I am ${person.age}`}
+        </p>
+      )}
+      <p className="Person__partner">
+        {partnerMessage}
+      </p>
+    </section>
+  );
+};
