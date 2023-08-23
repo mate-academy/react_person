bb01030e00e0afec07d21c06659a1fe38cb9e0fd
irinagos <irynagos47@gmail.com>
2023-08-23 09:51:35 +0300

    fixed code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,37 @@
 const PERSON_FEMALE_SEX = 'f';
 
 export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
   let partnerMessage = '';
 
-  if (!person.isMarried) {
+  if (!isMarried) {
     partnerMessage = 'I am not married';
   } else {
-    const partnerRole = person.sex === PERSON_FEMALE_SEX ? 'husband' : 'wife';
+    const partnerRole = sex === PERSON_FEMALE_SEX
+      ? 'husband'
+      : 'wife';
 
-    partnerMessage = `${person.partnerName} is my ${partnerRole}`;
+    partnerMessage = `${partnerName} is my ${partnerRole}`;
   }
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {`My name is ${person.name}`}
+        {`My name is ${name}`}
       </h2>
-      {person.age !== undefined && (
+
+      {Boolean(age) && (
         <p className="Person__age">
-            {`I am ${person.age}`}
+          {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
         {partnerMessage}
       </p>
