9cb805eb16341bf20104dd3536ab505d103c26b5
Dmytro Hrynchyi <desevoker@gmail.com>
2023-08-23 22:25:42 +0300

    change condition

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -23,7 +23,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {!!age && (
+      {Boolean(age) && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
