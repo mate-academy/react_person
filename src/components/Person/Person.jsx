129a24c2480b775221dbaee5bfa06fa3556e728a
Rostyslav Kravchuk <mr.shadov.05@gmail.com>
2023-08-23 12:10:00 +0300

    add all changes requested

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,8 @@
 import './Person.scss';
 
 export const Person = ({ person }) => {
+  const MALE_SEX = 'm';
+
   const {
     name,
     age,
@@ -9,29 +11,30 @@ export const Person = ({ person }) => {
     isMarried,
   } = person;
 
+  const personPartner = sex === MALE_SEX
+    ? 'wife'
+    : 'husband';
+
   return (
     <>
       <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        <h2 className="Person__name">
+          {`My name is ${name}`}
+        </h2>
 
-        {age && (
-          <p className="Person__age">{`I am ${age}`}</p>
-        )
-        }
-
-        {isMarried ? (
-          <p className="Person__partner">
-            {`${partnerName} is my ${sex === 'm'
-              ? 'wife'
-              : 'husband'
-            }`}
-          </p>
-        ) : (
-          <p className="Person__partner">
-            I am not married
+        {Boolean(age) && (
+          <p className="Person__age">
+            {`I am ${age}`}
           </p>
         )
         }
+
+        <p className="Person__partner">
+          {isMarried
+            ? `${partnerName} is my ${personPartner}`
+            : 'I am not married'
+          }
+        </p>
       </section>
     </>
   );
