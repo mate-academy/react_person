9929507cb761ffcab9ec46367537048f30c69e08
Tamara Chernetska <cty2802@gmail.com>
2023-08-23 12:03:37 +0300

    add fixed solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,44 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const WOMAN_GENDER = 'f';
+  const MAN_GENDER = 'm';
 
-    <>
-      {person.age && (
-        <p className="Person__age">
-          {`I am ${person.age}`}
-        </p>
-      )}
-    </>
+  const {
+    name,
+    age,
+    isMarried,
+    sex,
+    partnerName,
+  } = person;
 
-    <p className="Person__partner">
-      {person.isMarried && person.sex === 'm' && (
-        <>{`${person.partnerName} is my wife`}</>
-      )}
-      {person.isMarried && person.sex === 'f' && (
-        <>{`${person.partnerName} is my husband`}</>
-      )}
-      {!person.isMarried && <>I am not married</>}
-    </p>
-  </section>
-);
+  function getMarriedInfo() {
+    if (sex === MAN_GENDER && isMarried) {
+      return <>{`${partnerName} is my wife`}</>;
+    }
+
+    if (sex === WOMAN_GENDER && isMarried) {
+      return <>{`${partnerName} is my husband`}</>;
+    }
+
+    return <>I am not married</>;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      <>
+        {age && (
+          <p className="Person__age">
+            {`I am ${age}`}
+          </p>
+        )}
+      </>
+
+      <p className="Person__partner">
+        {getMarriedInfo()}
+      </p>
+    </section>
+  );
+};
