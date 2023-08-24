0edcb23509b266dfcdb11f77ee88536174f7fa39
myroslava-kukhta <miroslavakukhta@gmail.com>
2023-08-24 19:38:33 +0300

    React_person_second

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,31 +1,28 @@
 export const Person = ({ person }) => {
   const { name, age, partnerName, sex, isMarried } = person;
-  const FEMALE = 'f';
+  const FEMALE = sex === 'f'
+    ? 'husband'
+    : 'wife';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        { `My name is ${name}` }
+        {`My name is ${name}`}
       </h2>
 
       {
         age
-          ? (
-            <p className="Person__age">
-              {`I am ${age}`}
-            </p>
-          )
-          : null
+        && (
+          <p className="Person__age">
+            {`I am ${age}`}
+          </p>
+        )
       }
 
       <p className="Person__partner">
-        {
-          isMarried
-            ? `${partnerName} is my ${sex === FEMALE
-              ? 'husband'
-              : 'wife'
-            } `
-            : `I am not married`
+        {isMarried
+          ? `${partnerName} is my ${FEMALE}`
+          : 'I am not married'
         }
       </p>
     </section>
