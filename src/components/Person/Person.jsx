da652afd0623ed98c55365aa93d096a79bf4d4cb
Vladyslav Kulakov <vladyslav@Vladyslavs-MacBook-Pro.local>
2023-08-24 15:33:46 +0300

    do refactor changes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,25 +7,13 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
-  let personAge = null;
-  let partner = 'I am not married';
-
-  if (age) {
-    personAge = `I am ${age}`;
-  }
-
-  if (isMarried) {
-    if (sex === 'm') {
-      partner = `${partnerName} is my wife`;
-    } else if (sex === 'f') {
-      partner = `${partnerName} is my husband`;
-    }
-  }
+  const personPartner = sex === 'm' ? 'wife' : 'husband';
+  const partner = isMarried ? `${partnerName} is my ${personPartner}` : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {personAge && <p className="Person__age">{personAge}</p>}
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">{partner}</p>
     </section>
   );
