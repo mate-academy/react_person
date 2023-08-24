4b29e549a82f26fd32582fdd84acbddfc2864f4c
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-08-25 00:25:40 +0300

    minor fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,7 +6,8 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
+      {age !== undefined
+      && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">
         {isMarried
           ? marriedMessage
