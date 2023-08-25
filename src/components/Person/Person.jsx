e17a56b12bf28bd90401aee8223271ccaba82f13
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-08-25 21:56:44 +0300

    react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    {person.age && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    )}
+    {person.isMarried ? (
+      <p className="Person__partner">
+        {`${person.partnerName} is my ${
+          person.sex !== 'm' ? 'husband' : 'wife'
+        }`}
+      </p>
+    ) : (
+      <p>I am not married</p>
+    )}
+  </section>
+);
+
+export default Person;
