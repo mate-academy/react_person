9809c40e53159408447f0ab00dbbab1aefb11baa
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-08-30 19:42:23 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+
+    <p className="Person__age">
+      {person.hasOwnProperty('age') ? (
+        `I am ${person.age}`
+      ) : (
+        null
+      )}
+    </p>
+
+    <p className="Person__partner">
+      {!person.isMarried ? (
+        `I am not married`
+      ) : (
+        `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`
+      )}
+    </p>
+  </section>
+);
