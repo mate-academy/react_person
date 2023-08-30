ac612b32cf1f17633c051cef90fb0faa448bf75c
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-08-30 16:45:07 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,22 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+
+    {person.age !== undefined && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    )}
+
+    <p className="Person__partner">
+      {person.isMarried === false ? (
+        'I am not married') : (
+        `${person.partnerName} is my ${person.sex === 'f' ? (
+          'husband') : (
+          'wife')}`
+      )}
+    </p>
+  </section>
+);
