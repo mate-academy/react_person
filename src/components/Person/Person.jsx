fec0cd232c20cb1b890ef4565c3836a6f72514d4
Yurii Komanitskiy <komanitskiy200320@gmail.com>
2023-09-04 21:10:24 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,7 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.hasOwnProperty('age') && (
+    {person.age && (
       <p className="Person__age">{`I am ${person.age}`}</p>
     )}
     <p className="Person__partner">
