88814a17be05f752bda8c49480571e0113aee061
Oksana Maruniak <syper.ksenka@gmail.com>
2023-09-05 01:19:59 +0300

    changed ternary and others

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,23 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age > 0 && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-    )}
+export const Person = ({ person }) => {
+  const { age, isMarried, partnerName } = person;
+  const maritalStatus = person.sex === 'f' ? 'husband' : 'wife';
 
-    <p className="Person__partner">
-      {!person.isMarried && ('I am not married')}
-      {(person.isMarried && person.sex === 'f') && (`${person.partnerName} is my husband`)}
-      {(person.isMarried && person.sex === 'm') && (`${person.partnerName} is my wife`)}
-    </p>
-  </section>
-);
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried ? `${partnerName} is my ${maritalStatus}` : 'I am not married'}
+      </p>
+    </section>
+  );
+};
