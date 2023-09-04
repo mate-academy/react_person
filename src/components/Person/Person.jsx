57a47a84404098562514fe606642401c4ddbee65
Oksana Maruniak <syper.ksenka@gmail.com>
2023-09-04 19:11:28 +0300

    done Person React

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    {person.age > 0 && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    )}
+
+    <p className="Person__partner">
+      {!person.isMarried && ('I am not married')}
+      {(person.isMarried && person.sex === 'f') && (`${person.partnerName} is my husband`)}
+      {(person.isMarried && person.sex === 'm') && (`${person.partnerName} is my wife`)}
+    </p>
+  </section>
+);
