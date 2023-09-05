cffdc09179a1a683bbfe893cadc21e5c96e95687
unknown <maksoleksyuk@gmail.com>
2023-09-05 09:29:09 +0300

    fixed conditional rendering

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,10 +3,14 @@ export const Person = ({ person }) => (
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
     <p className="Person__age">{`${person.age === undefined ? '' : ` I am ${person.age}`}`}</p>
     <p className="Person__partner">
-      {person.isMarried ? (
+      {person.isMarried && person.sex === 'm' && (
         `${person.partnerName} is my wife`
-      ) : (
-        'I am not married'
+      )}
+      {person.isMarried && person.sex === 'f' && (
+        `${person.partnerName} is my husband`
+      )}
+      {person.isMarried === false && (
+        `I am not married`
       )}
     </p>
   </section>
