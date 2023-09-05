512df6fa954d38c3aef1909af375f31963922b32
unknown <maksoleksyuk@gmail.com>
2023-09-05 08:39:40 +0300

    added person props

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    <p className="Person__age">{`${person.age === undefined ? '' : ` I am ${person.age}`}`}</p>
+    <p className="Person__partner">
+      {person.isMarried ? (
+        `${person.partnerName} is my wife`
+      ) : (
+        'I am not married'
+      )}
+    </p>
+  </section>
+);
