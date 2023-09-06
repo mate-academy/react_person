412d7439791db3c8d60d2af6dc93e12af1939e28
Юрий Мисюра <apriory13@meta.ua>
2023-09-06 15:56:43 +0300

    React person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const {
+    age,
+    isMarried,
+    partnerName,
+  } = person;
+  const partnerStatus = person.sex === 'm' ? 'wife' : 'husbend';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      <p className="Person__age">
+        {age && `I am ${age}`}
+      </p>
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerStatus}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
