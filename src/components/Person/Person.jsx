dd0119e3bdd6c8800654c4e3b5f13e9e445b0e42
Oleh Obiukh <oleg.obiukh@gmail.com>
2023-09-11 12:02:00 +0100

    sentence spaces

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,27 +1,31 @@
 export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
+  <section className="person">
+    <h2 className="person__name">
       My name is
-      {' '}
-      {person.name}
+      <span className="person__space">
+        {person.name}
+      </span>
     </h2>
     {person.age && (
-    <p className="Person__age">
+    <p className="person__age">
       I am
-      {' '}
-      {person.age}
+      <span className="person__space">
+        {person.age}
+      </span>
     </p>
     )}
     {person.isMarried ? (
-      <p className="Person__partner">
+      <p className="person__partner">
         {person.partnerName}
-        {' '}
-        is my
-        {' '}
-        {person.sex === 'm' ? 'wife' : 'husband'}
+        <span className="person__space">
+          is my
+        </span>
+        <span className="person__space">
+          {person.sex === 'm' ? 'wife' : 'husband'}
+        </span>
       </p>
     ) : (
-      <p className="Person__partner">I am not married</p>
+      <p className="person__partner">I am not married</p>
     )}
   </section>
 );
