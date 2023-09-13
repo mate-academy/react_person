ca73fd513af6ec7d6d88fe97462c1a29d505784b
Andrii Solovei <solovey98.info@gmail.com>
2023-09-13 22:40:56 +0300

    splited file into a components

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      My name is {person.name}
+    </h2>
+    <p className="Person__age">
+      {person.age && `I am ${person.age}`}
+    </p>
+    <p className="Person__partner">
+      {!person.isMarried && 'I am not married'}
+      {person.sex === 'f' && `${person.partnerName} is my husband`}
+      {person.sex === 'm' && `${person.partnerName} is my wife`}
+    </p>
+  </section>
+);
