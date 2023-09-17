778a6cd2dedede9d8afd6b8888af1a4cc85aa072
Aleksandr <kartanovaleks@gmail.com>
2023-09-17 11:58:27 +0300

    Fixed solution

--- src/App.jsx
+++ src/App.jsx
@@ -26,35 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <Person person={
-      {
-        name: 'Misha',
-        age: 37,
-        sex: 'm',
-        isMarried: true,
-        partnerName: 'Natasha',
-      }
-    }
-    />
-
-    <Person person={
-      {
-        name: 'Olya',
-        sex: 'f',
-        isMarried: true,
-        partnerName: 'Maksym',
-      }
-    }
-    />
-
-    <Person person={
-      {
-        name: 'Alex',
-        age: 25,
-        sex: 'm',
-        isMarried: false,
-      }
-    }
-    />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
