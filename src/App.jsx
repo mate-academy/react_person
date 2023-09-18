4c5bc31e1285b5f6e09ceac3e2f9b99c242d316a
Astra09 <sichkarstepan09@gmail.com>
2023-09-18 17:11:37 +0300

    moved person objects to people.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from 'react';
 import './App.scss';
 import { Person } from './components/Person/Person';
-
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
+import { misha, olya, alex } from './people';
 
 export const App = () => (
   <div className="App">
