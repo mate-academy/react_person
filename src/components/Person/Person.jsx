8440a763822ba0ab74cb3b48ba85298dafb40086
Artem <gavr.artem.s@gmail.com>
2023-09-21 16:56:54 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,29 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const gender = sex => (
+    sex === 'm'
+      ? 'wife'
+      : 'husband'
+  );
 
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried
+          ? (`${person.partnerName} is my ${gender(person.sex)}`)
+          : (`I am not married`)}
       </p>
-    )}
-
-    <p className="Person__partner">
-      {person.isMarried
-        ? (`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`)
-        : ('I am not married')}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
