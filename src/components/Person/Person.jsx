f66f2f5a236210eac70a711439071406f4c964be
Iryna MP <iryna.mp@yahoo.com>
2023-09-21 22:43:25 -0600

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,30 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const getPartnerLabel = () => {
+    let marriedLabel;
+
+    if (isMarried) {
+      marriedLabel = sex === 'm'
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
+    }
+
+    return isMarried === true
+      ? marriedLabel
+      : 'I am not married';
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age !== undefined
+      && <p className="Person__age">{`I am ${age}`}</p>}
+
+      <p className="Person__partner">{getPartnerLabel()}</p>
+    </section>
+  );
+};
