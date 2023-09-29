4fb9f9838ed804b97fb79d3a857d9d75999bf01c
koros <korosov.j@gmail.com>
2023-09-29 19:33:43 +0300

    added task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
-const STATUSES = {
-  m: 'wife',
-  f: 'husband',
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const STATUSES = {
+    m: 'wife',
+    f: 'husband',
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {!age ? null : (
+        <p className="Person__age">{`I am ${age}`}</p>)}
+
+      <p className="Person__partner">
+        {
+          !isMarried
+            ? 'I am not married'
+            : `${partnerName} is my ${STATUSES[sex]}`
+        }
+      </p>
+    </section>
+  );
 };
-
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-
-    { !person.age ? null : (<p className="Person__age">{`I am ${person.age}`}</p>)}
-
-    <p className="Person__partner">
-      {
-        !person.isMarried
-          ? 'I am not married'
-          : `${person.partnerName} is my ${STATUSES[person.sex]}`
-      }
-    </p>
-  </section>
-);
