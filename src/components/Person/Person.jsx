0e969e8e825cfe32099cfc50a3649f55b411701c
Tetiana Poletaieva <ptupob@gmail.com>
2023-10-01 12:42:13 +0300

    react-person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,13 +7,11 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && (<p className="Person__age">{`I am ${age}`}</p>)}
-      {isMarried
-        ? (
-          <p className="Person__partner">
-            {`${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'} `}
-          </p>
-        )
-        : 'I am not married'}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
+          : 'I am not married'}
+      </p>
     </section>
   );
 };
