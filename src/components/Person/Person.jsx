91d1d4a4ca6b5aeaef60064c46b83f868de3c003
Vladyslav Atanov <gibepromt@gmail.com>
2023-10-02 13:27:14 +0300

    create person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let marriedMessage = 'I am not married';
+
+  if (person.isMarried === true) {
+    marriedMessage = person.sex === 'f'
+      ? `${person.partnerName} is my husband`
+      : `${person.partnerName} is my wife`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      { person.age !== undefined && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )
+      }
+      <p className="Person__partner">
+        {marriedMessage}
+      </p>
+    </section>
+  );
+};
