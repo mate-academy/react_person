956ae0b7cee208d0117d4b1ba6a22a8002b97b07
Zhekich123 <axelb380@gmail.com>
2023-10-03 14:00:46 +0300

    fixing bug

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,36 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age !== undefined && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      {person.isMarried === false ? (
+        <p className="Person__partner">I am not married</p>
+      ) : (
+        null
+      )}
+
+      {person.isMarried === true && person.sex === 'm' ? (
+        <p className="Person__partner">
+          {`${person.partnerName} is my wife`}
+        </p>
+      ) : (
+        null
+      )}
+
+      {person.isMarried === true && person.sex === 'f' ? (
+        <p className="Person__partner">
+          {`${person.partnerName} is my husband`}
+        </p>
+      ) : (
+        null
+      )}
+    </section>
+  </div>
+);
