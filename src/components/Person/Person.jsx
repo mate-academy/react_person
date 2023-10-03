1a0076475cfbed67a36f9a872802c6f49292c8d7
dmytro-popovych <pdp.dev.it@gmail.com>
2023-10-03 02:24:00 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const showAge = age
+    ? <p className="Person__age">{`I am ${age}`}</p>
+    : null;
+
+  const partnerSex = sex === 'm'
+    ? 'wife'
+    : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {showAge}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerSex}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
