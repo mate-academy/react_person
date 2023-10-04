4b8228fabf56d5773af0e51be387b57a63a07964
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-04 15:18:59 +0200

    fixed heading importance and code style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,12 +5,16 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h1 className="Person__name">{`My name is ${name}`}</h1>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
       {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
