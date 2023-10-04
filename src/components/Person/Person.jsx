b22958345721b2518181b06c98479d4664096914
OlhaKozuik <gadyakaban@gmail.com>
2023-10-04 16:58:55 +0300

    try to pass the tests

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,25 @@
 import './Person.scss';
 
-export const Person = ({ person: {
-  name,
-  age,
-  sex,
-  isMarried,
-  partnerName,
-} }) => (
+export const Person = ({
+  person: {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  },
+}) => (
   <section className="Person">
-    <h2 className="Person__name">{`My name is ${name}`}</h2>
+    <h2 className="Person__name">
+      {`My name is ${name}`}
+    </h2>
+
     {age && (
-      <p className="Person__age">{`I am ${age}`}</p>
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
     )}
+
     <p className="Person__partner">
       {isMarried
         ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
