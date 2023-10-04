c6959973bc5657acb011e9d0ca15f227bcdb55d7
donizer <bogdan.ua45@gmail.com>
2023-10-04 12:06:13 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,9 +5,14 @@ export const Person = ({ person }) => (
     <h2 className="Person__name">
       { `My name is ${person.name}` }
     </h2>
-    <p className="Person__age">
-      {person.age ? `I am ${person.age}` : ''}
-    </p>
+
+    {person.age ? (
+      <p className="Person__age">
+        {'I am '}
+        {person.age}
+      </p>
+    ) : ''}
+
     <p className="Person__partner">
       { person.isMarried ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}` : 'I am not married'}
     </p>
