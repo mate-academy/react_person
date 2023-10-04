c79e59acd5d8291b453ac124a7d470ad569150e4
Illia Drach <illyadrach@gmail.com>
2023-10-04 19:21:53 +0300

    implement mentor review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,23 +6,30 @@ export const Person = ({
     isMarried,
     partnerName,
   },
-}) => (
+}) => {
+  // Created Variable for the type of Partner
+  const partnerType = sex === 'm' ? (
+    `${partnerName} is my wife`
+  ) : (
+    `${partnerName} is my husband`
+  );
 
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
-    {age !== undefined
-      ? <p className="Person__age">{`I am ${age}`}</p>
-      : ''}
-    <p className="Person__partner">
-      {isMarried === false && 'I am not married'}
-      {isMarried === true
-      && (sex === 'm' ? (
-        `${partnerName} is my wife`
-      ) : (
-        `${partnerName} is my husband`
-      ))}
-    </p>
-  </section>
-);
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && ( // no more ternary operator
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {/* isMarried made into ternary operator */}
+        {isMarried === false ? 'I am not married' : partnerType}
+      </p>
+    </section>
+  );
+};
