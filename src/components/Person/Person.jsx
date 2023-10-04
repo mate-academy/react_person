0725a3e14aead9e6e7faef9908c1a9a6c03f5411
Olena Shchipina <shipina.olena@gmail.com>
2023-10-04 17:43:16 +0300

    react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+function getMarried({ isMarried, sex, partnerName }) {
+  if (!isMarried) {
+    return 'I am not married';
+  }
+
+  if (sex === 'm') {
+    return `${partnerName} is my wife`;
+  }
+
+  return `${partnerName} is my husband`;
+}
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+
+    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>
+    }
+    <p className="Person__partner">{getMarried(person)}</p>
+  </section>
+);
