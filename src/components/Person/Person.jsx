0c8bd3aa68e3a10253b1a366e0d8971f3f445bd5
Olena Shchipina <shipina.olena@gmail.com>
2023-10-04 18:53:47 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,32 @@
 import './Person.scss';
 
-function getMarried({ isMarried, sex, partnerName }) {
-  if (!isMarried) {
-    return 'I am not married';
-  }
-
-  if (sex === 'm') {
-    return `${partnerName} is my wife`;
-  }
-
-  return `${partnerName} is my husband`;
-}
-
-export const Person = ({ person }) => (
+export const Person = ({
+  person: {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  },
+}) => (
   <section className="Person">
     <h2 className="Person__name">
-      {`My name is ${person.name}`}
+      {`My name is ${name}`}
     </h2>
 
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>
-    }
-    <p className="Person__partner">{getMarried(person)}</p>
+    {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+    )}
+    <p className="Person__partner">
+      {
+        isMarried
+          ? `${partnerName} is my ${sex === 'm'
+            ? 'wife'
+            : 'husband'}`
+          : 'I am not married'
+      }
+    </p>
   </section>
 );
