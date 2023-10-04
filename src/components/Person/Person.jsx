c1fcab9bb14f1a6496670b4e6684d4a4eef4cdc4
donizer <bogdan.ua45@gmail.com>
2023-10-04 13:10:18 +0300

    nested ternary to variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      { `My name is ${person.name}` }
-    </h2>
+export const Person = ({ person }) => {
+  const partnerType = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.age ? (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        { `My name is ${person.name}` }
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+            {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {
+          person.isMarried
+            ? `${person.partnerName} is my ${partnerType}`
+            : 'I am not married'
+        }
       </p>
-    ) : ''}
-
-    <p className="Person__partner">
-      { person.isMarried ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}` : 'I am not married'}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
