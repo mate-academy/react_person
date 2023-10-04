f2109b4c6e647c0884a74efd319a19b5f56b535c
OlhaKozuik <gadyakaban@gmail.com>
2023-10-04 16:04:12 +0300

    coorect Partner conponent

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,17 +4,15 @@ export const Person = ({ person: {
   isMarried,
   partnerName,
 } }) => (
-  <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && (
-        <p className="Person__age">{`I am ${age}`}</p>
-      )}
-      {isMarried ? (
-        <p className="Person__partner">{`${partnerName} is my husband`}</p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
-    </section>
-  </div>
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${name}`}</h2>
+    {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+    )}
+    {isMarried ? (
+      <p className="Person__partner">{`${partnerName} is my husband`}</p>
+    ) : (
+      <p className="Person__partner">I am not married</p>
+    )}
+  </section>
 );
