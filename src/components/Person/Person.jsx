54d1b42ffa8cacf1fb772eeab6a17b570bc9bb1b
YevhenKharko <yevhenkharko@gmail.com>
2023-10-04 20:43:03 +0300

    task completed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName, sex } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age ? <p className="Person__age">{`I am ${age}`}</p> : null}
+
+      <p className="Person__partner">
+        {!isMarried
+          ? 'I am not married'
+          : `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`}
+      </p>
+    </section>
+  );
+};
