be0c986f38edbbbcd5da33ba78b1a72d00c5b718
Ivanna Zaiets <iva.zaiets@gmail.com>
2023-10-04 17:31:42 +0300

    solution react person task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person: {
+  name,
+  age,
+  sex,
+  isMarried,
+  partnerName,
+} }) => (
+  <>
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age === undefined ? (
+        null
+      ) : (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {!isMarried ? (
+          'I am not married'
+        ) : (
+          `${partnerName} is my ${sex === 'f' ? (
+            'husband'
+          ) : (
+            'wife'
+          )}`
+        )}
+      </p>
+    </section>
+  </>
+);
