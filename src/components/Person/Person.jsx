052bd6003d6ad6a5f2d0702fcc477d5bb0805313
BetterSol <94358102+BetterSol@users.noreply.github.com>
2023-10-05 16:25:24 +0300

    code changed according to mentor's suggestions

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,8 +10,7 @@ export const Person = ({ person }) => {
         {`My name is ${name}`}
       </h2>
 
-      {age && <p className="Person__age">{`I am ${age}`}</p>
-      }
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
 
       <p className="Person__partner">
         { isMarried
