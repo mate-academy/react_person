7e698159e9df81e7577386eedc5c2398a7ce420f
Danylo Morhun <ayogoharo@gmail.com>
2023-10-05 15:25:47 +0300

    make destructuring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,31 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${(person.age)}`}
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${(age)}`}
+        </p>
+      )}
+      <p className="Person__partner">
+        {(isMarried)
+          ? `${(sex === 'm')
+            ? `${partnerName} is my wife`
+            : `${partnerName} is my husband`}`
+          : 'I am not married'}
       </p>
-    )}
-    <p className="Person__partner">
-      {(person.isMarried)
-        ? `${(person.sex === 'm')
-          ? `${person.partnerName} is my wife`
-          : `${person.partnerName} is my husband`}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
