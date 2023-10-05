4c6d40a884911ff1051e503dd94c52a527eae064
BetterSol <94358102+BetterSol@users.noreply.github.com>
2023-10-05 16:11:38 +0300

    changed after review

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,27 +1,24 @@
-export const Person = ({
-  person: {
-    name,
-    age,
-    sex,
-    isMarried,
-    partnerName,
-  },
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
+import './Person .scss';
 
-    {(!age)
-      ? ''
-      : <p className="Person__age">{`I am ${age}`}</p>
-    }
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partnerRole = sex === 'f' ? 'husband' : 'wife';
 
-    <p className="Person__partner">
-      { isMarried
-        ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
-        : 'I am not married'
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && <p className="Person__age">{`I am ${age}`}</p>
       }
-    </p>
-  </section>
-);
+
+      <p className="Person__partner">
+        { isMarried
+          ? `${partnerName} is my ${partnerRole}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
