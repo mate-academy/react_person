183f919864dea5f1f6dd1da01fc702b655257d7d
Oleksandr <dedikad@gmail.com>
2023-10-05 10:12:25 +0200

    done1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,23 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const personSex = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.age && (
-      <p className="Person__age">
-          {`I am ${person.age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+            {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {!person.isMarried
+          ? 'I am not married'
+          : `${person.partnerName} is my ${personSex}`}
       </p>
-    )}
-
-    <p className="Person__partner">
-      {!person.isMarried
-        ? 'I am not married'
-        : `${person.partnerName} is my ${person.sex === 'm'
-          ? 'wife'
-          : 'husband'
-        }`}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
