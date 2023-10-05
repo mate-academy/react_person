b43a93bd04c891afc246e00ae780d2f3f779053f
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-10-05 09:35:41 +0300

    added recommended changes

--- src/components/Person/Person/Person.jsx
+++ src/components/Person/Person/Person.jsx
@@ -2,8 +2,10 @@ export const Person = ({ person }) => (
   <>
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age !== undefined && (
-        <p className="Person__age">{`I am ${person.age}`}</p>
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
       )}
       <p className="Person__partner">
         {person.isMarried
