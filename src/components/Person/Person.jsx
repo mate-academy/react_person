e31d5abe0961a334efc9440020a31e748f9f726c
OlhaKozuik <gadyakaban@gmail.com>
2023-10-05 11:36:35 +0300

    delete unessesery  ternar operator

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,22 +8,28 @@ export const Person = ({
     isMarried,
     partnerName,
   },
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${name}`}
-    </h2>
+}) => {
+  const partnerGender = sex === 'm'
+    ? 'wife'
+    : 'husband';
 
-    {age && (
-      <p className="Person__age">
-        {`I am ${age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerGender}`
+          : 'I am not married'}
       </p>
-    )}
-
-    <p className="Person__partner">
-      {isMarried
-        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
