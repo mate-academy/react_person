f91bfd4ab4042ab50daa3ae9a86a569657fdea49
YevhenKharko <yevhenkharko@gmail.com>
2023-10-05 10:47:39 +0300

    implemented some fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,24 @@
-export const Person = ({ person }) => {
-  const { name, age, isMarried, partnerName, sex } = person;
+export const Person = (props) => {
+  const {
+    name,
+    age,
+    isMarried,
+    partnerName,
+    sex,
+  } = props.person;
+
+  const definePartner = `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`;
 
   return (
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {age ? <p className="Person__age">{`I am ${age}`}</p> : null}
+
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
 
       <p className="Person__partner">
-        {!isMarried
-          ? 'I am not married'
-          : `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`}
+        {!isMarried ? 'I am not married' : definePartner}
       </p>
     </section>
   );
