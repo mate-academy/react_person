d50a6da988fa57ae0ab6f46422729047f34dc438
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-10-06 12:33:18 +0300

    change a logic little bit

--- src/components/Person/Person/Person.jsx
+++ src/components/Person/Person/Person.jsx
@@ -5,24 +5,31 @@ export const Person = ({
     isMarried,
     partnerName,
     sex,
+  },
+}) => {
+  let partnerMessage = 'I am not married';
+
+  if (isMarried) {
+    partnerMessage = `${partnerName} is my ${sex === 'f'
+      ? 'husband'
+      : 'wife'}`;
   }
-}) => (
-  <>
+
+  return (
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${sex === 'f'
-            ? `husband` : `wife`}`
-          : `I am not married`}
+        {partnerMessage}
       </p>
     </section>
-  </>
-);
+  );
+};
