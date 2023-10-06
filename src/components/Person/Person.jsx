6f54d564d00e273c3d8362fd9413c1fc8e3daa01
Daniil <danil.beshlebnov@gmail.com>
2023-10-06 17:46:48 +0300

    create props users

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const nameText = `My name is ${person.name}`;
+  const ageText = person.age ? `I am ${person.age}` : null;
+  let partnerText = '';
+
+  if (person.isMarried === false) {
+    partnerText = 'I am not married';
+  } else {
+    partnerText = person.sex === 'm'
+      ? `${person.partnerName} is my wife`
+      : `${person.partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{nameText}</h2>
+      <p className="Person__age">{ageText}</p>
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
