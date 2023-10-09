1a0c0ac1debbdbcb80cbe379411492cf3a0b24be
Nazar <bidenkonazar@gmail.com>
2023-10-09 13:11:06 +0300

    code style change

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -17,14 +17,18 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       { age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
       )}
+
       <p className="Person__partner">
-        {(isMarried) ? `${partnerName} is my ${partnerStatus}`
-          : `I am not married`
+        {
+          isMarried
+            ? `${partnerName} is my ${partnerStatus}`
+            : `I am not married`
         }
       </p>
     </section>
