a690e569237f072fdaf5c5628aaaa1566dd3260b
Denys <denzzlle171@gmail.com>
2023-10-09 13:32:04 +0300

    codestyle2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,14 +12,14 @@ export const Person = ({
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+
       {age && <p className="Person__age">{`I am ${age}`}</p>}
-      {isMarried ? (
-        <p className="Person__partner">
-          {`${partnerName} is my ${relationship}`}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${relationship}`
+          : 'I am not married'}
+      </p>
     </section>
   );
 };
