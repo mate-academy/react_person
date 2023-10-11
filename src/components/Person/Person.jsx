5d9cff47da980205d8809ea950a045d8029b85c9
Krzysztof Janiewicz <spojrzenie@gmail.com>
2023-10-11 22:25:40 +0200

    Clean some code in Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,35 +1,34 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {' '}
-      {person.name}
-    </h2>
+// eslint-disable-next-line
+export const Person = ({ person }) => {
+  const myName = `My name is ${person.name}`;
+  const myAge = `I am ${person.age}`;
+  const myPartner = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.age ? (
-      <p className="Person__age">
-        I am
-        { ' ' }
-        {person.age}
-      </p>
-    ) : (
-      null
-    )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {myName}
+      </h2>
 
-    {person.isMarried === true ? (
-      <p className="Person__partner">
-        {person.partnerName}
-        {' '}
-        is my
-        {' '}
-        {person.sex === 'm' ? 'wife' : 'husband'}
-      </p>
-    ) : (
-      <p className="Person__partner">
-        I am not married
-      </p>
-    )}
-  </section>
-);
+      {person.age ? (
+        <p className="Person__age">
+          {myAge}
+        </p>
+      ) : (
+        null
+      )}
+
+      {person.isMarried === true ? (
+        <p className="Person__partner">
+          {`${person.partnerName} is my ${myPartner}`}
+        </p>
+      ) : (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      )}
+    </section>
+  );
+};
