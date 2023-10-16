b3438c6be82384c1d20dc047476bab3fbac063a1
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-16 16:47:13 +0300

    fix Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,32 @@
-export const Person = ({ person }) => (
-
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-
-    {person.age
-    && <p className="Person__age">{`I am ${person.age}`}</p>
-    }
-
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {`${person.partnerName} is my `}
-        {person.sex === 'm' ? 'wife' : 'husband'}
-      </p>
-    )
-      : (
+export const Person = ({ person }) => {
+  const getPersonStatus = ({ isMarried, sex, partnerName }) => (
+    <>
+      {isMarried ? (
         <p className="Person__partner">
-          I am not married
+          {`${partnerName} is my `}
+          {sex === 'm' ? 'wife' : 'husband'}
         </p>
-      )}
-  </section>
-);
+      )
+        : (
+          <p className="Person__partner">
+            I am not married
+          </p>
+        )}
+    </>
+  );
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age
+      && <p className="Person__age">{`I am ${person.age}`}</p>
+      }
+
+      {getPersonStatus(person)}
+
+    </section>
+  );
+};
