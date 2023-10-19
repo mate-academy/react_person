d66d8dc3d80f79110ab483e4ffa57b2339bccdbd
Olha Rypich <olha.rypich@gmail.com>
2023-10-19 13:23:32 +0300

    react-person_changed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,25 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const partnerText = `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`;
 
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried
+          ? partnerText
+          : 'I am not married'
+        }
       </p>
-    )}
 
-    <p className="Person__partner">
-      {person.isMarried
-        ? (`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`)
-        : ('I am not married')}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
