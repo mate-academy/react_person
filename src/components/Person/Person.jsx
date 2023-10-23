d13fadc5f89a21db9122d82d083fab7552250145
Dmytro Lazarenko <lazarenkodmytro@gmail.com>
2023-10-23 23:40:48 +0200

    add task react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person || {};
+
+  let partnerStatus = '';
+
+  if (isMarried) {
+    if (sex === 'm') {
+      partnerStatus = `${partnerName} is my wife`;
+    } else {
+      partnerStatus = `${partnerName} is my husband`;
+    }
+  } else {
+    partnerStatus = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
+      <p className="Person__partner">{partnerStatus}</p>
+    </section>
+  );
+};
