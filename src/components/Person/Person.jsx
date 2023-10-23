a5f4f74b239fe9e1cbd70ddab47657da18874fec
Volodymyr Konoplianyi <volodymyr.konoplianyi@gmail.com>
2023-10-23 18:12:50 +0300

    react_person solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+function getMarriageStatus(person) {
+  return person.sex === 'm'
+    ? `${person.partnerName} is my wife`
+    : `${person.partnerName} is my husband`;
+}
+
+export const Person = ({ person }) => (
+  <>
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is `}
+        {person.name}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am `}
+          {person.age}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried
+          ? getMarriageStatus(person)
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  </>
+);
