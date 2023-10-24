8e7c3e8b01a2942594e77a86a04033dde7c2dbbf
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2023-10-24 13:48:12 +0300

    f3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,12 +2,18 @@ import '../../App.scss';
 
 const notMarried = 'I am not married';
 
-export const Person = ({ name, age, sex, isMarried, partnerName }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${name}`}</h2>
-    {age && (<p className="Person__age">{age && `I am ${age}`}</p>)}
-    <p className="Person__partner">
-      {isMarried ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}` : notMarried}
-    </p>
-  </section>
-);
+export const Person = ({ name, age, sex, isMarried, partnerName }) => {
+  const marriedType = sex === 'f' ? 'husband' : 'wife';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (<p className="Person__age">{age && `I am ${age}`}</p>)}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${marriedType}`
+          : notMarried}
+      </p>
+    </section>
+  );
+};
