658efa347a631388534d638f07139656627852fb
Nataliia Kukushkina <n.kukushkina@gmail.com>
2023-10-28 17:50:01 +0300

    updates

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,34 +1,27 @@
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  let partnerType = '';
+  const { name, age, sex, isMarried, partnerName } = person;
 
-  if (person.isMarried && person.sex === 'm') {
-    partnerType = 'wife';
-  }
-
-  if (person.isMarried && person.sex === 'f') {
-    partnerType = 'husband';
-  }
+  const partnerType = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {`My name is ${person.name}`}
+        {`My name is ${name}`}
       </h2>
-      {person.age && (
+      {age && (
         <p className="Person__age">
           <span>I am </span>
-          {person.age}
+          {age}
         </p>
       )}
       <p className="Person__partner">
-        {person.isMarried ? (
-          `${person.partnerName} is my ${partnerType}`
+        {isMarried ? (
+          `${partnerName} is my ${partnerType}`
         ) : (
           'I am not married'
         )}
-
       </p>
     </section>
   );
