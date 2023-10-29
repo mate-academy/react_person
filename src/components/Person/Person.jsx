41ca3ca8f14ff040691736bea6e4e320bb51b327
Ruslan Hrynchuk <r.momos@gmail.com>
2023-10-29 20:02:47 +0200

    add separate variable partnerRole

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,12 +9,16 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
+  const partnerRole = sex === 'f'
+    ? 'husband'
+    : 'wife';
+
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
       {isMarried
-        ? <p className="Person__partner">{`${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`}</p>
+        ? <p className="Person__partner">{`${partnerName} is my ${partnerRole}`}</p>
         : <p className="Person__partner">I am not married</p>
       }
     </section>
