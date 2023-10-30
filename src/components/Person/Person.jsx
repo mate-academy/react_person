d351163933e01794c18229b212f026911e6bdcab
NataliMax <maximovs@1876gmail.com>
2023-10-30 19:52:29 +0100

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,30 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age > 0 && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-    )}
+export const Person = ({ person }) => {
+  const personSex = person.sex === 'f' ? (
+    `${person.partnerName} is my husband`
+  ) : (
+    `${person.partnerName} is my wife`
+  );
 
-    {person.isMarried === false ? (
-      <p className="Person__partner">
-        I am not married
-      </p>
-    ) : (
-      <p className="Person__partner">
-        {person.sex === 'f' ? (
-          `${person.partnerName} is my husband`
-        ) : (
-          `${person.partnerName} is my wife`
-        )}
-      </p>
-    )}
-  </section>
-);
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age > 0 && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      {!person.isMarried ? (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      ) : (
+        <p className="Person__partner">
+          {personSex}
+        </p>
+      )}
+    </section>
+  );
+};
