d5df59da33670f37505fe98a331a46fcec241734
Ольга Салагуб <oli@MacBook-Pro-Olga.local>
2023-11-02 15:25:10 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,44 @@
 // export const Person = ({ person }) => ();
+
+export const Person = ({
+  person: {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  },
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      My name is
+      {' '}
+      {name}
+    </h2>
+
+    {
+    age ? (
+      <p className="Person__age">
+        I am
+        {' '}
+        {age}
+      </p>
+    ) : null
+    }
+    {
+      isMarried ? (
+        <p className="Person__partner">
+          {partnerName}
+          {' '}
+          is my
+          {' '}
+          {sex === 'f' ? 'husband' : 'wife'}
+        </p>
+      ) : (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      )
+    }
+  </section>
+);
