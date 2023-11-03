0a0499e4a50377052ebf23b3e187a582ed4111b8
Evhen Teneta <tenetaevgen@gmail.com>
2023-11-03 18:53:37 +0200

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    {
+      person.age > 0 && (
+        <p className="Person__age">
+          I am
+          {` ${person.age}`}
+        </p>
+      )}
+
+    {
+      person.isMarried === false
+        ? (`I am not married`)
+        : (
+          (person.sex === 'm'
+            && (`${person.partnerName} is my wife`)
+          )
+          || (person.sex === 'f'
+              && (`${person.partnerName} is my husband`)
+          )
+        )
+    }
+  </section>
+);
