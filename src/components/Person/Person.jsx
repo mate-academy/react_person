b6a8b1a5d1cfe4563107f68db9bfd9a4cdd2ff48
Ivan <your@mail.com>
2023-11-05 16:42:15 +0200

    Person components are create!

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
 // export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const manOrWoman = person.sex === 'm'
+    ? `${person.partnerName} is my wife`
+    : `${person.partnerName} is my husband`;
+
+  const marriedOrNo = person.isMarried ? manOrWoman : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {Object.hasOwn(person, 'age') && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
+
+      <p className="Person__partner">{`${marriedOrNo}`}</p>
+    </section>
+  );
+};
