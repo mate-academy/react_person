9cbf97885021a3be82e18e74e6ff17ddbf8a3e65
Ivan <your@mail.com>
2023-11-05 17:09:07 +0200

    rework logic of destructure

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,18 @@
 // export const Person = ({ person }) => ();
 export const Person = ({ person }) => {
-  const manOrWoman = person.sex === 'm'
-    ? `${person.partnerName} is my wife`
-    : `${person.partnerName} is my husband`;
+  const { name, partnerName, sex, age, isMarried } = person;
 
-  const marriedOrNo = person.isMarried ? manOrWoman : 'I am not married';
+  const manOrWoman = sex === 'm'
+    ? `${partnerName} is my wife`
+    : `${partnerName} is my husband`;
+
+  const marriedOrNo = isMarried ? manOrWoman : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {Object.hasOwn(person, 'age') && (
-        <p className="Person__age">{`I am ${person.age}`}</p>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
       )}
 
       <p className="Person__partner">{`${marriedOrNo}`}</p>
