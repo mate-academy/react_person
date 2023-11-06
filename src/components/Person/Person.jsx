7085e435ec48c238fd15b68a2c5b47902e14a65e
VKdrvtsv <valery.volikova1@gmail.com>
2023-11-06 15:37:28 +0200

    fix my code according to comments and whith mentor on the chat

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,19 +4,22 @@ export const Person = ({ person }) => (
       {`My name is ${person.name}`}
     </h2>
 
-    {person.age !== undefined
-    && (
-    <p className="Person__age">
+    {person.age !== undefined && (
+      <p className="Person__age">
         {`I am ${person.age}`}
-    </p>
+      </p>
     )}
+
     <p className="Person__partner">
-      {!person.isMarried && <p>I am not married</p> }
-      {(person.isMarried && person.sex === 'm')
-        && `${person.partnerName} is my wife`}
-      {(person.isMarried && person.sex === 'f')
-        && `${person.partnerName} is my husband`
-      }
+      {!person.isMarried && <p>I am not married</p>}
+      {person.isMarried && (
+        person.sex === 'm'
+          ? (
+            `${person.partnerName} is my wife`
+          ) : (
+            `${person.partnerName} is my husband`
+          )
+      )}
     </p>
   </>
 );
