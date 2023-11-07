4bc2541205677bfe821b1a9739b1ee4e02c2aa18
bohdanliutyi <liutyibohdan13@gmaail.com>
2023-11-07 17:15:08 +0200

    just minor fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,10 @@ import { Person } from './components/Person/Person';
 
 export const App = () => (
   <div className="App">
-    <Person
-      person={people.misha}
-    />
+    <Person person={people.misha} />
 
-    <Person
-      person={people.olya}
-    />
+    <Person person={people.olya} />
 
-    <Person
-      person={people.alex}
-    />
+    <Person person={people.alex} />
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,8 +5,18 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <h2 className="Person__name">
+        {' '}
+        My name is
+        {name}
+      </h2>
+      {age && (
+      <p className="Person__age">
+        {' '}
+        I am
+        {age}
+      </p>
+      )}
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${indefityPerson}`
