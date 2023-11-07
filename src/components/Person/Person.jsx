33c04b33ccca286bf9247d8aad1977baa85cf088
Анастасия Бурковская <burkovskaya.anastasiya@gmail.com>
2023-11-07 17:48:52 +0200

    react_person_was_checked

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,19 @@
-export const Person = ({
-  person,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
 
-    {person.age ? <p className="Person__age">{`I am ${person.age}`}</p> : null }
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+      {age ? <p className="Person__age">{`I am ${age}`}</p> : null }
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
