2d8918dc2ab37b963a2bce2351035ecdeb094c94
Анастасия Бурковская <burkovskaya.anastasiya@gmail.com>
2023-11-07 17:57:50 +0200

    react_person_wasChecked

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,10 +4,10 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">
-        {`My name is ${name}`}
+        My name is {name}
       </h2>
 
-      {age ? <p className="Person__age">{`I am ${age}`}</p> : null }
+      {age && <p className="Person__age">I am {age}</p>}
 
       <p className="Person__partner">
         {isMarried
