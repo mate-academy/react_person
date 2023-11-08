82291d391b59679dd239bc7ac5b43a90237162ec
Oleg Klivanets <oleg.klivanets@gmail.com>
2023-11-08 14:28:19 +0200

    fix task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,10 +9,6 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
-  const showAge = age
-    ? <p className="Person__age">{`I am ${age}`}</p>
-    : null;
-
   const partnerSex = sex === 'm'
     ? 'wife'
     : 'husband';
@@ -20,7 +16,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {showAge}
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${partnerSex}`
