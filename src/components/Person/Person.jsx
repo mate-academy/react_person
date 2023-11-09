ad672eeade50a633daac9e6f8bcd0b335150a5b9
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2023-11-09 11:46:27 +0000

    added variable partner

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,25 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const partner = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.age !== undefined && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
 
-    {person.isMarried ? (
-      <>
-        <p className="Person__partner">
-          {`${person.partnerName} is my `}
-          {person.sex === 'm' ? (
-            'wife'
-          ) : (
-            'husband'
-          )}
-        </p>
-      </>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+      {person.age && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
+
+      {person.isMarried ? (
+        <>
+          <p className="Person__partner">
+            {`${person.partnerName} is my ${partner}`}
+          </p>
+        </>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
