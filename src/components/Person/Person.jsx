904a59cbf3ec31424b8958393fffdbd41f682211
Vladimir Kolesnikov <9858713@gmail.com>
2023-11-13 17:27:06 +0200

    refactoring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,20 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && (<p className="Person__age">{`I am ${person.age}`}</p>)}
+export const Person = ({ person }) => {
+  const partnerType = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.isMarried && (
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && (<p className="Person__age">{`I am ${person.age}`}</p>)}
+
+      {person.isMarried && (
       <p className="Person__partner">
-        {`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
+        {`${person.partnerName} is my ${partnerType}`}
       </p>
-    )}
+      )}
 
-    {person.isMarried === false && (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+      {person.isMarried === false && (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
