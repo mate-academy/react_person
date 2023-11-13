36612931232337ed1961e1cdec97a38f6c410567
Maksym Svoboda <maksym.svoboda.kn.2021@lpnu.ua>
2023-11-13 12:21:43 +0200

    fixed code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,16 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+export const Person
+= ({ person: { name, age, isMarried, partnerName, sex } }) => {
+  const partnerStatusName = sex === 'f' ? 'husband' : 'wife';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+      <p className="Person__partner">
+        {isMarried ? `${partnerName} is my ${partnerStatusName}` : 'I am not married'}
+      </p>
+    </section>
+  );
+};
