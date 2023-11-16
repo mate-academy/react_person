d20898e8680d5907ece15078c518228699a32d1e
Fluebubble <Trots.anatoly@gmail.com>
2023-11-16 19:49:56 +0200

    added code formatting

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,7 +6,9 @@ export const Person = ({
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">
-        {isMarried ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}` : 'I am not married'}
+        {isMarried
+          ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
+          : 'I am not married'}
       </p>
     </section>
   </>
