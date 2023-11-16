981b1c32e733f0a0f89d9c80d7c88c4b519c4a8d
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-11-16 17:17:16 +0200

    task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = (
+  { person: { name, age, sex, isMarried, partnerName } },
+) => {
+  const partner = sex === 'm' ? 'wife' : 'husband';
+
+  const getPartner = () => {
+    if (isMarried) {
+      return `${partnerName} is my ${partner}`;
+    }
+
+    return 'I am not married';
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+      )}
+      <p className="Person__partner">{getPartner()}</p>
+    </section>
+  );
+};
