db6bd3ade5629a9bb9b552ee78cb4c5d398ecd14
Dmytro Yefimov <red.eye77seven@gmail.com>
2023-11-16 12:12:45 +0200

    uprade codeStyle

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,8 +6,15 @@ export const Person = (
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age ? (<p className="Person__age">{`I am ${age}`}</p>) : ''}
-      <p className="Person__partner">{isMarried ? (`${partnerName} is my ${partner}`) : 'I am not married'}</p>
+      {age && (
+      <p className="Person__age">{`I am ${age}`}</p>
+      )}
+      <p className="Person__partner">
+        {isMarried
+          ? (`${partnerName} is my ${partner}`)
+          : ('I am not married')
+        }
+      </p>
     </section>
   );
 };
