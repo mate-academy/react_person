c0e90b628da439762954b60bda6b0fd8f9027438
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2023-11-18 00:26:22 +0200

    fix magic values

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,28 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-    {person.age && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    )}
+  const MALE = 'm';
+  const spouse = sex === MALE ? 'wife' : 'husband';
 
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'
-      }
-    </p>
-  </section>
-);
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {person.age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${spouse}`
+          : 'I am not married'
+        }
+      </p>
+    </section>
+  );
+};
