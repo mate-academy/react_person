394274d43379d7e44034b820c85af12890b0eb74
Zyrak Nur <noorzirak7@gmail.com>
2023-11-18 19:55:29 +0000

    add Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = props => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${props.person.name}`}</h2>
+
+    {props.person.age && (
+      <p className="Person__age">{`I am ${props.person.age}`}</p>
+    )}
+
+    <p className="Person__partner">
+      {props.person.isMarried ? (
+        `${props.person.partnerName} is my ${props.person.sex === 'm' ? 'wife' : 'husband'}`
+      ) : 'I am not married'}
+    </p>
+  </section>
+);
