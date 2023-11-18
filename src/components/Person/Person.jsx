c9e263ab4f47b4216c96c151aad6d34fafa8208c
Fluebubble <Trots.anatoly@gmail.com>
2023-11-18 13:41:21 +0200

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,18 @@
-export const Person = ({
-  person: { name, age, sex, partnerName, isMarried },
-}) => (
-  <>
-    <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
-      <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
-          : 'I am not married'}
-      </p>
-    </section>
-  </>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, partnerName, isMarried } = person;
+  const SEX_FEMALE = 'f';
+
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
+        <p className="Person__partner">
+          {isMarried
+            ? `${partnerName} is my ${sex === SEX_FEMALE ? 'husband' : 'wife'}`
+            : 'I am not married'}
+        </p>
+      </section>
+    </>
+  );
+};
