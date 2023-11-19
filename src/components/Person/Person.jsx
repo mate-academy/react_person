6b0650283619dbab9a556c2934c0b7645e56ad4b
Zyrak Nur <noorzirak7@gmail.com>
2023-11-19 08:38:28 +0000

    improve code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,19 @@
-export const Person = props => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${props.person.name}`}</h2>
+export const Person = ({ person }) => {
+  const PARTNER_SEX = person.sex === 'm' ? 'wife' : 'husband';
 
-    {props.person.age && (
-      <p className="Person__age">{`I am ${props.person.age}`}</p>
-    )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-    <p className="Person__partner">
-      {props.person.isMarried ? (
-        `${props.person.partnerName} is my ${props.person.sex === 'm' ? 'wife' : 'husband'}`
-      ) : 'I am not married'}
-    </p>
-  </section>
-);
+      {person.age && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried ? (
+          `${person.partnerName} is my ${PARTNER_SEX}`
+        ) : 'I am not married'}
+      </p>
+    </section>
+  );
+};
