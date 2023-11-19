3f269765c19f3e9be05425babc4a41f39fcebdd7
Softjey <misivsvatoslav@gmail.com>
2023-11-19 18:12:24 +0100

    fix linter bug, destructurize object Person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,28 +1,28 @@
-export const Person = ({ person }) => (
+export const Person = (
+  { person: {
+    name,
+    sex,
+    age = 0,
+    isMarried,
+    partnerName,
+  } },
+) => (
   <section className="Person">
     <h2 className="Person__name">
-      My name is
-      {person.name}
+      {`My name is ${name}`}
     </h2>
 
-    {
-      person.hasOwnProperty('age') && (
-      <p className="Person__age">
-        I am
-        {person.age}
-      </p>
-      )
-    }
+    {age > 0 && (
+    <p className="Person__age">
+        {`I am ${age}`}
+    </p>
+    )}
 
     {
-      person.isMarried
+      isMarried
         ? (
           <p className="Person__partner">
-            {
-            `${person.partnerName} is my `
-
-            + `${person.sex === 'f' ? 'husband' : 'wife'}`
-          }
+            {`${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`}
           </p>
         )
         : (
