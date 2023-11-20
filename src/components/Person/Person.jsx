15f7f023f0704107311600fc06221b1f3994a82f
Fluebubble <Trots.anatoly@gmail.com>
2023-11-20 15:07:12 +0200

    added formatting and new var

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,23 @@
 export const Person = ({ person }) => {
   const { name, age, sex, partnerName, isMarried } = person;
   const SEX_FEMALE = 'f';
+  const partner = sex === SEX_FEMALE ? 'husband' : 'wife';
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
-        {age && <p className="Person__age">{`I am ${age}`}</p>}
-        <p className="Person__partner">
-          {isMarried
-            ? `${partnerName} is my ${sex === SEX_FEMALE ? 'husband' : 'wife'}`
-            : 'I am not married'}
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
         </p>
-      </section>
-    </>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'}
+      </p>
+    </section>
   );
 };
