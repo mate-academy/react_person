b4ca4d8daa214f1efd232919bf6b35cdab4f2215
Maria Berezhna <marua545@gmail.com>
2023-11-20 16:55:06 +0200

    react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = (props) => {
+  const { name, age, sex, isMarried, partnerName } = props.person;
+
+  const partnerTitle = sex === 'm' ? 'wife' : 'husband';
+
+  const myPartnerTitle = isMarried
+    ? `${partnerName} is my ${partnerTitle}`
+    : `I am not married`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {Number.isFinite(age) && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {myPartnerTitle}
+      </p>
+    </section>
+  );
+};
