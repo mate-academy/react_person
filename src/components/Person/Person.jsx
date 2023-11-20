4c1e34a17827d49ec3c16459392de4657e3c9700
Viktor Shaparenko <vkshadyafk@gmail.com>
2023-11-20 14:53:20 +0200

    fix mistake

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,24 +6,24 @@ export const Person = ({ person }) => {
     isMarried,
     partnerName,
   } = person;
-
-  const withPartner = sex === 'm'
-    ? `${partnerName} is my wife`
-    : `${partnerName} is my husband`;
+  const MALE_SEX = 'm';
+  const partnerSexLabel = sex === MALE_SEX
+    ? 'wife'
+    : 'husband';
+  const partner = isMarried
+    ? `${partnerName} is my ${partnerSexLabel}`
+    : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && (
       <p className="Person__age">
-        {age === undefined ? null : `I am ${age}`}
+        {`I am ${age}`}
       </p>
       )}
       <p className="Person__partner">
-        {isMarried
-          ? withPartner
-          : 'I am not married'
-        }
+        {partner}
       </p>
     </section>
   );
