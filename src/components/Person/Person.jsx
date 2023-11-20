2c842b0b1da8a0094e810debb2a08a32e3d3744e
Oleksii Mantserov <a.v.mantserov@gmail.com>
2023-11-20 03:01:56 +0200

    person done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    {person.age
+      ? <p className="Person__age">{`I am ${person.age}`}</p>
+      : ''}
+    {person.sex === 'm' && person.isMarried === true
+      ? <p className="Person__partner">{`${person.partnerName} is my wife`}</p>
+      : ''
+    }
+    {person.sex === 'f' && person.isMarried === true
+      ? <p className="Person__partner">{`${person.partnerName} is my husband`}</p>
+      : ''
+    }
+    {person.isMarried === false
+      ? <p className="Person__partner">I am not married</p>
+      : ''}
+  </section>
+);
