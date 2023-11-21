c603e8153c84078b851033c44a013cb0812f7856
Yuriy Sobakar <raizen.dj@gmail.com>
2023-11-21 20:19:31 +0200

    Fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,9 +6,9 @@ export const Person = ({ person }) => {
     <>
       <section className="Person">
         <h2 className="Person__name">{`My name is ${name}`}</h2>
-        {age
-          ? <p className="Person__age">{`I am ${age}`}</p>
-          : null }
+        {age && (
+          <p className="Person__age">{`I am ${age}`}</p>
+        )}
 
         <p className="Person__partner">
           {isMarried
