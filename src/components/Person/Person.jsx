a9d8770f71b729c07b146fd1155e634406166d5f
Adeveloper <feduniy@gmail.com>
2023-11-22 22:31:28 +0200

    simplified the Peson component html

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,28 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const relationshipStatus = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.age && (
-    <p className="Person__age">
-      {`I am ${person.age}`}
-    </p>
-    )}
+  const partnerText = person.isMarried
+    ? `${person.partnerName} is my ${relationshipStatus}`
+    : 'I am not married';
 
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
+  return (
+
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
       </p>
-    ) : (
+      )}
+
       <p className="Person__partner">
-        I am not married
+        {partnerText}
       </p>
-    )}
-  </section>
-);
+    </section>
+  );
+};
