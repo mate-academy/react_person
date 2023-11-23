68cfdec9619819fd034e902ca5937c73b51a6210
Oleksii Mantserov <a.v.mantserov@gmail.com>
2023-11-23 13:42:33 +0200

    react_person done..?

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,19 @@
-export const Person = ({
-  person,
-  partner = person.sex === 'm' ? 'wife' : 'husband',
-}) => (
+const MEN_SEX = 'm';
 
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const { name, age, partnerName } = person;
+  const partner = person.sex === MEN_SEX ? 'wife' : 'husband';
 
-    {person.age >= 0 && <p className="Person__age">{`I am ${person.age}`}</p>}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-    {person.isMarried ? (
-      <p className="Person__partner">{`${person.partnerName} is my ${partner}`}</p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+      {person.age >= 0 && <p className="Person__age">{`I am ${age}`}</p>}
+
+      {person.isMarried
+        ? <p className="Person__partner">{`${partnerName} is my ${partner}`}</p>
+        : <p className="Person__partner">I am not married</p>
+      }
+    </section>
+  );
+};
