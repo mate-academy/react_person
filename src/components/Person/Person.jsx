8a5a27444cc45dadab8d214adbd425fc628f481b
shikoryak <oksanashykoriak@gmail.com>
2023-11-27 14:13:33 +0200

    resolved comments

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,13 +2,16 @@ import './Person.scss';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
+  const partner = isMarried
+    ? `${partnerName} is my ${sex === 'm'
+      ? 'wife'
+      : 'husband'}`
+    : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {name}
+        {`My name is ${name}`}
       </h2>
       {age && (
         <p className="Person__age">
@@ -18,12 +21,7 @@ export const Person = ({ person }) => {
         </p>
       )}
       <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${sex === 'm'
-            ? 'wife'
-            : 'husband'}`
-          : 'I am not married'
-        }
+        { partner }
       </p>
     </section>
   );
