8761a315b3771bde3dbc318d9179706062b12a4c
Laura Wronska <laura.wronskaa@gmail.com>
2023-11-27 23:05:40 +0100

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = (props) => {
+  const { name, age, isMarried, sex, partnerName } = props.person;
+  let relationship;
+
+  if (isMarried) {
+    const spouse = sex === 'm' ? 'wife' : 'husband';
+
+    relationship = <p className="Person__partner">{`${partnerName} is my ${spouse}`}</p>;
+  } else {
+    relationship = <p className="Person__partner">I am not married</p>;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )
+      }
+
+      {relationship}
+    </section>
+  );
+};
