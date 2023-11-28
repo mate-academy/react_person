67cb81553f0a7f4f5ae510f3dbd62df1c9e68785
Marcin Ligmann <marcinligmann2@gmail.com>
2023-11-28 12:17:54 +0100

    destructuring person values

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,32 @@
-export function Person({ person }) {
+export function Person({ person: { name, age, sex, isMarried, partnerName } }) {
   let typeOfPartner = 'wife';
 
-  if (person.sex === 'f') {
+  if (sex === 'f') {
     typeOfPartner = 'husband';
   }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age ? <p className="Person__age">I am {person.age}</p> : null}
-      {person.isMarried
-        ? <p className="Person__partner">{person.partnerName} is my {typeOfPartner}</p>
+      <h2 className="Person__name">
+        My name is
+        {name}
+      </h2>
+      {age ? (
+        <p className="Person__age">
+          I am
+          {age}
+        </p>
+      ) : null}
+      {isMarried
+        ? (
+          <p className="Person__partner">
+            {partnerName}
+            {' '}
+            is my
+            {' '}
+            {typeOfPartner}
+          </p>
+        )
         : <p className="Person__partner">I am not married</p>}
     </section>
   );
