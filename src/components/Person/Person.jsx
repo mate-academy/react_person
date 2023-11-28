aed08cc6733239d4599328eb2ddbe25c6fe93481
Marcin Ligmann <marcinligmann2@gmail.com>
2023-11-28 12:22:26 +0100

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,11 +9,13 @@ export function Person({ person: { name, age, sex, isMarried, partnerName } }) {
     <section className="Person">
       <h2 className="Person__name">
         My name is
+        {' '}
         {name}
       </h2>
       {age ? (
         <p className="Person__age">
           I am
+          {' '}
           {age}
         </p>
       ) : null}
