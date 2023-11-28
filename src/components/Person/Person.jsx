527e4a407eb7389a9d9d503c09193ef3bf50f9c8
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2023-11-28 16:35:27 +0100

    And new changes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,12 @@
 export const Person = props => (
   <section className="Person">
     <h2 className="Person__name">
-      My name is 
+      My name is
       {props.person.name}
     </h2>
     { props.person.age ? (
       <p className="Person__age">
-        I am 
+        I am
         {props.person.age}
       </p>
     ) : <></> }
