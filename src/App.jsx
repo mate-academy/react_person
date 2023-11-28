9f05588b37ce2b36bbcc6483c637ea4becc190a8
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-11-28 13:16:44 +0100

    minor corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import Person from './components/Person/Person';
+import { Person } from './components/Person/Person';
 
 export const misha = {
   name: 'Misha',
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -32,5 +32,3 @@ export const Person = ({ person }) => {
     </section>
   );
 };
-
-export default Person;
