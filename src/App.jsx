624b1b6cd9e7c0966ac017f3e8dafdb3bd2aceb3
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-11-28 13:07:08 +0100

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Person } from './components/Person/Person';
+import Person from './components/Person/Person';
 
 export const misha = {
   name: 'Misha',
@@ -26,26 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <Person
-      name={misha.name}
-      age={misha.age}
-      sex={misha.sex}
-      isMarried={misha.isMarried}
-      partnerName={misha.partnerName}
-    />
-    <Person
-      name={olya.name}
-      age={olya.age}
-      sex={olya.sex}
-      isMarried={olya.isMarried}
-      partnerName={olya.partnerName}
-    />
-    <Person
-      name={alex.name}
-      age={alex.age}
-      sex={alex.sex}
-      isMarried={alex.isMarried}
-      partnerName={alex.partnerName}
-    />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,35 +1,36 @@
 // export const Person = ({ person }) => ();
+import './Person.scss';
 
-export const Person = ({
-  name,
-  age,
-  isMarried,
-  partnerName,
-  sex,
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {' '}
-      {name}
-    </h2>
-    { age && (
-    <p className="Person__age">
-      I am
-      {' '}
-      {age}
-    </p>
-    ) }
-    {isMarried ? (
-      <p className="Person__partner">
-        {partnerName}
-        {' '}
-        is my
-        {' '}
-        {sex === 'm' ? 'wife' : 'husband'}
-      </p>
-    ) : <p className="Person__partner">I am not married</p>}
-  </section>
-);
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    isMarried,
+    partnerName,
+    sex,
+  } = person;
+
+  const spouse = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+      {isMarried ? (
+        <p className="Person__partner">
+          {`${partnerName} is my ${spouse}`}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
 
 export default Person;
