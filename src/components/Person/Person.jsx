5e59f0329c2eb175d560d8e2e260c4c24906a02d
Marcin Ligmann <marcinligmann2@gmail.com>
2023-11-28 12:06:49 +0100

    solution with eslint error

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export function Person({ person }) {
+  let typeOfPartner = 'wife';
+
+  if (person.sex === 'f') {
+    typeOfPartner = 'husband';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age ? <p className="Person__age">I am {person.age}</p> : null}
+      {person.isMarried
+        ? <p className="Person__partner">{person.partnerName} is my {typeOfPartner}</p>
+        : <p className="Person__partner">I am not married</p>}
+    </section>
+  );
+}
