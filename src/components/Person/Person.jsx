5e9a2540e329eb4f29ffc0bea3d896b74f1d57ed
Tommaso Giacopini <giacopini@bluewin.ch>
2023-11-28 09:09:34 +0100

    task solved

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const WIFE = 'wife';
+  const HUSBAND = 'husband';
+
+  const spouse = person.sex === 'm' ? WIFE : HUSBAND;
+
+  const {
+    name,
+    age,
+    partnerName,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {person.age !== undefined && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried
+          ? `${partnerName} is my ${spouse}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
