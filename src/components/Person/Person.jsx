3f4e4cab8bfa92fa535f92ead1b39109718bad88
Vitalii Venher <vengervn@gmail.com>
2023-11-28 15:10:08 +0200

    fixed bugs

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,24 @@
-function wifeOrHusband(arg) {
-  return arg.sex === 'm' ? 'wife' : 'husband';
-}
+export const Person = ({ person }) => {
+  const wifeOrHusband = person.sex === 'm' ? 'wife' : 'husband';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age !== undefined && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {
+          person.isMarried
+            ? `${person.partnerName} is my ${wifeOrHusband}`
+            : `I am not married`
+        }
       </p>
-    )}
-
-    <p className="Person__partner">
-      {
-        person.isMarried
-          ? `${person.partnerName} is my ${wifeOrHusband(person)}`
-          : `I am not married`
-      }
-    </p>
-  </section>
-);
+    </section>
+  );
+};
