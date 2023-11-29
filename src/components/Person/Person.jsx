b6dcf3a8e8a51d2395e4d5015394ad67b82be937
Marek Maciejewski <marekmaciejewski1982@gmail.com>
2023-11-29 11:01:16 +0100

    tidier code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,9 +8,12 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+
       {age && (<p className="Person__age">{`I am ${age}`}</p>)}
-      {isMarried ? (<p className="Person__partner">{`${partnerName} is my ${partner}`}</p>)
-        : (<p className="Person__partner">I am not married</p>)}
+
+      <p className="Person__partner">
+        {isMarried ? `${partnerName} is my ${partner}` : 'I am not married'}
+      </p>
     </section>
   );
 };
