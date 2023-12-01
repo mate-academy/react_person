0ae2bb9c42ef8e26ce3ed857b7de58d94683209c
aurelia <aurelia.gorzna.dev@gmail.com>
2023-12-01 12:53:27 +0100

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable react/jsx-one-expression-per-line */
+export function Person({ person }) {
+  let marriage = 'I am not married';
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      marriage = `${person.partnerName} is my wife`;
+    }
+
+    if (person.sex === 'f') {
+      marriage = `${person.partnerName} is my husband`;
+    }
+  }
+
+  if ('age' in person) {
+    return (
+      <section className="Person">
+        <h2 className="Person__name">My name is {person.name}</h2>
+        <p className="Person__age">I am {person.age}</p>
+        <p className="Person__partner">{marriage}</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      <p className="Person__partner">{marriage}</p>
+    </section>
+  );
+}
