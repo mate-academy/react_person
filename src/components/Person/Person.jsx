0f0fc1bb377484476cb61c9f44e7525c04270bd0
ArtyWhit <julwer1k@gmail.com>
2023-12-01 16:41:03 +0200

    Update task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -9,23 +9,23 @@ export const Person = ({ person }) => {
     partnerName,
   } = person;
 
+  const partner = sex === 'f' ? 'husband' : 'wife';
+
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">
-          {`My name is ${name}`}
-        </h2>
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
 
-        {age && (
-          <p className="Person__age">{`I am ${age}`}</p>
-        )}
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
+      )}
 
-        <p className="Person__partner">
-          {isMarried
-            ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
-            : `I am not married`}
-        </p>
-      </section>
-    </>
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'}
+      </p>
+    </section>
   );
 };
