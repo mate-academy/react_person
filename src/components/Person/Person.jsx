9bdbc8dc4657aa4a37f387464afc2d8a832c04e5
Leo <you@example.com>
2023-12-03 21:11:30 +0200

    create

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  let isMaried = '<p className="Person__partner">I am not married</p>';
+
+  if (person.sex === 'm' && person.isMarried) {
+    isMaried = <p className="Person__partner">{person.partnerName} is my wife</p>
+  }
+  if (person.sex === 'f' && person.isMarried) {
+    isMaried = <p className="Person__partner">{person.partnerName} is my husband</p>
+
+  }
+
+  <>
+    <h2 className="Person__name">My name is {person.name}</h2>
+
+    {person.age && <p className="Person__age">I am {person.age}</p>}
+
+    {isMaried}
+  </>
+);
