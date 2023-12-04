2c0cabebdda6003757ceca20a3124ac45a93b57f
Krot Nikita <krotnikitaasd@gmail.com>
2023-12-04 14:20:15 +0200

    fix nested

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,17 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    <p className={`${person.age !== undefined ? 'Person__age' : 'person__age--omit'}`}>
-      {`I am ${person.age}`}
-    </p>
-    <p className="Person__partner">
-      {person.isMarried === true ? (
-        `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`
-      ) : 'I am not married'}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const sex = person.sex === 'f' ? 'husband' : 'wife';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <p className={`${person.age !== undefined ? 'Person__age' : 'person__age--omit'}`}>
+        {`I am ${person.age}`}
+      </p>
+      <p className="Person__partner">
+        {person.isMarried ? (
+          `${person.partnerName} is my ${sex}`
+        ) : 'I am not married'}
+      </p>
+    </section>
+  );
+};
