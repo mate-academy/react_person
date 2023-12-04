36c6e5a36c32e6e6e1e63f560fdad1857061d989
Krot Nikita <krotnikitaasd@gmail.com>
2023-12-04 17:54:35 +0200

    fix person.age

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,9 +4,13 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      <p className={`${person.age !== undefined ? 'Person__age' : 'Person__age--omit'}`}>
-        {`I am ${person.age}`}
-      </p>
+
+      {!!person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
       <p className="Person__partner">
         {person.isMarried ? (
           `${person.partnerName} is my ${sex}`
