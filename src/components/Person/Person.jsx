f191d1badad030a677eb137448b151a5833c14ec
Denys Vasylenkoe <kapodenim@gmail.com>
2023-12-04 19:20:49 +0200

    v3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -21,34 +21,20 @@ export const alex = {
   isMarried: false,
 };
 
-function partner(isMarried, sex, partnerName = undefined) {
-  if (isMarried) {
-    const personPartner = sex === 'm' ? 'wife' : 'husband';
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
 
-    return `${partnerName} is my ${personPartner}`;
-  }
+  const personPartner = sex === 'm' ? 'wife' : 'husband';
 
-  return 'I am not married';
-}
-
-function years(personAge) {
-  if (personAge) {
-    return `I am ${personAge}`;
-  }
-
-  return '';
-}
-
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    <p className="Person__age">
-      {years(person.age)}
-    </p>
-    <p className="Person__partner">
-      {partner(person.isMarried, person.sex, person.partnerName)}
-    </p>
-  </section>
-);
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">
+        {isMarried ? `${partnerName} is my ${personPartner}` : 'I am not married'}
+      </p>
+    </section>
+  );
+};
