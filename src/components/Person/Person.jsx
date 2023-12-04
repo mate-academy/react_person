212b6b2588f4785d582aa4d5a95c1fd9f12f1880
Krot Nikita <krotnikitaasd@gmail.com>
2023-12-04 13:19:07 +0200

    react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    <p className={`${person.age !== undefined ? 'Person__age' : 'person__age--omit'}`}>
+      {`I am ${person.age}`}
+    </p>
+    <p className="Person__partner">
+      {person.isMarried === true ? (
+        `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`
+      ) : 'I am not married'}
+    </p>
+  </section>
+);
