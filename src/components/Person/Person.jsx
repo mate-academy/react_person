ac049d05705bcc039d09e7a3ff03ddc97ea37b82
Olga Tsyb <opasyura@gmail.com>
2023-12-05 09:36:17 +0200

    fix bugs

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,27 +2,21 @@ import './Person.scss';
 
 export const Person = ({ person }) => {
   const { name, age, partnerName, isMarried, sex } = person;
+  const personStatus = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
-      {
-        age && (
-          <p className="Person__age">
-            {`I am ${age}`}
-          </p>
-        )
-      }
+      {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+      )}
       <p className="Person__partner">
-        {isMarried === true
-          ? (
-            <>
-              {`${partnerName} is my`}
-              {sex === 'm' ? ' wife' : ' husband'}
-            </>
-          )
+        {isMarried
+          ? `${partnerName} is my ${personStatus}`
           : 'I am not married'
         }
       </p>
