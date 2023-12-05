3b479298e8ec4ed60dde2645b2417f3f3daab7f0
aurelia <aurelia.gorzna.dev@gmail.com>
2023-12-05 20:39:32 +0100

    fixed bad code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,29 +2,20 @@
 export function Person({ person }) {
   let marriage = 'I am not married';
 
-  if (person.isMarried) {
-    if (person.sex === 'm') {
-      marriage = `${person.partnerName} is my wife`;
-    }
-
-    if (person.sex === 'f') {
-      marriage = `${person.partnerName} is my husband`;
-    }
+  if (person.isMarried && person.sex === 'm') {
+    marriage = `${person.partnerName} is my wife`;
   }
 
-  if ('age' in person) {
-    return (
-      <section className="Person">
-        <h2 className="Person__name">My name is {person.name}</h2>
-        <p className="Person__age">I am {person.age}</p>
-        <p className="Person__partner">{marriage}</p>
-      </section>
-    );
+  if (person.isMarried && person.sex === 'f') {
+    marriage = `${person.partnerName} is my husband`;
   }
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
+      {'age' in person
+        ? <p className="Person__age">I am {person.age}</p>
+        : null}
       <p className="Person__partner">{marriage}</p>
     </section>
   );
