6e281e61750a3c7961f02e7c227f87ac570c24e6
aurelia <aurelia.gorzna.dev@gmail.com>
2023-12-06 12:23:24 +0100

    minor fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,13 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable react/jsx-one-expression-per-line */
 export function Person({ person }) {
   let marriage = 'I am not married';
+
+  if (person.isMarried) {
+    person.sex === 'm'
+      ? marriage = `${person.partnerName} is my wife`
+      : marriage = `${person.partnerName} is my husband`;
+  }
 
   if (person.isMarried && person.sex === 'm') {
     marriage = `${person.partnerName} is my wife`;
@@ -13,7 +20,7 @@ export function Person({ person }) {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {'age' in person
+      {person.age != null
         ? <p className="Person__age">I am {person.age}</p>
         : null}
       <p className="Person__partner">{marriage}</p>
