71fc73bcda78f5c46a1955d8f7b22f31975d23f3
Roman Tarasov <rtarasov1k90@gmail.com>
2023-12-06 17:29:27 +0200

    ternary added

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,22 @@
 export const Person = ({ person }) => {
   const { name, age, isMarried, sex, partnerName } = person;
+  const partnerSex = sex === 'f' ? `${partnerName} is my husband` : `${partnerName} is my wife`;
 
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {` ${name}`}
+        {`My name is ${name}`}
       </h2>
 
       {age && (
         <p className="Person__age">
-          I am
-          {` ${age}`}
+          {`I am ${age}`}
         </p>
       )
       }
 
       <p className="Person__partner">
-        {!isMarried && `I am not married`}
-        {isMarried && sex === 'f' && `${partnerName} is my husband`}
-        {isMarried && sex === 'm' && `${partnerName} is my wife`}
+        {!isMarried ? `I am not married` : partnerSex}
       </p>
     </section>
   );
