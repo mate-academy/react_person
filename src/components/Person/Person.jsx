ded3f8ee786f564c7c4f3fe7221036b80f8304a3
Marcin Szymanski <mszymanski1709@outlook.com>
2023-12-06 10:53:11 +0100

    repetition for practice

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,19 @@
 export const Person = ({ person }) => {
-  const femaleSex = 'f';
+  const sexMale = 'm';
   const { name, age, partnerName } = person;
-  const partnerSex = person.sex === femaleSex ? 'husband' : 'wife';
+  const personPartner = person.sex === sexMale ? 'wife' : 'husband';
 
   return (
-    <section className="Person">
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {person.age > 0 && <p className="Person__age">{`I am ${age}`}</p>}
 
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {person.isMarried
+          ? <p className="Person__partner">{`${partnerName} is my ${personPartner}`}</p>
+          : <p className="Person__partner">`I am not married`</p>}
+      </section>
 
-      {person.age >= 0 && <p className="Person__age">{`I am ${age}`}</p>}
-
-      {person.isMarried
-        ? <p className="Person__partner">{`${partnerName} is my ${partnerSex}`}</p>
-        : <p className="Person__partner">I am not married</p>
-      }
-    </section>
+    </>
   );
 };
