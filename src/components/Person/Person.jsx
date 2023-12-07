9f306ed7276deca7473e33f88d59abf8a5c436cb
boytrend0108 <boytrend@gmail.com>
2023-12-07 14:31:09 +0200

    fix codestyle

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,9 @@ import './Person.scss';
 export const Person = ({ person }) => (
   <>
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
 
       {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
 
