d23413cd9d7bbc4acaad79b7c819edf8fd0d1ae9
Roman Dotsenko <docenkoromaha@gmail.com>
2023-12-08 21:42:27 +0200

    Output in jsx changed on template literal

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,18 +11,14 @@ export function Person({ person }) {
   return (
     <section className="Person">
       <h2 className="Person__name">
-        My name is
-        {' '}
-        {person.name}
+        {`My name is ${person.name}`}
       </h2>
-      
-        {person.age ? (
-          <p className="Person__age">
-            I am
-            {' '}
-            {person.age}
-          </p>
-          ) : ''}
+
+      {person.age ? (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      ) : ''}
       <p className="Person__partner">
         {message}
       </p>
