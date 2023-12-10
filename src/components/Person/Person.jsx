1dcb19d5ca764ad0437858558abc873761c00ac5
ebfidKate <ebfid.kate@gmail.com>
2023-12-10 19:26:24 +0000

    added solution for react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,22 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  let line = '';
+  const wifeOrHusband = person.sex === 'm' ? 'wife' : 'husband';
+
+  if (!person.isMarried) {
+    line = 'I am not married';
+  } else {
+    line = `${person.partnerName} is my ${wifeOrHusband}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age
+        ? <p className="Person__age">{`I am ${person.age}`}</p>
+        : ''}
+      <p className="Person__partner">{line}</p>
+    </section>
+  );
+};
