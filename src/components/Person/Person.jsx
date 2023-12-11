9b31ff632c472f9f68dd0d60476dfe849de5780c
ebfidKate <ebfid.kate@gmail.com>
2023-12-11 15:33:20 +0000

    minor changes according to suggestions

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,9 +13,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age
-        ? <p className="Person__age">{`I am ${person.age}`}</p>
-        : ''}
+      {!!person.age
+        && <p className="Person__age">{`I am ${person.age}`}</p>
+      }
       <p className="Person__partner">{line}</p>
     </section>
   );
