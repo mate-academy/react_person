bed125c9a9f1863d1e69d908ff2f66da98f341a3
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2023-12-13 16:49:58 +0100

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ ...person }) => {
+  const partner = person.sex === 'f' ? 'husband' : 'wife';
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">
+          {`My name is ${person.name}`}
+        </h2>
+
+        <p className="Person__age">
+          {person.age ? `I am ${person.age}` : null}
+        </p>
+
+        <p className="Person__partner">
+          {person.isMarried
+            ? `${person.partnerName} is my ${partner}`
+            : 'I am not married'}
+        </p>
+      </section>
+    </div>
+  );
+};
