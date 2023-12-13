e98b9cae9061b5d5aab6a62afc549b7e47326891
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2023-12-13 19:10:44 +0200

    delete radundant fragment in Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,25 +2,23 @@ export const Person = ({ person }) => {
   const partner = (person.sex === 'f') ? 'husband' : 'wife';
 
   return (
-    <>
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-        {person.age && (
-        <p className="Person__age">
-          {`I am ${person.age}`}
-        </p>
+      {person.age && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+      )}
+
+      <p className="Person__partner">
+
+        {person.isMarried ? (
+          `${person.partnerName} is my ${partner}`
+        ) : (
+          `I am not married`
         )}
-
-        <p className="Person__partner">
-
-          {person.isMarried ? (
-            `${person.partnerName} is my ${partner}`
-          ) : (
-            `I am not married`
-          )}
-        </p>
-      </section>
-    </>
+      </p>
+    </section>
   );
 };
