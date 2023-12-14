e8440334e832869f74025ea13edf4d286fa7cf2f
Volodymyr Baraban <barabanv2003@gmail.com>
2023-12-14 19:26:43 +0200

    person react

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName, sex } = person;
+
+  const isWife = () => {
+    if (sex === 'm') {
+      return 'is my wife';
+    }
+
+    return 'is my husband';
+  };
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${name}`}</h2>
+        {age && <p className="Person__age">{`I am ${age}`}</p>}
+        <p className="Person__partner">
+          {isMarried ? `${partnerName} ${isWife(sex)}` : 'I am not married'}
+        </p>
+      </section>
+    </div>
+  );
+};
