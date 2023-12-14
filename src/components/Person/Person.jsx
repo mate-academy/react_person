af9ce64e2908cefe599a63e44ac78b7ac87d53cf
Volodymyr Baraban <barabanv2003@gmail.com>
2023-12-14 21:17:39 +0200

    after the review update

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,7 @@
 export const Person = ({ person }) => {
   const { name, age, isMarried, partnerName, sex } = person;
 
-  const isWife = () => {
-    if (sex === 'm') {
-      return 'is my wife';
-    }
-
-    return 'is my husband';
-  };
+  const isWife = sex === 'm' ? 'is my wife' : 'is my husband';
 
   return (
     <div className="App">
@@ -15,7 +9,7 @@ export const Person = ({ person }) => {
         <h2 className="Person__name">{`My name is ${name}`}</h2>
         {age && <p className="Person__age">{`I am ${age}`}</p>}
         <p className="Person__partner">
-          {isMarried ? `${partnerName} ${isWife(sex)}` : 'I am not married'}
+          {isMarried ? `${partnerName} ${isWife}` : 'I am not married'}
         </p>
       </section>
     </div>
