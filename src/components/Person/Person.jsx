94a739b20244beb28dce6eb20229c99b64b1e26e
Mykhailo <mykhailoevo@mail.com>
2023-12-19 16:11:58 +0200

    Solution+

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,10 @@
+const MAN = 'm';
+const WOMAN = 'f';
+
 export const Person = ({
   person: { name, age, sex, partnerName, isMarried },
 }) => (
+
   <div className="App">
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
@@ -10,15 +14,15 @@ export const Person = ({
         </p>
       )}
       <p className="Person__partner">
-        {isMarried === true && sex === 'm'
+        {isMarried && sex === MAN
           ? <span>{`${partnerName} is my wife`}</span>
           : null}
 
-        {isMarried === true && sex === 'f'
+        {isMarried && sex === WOMAN
           ? <span>{`${partnerName} is my husband`}</span>
           : null}
 
-        {isMarried === false
+        {!isMarried
           ? <span>I am not married</span>
           : null}
       </p>
