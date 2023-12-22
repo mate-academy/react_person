a95f4c4cd7803364028edca7462fe02952b9cea8
Andrew Rich <andy.rich3a@gmail.com>
2023-12-22 16:16:44 +0200

    add task fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,29 @@
 // export const Person = ({ person }) => ();
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age !== undefined && (
-    <p
-      className="Person__age"
-      style={{
-        display: person.age ? 'block' : 'none',
-      }}
-    >
-      {`I am ${person.age}`}
-    </p>
-    )}
-    <p className="Person__partner">
-      {person.isMarried === true
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : `I am not married`
-      }
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const partner = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age && (
+      <p
+        className="Person__age"
+        style={{
+          display: person.age ? 'block' : 'none',
+        }}
+      >
+        {`I am ${person.age}`}
+      </p>
+      )}
+      <p className="Person__partner">
+        {person.isMarried === true
+
+          ? `${person.partnerName} is my ${partner}`
+          : `I am not married`
+        }
+      </p>
+    </section>
+  );
+};
