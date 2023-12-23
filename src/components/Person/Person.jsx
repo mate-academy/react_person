ee31628cf5d9e4906d43ad559b8e2797552d734a
Julia <julia@ocuhealthcare.com>
2023-12-23 10:48:31 -0600

    Adding more variables

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,23 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
   const partnerSex = sex === 'm' ? 'wife' : 'husband';
+  const marriedStatement = `${partnerName} is my ${partnerSex}`;
+  const singleStatement = 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+
       {age && (
       <p className="Person__age">
         {`I am ${age}`}
       </p>
       )}
+
       <p className="Person__partner">
         {isMarried
-          ? `${partnerName} is my ${partnerSex}`
-          : 'I am not married'
+          ? marriedStatement
+          : singleStatement
         }
       </p>
     </section>
