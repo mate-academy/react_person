d4b0285ff620109b7c36361f8bc562bf22bc4ab5
Ekaterina Bessonnaya <iekatierinovna9090@gmail.com>
2023-12-25 15:59:08 +0200

    component person was created

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <>
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      <p className="Person__age">
+        {person.age !== undefined ? `I am ${person.age}` : ''}
+      </p>
+      <p className="Person__partner">
+        {!person.isMarried && 'I am not married'}
+        {person.isMarried
+        && `${person.partnerName} is my ${person.sex === 'm'
+          ? 'wife' : 'husband'}`}
+      </p>
+    </section>
+  </>
+);
