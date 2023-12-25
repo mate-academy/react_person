4a2166bd5bad40f5a8f946c9ef01b945baac014b
Ekaterina Bessonnaya <iekatierinovna9090@gmail.com>
2023-12-25 16:14:11 +0200

    erroe is fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,14 +4,16 @@ export const Person = ({ person }) => (
       <h2 className="Person__name">
         {`My name is ${person.name}`}
       </h2>
-      <p className="Person__age">
-        {person.age !== undefined ? `I am ${person.age}` : ''}
-      </p>
+      {person.age && (
+        <p className="Person__age">
+            {`I am ${person.age}`}
+        </p>
+      )}
       <p className="Person__partner">
         {!person.isMarried && 'I am not married'}
         {person.isMarried
-        && `${person.partnerName} is my ${person.sex === 'm'
-          ? 'wife' : 'husband'}`}
+            && `${person.partnerName} is my ${person.sex === 'm'
+              ? 'wife' : 'husband'}`}
       </p>
     </section>
   </>
