4e88210ebbc7bace6a41af7a3b158e5a8394b2dc
unknown <anorifrell@gmail.com>
2023-12-27 19:12:47 +0200

    add reactive_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,31 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <>
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+
+    {person.age && (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    )}
+
+    {person.isMarried && person.sex === 'm' && (
+      <p className="Person__partner">
+        {`${person.partnerName} is my wife`}
+      </p>
+    )}
+
+    {person.isMarried && person.sex === 'f' && (
+      <p className="Person__partner">
+        {`${person.partnerName} is my husband`}
+      </p>
+    )}
+
+    {!person.isMarried && (
+      <p className="Person__partner">
+        I am not married
+      </p>
+    )}
+  </>
+);
