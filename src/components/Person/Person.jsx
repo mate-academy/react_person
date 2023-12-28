a76dcb23b3e636167eaa08f9828d55531033fc4e
Ekaterina Bessonnaya <iekatierinovna9090@gmail.com>
2023-12-28 10:51:15 +0200

    variable was created

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,24 @@
-export const Person = ({ person }) => (
-  <>
+export const Person = ({ person }) => {
+  const partner = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
     <section className="Person">
       <h2 className="Person__name">
         {`My name is ${person.name}`}
       </h2>
+
       {person.age && (
         <p className="Person__age">
-            {`I am ${person.age}`}
+          {`I am ${person.age}`}
         </p>
       )}
+
       <p className="Person__partner">
-        {!person.isMarried && 'I am not married'}
         {person.isMarried
-            && `${person.partnerName} is my ${person.sex === 'm'
-              ? 'wife' : 'husband'}`}
+          ? `${person.partnerName} is my ${partner}`
+          : 'I am not married'
+        }
       </p>
     </section>
-  </>
-);
+  );
+};
