553216d13c9ebc599216422ec0b3c1c78c7f6748
BodyaKuts <mkkbodya@gmail.com>
2023-12-30 15:05:30 +0200

    added variable and fixed code styling

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,22 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-    )}
+export const Person = ({ person }) => {
+  const RELATIONSHIP_STATUS = person.isMarried
+    ? `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`
+    : 'I\'m not married';
 
-    <p className="Person__partner">
-      {person.isMarried ? `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}` : 'I`m not married'}
-    </p>
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
 
-  </section>
-);
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">{RELATIONSHIP_STATUS}</p>
+
+    </section>
+  );
+};
