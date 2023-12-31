ec5f4b980381eb9f7c13af08f214a5d556eda1bd
Anton Duchenko <deragandi0@gmail.com>
2023-12-31 17:17:32 +0200

    Solution after finding checklist

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,11 +2,16 @@ export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
     {person.age && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
+    <p className="Person__age">{`I am ${person.age}`}</p>
     )}
 
     {person.isMarried ? (
-      <p className="Person__partner">{`${person.partnerName} is ${person.sex === 'm' ? 'my wife' : 'my husband'}`}</p>
+      <p className="Person__partner">
+        {`${person.partnerName} is ${person.sex === 'm'
+          ? 'my wife'
+          : 'my husband'}`
+        }
+      </p>
     ) : (
       <p className="Person__partner">I am not married</p>
     )}
