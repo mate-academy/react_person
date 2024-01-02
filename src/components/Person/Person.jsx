81fffc146b8630e237ee9ed0b380d4eb91f70374
ValeriaMyronets <valery.mir.11@gmail.com>
2024-01-02 20:29:17 +0200

    removed repetitions and edited person age and person married

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,21 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    <p className={`${person.age !== undefined ? 'Person__age' : ''}`}>
-      {`I am ${person.age}`}
-    </p>
+    {person.age !== undefined ? (
+      <p className="Person__age">
+        {`I am ${person.age}`}
+      </p>
+    ) : ''
+    }
 
-    <p className={`${person.sex === 'f' ? 'Person__partner' : ''}`}>
-      {person.isMarried ? `${person.partnerName} is my husband` : 'I am not married'}
-    </p>
-
-    <p className={`${person.sex === 'm' ? 'Person__partner' : ''}`}>
-      {person.isMarried ? `${person.partnerName} is my wife` : 'I am not married'}
-    </p>
+    {person.isMarried ? (
+      <p className="Person__partner">
+        {person.sex === 'f'
+          ? `${person.partnerName} is my husband`
+          : `${person.partnerName} is my wife`}
+      </p>
+    ) : (
+      <p className="Person__partner">I am not married</p>
+    )}
   </section>
 );
