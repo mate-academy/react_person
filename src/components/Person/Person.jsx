244f2b10d96be0ba41471e599181ca855aa72ca0
Ruslan Voshchylo <ruslan.voshchylo2@gmail.com>
2024-01-02 21:16:09 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,41 +1,32 @@
-export const Person = ({ person }) => (
-  <section className="Person">
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+  const partner = sex === 'm' ? 'wife' : 'husband';
 
-    {person.age && (
-    <p className="Person__age">
-      {`I am ${person.age}`}
-    </p>
-    )}
+  return (
+    <section className="Person">
 
-    {!person.isMarried
-      ? (<p className="Person__partner">I am not married</p>)
-      : (
-        <p className="Person__partner">
-          {`${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`}
-        </p>
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
       )}
-  </section>
-);
-
-/* <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">Natasha is my wife</p>
+      <p className="Person__partner">
+        {!isMarried
+          ? 'I am not married'
+          : `${partnerName} is my ${partner}`
+        }
+      </p>
     </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">Maksym is my husband</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
-  </div> */
+  );
+};
