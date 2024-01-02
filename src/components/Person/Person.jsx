c273502576d2137a16a040af79b47d3c6a7ab900
Anton Duchenko <deragandi0@gmail.com>
2024-01-02 13:30:43 +0200

    add style fixes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,19 @@
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
     {person.age && (
     <p className="Person__age">{`I am ${person.age}`}</p>
     )}
 
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {`${person.partnerName} is ${person.sex === 'm'
+    <p className="Person__partner">
+      {person.isMarried ? (
+        `${person.partnerName} is ${person.sex === 'm'
           ? 'my wife'
           : 'my husband'}`
-        }
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
+      ) : (
+        'I am not married'
+      )}
+    </p>
   </section>
 );
