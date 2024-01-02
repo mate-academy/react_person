b946ef8ee4efb18a13d4bcb873ac59c640718843
Dmytro Pivkopa <alien_dima@yahoo.com>
2024-01-02 17:52:53 +0200

    add edits of the mentor

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,29 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  const partner = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'}
       </p>
-    )}
-
-    <p className="Person__partner">
-      {person.isMarried
-        ? (`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`)
-        : ('I am not married')}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
