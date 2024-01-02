9d719ffa0120ad40210916ef43b97474ebcfc0b3
Yaroslav Samoilenko <yasik2255@live.com>
2024-01-02 20:48:44 +0200

    added object destructure, fixed ternar !isMarried

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,32 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {
-      person.age && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    partnerName,
+    sex,
+    isMarried,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p className="Person__age">{`I am ${age}`}</p>
       )
-    }
-    {
-      person.isMarried !== true ? (
+      }
+
+      {!isMarried ? (
         <p className="Person__partner">I am not married</p>
       ) : (
         <p className="Person__partner">
-          {`${person.partnerName} is my ${
-            person.sex === 'm' ? ('wife') : ('husband')
+          {`${partnerName} is my ${
+            sex === 'm' ? 'wife' : 'husband'
           }`}
         </p>
-      )
-
-    }
-
-  </section>
-);
+      )}
+    </section>
+  );
+};
