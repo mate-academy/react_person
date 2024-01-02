4df1c58b836a6419b13c72c2e3baf5258187101e
Nazarii <nazarisaviak@gmail.com>
2024-01-02 17:25:17 +0200

    fixed code style, created partner variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,7 +2,7 @@ import './Person.css';
 
 export function Person({ person }) {
   const { name, age, sex, isMarried, partnerName } = person;
-  const isMan = sex === 'm';
+  const partner = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
@@ -10,21 +10,17 @@ export function Person({ person }) {
         {`My name is ${name}`}
       </h2>
 
-      {
-        age
-          ? (
-            <p className="Person__age">
-              {age && `I am ${age}`}
-            </p>
-          )
-          : null
+      {age && (
+        <p className="Person__age">
+          {age && `I am ${age}`}
+        </p>
+      )
       }
 
       <p className="Person__partner">
-        {
-          isMarried
-            ? `${partnerName} is my ${isMan ? 'wife' : 'husband'}`
-            : 'I am not married'
+        {isMarried
+          ? `${partnerName} is my ${partner}`
+          : 'I am not married'
         }
       </p>
     </section>
