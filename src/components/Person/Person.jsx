2c437e69c895068615e92a31ffe7e1b5fd414f93
Yevhen Prymushko <yevhenpetrov6@gmail.com>
2024-01-03 14:14:56 +0200

    chenges made

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      My name is
-      {` ${person.name}`}
-    </h2>
-    {person.age ? (
-      <p className="Person__age">
-        I am
-        {` ${person.age}`}
+export const Person = ({ person }) => {
+  const partnerInfo = !person.isMarried
+    ? 'I am not married'
+    : `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age ? (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      ) : null}
+      <p className="Person__partner">
+        {partnerInfo}
       </p>
-    ) : null}
-    <p className="Person__partner">
-      {!person.isMarried
-        ? 'I am not married'
-        : `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
