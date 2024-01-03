bc39f1b68c56493badaaf36166c42e29259616a0
Kostrubitskiy Nazarii <kostrubitskiiwork@gmail.com>
2024-01-03 16:48:44 +0200

    fix format style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
 
-  const ageInfo = age
-    ? <p className="Person__age">{`I am ${age}`}</p>
-    : null;
+  const ageInfo = age && <p className="Person__age">{`I am ${age}`}</p>;
   const Male = sex === 'm';
   const partnerSex = Male ? 'wife' : 'husband';
   const marriedStatus = isMarried ? `${partnerName} is my ${partnerSex}` : 'I am not married';
@@ -13,7 +17,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
+
       {ageInfo}
+
       <p className="Person__partner">{marriedStatus}</p>
     </section>
   );
