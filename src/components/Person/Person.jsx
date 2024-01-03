fae22c64c5657f1fb22672c5f3f65aa1ad8c356f
White EdenI <danil.gayvoronsky2201@gmail.com>
2024-01-04 01:43:09 +0200

    Done!(try1)

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,37 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const {
+    name,
+    sex,
+    age,
+    partnerName,
+    isMarried,
+  } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age !== undefined && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+
+      {isMarried
+        ? (
+          <p className="Person__partner">
+            {`${partnerName} is my ${sex === 'f'
+              ? 'husband'
+              : 'wife'}`}
+          </p>
+        )
+        : (
+          <p className="Person__partner">I am not married</p>
+        )}
+    </section>
+  );
+};
