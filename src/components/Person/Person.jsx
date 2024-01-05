a5439834603b65a9120ddd09d4e8b63c6e08363d
mrsvolodya <vovamursky@gmail.com>
2024-01-05 16:39:56 +0200

    solutin

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,19 @@
-export const Person = ({ person }) => (
-  <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age && (
-        <p className="Person__age">
-          {`I am ${person.age}`}
+export const Person = ({ person }) => {
+  const isMerried = `${person.sex === 'm' ? `${person.partnerName} is my wife` : `${person.partnerName} is my husband`}`;
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+        {person.age && (
+          <p className="Person__age">
+            {`I am ${person.age}`}
+          </p>
+        )}
+        <p className="Person__partner">
+          {person.isMarried ? isMerried : 'I am not married'}
         </p>
-      )
-          }
-      <p className="Person__partner">
-        {person.isMarried ? `${person.sex === 'm' ? `${person.partnerName} is my wife` : `${person.partnerName} is my husband`}` : 'I am not married'}
-      </p>
-    </section>
-  </div>
-);
+      </section>
+    </div>
+  );
+};
