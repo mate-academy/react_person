afb116a63e367d9d17f9d5c96928fb09c3f3e1ca
mrsvolodya <vovamursky@gmail.com>
2024-01-05 16:08:33 +0200

    solo

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )
+          }
+      <p className="Person__partner">
+        {person.isMarried ? `${person.sex === 'm' ? `${person.partnerName} is my wife` : `${person.partnerName} is my husband`}` : 'I am not married'}
+      </p>
+    </section>
+
+  </div>
+);
