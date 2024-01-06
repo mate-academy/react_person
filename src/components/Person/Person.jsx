e0c9fee67e01962550443ca21a1054d077254ba1
Mariana <marjanavv@gmail.com>
2024-01-06 08:52:15 +0100

    message

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,14 +1,16 @@
 export function Person({ person }) {
-  const personPartnerType = person.sex === "m" ? "wife" : "husband";
+  const personPartnerType = person.sex === 'm' ? 'wife' : 'husband';
 
   const personPartner = person.isMarried
     ? `${person.partnerName} is my ${personPartnerType}`
-    : "I am not married";
+    : 'I am not married';
 
   const name = `My name is ${person.name}`;
   const age = person.age && (
     <p className="Person__age">
-      I am {person.age}
+      I am
+      {' '}
+      {person.age}
     </p>
   );
 
