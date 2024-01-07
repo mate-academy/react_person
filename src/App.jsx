580cf7f4170f1ff7353ef16b32957ea82a5dd32f
Yura Bublyk <yurabublik703@gmail.com>
2024-01-07 16:28:46 +0200

    solution fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 import './App.scss';
 import { Person } from './components/Person/Person';
 
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
+const people = [
+  {
+    name: 'Misha',
+    age: 37,
+    sex: 'm',
+    isMarried: true,
+    partnerName: 'Natasha',
+  },
+  {
+    name: 'Olya',
+    sex: 'f',
+    isMarried: true,
+    partnerName: 'Maksym',
+  },
+  {
+    name: 'Alex',
+    age: 25,
+    sex: 'm',
+    isMarried: false,
+  },
+];
 
 export const App = () => (
   <div className="App">
-    <Person person={misha} />
-    <Person person={olya} />
-    <Person person={alex} />
+    {people.map(person => (
+      <Person person={person} />
+    ))}
   </div>
 );
