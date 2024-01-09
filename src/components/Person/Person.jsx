9039478fac735e2b1e65243b732abd251cd3cfc1
Iryna Lysko <goiralysko@gmail.com>
2024-01-09 18:54:49 +0200

    simplify family status check

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,26 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
+export const Person = ({ person }) => {
+  const partnerType = person.sex === 'f'
+    ? 'husband'
+    : 'wife';
 
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
+      <p className="Person__partner">
+        {person.isMarried
+          ? `${person.partnerName} is my ${partnerType}`
+          : 'I am not married'
+        }
       </p>
-    )}
-
-    {person.isMarried
-      ? (
-        <p className="Person__partner">
-          {`${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`}
-        </p>
-      )
-      : (
-        <p className="Person__partner">
-          I am not married
-        </p>
-      )
-    }
-  </section>
-);
+    </section>
+  );
+};
