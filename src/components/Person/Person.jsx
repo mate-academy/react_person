5bf7775b4304d03ec998dd174331befa9d6614cf
Kirill Stetsenko <stetsenkoki623@gmail.com>
2024-01-10 15:44:40 +0200

    minor fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,21 @@
 export const Person = ({ person }) => {
-  const { name, age = 0, sex, isMarried, partnerName } = person;
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
   const personPartnerMessage = (sex === 'm')
     ? `${partnerName} is my wife`
     : `${partnerName} is my husband`;
 
   return (
-
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age
-        ? <p className="Person__age">{`I am ${age}`}</p>
-        : null}
+
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+
       <p className="Person__partner">
         {isMarried
           ? personPartnerMessage
