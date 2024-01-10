876948a632b002f24b33297053e573734b0bb078
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-01-10 22:16:22 +0200

    add after first check

--- src/App.jsx
+++ src/App.jsx
@@ -27,17 +27,11 @@ export const alex = {
 export const App = () => (
   <div className="App">
 
-    <Person
-      person={misha}
-    />
+    <Person person={misha} />
 
-    <Person
-      person={olya}
-    />
+    <Person person={olya} />
 
-    <Person
-      person={alex}
-    />
+    <Person person={alex} />
 
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,29 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-    )}
-    {person.isMarried === true ? (
-      <p className="Person__partner">
-        {`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
-      </p>
-    ) : (
-      <p className="Person__partner">
-        I am not married
-      </p>
-    )}
-    {/* <p className="Person__partner">
-      {`${person.partnerName} is my wife`}
-    </p> */}
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, isMarried, partnerName, sex } = person;
+  const wifeOrHusbandWord = (sex === 'm' ? 'wife' : 'husband');
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+      {isMarried ? (
+        <p className="Person__partner">
+          {`${partnerName} is my ${wifeOrHusbandWord}`}
+        </p>
+      ) : (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      )}
+
+    </section>
+  );
+};
