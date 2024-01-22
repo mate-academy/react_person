b6f905b3b2aa2255af22dfc35a9633190a11e001
Illya Katynskyi <katinskyi.illia@lll.kpi.ua>
2024-01-22 18:52:20 +0200

    Rafactor code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,18 @@
 import '../../App.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <p className="Person__partner">
-      {
-      person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'
-      }
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const personPartnerStatus = person.sex === 'm' ? 'wife' : 'husband';
+  const personPartner = person.isMarried
+    ? `${person.partnerName} is my ${personPartnerStatus}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      <p className="Person__partner">
+        {personPartner}
+      </p>
+    </section>
+  );
+};
