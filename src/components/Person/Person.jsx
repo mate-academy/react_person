6aadfab5e3ffb7aaaa08b006565f34997c3aa6d9
Illya Katynskyi <122596697+GIroSTarK@users.noreply.github.com>
2024-01-22 18:37:42 +0200

    Update src/components/Person/Person.jsx
    Co-authored-by: Denys Semtso <70220667+etojeDenys@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,7 @@ import '../../App.scss';
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age ? <p className="Person__age">{`I am ${person.age}`}</p> : null}
+    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
     <p className="Person__partner">
       {
       person.isMarried
