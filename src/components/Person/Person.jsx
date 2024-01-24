d3d1b6678d628b614714fa526e098c09a425194b
Tvinki228 <pes-log@ukr.net>
2024-01-24 19:13:36 +0100

    add react person solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,24 @@
 
-export const Person = ({ person }) => (
-  <>
-    <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const partnerType = person.sex === 'm' ? 'wife' : 'husband';
 
-      {person.age && (
-        <p className="Person__age">{`I am ${person.age}`}</p>
-      )}
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-      {person.isMarried && (
-        person.sex === 'm'
-          ? <p className="Person__partner">{`${person.partnerName} is my wife`}</p>
-          : <p className="Person__partner">{`${person.partnerName} is my husband`}</p>
-      )}
+        {person.age && (
+          <p className="Person__age">{`I am ${person.age}`}</p>
+        )}
 
-      {!person.isMarried && (
-      <p className="Person__partner">I am not married</p>
-      )}
-    </section>
-  </>
-);
+        {person.isMarried && (
+          <p className="Person__partner">{`${person.partnerName} is my ${partnerType}`}</p>
+        )}
+
+        {!person.isMarried && (
+          <p className="Person__partner">I am not married</p>
+        )}
+      </section>
+    </>
+  );
+};
