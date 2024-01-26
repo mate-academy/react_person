bbcefafe8a3fab92dfac6645f8846f3dd4870ff7
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2024-01-26 12:43:44 +0200

    add task correct react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,23 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age && (
-    <p className="Person__age">
-      I am
-      {' '}
-      {person.age}
-    </p>
-    )}
-    <p className="Person__partner">
-      {person.isMarried ? `${person.partnerName} is my ` : `I am not married`}
-      {person.sex === 'm' && person.isMarried ? `wife` : ''}
-      {person.sex === 'f' && person.isMarried ? 'husband' : ''}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const personPartnerType = sex === 'm' ? 'wife' : 'husband';
+  const personPartner = isMarried ? `${partnerName} is my ${personPartnerType}` : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+      {person.age && (
+      <p className="Person__age">
+        {`I am ${age}`}
+      </p>
+      )}
+      <p className="Person__partner">
+        {personPartner}
+      </p>
+    </section>
+  );
+};
