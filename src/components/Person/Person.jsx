1b1049cd925981d8af4c26cd9610017c4bd7ea45
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2024-01-26 12:57:09 +0200

    add task corrects react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,10 +10,11 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">
         {`My name is ${name}`}
       </h2>
+
       {person.age && (
-      <p className="Person__age">
-        {`I am ${age}`}
-      </p>
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
       )}
       <p className="Person__partner">
         {personPartner}
