4214c0b961d6358c389c8bdfc4df03b13bd46c4f
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2024-01-26 11:50:27 +0200

    add task react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">
+      {`My name is ${person.name}`}
+    </h2>
+    {person.age && (
+    <p className="Person__age">
+      I am
+      {' '}
+      {person.age}
+    </p>
+    )}
+    <p className="Person__partner">
+      {person.isMarried ? `${person.partnerName} is my ` : `I am not married`}
+      {person.sex === 'm' && person.isMarried ? `wife` : ''}
+      {person.sex === 'f' && person.isMarried ? 'husband' : ''}
+    </p>
+  </section>
+);
