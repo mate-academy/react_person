f25f9cf5b509a28781d0a75b9577afdc022c0cf3
Nataliia Nikolaienko <tasinka.nn@gmail.com>
2024-01-27 12:56:12 +0100

    corrected1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,23 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-    )}
-    {person.sex === 'f' ? (
+export const Person = ({ person }) => {
+  const partnerType = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+
       <p className="Person__partner">
-        {`${person.partnerName} is my husband`}
+        {person.isMarried
+          ? `${person.partnerName} is my ${partnerType}`
+          : 'I am not married'}
       </p>
-    ) : (
-      <p className="Person__partner">
-        {`${person.partnerName} is my wife`}
-      </p>
-    )}
-    {!person.isMarried && (
-      <p className="Person__partner">
-        I am not married
-      </p>
-    )}
-  </section>
-);
+    </section>
+  );
+};
