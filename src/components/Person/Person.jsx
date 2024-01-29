761292df56181c542ebd8168217481844843fa24
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-01-29 13:00:15 +0100

    Refaktoring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
-    {person.isMarried
-      ? (
-        <p className="Person__partner">{person.partnerName} is my
-          {' '}
-          {person.sex === 'm' ? 'wife' : 'husband'}
-        </p>
-      )
-      : (<p className="Person__partner">I am not married</p>)}
-  </section>
-);
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const personPartnerType = sex === 'm' ? 'wife' : 'husband';
+  const personPartner = isMarried ? `${partnerName} is my ${personPartnerType}` : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age && <p className="Person__age">I am {age}</p>}
+
+      <p className="Person__partner">{personPartner}</p>
+    </section>
+  );
+};
