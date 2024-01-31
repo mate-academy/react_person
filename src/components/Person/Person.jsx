f579df0bef46d6ee86ab42d5c7e27945e6337bd2
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-01-31 20:19:33 +0100

    sol

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,34 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        My name is
+        {' '}
+        {name}
+      </h2>
+      {age && (
+        <p className="Person__age">
+          I am
+          {' '}
+          {age}
+        </p>
+      )}
+      {isMarried ? (
+        <p className="Person__partner">
+          {partnerName}
+          {' '}
+          is my
+          {' '}
+          {sex === 'm' ? 'Wife'.charAt(0).toUpperCase() + 'Wife'.slice(1)
+            : 'Husband'.charAt(0).toUpperCase() + 'Husband'.slice(1)}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
+
+export default Person;
