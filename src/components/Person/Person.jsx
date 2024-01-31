c432028e4a806aa7ddc49b291ccc867297677a89
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-01-31 20:53:36 +0100

    solutionn

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,6 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partner = sex === 'm' ? 'wife' : 'husband'
+  const partner = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
@@ -9,9 +9,7 @@ export const Person = ({ person }) => {
       </h2>
       {age && (
         <p className="Person__age">
-          I am
-          {' '}
-          {age}
+          {`I am ${age}`}
         </p>
       )}
       <p className="Person__partner">
