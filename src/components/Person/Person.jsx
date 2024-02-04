686799a782b16c92ca9f78d26c68624c86bd439b
Alina <alinaklimuk04@gmail.com>
2024-02-04 14:56:49 +0200

    fix the problem

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,27 +1,32 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    {person.name && (
-      <h2 className="Person__name">
-        {`My name is ${person.name}`}
-      </h2>
-    )}
-    {person.age && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partner = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      {name && (
+        <h2 className="Person__name">
+          {`My name is ${name}`}
+        </h2>
+      )}
+      {age && (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+      )}
+      <p className="Person__partner">
+        {isMarried && (
+          <>
+            {sex === 'm'
+              ? `${partnerName} is my ${partner}`
+              : `${partnerName} is my ${partner}`
+            }
+          </>
+        )}
+        {!isMarried && (
+          'I am not married'
+        )}
       </p>
-    )}
-    <p className="Person__partner">
-      {person.isMarried && (
-        <>
-          {person.sex === 'm'
-            ? `${person.partnerName} is my wife`
-            : `${person.partnerName} is my husband`
-          }
-        </>
-      )}
-      {!person.isMarried && (
-        'I am not married'
-      )}
-    </p>
-  </section>
-);
+    </section>
+  );
+};
