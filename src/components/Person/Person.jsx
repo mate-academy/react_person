eef0f68b9c0f4b16192c38c9342a55b505340af6
Ivan <tixehack@gmail.com>
2024-02-09 17:07:16 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,24 @@
-/* eslint-disable no-nested-ternary */
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
-    </h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <p className="Person__partner">
-      {person.isMarried ? (
-        person.sex === 'm' ? `${person.partnerName} is my wife` : `${person.partnerName} is my husband`
-      ) : `I am not married`}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const partner
+    = sex === 'm'
+      ? `${partnerName} is my wife`
+      : `${partnerName} is my husband`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">
+        {`My name is ${name}`}
+      </h2>
+
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+      <p className="Person__partner">
+        {isMarried ? partner : `I am not married`}
+      </p>
+    </section>
+  );
+};
