7dd448f6c255f1dbd51bffe3a814409fa41281ce
Anton Tur <ant.tur85@gmail.com>
2024-02-14 08:29:12 +0200

    fix bugs

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,16 +8,18 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {age !== undefined ? (
+      {age && (
         <p className="Person__age">
           {`I am ${age}`}
         </p>
-      ) : ''}
+      )}
 
-      {isMarried === false
-        ? (<p className="Person__partner">I am not married</p>)
-        : (<p className="Person__partner">{ str }</p>)
-      }
+      <p className="Person__partner">
+        {isMarried
+          ? `${str}`
+          : `I am not married`
+        }
+      </p>
     </section>
   );
 };
