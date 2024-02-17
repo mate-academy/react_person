7bf7983f2bbd5ef38324f4dfe6cb455e280f9c22
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-02-17 13:44:10 +0100

    Fixes after CR

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,28 +1,30 @@
 // export const Person = ({ person }) => ();
 import React from 'react';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">
-      {`My name is ${person.name}`}
+export const Person = ({ person }) => {
+  const personPartnerType = person.sex === 'm' ? 'wife' : 'husband';
+  const personPartner = person.isMarried ? `${person.partnerName} is my ${personPartnerType}` : 'I am not married';
 
-    </h2>
-    {person.age
-      && (
-      <p className="Person__age">
-        {`I am ${person.age}`}
-      </p>
-      )
-      }
-    {person.isMarried
-      ? (
-        <p className="Person__partner">
-          { `${person.partnerName} is my `}
-          {person.sex === 'm' ? ('wife') : ('husband')}
-        </p>
-      )
-      : (
-        <p className="Person__partner">I am not married</p>
-      )}
-  </section>
-);
+  return (
+    (
+      <section className="Person">
+        <h2 className="Person__name">
+          {`My name is ${person.name}`}
+
+        </h2>
+        {person.age
+          && (
+          <p className="Person__age">
+            {`I am ${person.age}`}
+          </p>
+          )
+          }
+        {
+          <p className="Person__partner">
+            {personPartner}
+          </p>
+        }
+      </section>
+    )
+  );
+};
