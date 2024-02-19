37a8f7f6c1e3bd30b9786442d187ba8a3befb2b9
Nikita Ganzha <geekneek28@gmail.com>
2024-02-19 09:16:02 +0200

    moved function outside and renamed it

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,12 @@
+const getParterInfo = (sex, partnerName) => {
+  if (sex === 'm') {
+    return `${partnerName} is my wife`;
+  }
+
+  return `${partnerName} is my husband`;
+};
+
 export const Person = ({ person }) => {
-  const married = (sex, partnerName) => {
-    if (sex === 'm') {
-      return `${partnerName} is my wife`;
-    }
-
-    return `${partnerName} is my husband`;
-  };
-
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
@@ -15,7 +15,7 @@ export const Person = ({ person }) => {
 
       <p className="Person__partner">
         {person.isMarried
-          ? married(person.sex, person.partnerName)
+          ? getParterInfo(person.sex, person.partnerName)
           : 'I am not married'}
       </p>
     </section>
