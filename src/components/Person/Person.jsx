4475661201102d6144d7413e610f7f1701322c35
HolyRaveOS <kitlaruk1995@gmail.com>
2024-02-20 13:03:23 +0200

    add corrections

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,17 +5,17 @@ export const Person = ({ person }) => {
 
   const genderMessage = sex === 'm' ? 'wife' : 'husband';
 
+  const isMarriedMessage = isMarried
+    ? `${partnerName} is my ${genderMessage}`
+    : 'I am not married';
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
       {age && <p className="Person__age">I am {age}</p>}
 
-      <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${genderMessage}`
-          : 'I am not married'}
-      </p>
+      <p className="Person__partner">{isMarriedMessage}</p>
     </section>
   );
 };
