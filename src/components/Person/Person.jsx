3f3b11451f4a108f7838e785a6a67a9986f003e0
HolyRaveOS <kitlaruk1995@gmail.com>
2024-02-20 12:35:13 +0200

    made corrections

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,3 +1,5 @@
+import './Person.scss';
+
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
@@ -6,7 +8,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
+
       {age && <p className="Person__age">I am {age}</p>}
+
       <p className="Person__partner">
         {isMarried
           ? `${partnerName} is my ${genderMessage}`
