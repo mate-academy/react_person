8ae2a2e693455d5fea8123311855c6a2653c4091
Ann Busyhina <070196ann@gmail.com>
2024-02-20 22:08:52 +0200

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,17 @@
 import './person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age !== undefined && (
-      <p className="Person__age">I am {person.age}</p>
-    )}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const partnerRelationship = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <p className="Person__partner">
+        {person.isMarried
+          ? `${person.partnerName} is my ${partnerRelationship}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
