383821fa20d69774850efd3ea3aed87baa3a291b
Roman Babii <ggdiscone@gmail.com>
2024-02-21 22:36:22 +0200

    refactored Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,12 +7,9 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && <p className="Person__age">I am {person.age}</p>}
-
-      {person.isMarried ? (
-        <p className="Person__partner">{partner}</p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">
+        {person.isMarried ? partner : `I am not married`}
+      </p>
     </section>
   );
 };
