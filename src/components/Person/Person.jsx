67eedd8f48d921989d6d7b4245f78e4b741808a9
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-02-22 18:21:29 +0200

    add better task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,18 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const marriedMessage = isMarried ? (
+    <p className="Person__partner">
+      {`${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`}
+    </p>
+  ) : (
+    <p className="Person__partner">I am not married</p>
+  );
+
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && <p className="Person__age">I am {person.age}</p>}
-      {person.isMarried === true ? (
-        <p className="Person__partner">
-          {`${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {marriedMessage}
     </section>
   );
 };
