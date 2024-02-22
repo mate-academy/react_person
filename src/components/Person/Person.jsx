991914d61e8c3aa29828dad8265f437020fbca77
Anastasiia <vasilisandreeva00@gmail.com>
2024-02-22 09:27:47 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,14 +3,16 @@ export function Person({ person }) {
   const partner = sex === 'm' ? 'wife' : 'husband';
 
   return (
-      <section className="Person">
-        <h2 className="Person__name">{`My name is ${name}`}</h2>
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-        <p className="Person__age">{age && `I am ${age}`}</p>
+      {age && <p className="Person__age">I am {age}</p>}
 
-        <p className="Person__partner">
-          {isMarried ? `${partnerName} is my ${partner}` : `I am not married`}
-        </p>
-      </section>
+      {/* <p className="Person__age">{age && `I am ${age}`}</p> */}
+
+      <p className="Person__partner">
+        {isMarried ? `${partnerName} is my ${partner}` : `I am not married`}
+      </p>
+    </section>
   );
 }
