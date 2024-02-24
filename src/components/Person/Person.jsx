2f5ab2a9c6dd648db0f817aca8caefaed0b3698e
Богдан <BoGoBooM@MacBook-Pro---Bohdan.local>
2024-02-24 20:29:18 +0200

    bug was fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,9 +5,7 @@ export function Person({ person }) {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-      {person.age && 
-        <p className="Person__age">I am {`I am ${person.age}`}</p>
-      }
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
 
       <p className="Person__partner">
         {person.isMarried
