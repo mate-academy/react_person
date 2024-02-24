6f418d75849111b5bfd524d25aa1dad3056f3cd1
Robert Szałapak <robsza1212@gmail.com>
2024-02-24 17:52:28 +0100

    changing person's name class

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -15,7 +15,7 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2 className="Person__title">My name is {name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
         {isMarried
