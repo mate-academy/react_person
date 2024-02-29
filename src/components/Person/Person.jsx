9337a4f951edfc380185a712972df84602873c63
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-02-29 21:32:27 +0200

    feat [react]: implement `Person` component to render it`s details

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+const MerriageInfo = ({ message = 'I am not married' }) => (
+  <p className="Person__partner">{message}</p>
+);
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age && <p className="Person__age">I am {person.age}</p>}
+    {person.isMarried ? (
+      <MerriageInfo
+        message={`${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
+      />
+    ) : (
+      <MerriageInfo />
+    )}
+  </section>
+);
