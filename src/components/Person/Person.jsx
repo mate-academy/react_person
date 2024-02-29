b32cfce4db734da96c658849c63ce576d97d4bec
Denys209818 <deniskravcuk63@gmail.com>
2024-02-29 02:01:35 +0200

    Load unmarried statues

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,9 +3,9 @@ export const Person = ({ person }) => (
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && <p className="Person__age">I am {person.age}</p> }
       {
-        person.isMarried &&
+        person.isMarried ?
         <p className="Person__partner">
           {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
-          </p>}
+          </p> : <p className="Person__partner">I am not married</p>}
     </section>
 );
