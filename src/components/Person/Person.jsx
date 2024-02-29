ee5cfc863bdb2839b41241a76bcfe1196c138dcb
Denys209818 <deniskravcuk63@gmail.com>
2024-02-29 09:47:05 +0200

    Add variables to compute them before displaying

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,15 @@
-export const Person = ({ person }) => (
-  <section className="Person">
+export const Person = ({ person }) => {
+  const marriedPartnerSex = person.sex === 'f' ? 'husband' : 'wife';
+  const marriedType = person.isMarried ?
+    `${person.partnerName} is my ${marriedPartnerSex}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && <p className="Person__age">I am {person.age}</p> }
-      {
-        person.isMarried ?
-        <p className="Person__partner">
-          {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
-          </p> : <p className="Person__partner">I am not married</p>}
+
+      <p className="Person__partner">{marriedType}</p>
     </section>
-);
+  )
+};
