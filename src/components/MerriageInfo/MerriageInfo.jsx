1d456b3c0b569e523857f3c68766931d6cf47502
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-02-29 21:55:17 +0200

    refactor [react]: move `MerriageInfo` into separate component

---
+++ src/components/MerriageInfo/MerriageInfo.jsx
@@ -0,0 +1,3 @@
+export const MerriageInfo = ({ message = 'I am not married' }) => (
+  <p className="Person__partner">{message}</p>
+);
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,4 @@
-const MerriageInfo = ({ message = 'I am not married' }) => (
-  <p className="Person__partner">{message}</p>
-);
+import { MerriageInfo } from '../MerriageInfo/MerriageInfo';
 
 export const Person = ({ person }) => (
   <section className="Person">
