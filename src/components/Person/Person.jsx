d954cc89e8c777edfd65df689bd76d01d5542a9e
Oleksandr Yepik <sepika534@gmail.com>
2024-03-02 17:20:44 +0200

    task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, sex, isMarried, partnerName, age } = person;
+  let status = 'I am not married';
+
+  if (isMarried) {
+    if (sex === 'f') {
+      status = `${partnerName} is my husband`;
+    } else {
+      status = `${partnerName} is my wife`;
+    }
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{status}</p>
+    </section>
+  );
+};
