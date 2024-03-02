f3ad48ec9dc3780adc29b1488ce396e0eaf83113
Arkadiusz <stefanski.a00@gmail.com>
2024-03-02 15:55:30 +0100

    solution

---
+++ src/components/People/People.js
@@ -0,0 +1,23 @@
+const people = {
+  misha: {
+    name: 'Misha',
+    age: 37,
+    sex: 'm',
+    isMarried: true,
+    partnerName: 'Natasha',
+  },
+  olya: {
+    name: 'Olya',
+    sex: 'f',
+    isMarried: true,
+    partnerName: 'Maksym',
+  },
+  alex: {
+    name: 'Alex',
+    age: 25,
+    sex: 'm',
+    isMarried: false,
+  },
+};
+
+export { people };
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let partner;
+
+  if (isMarried) {
+    if (sex === 'm') {
+      const partnerType = 'wife';
+
+      partner = `${partnerName} is my ${partnerType}`;
+    } else {
+      const partnerType = 'husband';
+
+      partner = `${partnerName} is my ${partnerType}`;
+    }
+  } else {
+    partner = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">{partner}</p>
+    </section>
+  );
+};
