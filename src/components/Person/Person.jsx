96161e4c984c292c7b6572a6ab1d24599f852b6c
Denis Kotov <dionisiy2904@gmail.com>
2024-03-03 20:38:42 +0200

    person fix value

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,17 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
+export const Person = ({ person }) => {
+  const partnerSex = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.age !== undefined ? (
-      <p className="Person__age">I am {person.age}</p>
-    ) : null}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
 
-    <p className="Person__partner">
-      {person.isMarried === false
-        ? 'I am not married'
-        : `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`}
-    </p>
-  </section>
-);
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+
+      <p className="Person__partner">
+        {person.isMarried
+          ? `${person.partnerName} is my ${partnerSex}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
