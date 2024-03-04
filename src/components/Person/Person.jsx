2d761e509da54c5fb66a9028c50cedfa81a49a8b
Dmitriy Luhasnkiy <luhanskiydmitriy@gmail.com>
2024-03-04 21:14:03 +0300

    update solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,7 +11,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
 
-      {person.age && <p className="Person__age">I am {person.age} </p>}
+      {person.age && <p className="Person__age">I am {person.age}</p>}
 
       <p className="Person__partner">
         {person.isMarried
