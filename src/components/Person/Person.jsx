6cfe6031df8260113dda333ab8db6863616425fe
Tetiana Lisne <tplovic@gmail.com>
2024-03-04 10:01:24 +0200

    add react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,22 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let partnerMessage;
+
+  if (person.isMarried) {
+    partnerMessage =
+      person.sex === 'm'
+        ? `${person.partnerName} is my wife`
+        : `${person.partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {person.isMarried ? (
+        <p className="Person__partner">{partnerMessage}</p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
