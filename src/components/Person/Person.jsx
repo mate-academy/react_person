4e5287f93384ee23f70334e2987a10b0c7083bb2
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-03-04 23:06:42 +0200

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let status = null;
+
+  if (!isMarried) {
+    status = 'I am not married';
+  } else {
+    status =
+      sex === 'm'
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{status}</p>
+    </section>
+  );
+};
