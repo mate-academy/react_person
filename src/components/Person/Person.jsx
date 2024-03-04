a540561787cbc7fea1bda560532c5ac5d525b332
Dmytro Haidash <tipocto.ckujiji@gmail.com>
2024-03-04 21:32:14 +0200

    add task solution but not all tasks have passed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const partnerStatus = `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`;
+
+  return (
+    <>
+      <section className="Person">
+        <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+        <p className="Person__age">{person.age && `I am ${person.age}`}</p>
+        <p className="Person__partner">
+          {person.isMarried ? partnerStatus : 'I am not married'}
+        </p>
+      </section>
+    </>
+  );
+};
