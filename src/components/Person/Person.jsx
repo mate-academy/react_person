24be4f2fb52dd7451fb521f68c3024a8c503f76b
Kirill <ovoo1339@gmail.com>
2024-03-06 22:47:01 +0200

    work

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const partner = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <p className="Person__age">{`I am ${person.age}`}</p>
+      {person.isMarried === true ? (
+        <p className="Person__partner">{`${person.partnerName} is my ${partner}`}</p>
+      ) : (
+        'I am not married'
+      )}
+    </section>
+  );
+};
