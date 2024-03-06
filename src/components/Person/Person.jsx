1455f58031b1432a39d65faecce4bcd77ac2c4bc
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-03-06 22:26:57 +0100

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,30 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let contentPartner;
+  let contentAge;
+
+  if ('age' in person) {
+    contentAge = <p className="Person__age">I am {person.age}</p>;
+  } else {
+    contentAge = null;
+  }
+
+  if (person.isMarried === false) {
+    contentPartner = <p className="Person__partner">I am not married</p>;
+  } else if (person.isMarried === true && person.sex === 'm') {
+    contentPartner = (
+      <p className="Person__partner">{person.partnerName} is my wife</p>
+    );
+  } else if (person.isMarried === true && person.sex === 'f') {
+    contentPartner = (
+      <p className="Person__partner">{person.partnerName} is my husband</p>
+    );
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {contentAge}
+      {contentPartner}
+    </section>
+  );
+};
