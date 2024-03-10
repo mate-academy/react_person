1476545ad27d7bfa427fb67cb7ba70c99c067ea0
deezhizyu <kalenskuja@gmail.com>
2024-03-10 16:14:25 +0100

    Move person partner string to a separate variable

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,15 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  let personPartner = 'I am not married';
+
+  if (person.isMarried) {
+    personPartner = `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <p className="Person__partner">{personPartner}</p>
+    </section>
+  );
+};
