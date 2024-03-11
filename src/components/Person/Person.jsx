ec0bb85a700e90c4784330afc35108e97d4477c3
Oleh Marushchak <westernpc22@gmail.com>
2024-03-11 13:00:47 +0200

    add test

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable no-nested-ternary */
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {`${person.name}`}</h2>
+
+    {person.age !== undefined && (
+      <p className="Person__age">I am {`${person.age}`}</p>
+    )}
+
+    {person.isMarried === true && person.sex === 'f' ? (
+      <p className="Person__partner">{`${person.partnerName}`} is my husband</p>
+    ) : person.isMarried === false ? (
+      <p className="Person__partner">I am not married</p>
+    ) : (
+      <p className="Person__partner">{`${person.partnerName}`} is my wife</p>
+    )}
+  </section>
+);
