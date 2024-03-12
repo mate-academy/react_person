6fd62312454ec0561337c03976a6a09e8e1b8de4
Stanislav Kukharuk <kukharuk.stan1slav@gmail.com>
2024-03-12 08:56:42 +0000

    v: 2

--- src/App.jsx
+++ src/App.jsx
@@ -29,18 +29,5 @@ export const App = () => (
     <Person person={misha} />
     <Person person={olya} />
     <Person person={alex} />
-    {/* <Person name="Misha" age={37} isMarried="Natasha is my wife" />
-    <Person name="Olya" isMarried="Maksym is my husband" />
-    <Person name="Alex" age={25} isMarried="I am not married" />
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">Maksym is my husband</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section> */}
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,14 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age !== undefined && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    )}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const personPartnerType = person.sex === 'm' ? 'wife' : 'husband';
+  const personPartner = person.isMarried
+    ? `${person.name} is my ${personPartnerType}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      <p className="Person__partner">{personPartner}</p>
+    </section>
+  );
+};
