b581080ae6465806a9d3f300631c984518c96277
MaximAV <atanasovmaksim56@gmail.com>
2024-03-12 21:48:09 +0200

    version 1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,16 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age ? <p className="Person__age">I am {person.age}</p> : ''}
+    {person.sex === 'm' && person.isMarried === true ? (
+      <p className="Person__patner">{person.partnerName} is my wife</p>
+    ) : (
+      <p className="Person__patner">{person.partnerName} is my husband</p>
+    )}
+    {person.isMarried === false ? (
+      <p className="Person__patner">I am not married</p>
+    ) : (
+      ''
+    )}
+  </section>
+);
