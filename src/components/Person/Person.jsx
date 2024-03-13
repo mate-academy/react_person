12ee937756433daed4e13fb17b415f25c1d0aba4
Vladyslav Neholiuk <vnegoluk@gmail.com>
2024-03-13 16:57:22 +0100

    add task react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
 // export const Person = ({ person }) => ();
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => {
+  let partnerStatus;
+
+  if (isMarried) {
+    partnerStatus =
+      sex === 'm'
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
+  } else {
+    partnerStatus = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && age !== 0 && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerStatus}</p>
+    </section>
+  );
+};
