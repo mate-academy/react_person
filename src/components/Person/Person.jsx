6091427b237303c1cc138a819263d228e6170f87
Ярина Пугач <puhachyaryna@gmail.com>
2024-03-17 10:41:39 +0100

    React person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const marriedText = isMarried
+    ? `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`
+    : 'I am not married';
+
+  const ageText = age && `I am ${age}`;
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      <p className="Person__age">{ageText}</p>
+      <p className="Person__partner">{marriedText}</p>
+    </section>
+  );
+};
