6edaa896307c289d8f7cae31c4c535624207c9bb
Maryna Karpova <mari.artarea@gmail.com>
2024-03-18 18:40:55 +0100

    test 1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,22 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let textMarried;
+
+  if (isMarried) {
+    if (sex === 'f') {
+      textMarried = `My husband is ${partnerName}`;
+    } else if (sex === 'm') {
+      textMarried = `My wife is ${partnerName}`;
+    }
+  } else {
+    textMarried = `I am not married`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{textMarried}</p>
+    </section>
+  );
+};
