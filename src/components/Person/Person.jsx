99808441339db6a081dabcb697a78247f8a63ead
Valentyna Lytvynenko <valentina.vik10@gmail.com>
2024-03-18 20:47:50 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,54 +1,15 @@
 export const Person = ({ person }) => {
-  const getmaried = [];
-
-  if (person.isMarried && person.sex === 'f') {
-    getmaried.push('husband');
-  }
-
-  if (person.isMarried && person.sex === 'm') {
-    getmaried.push('wife');
-  }
-
-  if (!person.isMarried && person.age) {
-    getmaried.push('I am not married');
-
-    return (
-      <section className="Person">
-        <h2 className="Person__name">My name is {person.name}</h2>
-        <p className="Person__age">I am {person.age}</p>
-        <p className="Person__partner">{getmaried}</p>
-      </section>
-    );
-  }
-
-  if (!person.isMarried && !person.age) {
-    getmaried.push('I am not married');
-
-    return (
-      <section className="Person">
-        <h2 className="Person__name">My name is {person.name}</h2>
-        <p className="Person__partner">{getmaried}</p>
-      </section>
-    );
-  }
-
-  if (!person.age) {
-    return (
-      <section className="Person">
-        <h2 className="Person__name">My name is {person.name}</h2>
-        <p className="Person__partner">
-          {person.partnerName} is my {getmaried}
-        </p>
-      </section>
-    );
-  }
+  const { name, age, sex, isMarried, partnerName } = person;
+  const personStatus = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      <p className="Person__age">I am {person.age}</p>
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
-        {person.partnerName} is my {getmaried}
+        {isMarried
+          ? `${partnerName} is my ${personStatus}`
+          : 'I am not married'}
       </p>
     </section>
   );
