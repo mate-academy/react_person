be5581be4c1b583173c3b14c1e8ff764d2deab59
Vladyslav <fantom2569@gmail.com>
2024-03-19 21:27:49 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,19 +5,21 @@ import React from 'react';
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  const getPartnerDescription = () => {
+    if (!isMarried) {
+      return 'I am not married';
+    }
+
+    const partnerType = sex === 'm' ? 'wife' : 'husband';
+
+    return `${partnerName} is my ${partnerType}`;
+  };
+
   return (
     <div className="Person">
       <p className="Person__name">My name is {name}</p>
       {age && <p className="Person__age">I am {age}</p>}
-      {isMarried ? (
-        sex === 'm' ? (
-          <p className="Person__partner">{partnerName} is my wife</p>
-        ) : (
-          <p className="Person__partner">{partnerName} is my husband</p>
-        )
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">{getPartnerDescription()}</p>
     </div>
   );
 };
