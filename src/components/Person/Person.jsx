cf2e596e0cf9a26d99bd613b20f9ecc2a46ba5f7
Edward Tuzimek <edwardtuzimek@gmail.com>
2024-03-19 15:22:58 +0100

    destructur person and corretion for ternary operator

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,14 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
-        {person.isMarried ? (
-          `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`
+        {isMarried ? (
+          `${partnerName} is my 
+            ${sex === 'f' ? 'husband' : 'wife'}`
         ) : (
           <p>I am not married</p>
         )}
