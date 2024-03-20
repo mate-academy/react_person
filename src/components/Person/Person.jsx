7fc9202790d4870dde8cf433c0109bafdc24f76a
Oleksandr Chaika <pti4ka121@gmail.com>
2024-03-20 12:00:12 +0200

    task done v2.0

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,20 +2,20 @@
 // eslint-disable-next-line import/no-unresolved
 import './person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <p className="Person__partner">
-      {person.isMarried === true ? (
-        person.sex === 'm' ? (
-          <>{person.partnerName} is my wife</>
-        ) : (
-          <>{person.partnerName} is my husband</>
-        )
-      ) : (
-        'I am not married'
-      )}
-    </p>
-  </section>
-);
+export const Person = ({
+  person: { name, age, isMarried, sex, partnerName },
+}) => {
+  const maritalStatus = isMarried
+    ? sex === 'm'
+      ? `${partnerName} is my wife`
+      : `${partnerName} is my husband`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">{maritalStatus}</p>
+    </section>
+  );
+};
