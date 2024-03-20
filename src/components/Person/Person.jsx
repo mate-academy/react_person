9aafcc51269c856ca58af292e8b05b287a8dfabf
Oleksandr Chaika <pti4ka121@gmail.com>
2024-03-20 10:57:02 +0200

    task done v1.0

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable no-nested-ternary */
+// eslint-disable-next-line import/no-unresolved
+import './person.scss';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+    <p className="Person__partner">
+      {person.isMarried === true ? (
+        person.sex === 'm' ? (
+          <>{person.partnerName} is my wife</>
+        ) : (
+          <>{person.partnerName} is my husband</>
+        )
+      ) : (
+        'I am not married'
+      )}
+    </p>
+  </section>
+);
