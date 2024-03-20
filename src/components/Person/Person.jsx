b9da213392e7cc408f68c61fde67f9792d48e2cd
hyriix <shyrii11bohdan@gmail.com>
2024-03-20 10:11:34 +0100

    overloaded

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,14 @@
 export const Person = ({ person }) => {
+  const personGender = person.sex === 'm' ? 'wife' : 'husband';
+  const personStatus = person.isMarried
+    ? `${person.partnerName} is my ${personGender}`
+    : 'I am not married';
+
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && <p className="Person__age">I am {person.age}</p>}
-      {person.isMarried === true ? (
-        <p className="Person__partner">
-          {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">{personStatus}</p>
     </section>
   );
 };
