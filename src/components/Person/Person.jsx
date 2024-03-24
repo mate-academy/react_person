eb579fe0fcb7b374d201ce864875676f6ec9a186
Voloshynd <Voloshynd13@gmail.com>
2024-03-24 10:40:03 +0100

    Add solution for marital status

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,18 +3,15 @@ import './Person.scss';
 export const Person = ({ person }) => {
   const { name, age, isMarried, sex, partnerName } = person;
   const partnersGender = sex === 'm' ? 'wife' : 'husband';
+  const message = isMarried
+    ? `${partnerName} is my ${partnersGender}`
+    : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
-      {isMarried ? (
-        <p className="Person__partner">
-          {partnerName} is my {partnersGender}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">{message}</p>
     </section>
   );
 };
