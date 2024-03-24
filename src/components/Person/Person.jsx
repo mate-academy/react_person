84e7334d7113837b84eb35d4e34030f35566c6e6
Voloshynd <Voloshynd13@gmail.com>
2024-03-24 10:16:01 +0100

    Removed nested ternary operators

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,17 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, isMarried, sex, partnerName } = person;
+  const partnersGender = sex === 'm' ? 'wife' : 'husband';
+  const personMarried = isMarried
+    ? `${partnerName} is my ${partnersGender}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {personMarried}
+    </section>
+  );
+};
