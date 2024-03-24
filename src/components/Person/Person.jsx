7ae66275364861f4a06dd97cf8f95cc058b8f6a6
Yevhenii Kushnir <aroblack17@gmail.com>
2024-03-24 15:18:35 +0100

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,7 +5,7 @@ export const Person = ({ person }) => (
     <h2 className="Person__name">My name is {person.name}</h2>
     {person.age && <p className="Person__age">I am {person.age}</p>}
     <p className="Person__partner">
-      {person.isMarried === true
+      {person.isMarried
         ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
         : `I am not married`}
     </p>
