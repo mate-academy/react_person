bf8a76a455091d580169b92d682f6d5379fc123b
Stepan Rad <stepanrad@MacBook-Air-Stepan.local>
2024-03-26 20:05:25 +0200

    task done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+    <p className="Person__partner">
+      {person.isMarried === true &&
+        person.sex === 'm' &&
+        `${person.partnerName} is my wife`}
+      {person.isMarried === true &&
+        person.sex === 'f' &&
+        `${person.partnerName} is my husband`}
+      {person.isMarried === false && `I am not married`}
+    </p>
+  </section>
+);
