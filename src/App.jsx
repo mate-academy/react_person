e5cae6bca9d208a86fbb0b4df6b1cf9e7b52aa67
Stepan Rad <stepanrad@MacBook-Air-Stepan.local>
2024-03-27 22:05:09 +0200

    my last request for that task

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <>
-      <Person person={misha} />
-      <Person person={olya} />
-      <Person person={alex} />
-    </>
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,14 +1,12 @@
 export const Person = ({
   person,
-  partner = person.sex === 'm' ? 'wife' : 'husband',
+  partner = person.isMarried
+    ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+    : 'I am not married',
 }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
     {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${partner}`
-        : 'I am not married'}
-    </p>
+    <p className="Person__partner">{partner}</p>
   </section>
 );
