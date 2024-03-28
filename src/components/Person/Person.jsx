c895dc216c0a652ac6da3b78f0a90d5ef8a68f38
Bohdan <mylyibohdan18@gmail.com>
2024-03-28 10:17:08 +0200

    little code changes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
-
-export const Person = ({ person }) => (
-  <React.Fragment>
-    <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && <p className="Person__age">I am {person.age}</p>}
-      {person.isMarried === true && person.sex === 'm' && (
-        <p className="Person__partner">{person.partnerName} is my wife</p>
-      )}
-      {person.isMarried === true && person.sex === 'f' && (
-        <p className="Person__partner">{person.partnerName} is my husband</p>
-      )}
-      {person.isMarried === false && (
-        <p className="Person__partner">I am not married</p>
-      )}
-    </section>
-  </React.Fragment>
+export const Person = ({ name, age, isMarried, sex, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+    {age && <p className="Person__age">I am {age}</p>}
+    {isMarried && sex === 'm' && (
+      <p className="Person__partner">{partnerName} is my wife</p>
+    )}
+    {isMarried && sex === 'f' && (
+      <p className="Person__partner">{partnerName} is my husband</p>
+    )}
+    {isMarried && <p className="Person__partner">I am not married</p>}
+  </section>
 );
