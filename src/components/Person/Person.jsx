88b8e041df4f13f99ef0b9c1941cdeccf696680e
Bohdan <mylyibohdan18@gmail.com>
2024-03-28 09:02:23 +0200

    move solution to develop

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => (
+  <React.Fragment>
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age !== undefined ? (
+        <p className="Person__age">I am {person.age}</p>
+      ) : (
+        <p className="Person__age">{person.age}</p>
+      )}
+      {person.isMarried === true && person.sex === 'm' ? (
+        <p className="Person__partner">{person.partnerName} is my wife</p>
+      ) : (
+        ''
+      )}
+      {person.isMarried === true && person.sex === 'f' ? (
+        <p className="Person__partner">{person.partnerName} is husband</p>
+      ) : (
+        ''
+      )}
+      {person.isMarried === false ? (
+        <p className="Person__partner">I am not married</p>
+      ) : (
+        ''
+      )}
+    </section>
+  </React.Fragment>
+);
