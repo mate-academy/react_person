4562b5e4a7320849d42aa2d470fc644ea05b9267
Bohdan <mylyibohdan18@gmail.com>
2024-03-28 09:58:31 +0200

    simplify ternary operator

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,25 +4,15 @@ export const Person = ({ person }) => (
   <React.Fragment>
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age !== undefined ? (
-        <p className="Person__age">I am {person.age}</p>
-      ) : (
-        ''
-      )}
-      {person.isMarried === true && person.sex === 'm' ? (
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {person.isMarried === true && person.sex === 'm' && (
         <p className="Person__partner">{person.partnerName} is my wife</p>
-      ) : (
-        ''
       )}
-      {person.isMarried === true && person.sex === 'f' ? (
+      {person.isMarried === true && person.sex === 'f' && (
         <p className="Person__partner">{person.partnerName} is my husband</p>
-      ) : (
-        ''
       )}
-      {person.isMarried === false ? (
+      {person.isMarried === false && (
         <p className="Person__partner">I am not married</p>
-      ) : (
-        ''
       )}
     </section>
   </React.Fragment>
