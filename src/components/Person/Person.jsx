7df9eab94a8b16eb5e1525b383fee8a3f6d5d98a
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-23 15:22:33 +0300

    [Feature] added Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+const PARTNER_VARIANTS = {
+  f: 'husband',
+  m: 'wife',
+};
+
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => {
+  const personName = `My name is ${name}`;
+  const personAge = age ? `I am ${age}` : null;
+  const marriageTitle = isMarried
+    ? `${partnerName} is my ${PARTNER_VARIANTS[sex]}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{personName}</h2>
+
+      {personAge && <p className="Person__age">{personAge}</p>}
+
+      <p className="Person__partner">{marriageTitle}</p>
+    </section>
+  );
+};
