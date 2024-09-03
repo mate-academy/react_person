cc82e442207ea5818455f89ca5b500a17c073d5a
Daryna Kopytko <dashindafox@gmail.com>
2024-09-03 14:20:43 +0300

    add solution of react_person task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const personPartnerType = sex === 'm' ? 'wife' : 'husband';
+  const personIsMarried = isMarried
+    ? `${partnerName} is my ${personPartnerType}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{personIsMarried}</p>
+    </section>
+  );
+};
