e3f32bae3adbd269ebf8c4921f21c3546c68b6ce
PAPAYA . <vlad.teut2015@gmail.com>
2024-09-09 13:55:21 +0300

    taskPerson

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const getPartner = () => {
+    if (person.isMarried) {
+      if (person.sex === 'm') {
+        return 'wife';
+      }
+
+      return 'husband';
+    }
+
+    return 'I am not married';
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+
+      <p className="Person__partner">
+        {person.partnerName} is my {getPartner()}
+      </p>
+    </section>
+  );
+};
