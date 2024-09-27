2a56ae4f22a6e96b195c1133ebda7ca8377d964d
artemivasenko <artem.iv@mate.academy>
2024-09-27 14:15:29 +0300

    sol2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let statusPartner;
+
+  if (isMarried) {
+    statusPartner =
+      sex === 'm'
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
+  } else {
+    // eslint-disable-next-line no-unused-vars
+    statusPartner = 'I am not married';
+  }
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <h2 className="Person__name">My name is {name}</h2>
+
+        {age && <p className="Person__age">I am {age}</p>}
+
+        <p className="Person__partner">{statusPartner}</p>
+      </section>
+    </div>
+  );
+};
