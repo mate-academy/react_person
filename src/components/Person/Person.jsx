39c04bceac9f898f60ab287ab55db5bec3b84213
nex1994 <bilicki1994@gmail.com>
2025-01-04 19:22:52 +0100

    init commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,19 +3,23 @@ export const Person = ({ person }) => {
 
   if (person.isMarried) {
     if (person.sex === 'f') {
-      marriedTo = (<p className="Person__partner">{person.partnerName} is my husband</p>)
+      marriedTo = (
+        <p className="Person__partner">{person.partnerName} is my husband</p>
+      );
     } else {
-      marriedTo = (<p className="Person__partner">{person.partnerName} is my wife</p>)
+      marriedTo = (
+        <p className="Person__partner">{person.partnerName} is my wife</p>
+      );
     }
   } else {
-    marriedTo = <p className="Person__partner">I am not married</p>
+    marriedTo = <p className="Person__partner">I am not married</p>;
   }
 
-  return <>
+  return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age &&  <p className="Person__age">I am {person.age}</p>}
+      {person.age && <p className="Person__age">I am {person.age}</p>}
       {marriedTo}
     </section>
-  </>
+  );
 };
