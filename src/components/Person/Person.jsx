118c251fedc868c57fee0a0fc55cf0717e3c79ed
Varvara00 <varvara.galy171203@gmail.com>
2025-01-13 20:57:13 +0100

    Finished task react_person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let relationship = null;
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      relationship = 'wife';
+    } else if (person.sex === 'f') {
+      relationship = 'husband';
+    }
+  }
+
+  return (
+    <div className="App">
+      <section className="Person">
+        <p className="Person__name">My name is {person.name}</p>
+        {person.age > 0 && <p className="Person__age">I am {person.age}</p>}
+        <p className="Person__partner">
+          {person.isMarried
+            ? `${person.partnerName} is my ${relationship}`
+            : 'not married'}
+        </p>
+      </section>
+    </div>
+  );
+};
