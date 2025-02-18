03f9fb1dec5f6c48b6b5a7649f093c59161bb2ea
Ivan Lavruk <ilavr8725@gmail.com>
2025-02-18 22:19:59 +0100

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+    {age != null && <p className="Person__age">I am {age}</p>}
+    {isMarried === true ? (
+      <p className="Person__partner">
+        {sex === 'm'
+          ? `${partnerName} is my wife`
+          : `${partnerName} is my husband`}
+      </p>
+    ) : (
+      <p className="Person__partner">I am not married</p>
+    )}
+  </section>
+);
