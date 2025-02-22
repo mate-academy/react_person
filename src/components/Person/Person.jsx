9128250f5d4d4e5de2c80fc5e3caa6b21720dfa8
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2025-02-22 14:09:38 +0800

    Completed the assignment with all required features

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
 // export const Person = ({ person }) => ();
+import React from 'react'
+
+const Person = ({name,age,isMarried,partnerName}) => {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      <p className="Person__age">I am {age}</p>
+      {
+      isMarried ? 
+      <p className="Person__partner">{partnerName} is my wife</p>
+      :
+      <p className="Person__partner">I am not married</p>
+      }
+
+    </section>
+  )
+}
+
+export default Person
