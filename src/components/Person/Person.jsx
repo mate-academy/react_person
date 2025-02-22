0e42c147e7bd90101b0062adaf0e93e6b05adc47
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2025-02-22 15:00:07 +0800

    Resubmission: Fixed issues and updated documentation

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+import React from 'react'
+
+export default function Person({ person }) {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person?.age != null ? <p className="Person__age">I am {person.age}</p> : null}
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+}
