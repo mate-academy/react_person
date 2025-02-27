f6099f97ca5053493d9c0bc5b62bff7d176a7dbf
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-02-27 17:21:04 +0200

    Implement solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let ageLabel = null;
+  let partnerLabel = 'I am not married';
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      partnerLabel = `${person.partnerName} is my wife`;
+    } else {
+      partnerLabel = `${person.partnerName} is my husband`;
+    }
+  }
+
+  if (person.age) {
+    ageLabel = <p className="Person__age">I am {person.age}</p>;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {ageLabel}
+      <p className="Person__partner">{partnerLabel}</p>
+    </section>
+  );
+};
