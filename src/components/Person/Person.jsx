6402067aaa91f52a78d8070f6570edbf24a5dfc9
Che <rererdom@gmail.com>
2025-02-28 09:06:50 +0000

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2
+      className="Person__name"
+      style={{
+        backgroundColor: person.isMarried ? 'lightgreen' : 'red',
+      }}
+    >
+      My name is {person.name}
+    </h2>
+    {person.age && <p className="Person__age">I am {person.age}</p>}
+    {person.sex === 'm' && person.isMarried && (
+      <p className="Person__partner">{person.partnerName} is my wife</p>
+    )}
+    {person.sex === 'f' && person.isMarried && (
+      <p className="Person__partner">{person.partnerName} is my husband</p>
+    )}
+    {!person.isMarried && <p className="Person__partner"> I am not married</p>}
+  </section>
+);
