d25b5ada2ad1a295a0eb440a0dc8d5b9e8cf6040
LX-y-ka <alexandravirtualife@gmail.com>
2025-03-03 19:56:47 +0200

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  return (
+    <>
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age !== undefined ? (
+        <p className="Person__age">I am {person.age}</p>
+      ) : (
+        ''
+      )}
+      {person.isMarried === false && (
+        <p className="Person__partner">I am not married</p>
+      )}
+      {person.sex === 'm' && person.isMarried && (
+        <p className="Person__partner">{person.partnerName} is my wife</p>
+      )}
+      {person.sex === 'f' && person.isMarried && (
+        <p className="Person__partner">{person.partnerName} is my husband</p>
+      )}
+    </>
+  );
+};
