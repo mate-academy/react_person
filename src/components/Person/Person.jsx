ae8a1b406b65a2dc8125011ae1ae7dfd9745f150
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-03-03 21:52:02 +0200

    Done task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let personStatus = 'I am not married';
+
+  if (isMarried && partnerName) {
+    personStatus = `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{personStatus}</p>
+    </section>
+  );
+};
