365d74db632b31a05fd7235b1978fc30d58b2edc
Olívio Neves <olivio.servio@yahoo.com.br>
2025-03-06 08:39:02 -0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,35 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const renderPartnerStatus = () => {
+    if (person.isMarried) {
+      if (person.sex === 'm') {
+        return (
+          <p className="Person__partner">{`${person.partnerName} is my wife`}</p>
+        );
+      }
+
+      if (person.sex === 'f') {
+        return (
+          <p className="Person__partner">{`${person.partnerName} is my husband`}</p>
+        );
+      }
+
+      return (
+        <p className="Person__partner">{`${person.partnerName} is my partner`}</p>
+      );
+    }
+
+    return <p className="Person__partner">I am not married</p>;
+  };
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <div>
+        {person.age && <p className="Person__age">I am {person.age}</p>}
+      </div>
+      <div>{renderPartnerStatus()}</div>
+    </div>
+  );
+};
