57c4f743278745d80477c0a12f62eaa02b6b638a
Andriian <an.hrytsan@gmail.com>
2025-03-22 16:36:51 +0200

    fixed ternary operator formatting

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,8 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partner = sex === 'm' ? 'wife' : 'husband';
+  const partner = sex === 'm'
+    ? 'wife'
+    : 'husband';
   const partnerMessage = isMarried
     ? `${partnerName} is my ${partner}`
     : 'I am not married';
