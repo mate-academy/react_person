7fcf25c72b4063191bda8684c4c69e5962c5352f
Mac-OK <khmilovskyi@gmail.com>
2025-03-22 17:24:49 +0100

    second commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,7 +6,8 @@ export const Person = ({ person }) => (
 
     <p className="Person__partner">
       {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+        ? `${person.partnerName} is my
+        ${person.sex === 'm' ? 'wife' : 'husband'}`
         : 'I am not married'}
     </p>
   </section>
