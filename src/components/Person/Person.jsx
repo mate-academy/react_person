ba7463f34fe05dabf54576fe8a6a0dc1c72434dd
Andrew Siemakin <andrewsiemakin@gmail.com>
2025-04-06 02:58:11 +0300

    Fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,7 +7,8 @@ export const Person = ({ person }) => {
       {person.age && <p className="Person__age">I am {person.age}</p>}
       {person.isMarried ? (
         <p className="Person__partner">
-          {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
+          {person.partnerName} is my
+          {person.sex === 'f' ? 'husband' : 'wife'}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
