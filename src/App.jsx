848a9ee352e3ec51ebcbb64088a67da86c619dc5
Yaroslav <freefrip@gmail.com>
2025-05-12 18:59:54 +0300

    fixxx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Person } from './components/Person/Person'
+import { Person } from './components/Person/Person';
 
 export const misha = {
   name: 'Misha',
@@ -26,8 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <Person person={misha}/>
-    <Person person={olya}/>
-    <Person person={alex}/>
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
