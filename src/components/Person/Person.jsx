b8067458e2416845f44f7ac634ae658d7b307b62
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-05-16 13:48:12 +0300

    React person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,9 +2,11 @@ export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
 
-    {person.age > 0 && <p className="Person__age">I am {person.age}</p>}
+    {person.age !== undefined && (
+      <p className="Person__age">I am {person.age}</p>
+    )}
 
-    {person.isMarried > 0 ? (
+    {person.isMarried === true ? (
       <p className="Person__partner">
         {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
       </p>
