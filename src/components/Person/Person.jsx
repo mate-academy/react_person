732099d20dce4ffc31162a5efa6e84c8f09934e6
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-05-20 23:24:22 +0100

    s

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,7 +12,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      <p className="Person__age">I am {person.age}</p>
+
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
