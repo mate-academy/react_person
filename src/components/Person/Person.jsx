8ee095ba0a8e857806150fb2424c551e2d08065f
Renato <renatovalerio@gmail.com>
2025-06-02 16:27:34 -0300

    alteration in jsx app and person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,16 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ name, age, sex, isMarried, partnerName }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+    {age && <p className="Person__age">{`I am ${age}`}</p>}
+
+    {partnerName ? (
+      <p className="Person__partner">
+        {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+      </p>
+    ) : (
+      <p className="Person__partner">I am not married</p>
+    )}
+  </section>
+);
+
+// - If a man is married, refer to his partner as `wife`; if a woman is married, refer to her partner as `husband`;
