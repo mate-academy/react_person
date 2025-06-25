36b15d7d59ab9401656a4de2d4b16d8cc4b7f191
Irina <weedfox053@gmail.com>
2025-06-25 17:14:52 +0300

    --no-verify

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import classNames from 'classnames';
+
+export const Person = ({ person }) => {
+  let {name, age, married} = person
+
+  let isfemale = name === 'olya';
+  let partner = isfemale ? 'husband' : 'wife';
+  return (
+    <React.Fragment>
+    <div> name: { name }</div>
+    <div> age: { age }</div>
+    <div> married: { married }</div>
+    { married ? (
+      <div>{ partner }</div>
+    ): <div>I'm not married</div>}
+    </React.Fragment>
+  )
+};
+
+
