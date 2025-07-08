d3bc921ef817acf9342dc314349cc178335241a2
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-08 18:35:30 +0200

    Update Person.jsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
+
+      {isMarried === false ? (
+        <p className="Person__partner">I am not married</p>
+      ) : (
+        <p className="Person__partner">
+          {`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}
+        </p>
+      )}
+    </section>
+  );
+};
