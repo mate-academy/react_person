12af9718e34ea3fc2d504350a58c1f2c2884cd79
Anastasiia Romaniuk <anast.vozniuk@gmail.com>
2025-07-10 10:42:11 +0300

    add

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,11 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ name, age = null, sex, isMarried, partnerName }) => (
+  <>
+    <p>{`My name is ${name}`}</p>
+    {age !== undefined && <p>{`I am ${age}`}</p>}
+    {isMarried &&
+      partnerName &&
+      (sex === 'f'
+        ? `${partnerName} is my husband`
+        : `${partnerName} is my wife`)}
+  </>
+);
