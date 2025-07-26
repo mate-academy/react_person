a15596ea7508f5d6bb392a56b31732f9bd0d9e38
moussi900 <studenttec8@gmail.com>
2025-07-26 18:51:07 +0100

    info

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+const pInfo = person => {
+  let pText = '';
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      pText = `${person.partnerName} is my wife`;
+    } else {
+      pText = `${person.partnerName} is my husband`;
+    }
+  } else {
+    pText = 'I am not married';
+  }
+
+  return pText;
+};
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age && <p className="Person__age">I am {person.age} </p>}
+    <p className="Person__partner">{pInfo(person)}</p>
+  </section>
+);
