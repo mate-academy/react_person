4c9b68a23fff4ace27ed7495b8a8bc1f60a65836
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-07-26 20:00:31 -0300

    Concretização do componente Person sob os requisitos especificados

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{person.name}</h2>
+      {person.age && <p className="Person__age">{person.age}</p>}
+      <p className="Person__partner">
+        {person.isMarried ? person.partnerName : 'I am not married'}
+      </p>
+    </section>
+  );
+};
