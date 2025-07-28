857416f487c8bdb48140103d71675997aaca0e53
Poltorak Denys <itwfrontend@gmail.com>
2025-07-28 23:26:10 +0300

    feat: add person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+const Person = ({ person }) => {
+  const { name, age = 0, sex, isMarried, partnerName } = person;
+  const partnerStatus = sex === 'f' ? 'husband' : 'wife';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age !== 0 && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerStatus}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
+
+export { Person };
