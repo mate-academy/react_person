3effa051614e5f1e4334716a6ed0dfaaea8183f8
Artem Vikuliev <ffox2645@gmail.com>
2025-07-30 17:28:42 +0300

    Fixed some issue

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -17,7 +17,9 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      {person.age !== undefined && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
 
       {partnerOfPerson}
     </section>
