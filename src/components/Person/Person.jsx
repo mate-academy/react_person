e576c02c2113072a35a18dd2d2a67e08170366f2
YuliaProkipchuk <yulayulayula2003@gmail.com>
2025-07-30 17:18:17 +0300

    fix solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,13 @@
 import './Person.scss';
 
 export const Person = ({ person }) => {
-  let personPartner = '';
-
-  if (person.sex === 'f') {
-    personPartner = 'husband';
-  } else {
-    personPartner = 'wife';
-  }
-
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
       {person.age && <p className="Person__age">I am {person.age}</p>}
       {person.isMarried && (
         <p className="Person__partner">
-          {person.partnerName} is my {personPartner}
+          {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
         </p>
       )}
       {!person.isMarried && <p className="Person__partner">I am not married</p>}
