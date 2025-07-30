a6783787403fff6f5b41fb7a478f66288e8a13e2
Artem Vikuliev <ffox2645@gmail.com>
2025-07-30 17:45:56 +0300

    fix: person.age issue

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -17,9 +17,10 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-      {person.age !== undefined && (
+      {person.age ? <p className="Person__age">I am {person.age}</p> : null}
+      {/* {person.age !== undefined && (
         <p className="Person__age">{`I am ${person.age}`}</p>
-      )}
+      )} */}
 
       {partnerOfPerson}
     </section>
