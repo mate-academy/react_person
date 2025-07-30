0a1fa2b12db9fd46640944f6c0dd88e6b430869f
Вадим <vadym@MacBook-Pro-Vadim.local>
2025-07-30 09:14:36 +0300

    First commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerText = 'I am not married';
+
+  if (isMarried) {
+    if (sex === 'm') {
+      partnerText = `${partnerName} is my wife`;
+    } else {
+      partnerText = `${partnerName} is my husband`;
+    }
+  }
+
+  return (
+    <div className="Person">
+      <div className="Person__name">My name is {name}</div>
+
+      {age !== undefined && <div className="Person__age">I am {age}</div>}
+
+      <div className="Person__partner">{partnerText}</div>
+    </div>
+  );
+};
