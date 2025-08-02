8d037b4e7e8376a7b9117e9763ca2287aa9b03b6
Emaan Hookey <em.grey28@gmail.com>
2025-08-02 17:54:56 -0230

    followed instructions

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerText;
+
+  if (!isMarried) {
+    partnerText = 'I am not married';
+  } else if (sex === 'm') {
+    partnerText = `${partnerName} is my wife`;
+  } else {
+    partnerText = `${partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age != null && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
