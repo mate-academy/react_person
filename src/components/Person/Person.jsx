47d725a290fb248e8874c085999a36f80af4f015
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-08-03 14:35:50 +0300

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    {person.age !== undefined && (
+      <p className="Person__age">{`I am ${person.age}`}</p>
+    )}
+    {person.isMarried ? (
+      person.sex === 'm' ? <p className="Person__partner">{`${person.partnerName} is my wife`}</p> : <p className="Person__partner">{`${person.partnerName} is my husband`}</p>
+    ) : (
+      <p className="Person__partner">I am not married</p>
+    )}
+  </section>
+);
