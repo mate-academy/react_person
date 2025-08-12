c37b05fa3cd6cbb625eca7321de99a0adc4f1fad
Ivan <rusev065@gmail.com>
2025-08-12 11:28:31 +0300

    add task Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const isMale = sex === 'm' || sex === 'male';
+  const partnerWord = isMale ? 'wife' : 'husband';
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== null && age !== undefined && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerWord}`
+          : 'I am not married'}
+      </p>
+    </div>
+  );
+};
