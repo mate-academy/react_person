1193f1ac3643a500c9037b2cd5f2965df8d31bce
Ivan <rusev065@gmail.com>
2025-08-12 11:38:33 +0300

    add changes in formatting code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,15 +2,17 @@ export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
   const isMale = sex === 'm' || sex === 'male';
   const partnerWord = isMale ? 'wife' : 'husband';
+  const shouldShowAge = age !== undefined && age !== null;
 
   return (
     <div className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age !== null && age !== undefined && <p className="Person__age">I am {age}</p>}
+      {shouldShowAge && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${partnerWord}`
-          : 'I am not married'}
+        {isMarried 
+        ? `${partnerName} is my ${partnerWord}` 
+        : 'I am not married'
+        }
       </p>
     </div>
   );
