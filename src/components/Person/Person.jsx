899bf2222156e7885105dad3192537cb6c0910d1
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2025-08-18 13:19:48 -0300

    person.age improvement

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,7 +13,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {person.age !== undefined && (
+        <p className="Person__age">I am {person.age}</p>
+      )}
       <p className="Person__partner">{partnerLabel}</p>
     </section>
   );
