5f60911e39c3b1b04f8057c53de7832d69b13595
Nadiia Chernygina <nadja.chernygina@gmail.com>
2025-08-23 18:36:59 +0300

    the second commit

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,24 +1,22 @@
 export const Person = ({ person }) => {
   const { name, age, partnerName, isMarried, sex } = person;
-  let status;
   const hasAge = age != null;
 
-  if (isMarried) {
-    status = sex === 'm' ? 'wife' : 'husband';
-  }
+  const status = sex === 'm' ? 'wife' : 'husband';
+
+  const partnerInfo = isMarried ? (
+    <p className="Person__partner">
+      {partnerName} is my {status}
+    </p>
+  ) : (
+    <p className="Person__partner">I am not married</p>
+  );
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {hasAge && <p className="Person__age">{`I am ${age}`}</p>}
-
-      {isMarried ? (
-        <p className="Person__partner">
-          {partnerName} is my {status}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      {partnerInfo}
     </section>
   );
 };
