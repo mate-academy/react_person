9e91a6e58952f3001d69bb3494ac5f46deffe6b3
Sagi4A <voitko.katya.1@gmail.com>
2025-08-30 17:43:00 +0300

    solved

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+import classNames from 'classnames';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const role = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <section className={classNames('Person', { 'Person--married': isMarried })}>
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">
+        {isMarried ? `${partnerName} is my ${role}` : 'I am not married'}
+      </p>
+    </section>
+  );
+};
