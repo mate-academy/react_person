05b64276ee4042c73acca7282424a98d04376cf2
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2025-09-11 00:23:12 +0300

    added a new component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import PropTypes from 'prop-types';
+import './Person.scss';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+    {person.age !== undefined && (
+      <p className="Person__age">{`I am ${person.age}`}</p>
+    )}
+    {person.isMarried ? (
+      <p className="Person__partner">{`${person.partnerName} is ${person.sex === 'm' ? 'wife' : 'husband'}`}</p>
+    ) : (
+      <p className="Person__partner">I am not married</p>
+    )}
+  </section>
+);
+
+Person.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string,
+    age: PropTypes.number,
+    sex: PropTypes.string,
+    isMarried: PropTypes.bool,
+    partnerName: PropTypes.string,
+  }).isRequired,
+};
