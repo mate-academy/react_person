68a1c44f588e055836bf5ffc7e53630dd9f85150
Serhii Mamchur <kovaniysvit@gmail.com>
2025-09-11 16:21:50 +0200

    added tern oper in fwe lines

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,18 +1,20 @@
 // export const Person = ({ person }) => ();
 
 export const Person = ({ person }) => {
-  const { name, age, sex, isMarried } = person;
+  const { name, age, sex, isMarried, partnerName } = person;
   const partner = sex === 'f' ? 'husband' : 'wife';
-  const ageElement = age ? <p className="Person__age">{`I am ${age}`}</p> : '';
-  const parterMessage = isMarried
-    ? `${person.partnerName} is my ${partner}`
+  const ageElement = age ? (
+    <p className="Person__age">{`I am ${age}`}</p>
+  ) : null;
+  const partnerMessage = isMarried
+    ? `${partnerName} is my ${partner}`
     : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {ageElement}
-      <p className="Person__partner">{parterMessage}</p>
+      <p className="Person__partner">{partnerMessage}</p>
     </section>
   );
 };
