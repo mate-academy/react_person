5584d55b44a000b6d914965ad3f7499829f90bf3
Serhii Mamchur <kovaniysvit@gmail.com>
2025-09-11 15:26:06 +0200

    made changes, destructuring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,18 @@
 // export const Person = ({ person }) => ();
 
 export const Person = ({ person }) => {
-  const partner = person.sex === 'f' ? 'husband' : 'wife';
+  const { name, age, sex, isMarried } = person;
+  const partner = sex === 'f' ? 'husband' : 'wife';
+  const ageElement = age ? <p className="Person__age">{`I am ${age}`}</p> : '';
+  const parterMessage = isMarried
+    ? `${person.partnerName} is my ${partner}`
+    : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age ? <p className="Person__age">{`I am ${person.age}`}</p> : ''}
-      <p className="Person__partner">
-        {person.isMarried
-          ? `${person.partnerName} is my ${partner}`
-          : 'I am not married'}
-      </p>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {ageElement}
+      <p className="Person__partner">{parterMessage}</p>
     </section>
   );
 };
