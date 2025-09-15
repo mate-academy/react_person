5978ed0a3482e19f89de54d4637659795be1132a
Matvey Turluk <matychencia.ua@gmail.com>
2025-09-15 18:38:59 +0300

    added task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import '../../App.scss';
+
+export function Person({ person }) {
+  let partnerMessage;
+
+  if (person.isMarried === false) {
+    partnerMessage = <span>I am not married</span>;
+  } else if (person.sex === 'm') {
+    partnerMessage = <span>{person.partnerName} is my wife</span>;
+  } else if (person.sex === 'f') {
+    partnerMessage = <span>{person.partnerName} is my husband</span>;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
+      {person.age && (
+        <p className="Person__age">
+          <span>{person.age}</span>
+        </p>
+      )}
+
+      <p className="Person__partner">{partnerMessage}</p>
+    </section>
+  );
+}
