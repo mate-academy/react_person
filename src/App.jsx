55b5c6db7551097e6422bf21d3b50f9815cc9313
Taras Khomenko <khomenkotaras1305@gmail.com>
2025-09-22 20:55:37 +0300

    fixed buddy comments

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <Person {...misha} />
-    <Person {...olya} />
-    <Person {...alex} />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
