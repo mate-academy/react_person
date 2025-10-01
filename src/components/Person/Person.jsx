1cb994a752e388ba68293546241ef0f78f6f0779
Vladyslav Hartman <vladyslav.gartman@gmail.com>
2025-10-01 05:07:47 +0300

    fixes the issues

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,16 +4,11 @@ export const Person = ({ person }) => (
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
     {person.age !== undefined ? (
       <p className="Person__age">{`I am ${person.age}`}</p>
-    ) : (
-      ''
-    )}
-    {person.isMarried === true ? (
-      <p className="Person__partner">
-        {person.partnerName} is my
-        {person.sex === 'm' ? ' wife' : ' husband'}
-      </p>
-    ) : (
-      'I am not married'
-    )}
+    ) : null}
+
+    <p className="Person__partner">
+      person.isMarried === true ? ({person.partnerName} is my
+      {person.sex === 'm' ? ' wife' : ' husband'}) : (`I am not married`)
+    </p>
   </section>
 );
