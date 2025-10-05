f48bbbc3977e7cf78d62935557d5a1f5261a8313
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-10-05 12:23:04 +0300

    update person.jsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,5 +1,6 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
+  const hasAge = age !== undefined && age !== 0;
   let partnerText;
 
   if (!isMarried) {
@@ -15,7 +16,8 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">My name is {name}</h2>
       {/* Якщо існує і якщо не 0 */}
       {/* {age && age !== 0 ? <p className="Person__age">I am {age}</p> : ''} */}
-      {age && age !== 0 && <p className="Person__age">I am {age}</p>}
+      {/* {age && age !== 0 && <p className="Person__age">I am {age}</p>} */}
+      {hasAge && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
