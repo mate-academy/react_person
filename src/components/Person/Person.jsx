21cfa7492de406e908041a4de0737f5459f82d04
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-10-08 21:16:42 +0100

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,18 +2,20 @@
 export const Person = ({ person }) => {
   const { name, sex, age, partnerName, isMarried } = person;
 
-  const partnerRelation = sex === 'f' ? 'husband' : 'wife';
+  const partnerRelation = sex === 'f'
+    ? 'husband'
+    : 'wife';
 
   const partnerText = isMarried
     ? `${partnerName} is my ${partnerRelation}`
     : 'I am not married';
 
   return (
-    <>
+    <div className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">{partnerText}</p>
-    </>
+    </div>
   );
 };
 
