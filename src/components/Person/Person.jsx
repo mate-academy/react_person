bb3b22750277126bcdb601a4adc6fa2d2d6a6e69
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-10-08 16:03:24 +0100

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,14 @@
 // export const Person = ({ person }) => ();
+export const Person = ({ name, sex, age, partnerName, isMarried }) => (
+  <>
+    <h2 className="Person__name">{`My name is ${name}`}</h2>
+    <p className="Person__age">{`I am ${age}`}</p>
+    <p className="Person__partner">
+      {isMarried
+        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+        : 'I am not married'}
+    </p>
+  </>
+);
+
+export default Person;
