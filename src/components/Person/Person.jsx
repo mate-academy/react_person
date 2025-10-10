57691885a8e0e47056849f6bbb54f44aaf90de59
DropPredator <ionchiccc@gmail.com>
2025-10-10 10:47:07 +0300

    ready steady

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,15 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+
+    {person.age ? <p className="Person__age">I am {person.age}</p> : null}
+
+    {person.isMarried && person.sex.toLowerCase() === 'm' ? (
+      <p className="Person__partner">{person.partnerName} is my wife</p>
+    ) : null}
+
+    {person.isMarried && person.sex.toLowerCase() === 'f' ? (
+      <p className="Person__partner">{person.partnerName} is my husband</p>
+    ) : null}
+  </section>
+);
