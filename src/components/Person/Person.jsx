e4fa14d54982e1a9265901edcb1191b29761d640
Yana <marusina.yana@gmail.com>
2025-10-18 23:32:30 +0300

    Resolved

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,29 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = props => {
+  const { person } = props;
+
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerInfo;
+
+  if (!isMarried) {
+    partnerInfo = 'I am not married';
+  } else if (sex === 'm') {
+    partnerInfo = `${partnerName} is my wife`;
+  } else {
+    partnerInfo = `${partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age && <p className="Person__age">I am {age}</p>}
+
+      <p className="Person__partner">{partnerInfo}</p>
+    </section>
+  );
+};
+
+export default Person;
