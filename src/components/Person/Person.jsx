7dc32a87f27289020d6dd79896057d7015785140
Yevhen Hryvastov <egrivaster@gmail.com>
2025-11-05 21:22:59 +0200

    create new variable maleFamele

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,13 +2,19 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  let maleFemale = '';
+
+  if (sex === 'm') {
+    maleFemale = 'wife';
+  } else {
+    maleFemale = 'husband';
+  }
+
   let marriageStatus = '';
 
   if (isMarried) {
     marriageStatus = (
-      <p className="Person__partner">
-        {`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}
-      </p>
+      <p className="Person__partner">{`${partnerName} is my ${maleFemale}`}</p>
     );
   } else {
     marriageStatus = <p className="Person__partner">I am not married</p>;
