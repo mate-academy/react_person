1b150e311987799da1bb1d72d72d42290f688c23
Yevhen Hryvastov <egrivaster@gmail.com>
2025-11-05 21:09:09 +0200

    create new variable before the return statement

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,17 +2,23 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  let marriageStatus = '';
+
+  if (isMarried) {
+    marriageStatus = (
+      <p className="Person__partner">
+        {`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}
+      </p>
+    );
+  } else {
+    marriageStatus = <p className="Person__partner">I am not married</p>;
+  }
+
   return (
     <div className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">I am {age}</p>}
-      {isMarried ? (
-        <p className="Person__partner">
-          {`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      {marriageStatus}
     </div>
   );
 };
