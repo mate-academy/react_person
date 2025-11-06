f6f6cd9bcf5f756964ef8a9bda635835089d42bf
Oskar <proton2006tom@gmail.com>
2025-11-06 15:02:52 +0200

    solution x2

--- src/App.jsx
+++ src/App.jsx
@@ -31,38 +31,3 @@ export const App = () => (
     <Person person={alex} />
   </div>
 );
-
-/* import React from 'react';
-import { Person } from './components/Person/Person';
-import './App.scss';
-
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
-
-export const App = () => (
-  <div className="App">
-    <Person person={misha} />
-    <Person person={olya} />
-    <Person person={alex} />
-  </div>
-);
-*/
