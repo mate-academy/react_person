ff19e8134678b896f5ff62d5e53f0b652ac16ffe
Iryna Knyzh <iryna.knyzh@gmail.com>
2025-11-07 10:46:31 +0200

    task solution

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <div className="App">
-      <Person person={misha} />
-      <Person person={olya} />
-      <Person person={alex} />
-    </div>
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
