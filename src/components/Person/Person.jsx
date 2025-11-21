182656a034a982021e2eb1a531012984025be8bf
Vital <vitaligna@gmail.com>
2025-11-21 08:52:11 +0200

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried } = person;
+
+  const partner =
+    person.partner ||
+    person.spouse ||
+    person.partnerName ||
+    person.partner_name;
+
+  const married = typeof isMarried === 'boolean' ? isMarried : Boolean(partner);
+
+  const partnerLabel = sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
+
+      {married ? (
+        <p className="Person__partner">{`${partner} is my ${partnerLabel}`}</p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </div>
+  );
+};
