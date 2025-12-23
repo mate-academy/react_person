4a361cd3a87365811f85a3c65e793ac9d4c0d494
igor rocha <iogorrochamiranda2@gmail.com>
2025-12-23 15:49:28 -0300

    Formatação com prettier

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,19 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
+export const Person = ({ person }) => {
+  const { name, age, isMarried, sex, partnerName } = person;
 
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'f' ? 'husband' : 'wife'}`
-        : 'I am not married'}
-    </p>
-  </section>
-);
+  const partnerTitle = sex === 'f' ? 'husband' : 'wife';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age && <p className="Person__age">I am {age}</p>}
+
+      <p className="Person__partner">
+        {isMarried
+          ? `${partnerName} is my ${partnerTitle}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
