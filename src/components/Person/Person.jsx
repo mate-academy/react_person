830baa23bccb693b129ede1918aecd4193f3c198
wzfxc <maksohrimenko1@gmail.com>
2025-12-23 14:15:04 +0200

    2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,10 +1,12 @@
-export const Person = ({ person }) => (
+export const Person = ({ name, age, isMarried, sex, partnerName }) => (
   <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age ? <p className="Person__age">I am {person.age}</p> : ''}
-    {person.isMarried ? (
+    <h2 className="Person__name">My name is {name}</h2>
+
+    {age ? <p className="Person__age">I am {age}</p> : ''}
+
+    {isMarried ? (
       <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
+        {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
       </p>
     ) : (
       <p className="Person__partner">I am not married</p>
