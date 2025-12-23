7c3834835a70fdb5be2a26167a5ee45ad8f5ca36
igor rocha <iogorrochamiranda2@gmail.com>
2025-12-23 15:57:27 -0300

    Formatação ignorando Prettier

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,25 @@
 export const Person = ({ person }) => {
-  const { name, age, isMarried, sex, partnerName } = person;
+  const {
+    name,
+    age,
+    isMarried,
+    sex,
+    partnerName,
+  } = person;
 
-  const partnerTitle = sex === 'f' ? 'husband' : 'wife';
+  const partnerTitle = sex === 'f'
+    ? 'husband'
+    : 'wife';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
-      {age && <p className="Person__age">I am {age}</p>}
+      {age && (
+        <p className="Person__age">
+          I am {age}
+        </p>
+      )}
 
       <p className="Person__partner">
         {isMarried
