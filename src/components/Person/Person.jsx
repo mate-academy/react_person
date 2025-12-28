152af727d4c63816ad71da5492279e4c1b490832
Alex <1inboxna@gmail.com>
2025-12-28 18:04:09 +0200

    commit message

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { age, name, isMarried, partnerName, sex } = person;
+
+  let hasAge = false;
+
+  if (age !== undefined && age !== 0) {
+    hasAge = true;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {hasAge ? <p className="Person__age">I am {age}</p> : null}
+
+      {isMarried === true ? (
+        <p className="Person__partner">
+          {partnerName} is my{' '}
+          {sex === 'm' ? <span>wife</span> : <span>husband</span>}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
