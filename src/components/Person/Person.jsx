1df29094ca6c8354d516bd03c01fcbd94680f585
Diana <dianka_9@ukr.net>
2025-12-30 21:32:44 +0200

    fix task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,40 @@
-import cn from "classnames";
+import cn from 'classnames';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  const isMarriedMan = sex === 'm' && isMarried;
+  const isMarriedWoman = sex === 'f' && isMarried;
+  const isSingle = !isMarried;
+
   let partnerInfo;
 
-  if (isMarried) {
-    partnerInfo =
-      sex === 'm'
-        ? `${partnerName} is my wife`
-        : `${partnerName} is my husband`;
-  } else {
+  if (isMarriedMan) {
+    partnerInfo = `${partnerName} is my wife`;
+  } else if (isMarriedWoman) {
+    partnerInfo = `${partnerName} is my husband`;
+  } else if (isSingle) {
     partnerInfo = 'I am not married';
   }
 
   return (
-    <section className={cn('Person', { 'Person--married': isMarried, 'Person--single': !isMarried, })} >
+    <section
+      className={cn('Person', {
+        'Person--married': isMarried,
+        'Person--single': !isMarried,
+      })}
+    >
       <h2 className={cn('Person__name')}>My name is {name}</h2>
 
       {age && <p className={cn('Person__age')}>I am {age}</p>}
 
-      <p className={cn('Person__partner', { 'Person__partner--wife': sex === 'm' && isMarried, 'Person__partner--husband': sex === 'f' && isMarried, 'Person__partner--none': !isMarried, })} >
+      <p
+        className={cn('Person__partner', {
+          'Person__partner--wife': isMarriedMan,
+          'Person__partner--husband': isMarriedWoman,
+          'Person__partner--none': isSingle,
+        })}
+      >
         {partnerInfo}
       </p>
     </section>
