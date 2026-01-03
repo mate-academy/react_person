f4952b1b35c36a4f2454b5f63e89c9d37f95f08b
Yaroslav Ivasenko <yarik.dock@gmail.com>
2026-01-03 19:46:01 +0200

    created solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+function createRow(sex, isMarried, partnerName) {
+  if (isMarried) {
+    return sex === 'm'
+      ? `${partnerName} is my wife`
+      : `${partnerName} is my husband`;
+  }
+
+  return `I am not married`;
+}
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      <p className="Person__partner">
+        {createRow(sex, isMarried, partnerName)}
+      </p>
+    </section>
+  );
+};
