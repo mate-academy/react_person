c0f36757db3bcd2b6daf49324b671f2a6272f022
Daniel Nykyforov <nykyforov.dan@gmail.com>
2026-01-07 02:18:55 +0200

    Solution change

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,25 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const isFemaleMarried = isMarried && sex === 'f';
+  const isMaleMarried = isMarried && sex === 'm';
 
-    {person.age !== undefined && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    )}
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-    {!person.isMarried && <p className="Person__partner">I am not married</p>}
+      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
 
-    {person.isMarried && person.sex === 'f' && (
-      <p className="Person__partner">{`${person.partnerName} is my husband`}</p>
-    )}
+      {!isMarried && <p className="Person__partner">I am not married</p>}
 
-    {person.isMarried && person.sex === 'm' && (
-      <p className="Person__partner">{`${person.partnerName} is my wife`}</p>
-    )}
-  </section>
-);
+      {isFemaleMarried && (
+        <p className="Person__partner">{`${partnerName} is my husband`}</p>
+      )}
+
+      {isMaleMarried && (
+        <p className="Person__partner">{`${partnerName} is my wife`}</p>
+      )}
+    </section>
+  );
+};
