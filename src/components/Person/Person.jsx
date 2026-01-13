352c1921038b889bd8fde2fc873483ff8afcd1da
Nazarii Lesniak <naziksay@gmail.com>
2026-01-13 09:15:50 +0200

    changed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,20 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
   let partnerMessage = '';
 
-  if (!person.isMarried) {
+  if (!isMarried) {
     partnerMessage = 'I am not married';
-  } else if (person.sex === 'm') {
-    partnerMessage = `${person.partnerName} is my wife`;
+  } else if (sex === 'm') {
+    partnerMessage = `${partnerName} is my wife`;
   } else {
-    partnerMessage = `${person.partnerName} is my husband`;
+    partnerMessage = `${partnerName} is my husband`;
   }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
 
       <p className="Person__partner">{partnerMessage}</p>
     </section>
