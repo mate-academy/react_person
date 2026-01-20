e9858976f155d85aef3f674793cc10f7192c8b08
Andrey <kagamlykas@gmail.com>
2026-01-20 17:43:09 +0200

    Created person components

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const merried = (
+    <p className="Person__partner">
+      {person.isMarried
+        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+        : `I am not married`}
+    </p>
+  );
+
+  const age = person.age && <p className="Person__age">I am {person.age}</p>;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {age}
+      {merried}
+    </section>
+  );
+};
