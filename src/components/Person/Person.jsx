942d3c86588306a890b19b42a0339fab0f1776c5
Andrey <kagamlykas@gmail.com>
2026-01-20 17:49:09 +0200

    Changed person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,19 @@
 export const Person = ({ person }) => {
-  const merried = (
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const married = (
     <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+      {isMarried
+        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
         : `I am not married`}
     </p>
   );
 
-  const age = person.age && <p className="Person__age">I am {person.age}</p>;
-
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {age}
-      {merried}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {married}
     </section>
   );
 };
