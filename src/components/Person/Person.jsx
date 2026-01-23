5de1a152065debcf5786ca56c932f6770baeabc3
ExtymAndriy <petrovskiyandriyko@gmail.com>
2026-01-23 11:02:36 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// export const Person = ({ person }) => ();
+import cn from 'classnames';
+
+export const Person = ({ person }) => {
+  return (
+    <section
+      className={cn('Person', {
+        'Person--married': person.isMarried,
+        'Person--male': person.sex === 'm',
+        'Person--female': person.sex === 'f',
+      })}
+    >
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age !== undefined && (
+        <p className="Person__age">I am {person.age}</p>
+      )}
+      <p
+        className={cn('Person__partner', {
+          'Person__partner--married': person.isMarried,
+          'Person__partner--single': !person.isMarried,
+        })}
+      >
+        {person.isMarried
+          ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
