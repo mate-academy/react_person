5278e366623f81aed13b60e062ca542024fbb553
Samuel Lazarin <samuel.lazarin12@gmail.com>
2026-01-24 23:39:57 -0300

    add task solution test one

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,25 @@
 import React from 'react';
 import './App.scss';
-
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
+import { misha } from './components/Person/Person';
+import { olya } from './components/Person/Person';
+import { alex } from './components/Person/Person';
 
 export const App = () => (
   <div className="App">
     <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">Natasha is my wife</p>
+      <h2 className="Person__name">My name is {misha.name}</h2>
+      <p className="Person__age">I am {misha.age}</p>
+      <p className="Person__partner"> I am married to {misha.partnerName}</p>
     </section>
 
     <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">Maksym is my husband</p>
+      <h2 className="Person__name">My name is {olya.name}</h2>
+      <p className="Person__partner">{olya.partnerName} is my husband</p>
     </section>
 
     <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
+      <h2 className="Person__name">My name is {alex.name}</h2>
+      <p className="Person__age">I am {alex.age}</p>
       <p className="Person__partner">I am not married</p>
     </section>
   </div>
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
+};
