2f8e1eda28376e6be60ba5f14727684bda10a98e
glauccoeng-prog <glauccoeng@gmail.com>
2026-01-29 20:34:58 -0300

    feat: implement Person component with conditional rendering logic

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerText;
+
+  if (isMarried) {
+    const partnerType = sex === 'm' ? 'wife' : 'husband';
+
+    partnerText = `${partnerName} is my ${partnerType}`;
+  } else {
+    partnerText = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
