d24e685eb7cde82854af3234494c5a331f18184c
DenisNosov22 <dennosov2210@gmail.com>
2026-02-05 15:07:31 +0200

    react person 1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const partnerLabel = person.sex === 'm' ? 'wife' : 'husband';
+
+  return (
+    <div className="Person">
+      <div className="Person__name">My name is {person.name}</div>
+      {person.age && <div className="Person__age">I am {person.age}</div>}
+      <div className="Person__partner">
+        {person.isMarried
+          ? `${person.partnerName} is my ${partnerLabel}`
+          : 'I am not married'}
+      </div>
+    </div>
+  );
+};
