34584a795bdba54de9f9bd5281daef0060b91f5b
DenisNosov22 <dennosov2210@gmail.com>
2026-02-05 15:15:31 +0200

    react person 2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 export const Person = ({ person }) => {
-  const partnerLabel = person.sex === 'm' ? 'wife' : 'husband';
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partnerLabel = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <div className="Person">
-      <div className="Person__name">My name is {person.name}</div>
-      {person.age && <div className="Person__age">I am {person.age}</div>}
+      <div className="Person__name">My name is {name}</div>
+      {age && <div className="Person__age">I am {age}</div>}
       <div className="Person__partner">
-        {person.isMarried
-          ? `${person.partnerName} is my ${partnerLabel}`
+        {isMarried
+          ? `${partnerName} is my ${partnerLabel}`
           : 'I am not married'}
       </div>
     </div>
