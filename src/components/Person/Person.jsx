d43f37dcebc8ee44a4bfb03e867208c7f622dfa4
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-02-07 22:05:02 +0200

    done React person task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable prettier/prettier */
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let marriageStatus = 'I am not married';
+
+  if (isMarried) {
+    marriageStatus =
+      sex === 'm'
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age !== undefined && (
+        <p className="Person__age">I am {age}</p>
+      )}
+
+      <p className="Person__partner">{marriageStatus}</p>
+    </section>
+  );
+};
