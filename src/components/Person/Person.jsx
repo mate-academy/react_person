fcd5fb1e3d25bacc4de737583964836e449401be
Evg3n22 <evgen.gulevaty@gmail.com>
2026-02-09 09:50:37 +0200

    fix: hide age tag if there no data

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -15,7 +15,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      <p className="Person__age">{person.age && `I am ${person.age}`}</p>
+      {person.age && <p className="Person__age">{`I am ${person.age}`}</p>}
       <p className="Person__partner">{marriedText}</p>
     </section>
   );
