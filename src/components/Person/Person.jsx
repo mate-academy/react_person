e6d3cf236407cf2ddfbcbee71b74da4876ebb063
Taras Yelahin <inflades@gmail.com>
2026-02-19 19:18:23 +0200

    React_person_task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  let partnerText = 'I am not married';
+
+  if (isMarried) {
+    partnerText =
+      sex === 'm' ? `My wife: ${partnerName}` : `My husband: ${partnerName}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{name}</h2>
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
