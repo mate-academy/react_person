083ec53a181b0b281cf3a122ca37496220b9e6ff
Liena Grytsyna <grytsynaliena@gmail.com>
2026-03-12 12:50:05 +0100

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,20 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
   let partnerText;
 
-  if (!person.isMarried) {
+  if (!isMarried) {
     partnerText = 'I am not married';
-  } else if (person.sex === 'm') {
-    partnerText = `${person.partnerName} is my wife`;
+  } else if (sex === 'm') {
+    partnerText = `${partnerName} is my wife`;
   } else {
-    partnerText = `${person.partnerName} is my husband`;
+    partnerText = `${partnerName} is my husband`;
   }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
 
-      {/* Правило 2: Тернарний оператор на окремих рядках */}
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {age && <p className="Person__age">I am {age}</p>}
 
       <p className="Person__partner">{partnerText}</p>
     </section>
