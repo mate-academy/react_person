ed34f247459f17ab0d78e6e18c22e4ab4c6242b1
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-03-25 23:18:57 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import './App.scss';
 
+/*
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">My name is Misha</h2>
+      <p className="Person__age">I am 37</p>
+      <p className="Person__partner">Natasha is my wife</p>
+    </section>
+
+    <section className="Person">
+      <h2 className="Person__name">My name is Olya</h2>
+      <p className="Person__partner">Maksym is my husband</p>
+    </section>
+
+    <section className="Person">
+      <h2 className="Person__name">My name is Alex</h2>
+      <p className="Person__age">I am 25</p>
+      <p className="Person__partner">I am not married</p>
+    </section>
+  </div>
+*/
+import { Person } from './components/Person/Person';
+
 export const misha = {
   name: 'Misha',
   age: 37,
@@ -23,23 +45,12 @@ export const alex = {
   isMarried: false,
 };
 
-export const App = () => (
-  <div className="App">
-    <section className="Person">
-      <h2 className="Person__name">My name is Misha</h2>
-      <p className="Person__age">I am 37</p>
-      <p className="Person__partner">Natasha is my wife</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Olya</h2>
-      <p className="Person__partner">Maksym is my husband</p>
-    </section>
-
-    <section className="Person">
-      <h2 className="Person__name">My name is Alex</h2>
-      <p className="Person__age">I am 25</p>
-      <p className="Person__partner">I am not married</p>
-    </section>
-  </div>
-);
+export const App = () => {
+  return (
+    <>
+      <Person person={misha} />
+      <Person person={olya} />
+      <Person person={alex} />
+    </>
+  );
+};
