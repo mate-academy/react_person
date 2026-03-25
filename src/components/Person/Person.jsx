ee7146f7977496e507296e790a31b6367fd62768
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-03-25 23:27:54 +0200

    Solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -14,8 +14,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      <p className="Person__partner">{partnerText}</p>
       {age != null && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerText}</p>
+      
     </section>
   );
 };
