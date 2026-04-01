c064383e5ada48176acfd18c82766dbd3ae33576
Artem <muha13artem@gmail.com>
2026-04-01 21:48:00 +0300

    finishhh

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,9 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const partnerTitle = sex === 'm' ? 'wife' : 'husband';
+  const partnerTitle = sex === 'm'
+    ? 'wife'
+    : 'husband';
 
   return (
     <section className="Person">
