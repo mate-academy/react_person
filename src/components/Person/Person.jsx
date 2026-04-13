b8f69253b516b133f6578b5a0de43980f9e49286
Tymur <timurradkevic84@mail.com>
2026-04-13 16:48:14 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,7 +4,9 @@ export const Person = ({ person }) => (
     {person.age && <p className="Person__age">I am {person.age}</p>}
     {person.isMarried ? (
       <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
+        {person.partnerName} is my {person.sex === 'm'
+          ? 'wife' :
+          'husband'}
       </p>
     ) : (
       <p className="Person__partner">I am not married</p>
