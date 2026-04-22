cfcbce1096c66ae528113dcd7ec22df7c0d913b2
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-22 17:58:14 +0000

    refactor: simplify logic in Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,22 @@
 import './Person.scss';
 
+const partnerBySex = {
+  f: 'husband',
+  m: 'wife',
+};
+
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const partner = sex === 'f' ? 'husband' : 'wife';
-
   const partnerInfo = isMarried
-    ? `${partnerName} is my ${partner}`
+    ? `${partnerName} is my ${partnerBySex[sex]}`
     : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
 
-      {Boolean(age) && <p className="Person__age">{`I am ${age}`}</p>}
+      {age > 0 && <p className="Person__age">I am {age}</p>}
 
       <p className="Person__partner">{partnerInfo}</p>
     </section>
