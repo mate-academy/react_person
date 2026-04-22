a6a16d49dae9a383c576f692564aba710131d8bd
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-22 17:23:41 +0000

    feat: implement Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+import './Person.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const partner = sex === 'f' ? 'husband' : 'wife';
+
+  const partnerInfo = isMarried
+    ? `${partnerName} is my ${partner}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+
+      {Boolean(age) && <p className="Person__age">{`I am ${age}`}</p>}
+
+      <p className="Person__partner">{partnerInfo}</p>
+    </section>
+  );
+};
