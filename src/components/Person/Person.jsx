97d0a8be83d6814f6a39ebdc87e0db83de98d328
Inna K <innakonopatcka@gmail.com>
2026-05-05 19:23:50 +0300

    changed isMarried and added pret-ign

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -10,9 +10,12 @@ export const Person = ({ person }) => {
       {age && <p className="Person__age">{`I am ${age}`}</p>}
 
       <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
-          : 'I am not married'}
+        {
+          /* prettier-ignore */
+          isMarried
+            ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+            : 'I am not married'
+        }
       </p>
     </section>
   );
