0c893d3b062a17e01e23d7be18e3e5681fb9a017
Romulo Brasil <romulop.brasil25@gmail.com>
2026-05-22 09:00:35 -0300

    all test ok

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 
 // 1. Importamos o Person E também os objetos que você criou lá dentro
-import { Person, misha, olya, alex } from './Person'; 
+import { Person, misha, olya, alex } from './components/Person/Person';
 
 createRoot(document.getElementById('root')).render(
   <div className="main">
     <Person person={misha} />
     <Person person={olya} />
     <Person person={alex} />
-  </div>
+  </div>,
 ); // 2. Parênteses e ponto e vírgula devidamente fechados aqui!
