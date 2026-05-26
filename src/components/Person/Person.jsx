6fe596cb94ef662a32d92200e05d87a46f625400
Romulo Brasil <romulop.brasil25@gmail.com>
2026-05-26 13:24:40 -0300

    erro sem sentido

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -31,8 +31,9 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">My name is {name}</h2>
       {age !== undefined && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
-        {isMarried ?
-          `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+        {isMarried
+          ? `${partnerName} is my 
+          ${sex === 'm' ? 'wife' : 'husband'}`
           : 'not married'}
       </p>
     </section>
