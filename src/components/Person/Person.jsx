252e38489550c28c690e4e25b3f058d941503545
Ivan Sierov <VanSer1989@gmail.com>
2026-06-15 11:13:48 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  // По умолчанию считаем, что человек не в браке
+  let partnerText = 'I am not married';
+
+  // Если в браке, проверяем пол и меняем текст
+  if (person.isMarried) {
+    partnerText =
+      person.sex === 'm'
+        ? `${person.partnerName} is my wife`
+        : `${person.partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {/* Условный рендеринг возраста */}
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+
+      {/* Выводим подготовленный текст про партнера */}
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
