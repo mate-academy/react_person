bce95f30485658c09492952e08c5e75f98e40b36
apaniutin48393 <anton.paniutin@cengage.com>
2026-07-02 17:50:24 +0200

    Fix code style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,16 +3,15 @@ export const Person = ({ person }) => {
 
   if (person.isMarried) {
     partnerString = person.sex === 'f'
-  ? `${person.partnerName} is my husband`
-  : `${person.partnerName} is my wife`;
+      ? `${person.partnerName} is my husband`
+      : `${person.partnerName} is my wife`;
   }
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && 
-      <p className="Person__age">I am {person.age}</p>}
+      {person.age && <p className="Person__age">I am {person.age}</p>}
       <p className="Person__partner">{partnerString}</p>
     </section>
-  )
+  );
 };
