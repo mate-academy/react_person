c3c52749c5e28510c6c717ec9d6df8147314bbe8
Vadim <vadim.bazaliy228@gmail.com>
2026-07-18 20:35:41 +0300

    style: format ternary operators according to react task guideline

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,28 @@ import React from 'react';
 import './App.scss';
 import { Person } from './components/Person/Person';
 
-export const misha = {
+const misha = {
   name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-export const alex = {
-  name: 'Alex',
   age: 25,
   sex: 'm',
-  isMarried: false,
+  isMarried: true,
+  partner: { name: 'Olya' },
 };
+const olya = {
+  name: 'Olya',
+  age: 23,
+  sex: 'f',
+  isMarried: true,
+  partner: { name: 'Misha' },
+};
+const alex = { name: 'Alex', sex: 'm', isMarried: false };
 
-export const App = () => (
-  <div className="App">
-    <Person person={misha} />
-    <Person person={olya} />
-    <Person person={alex} />
-  </div>
-);
+export function App() {
+  return (
+    <div className="App">
+      <Person person={misha} />
+      <Person person={olya} />
+      <Person person={alex} />
+    </div>
+  );
+}
