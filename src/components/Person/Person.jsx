4ad19df435b11a70912ffa9d54a373a7c5aba589
LimeRider <cgakaviy@gmail.com>
2026-08-02 13:57:36 +0300

    .

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,6 @@
-export const Person = ({ person }) => {
-  const { name, age, isMarried, partnerName, sex } = person;
-
+export const Person = ({
+  person: { name, age, isMarried, sex, partnerName },
+}) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
