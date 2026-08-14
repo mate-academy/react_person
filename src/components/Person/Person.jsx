9f2a3caaf50fa4b4de426a759a108bd103a24b98
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-08-14 18:15:05 +0300

    complete task

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,11 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    <p className={`${person.age ? 'Person__age' : ''}`}>
+      {person.age ? `I am ${person.age}` : null}
+    </p>
+    <p className="Person__partner">
+      {person.isMarried === true ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}` : 'I am not married'}
+    </p>
+  </section>
+);
