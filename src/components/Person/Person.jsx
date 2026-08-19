53bf7d61b441ced9716deb8651bd34542a6c3d3d
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-19 17:57:17 +0300

    Refactoring again

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,8 +8,11 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age !== undefined
-        ? <p className="Person__age">I am {age}</p>
-        : ''}
+        ? (
+          <p className="Person__age">I am {age}</p>
+        )
+        : ''
+      }
       {isMarried === true ? (
         <p className="Person__partner">
           {`${partnerName} is my `}
