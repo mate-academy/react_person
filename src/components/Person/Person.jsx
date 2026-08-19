cecae9613fb2ee924a27cb1ccb407390120d73ed
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-19 17:52:27 +0300

    Minnor issues resolved

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,15 +2,18 @@ import './Person.css';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
+  const partnerTitle = sex === 'f' ? 'husband' : 'wife';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age !== undefined ? <p className="Person__age">I am {age}</p> : ''}
+      {age !== undefined
+        ? <p className="Person__age">I am {age}</p>
+        : ''}
       {isMarried === true ? (
         <p className="Person__partner">
           {`${partnerName} is my `}
-          {sex === 'f' ? 'husband' : 'wife'}
+          {partnerTitle}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
