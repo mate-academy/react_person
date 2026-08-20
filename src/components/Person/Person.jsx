166a47b99b30aa511a8c99f025b768853e19ce6e
Mykyta Arkatov <nikitaarkatov4@gmail.com>
2026-08-20 22:16:22 +0300

    implement Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,24 @@
 // export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let partnerInfo = 'I am not married';
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      partnerInfo = `${person.partnerName} is my wife`;
+    } else {
+      partnerInfo = `${person.partnerName} is my husband`;
+    }
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age !== undefined && (
+        <p className="Person__age">I am {person.age}</p>
+      )}
+
+      <p className="Person__partner">{partnerInfo}</p>
+    </section>
+  );
+};
