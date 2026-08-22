740826793355a25e50ee28b610bb110a1f5f3273
Petrenko Oleksii <petrenko1694@gmail.com>
2026-08-22 19:55:57 +0300

    react-person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable prettier/prettier */
+export const Person = ({ person }) => {
+  const {
+    name,
+    age,
+    sex,
+    isMarried,
+    partnerName,
+  } = person;
+
+  const nameText = `My name is ${name}`;
+  const spouse = (sex === 'm')
+    ? 'wife'
+    : 'husband';
+  const partnerText = (isMarried === true)
+    ? `${partnerName} is my ${spouse}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{nameText}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
