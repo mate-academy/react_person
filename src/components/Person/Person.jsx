6906c8549cefd9810f0a7641739e5386cc3400c7
vasylyx <mr.vasilishin@gmail.com>
2026-08-24 17:20:04 +0300

    fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,7 +4,9 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age ? <p className="Person__age">I am {age}</p> : ''}
+      {age
+        ? <p className="Person__age">I am {age}</p>
+        : ''}
       {isMarried ? (
         <p className="Person__partner">
           {sex === 'm'
