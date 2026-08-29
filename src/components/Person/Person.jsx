30f8c58eeb35d3d1ccb280fc853b96bc224b1941
Tetiana Petrishena <petrisenatetana92@gmail.com>
2026-08-29 14:30:58 +0300

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,13 +7,17 @@ export const Person = ({ person }) => {
       <h2 className="Person__name">My name is {name}</h2>
 
       {age && <p className="Person__age">I am {age}</p>}
-      {isMarried ? (
-        <p className="Person__partner">
-          {partnerName} is my {partner}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      {isMarried
+  ? (
+    <p className="Person__partner">
+      {partnerName} is my {partner}
+    </p>
+  )
+  : (
+    <p className="Person__partner">
+      I am not married
+    </p>
+  )}
     </section>
   );
 };
