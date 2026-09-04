b08c091f69d8cb79c5bc4086a7c64f01a8a48e28
Ol. <holmate@dontsp.am>
2026-09-04 19:20:18 +0300

    Solution added

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {Object.prototype.hasOwnProperty.call(person, 'age') && (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      )}
+      <p className="Person__partner">
+        {person.sex === 'f' &&
+          person.isMarried &&
+          `${person.partnerName} is my husband`}
+        {person.sex === 'm' &&
+          person.isMarried &&
+          `${person.partnerName} is my wife`}
+        {!person.isMarried && 'I am not married'}
+      </p>
+    </section>
+  );
+};
