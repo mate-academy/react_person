735504ceac1c28b49c96358950e7da4c33651225
Natalia Onufrienko <natalia_anamalia@icloud.com>
2026-09-10 12:04:21 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,27 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  // Формуємо текст про партнера відповідно до тестів
+  let partnerText = 'I am not married';
+
+  if (isMarried) {
+    if (sex === 'm') {
+      partnerText = `${partnerName} is my wife`;
+    } else if (sex === 'f') {
+      partnerText = `${partnerName} is my husband`;
+    }
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {/* Показуємо вік лише якщо він переданий */}
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
